b9025b713d9b53ddf78836e617f9606851cb38ec
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-01-14 18:17:27 +0100

    Add insertion sort.

---
+++ sorting_algorithms/source/algorithms/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,19 @@
+//
+// Created by andreas on 14.01.22.
+//
+
+#include "insertion_sort.h"
+void InsertionSort::sort(std::vector<int64_t> &array)
+{
+	auto size = array.size();
+	for (size_t index1 =0; index1 < size; ++index1 )
+	{
+		for(size_t index2=index1 ; index2 >0 ; --index2)
+		{
+			if(array[index2] < array[index2-1])
+			{
+				swap(array, index2, index2-1);
+			}
+		}
+	}
+}
