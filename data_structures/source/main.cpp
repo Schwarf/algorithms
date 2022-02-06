92f5eba807350ce6bc36dbdfe906fbc6cc5e8300
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-02-06 11:31:22 +0100

    Implement singly linked-listed based queue.

--- data_structures/source/main.cpp
+++ data_structures/source/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "./binary_search_tree/binary_search_tree.h"
 #include "./linked_lists/singly_linked_list.h"
-#include "stacks/stack_linked_list_based.h"
-#include "stacks/stack_array_based.h"
+#include "stacks_and_queues/stack_linked_list_based.h"
+#include "stacks_and_queues/stack_array_based.h"
+#include "stacks_and_queues/queue_linked_list_based.h"
 int main()
 {
 	StackAB<int, 3> stack;
@@ -15,6 +16,17 @@ int main()
 	std::cout << stack.pop() <<std::endl;
 	std::cout << stack.pop() <<std::endl;
 	//std::cout << stack.top() <<std::endl;
+
+	QueueLLB<int> queue;
+	queue.enqueue(1);
+	queue.enqueue(2);
+	queue.enqueue(3);
+	std::cout << "Queue \n" ;
+
+	std::cout << queue.dequeue() <<std::endl;
+	std::cout << queue.dequeue() <<std::endl;
+	std::cout << queue.dequeue() <<std::endl;
+
 	return 0;
 }
 
--- data_structures/source/stacks_and_queues/queue_linked_list_based.h
+++ data_structures/source/stacks_and_queues/queue_linked_list_based.h
@@ -4,5 +4,94 @@
 
 #ifndef QUEUE_LINKED_LIST_BASED_H
 #define QUEUE_LINKED_LIST_BASED_H
+#include "i_queue.h"
+
+template<typename T>
+class QueueLLB: IQueue<T>
+{
+private:
+	struct Node
+	{
+		Node()
+		{
+			next = nullptr;
+		}
+		explicit Node(const T &val)
+		{
+			value = val;
+			next = nullptr;
+		}
+		T value{};
+		Node *next;
+
+	};
+	Node *head_;
+	Node *tail_;
+	size_t size_{};
+public:
+	QueueLLB()
+	{
+		head_ = nullptr;
+		tail_ = nullptr;
+	}
+
+	bool is_empty() const final
+	{
+		return head_ == nullptr;
+	}
+
+	size_t size() const final
+	{
+		return size_;
+	}
+
+	void enqueue(const T & value)
+	{
+		if(is_empty())
+		{
+			head_ = new Node(value);
+			tail_ = head_;
+			size_++;
+			return;
+		}
+		auto help = tail_;
+		tail_ = new Node(value);
+		help->next = tail_;
+		size_++;
+	}
+
+	T dequeue()
+	{
+		if(is_empty())
+		{
+			throw std::out_of_range("Can not dequeue. The queue is empty.");
+		}
+		auto help = head_;
+		head_ =  head_->next;
+		auto value = help->value;
+		delete help;
+		size_--;
+		return value;
+	}
+
+	T front() const final
+	{
+		if(is_empty())
+		{
+			throw std::out_of_range("Can not return front element. The queue is empty.");
+		}
+		return head_->value;
+	}
+
+	T back() const final
+	{
+		if(is_empty())
+		{
+			throw std::out_of_range("Can not return back element. The queue is empty.");
+		}
+		return tail_->value;
+	}
+
+};
 
 #endif //QUEUE_LINKED_LIST_BASED_H
