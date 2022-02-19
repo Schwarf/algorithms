69a8207c1da6c571ff0ff6a84005a43116ea7741
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-02-19 12:10:53 +0100

    Add boost library.

---
+++ data_structures/source/hash_functions/testing_hash_functions/test_hash_functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andreas on 19.02.22.
+//
+
+#include "test_hash_functions.h"
---
+++ data_structures/source/hash_functions/testing_hash_functions/test_hash_functions.h
@@ -0,0 +1,15 @@
+//
+// Created by andreas on 19.02.22.
+//
+
+#ifndef TEST_HASH_FUNCTIONS_H
+#define TEST_HASH_FUNCTIONS_H
+
+
+class test_hash_functions
+{
+
+};
+
+
+#endif //TEST_HASH_FUNCTIONS_H
