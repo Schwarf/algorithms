94ba79a87b20855fde0f4f7c445d5c3d127ef171
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-02-20 14:10:33 +0100

    Generate some prime numbers with sieve of Sundaram.

---
+++ data_structures/source/miscellaneous/generate_prime_numbers_up_to.h
@@ -0,0 +1,12 @@
+//
+// Created by andreas on 20.02.22.
+//
+
+#ifndef GENERATE_PRIMES_IN_RANGE_H
+#define GENERATE_PRIMES_IN_RANGE_H
+#include <fstream>
+
+
+void generate_prime
+
+#endif //GENERATE_PRIMES_IN_RANGE_H
