c71ec009b5262f60c561d6d34ac184d839052a47
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-02-20 14:12:54 +0100

    Update prime numbers output.

--- data_structures/source/miscellaneous/generate_prime_numbers_up_to.h
+++ data_structures/source/miscellaneous/generate_prime_numbers_up_to.h
@@ -5,8 +5,48 @@
 #ifndef GENERATE_PRIMES_IN_RANGE_H
 #define GENERATE_PRIMES_IN_RANGE_H
 #include <fstream>
+#include <vector>
+
+std::vector<bool> sieve_of_sundaram(size_t n)
+{
+	size_t k = (n - 1)/2;
+	auto is_prime_number_list = std::vector<bool>(k + 1, true);
+	size_t index1{};
+	size_t index2{};
+	for (index1 = 1; index1 < k + 1; index1++)
+	{
+		index2 = index1;
+		while(index1 + index2 + 2*index1*index2 <= k)
+		{
+			is_prime_number_list[index1 + index2 + 2*index1*index2] = false;
+			index2++;
+		}
+	}
+	return is_prime_number_list;
+}
 
 
-void generate_prime
+void generate_prime_numbers_up_to(size_t n)
+{
+	auto sieve = sieve_of_sundaram(n);
+	std::fstream file;
+	file.open("./../hash_functions/testing_hash_functions/prime_numbers.txt", std::ios::out);
+	size_t index{};
+	size_t k = (n-1)/2;
+	if(file.is_open())
+	{
+		file << 2 << "\n";
+		for(index = 1; index <= k; index++)
+		{
+			if(sieve[index])
+			{
+				file << 2*index + 1 << "\n";
+			}
+		}
+		file.close();
+	}
+
+
+}
 
 #endif //GENERATE_PRIMES_IN_RANGE_H
