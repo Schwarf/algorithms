bc1bdfcb24885e1333820e906ec21482e16bd1c7
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-03-13 10:41:26 +0100

    Add fill function for 2d matrix.

--- arrays_and_strings/rotate_matrix_1_7.cpp
+++ arrays_and_strings/rotate_matrix_1_7.cpp
@@ -36,13 +36,41 @@ private:
 	std::vector<matrix_type> data_;
 };
 
+template <typename matrix_type, size_t dimension>
+void fill_symmetric_matrix(SymmetricMatrix<matrix_type, dimension> & matrix)
+{
+	for(size_t row_index = 0; row_index < dimension; row_index++)
+	{
+		for(size_t column_index = 0; column_index < dimension; column_index++)
+		{
+			matrix(row_index, column_index) = 1 + column_index + row_index*dimension;
+		}
+	}
+
+}
+
+
+template <typename matrix_type, size_t dimension>
+void rotate_symmetric_matrix(SymmetricMatrix<matrix_type, dimension> & matrix)
+{
+
+}
+
+
 
 int main()
 {
+	SymmetricMatrix<int, 2> dim2;
 	SymmetricMatrix<int, 3> dim3;
-	dim3.operator()(0,0) = 2;
-	dim3(2,2) = 1;
+	SymmetricMatrix<int, 4> dim4;
+
+	fill_symmetric_matrix<int, 2>(dim2);
+	fill_symmetric_matrix<int, 3>(dim3);
+	fill_symmetric_matrix<int, 4>(dim4);
+
+	dim2.print_matrix();
 	dim3.print_matrix();
+	dim4.print_matrix();
 
 	return 0;
 }
