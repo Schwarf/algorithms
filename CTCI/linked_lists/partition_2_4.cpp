f85c9913c051e4a3debdc2ad1841d719882e5670
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-03-20 09:27:02 +0100

    Finish partition.

--- CTCI/linked_lists/partition_2_4.cpp
+++ CTCI/linked_lists/partition_2_4.cpp
@@ -88,6 +88,34 @@ public:
 			}
 
 		}
+		prev_node = node;
+		node = node->next;
+		while (node != nullptr) {
+			if (node->value < value) {
+				auto new_head_node = new Node<int>(node->value);
+				new_head_node->next = head_;
+				head_ = new_head_node;
+				if (node == end_node) {
+					end_node = prev_node;
+					delete node;
+					end_node->next = nullptr;
+					break;
+				}
+				else {
+					auto help = node;
+					prev_node->next = node->next;
+					node = node->next;
+					delete help;
+				}
+
+			}
+			else {
+				prev_node = node;
+				node = node->next;
+			}
+
+		}
+
 	}
 
 };
@@ -102,11 +130,65 @@ int main()
 	single.push_back(5);
 	single.push_back(1);
 	single.push_back(2);
+	single.push_back(17);
 	single.push_back(3);
 	single.print_list();
 	single.partition(5);
 	single.print_list();
 
+	auto single2 = LinkedList<int>();
+	single2.push_back(5);
+	single2.push_back(1);
+	single2.push_back(2);
+	single2.push_back(3);
+	single2.print_list();
+	single2.partition(5);
+	single2.print_list();
+
+
+	auto single3 = LinkedList<int>();
+	single3.push_back(1);
+	single3.push_back(2);
+	single3.push_back(3);
+	single3.push_back(5);
+	single3.push_back(6);
+	single3.push_back(7);
+	single3.print_list();
+	single3.partition(5);
+	single3.print_list();
+
+	auto single4 = LinkedList<int>();
+	single4.push_back(1);
+	single4.push_back(7);
+	single4.push_back(3);
+	single4.push_back(5);
+	single4.push_back(2);
+	single4.push_back(7);
+	single4.print_list();
+	single4.partition(5);
+	single4.print_list();
+
+
+	auto single5 = LinkedList<int>();
+	single5.push_back(7);
+	single5.push_back(5);
+	single5.print_list();
+	single5.partition(5);
+	single5.print_list();
+
+
+	auto single6 = LinkedList<int>();
+	single6.push_back(5);
+	single6.push_back(1);
+	single6.print_list();
+	single6.partition(5);
+	single6.print_list();
+
+	auto single7 = LinkedList<int>();
+	single7.push_back(1);
+	single7.print_list();
+	single7.partition(5);
+	single7.print_list();
 
 	return 0;
 }
