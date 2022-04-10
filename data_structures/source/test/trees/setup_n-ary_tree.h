78039198af0bfe1d6dbe70f22714cfb992cc9633
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-04-10 10:10:07 +0200

    Start tests with binary tree.

--- data_structures/source/test/trees/setup_n-ary_tree.h
+++ data_structures/source/test/trees/setup_n-ary_tree.h
@@ -36,6 +36,29 @@ protected:
 	std::vector<int> inorder{4, 10, 12, 15, 18, 22, 24, 25, 31, 35, 44, 50, 66, 70, 90};
 	std::vector<int> preorder{25, 15, 10, 4, 12, 22, 18, 24, 50, 35, 31, 44, 70, 66, 90};
 	std::vector<int> postorder{4, 12, 10, 18, 24, 22, 15, 31, 44, 35, 66, 90, 70, 50, 25};
+
+
+	static N_aryTree<int, 2> * binary_tree()
+	{
+		auto binary_tree = new N_aryTree<int, 2>(25);
+		binary_tree->root()->add_child(15);
+		binary_tree->root()->add_child(50);
+		binary_tree->root()->children.at(0)->add_child(10);
+		binary_tree->root()->children.at(0)->add_child(22);
+		binary_tree->root()->children.at(1)->add_child(35);
+		binary_tree->root()->children.at(1)->add_child(70);
+		binary_tree->root()->children.at(0)->children.at(0)->add_child(4);
+		binary_tree->root()->children.at(0)->children.at(0)->add_child(12);
+		binary_tree->root()->children.at(0)->children.at(1)->add_child(18);
+		binary_tree->root()->children.at(0)->children.at(1)->add_child(24);
+		binary_tree->root()->children.at(1)->children.at(0)->add_child(31);
+		binary_tree->root()->children.at(1)->children.at(0)->add_child(44);
+		binary_tree->root()->children.at(1)->children.at(1)->add_child(66);
+		binary_tree->root()->children.at(1)->children.at(1)->add_child(90);
+
+		return binary_tree;
+	}
+
 };
 
 #endif //SETUP_N_ARY_TREE_H
--- data_structures/source/test/trees/test_binary_tree.cpp
+++ data_structures/source/test/trees/test_binary_tree.cpp
@@ -4,19 +4,28 @@
 
 #include "setup_n-ary_tree.h"
 
+
+
 TEST_F(SetupN_aryTree, test_preorder_traversal)
 {
-	auto binary_tree = new N_aryTree<int, 2>();
-	for(const auto & element: insertion)
-	{
-		binary_tree->insert(element);
-	}
-
+	auto binary_tree = SetupN_aryTree::binary_tree();
 	auto help = HelperClass<int>();
 	binary_tree->traverse_preorder(std::ref(help));
 	for(size_t index =0; index < inorder.size(); ++index)
 	{
-		std::cout<< index << std::endl;
 		EXPECT_EQ(help.result().at(index), preorder.at(index));
+	}
+}
+
+
+TEST_F(SetupN_aryTree, test_inorder_traversal)
+{
+	auto binary_tree = SetupN_aryTree::binary_tree();
+
+	auto help = HelperClass<int>();
+	binary_tree->traverse_inorder(std::ref(help));
+	for(size_t index =0; index < inorder.size(); ++index)
+	{
+		EXPECT_EQ(help.result().at(index), inorder.at(index));
 	}
 }
