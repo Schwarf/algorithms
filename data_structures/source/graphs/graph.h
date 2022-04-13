a20722549db1e2696d7510320bb825dc7259a934
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-04-13 22:29:41 +0200

    Play with graph implementations.

--- data_structures/source/graphs/graph.h
+++ data_structures/source/graphs/graph.h
@@ -7,33 +7,54 @@
 #include <cstddef>
 #include <list>
 
-template<typename T>
+template<typename data_T>
+class Vertex;
+
+
+template<typename data_T, typename weight_T>
+struct UndirectedConnection
+{
+	UndirectedConnection(Vertex<data_T> * vertex1, Vertex<data_T> * vertex2, weight_T weight)
+	{
+		vertex1_ = vertex1;
+		vertex2_ = vertex2;
+		weight_ = weight;
+	}
+	Vertex<data_T> * vertex1_;
+	Vertex<data_T> * vertex2_;
+	weight_T weight_;
+};
+
+
+
+template<typename data_T>
 class Vertex
 {
-	explicit Vertex(){
-		label_ = Vertex<T>::label_counter_++;
-		Vertex<T>::label_counter_++;
-	}
-	void add_vertex(const Vertex<T> & vertex)
-	{
-		connected_vertices_.insert(vertex);
+	explicit Vertex(data_T value){
+		value_ = value;
 	}
 
-	size_t label()
+	void connect_to_vertex(Vertex<data_T> * vertex)
 	{
-		return label_;
+		connections_.insert(new UndirectedConnection<data_T, size_t>(this, vertex));
+	}
+
+	std::list<UndirectedConnection<data_T, size_t>> vertices()
+	{
+		return connections_;
+	}
+
+	data_T value()
+	{
+		return value_;
 	}
 
 private:
-	static size_t label_counter_;
-	size_t label_{};
-	T value_;
-	std::list<Vertex> connected_vertices_;
+	data_T value_;
+	std::list<UndirectedConnection<data_T, size_t> * > connections_;
 
 };
 
-template<typename T>
-size_t Vertex<T>::label_counter_ = 0;
 
 
 #endif //GRAPH_H
