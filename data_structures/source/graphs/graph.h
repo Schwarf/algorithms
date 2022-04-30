a1769b6f4e22b7e1cbc2fbb6e6f68b2350edb64c
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-04-30 09:07:29 +0200

    Work in progress on graph.

--- data_structures/source/graphs/graph.h
+++ data_structures/source/graphs/graph.h
@@ -11,10 +11,10 @@ template<typename data_T>
 class Vertex;
 
 
-template<typename data_T, typename weight_T>
-struct UndirectedConnection
+template<typename data_T>
+struct UndirectedEdge
 {
-	UndirectedConnection(Vertex<data_T> * vertex1, Vertex<data_T> * vertex2, weight_T weight)
+	UndirectedEdge(Vertex<data_T> * vertex1, Vertex<data_T> * vertex2, size_t weight)
 	{
 		vertex1_ = vertex1;
 		vertex2_ = vertex2;
@@ -22,7 +22,7 @@ struct UndirectedConnection
 	}
 	Vertex<data_T> * vertex1_;
 	Vertex<data_T> * vertex2_;
-	weight_T weight_;
+	size_t weight_;
 };
 
 
@@ -32,29 +32,46 @@ class Vertex
 {
 	explicit Vertex(data_T value){
 		value_ = value;
+		vertex_id_ = vertex_count_;
+		vertex_count_++;
+
 	}
 
-	void connect_to_vertex(Vertex<data_T> * vertex)
+	void connect_to_vertex(Vertex<data_T> * vertex, size_t weight)
 	{
-		connections_.insert(new UndirectedConnection<data_T, size_t>(this, vertex));
+		edges_.insert(new UndirectedEdge<data_T>(this, vertex, weight));
 	}
 
-	std::list<UndirectedConnection<data_T, size_t>> vertices()
+	std::list<UndirectedEdge<data_T>> edges()
 	{
-		return connections_;
+		return edges_;
 	}
 
+	size_t id()
+	{
+		return vertex_id_;
+	}
 	data_T value()
 	{
 		return value_;
 	}
+	~Vertex()
+	{
+		for(auto connection : edges_)
+		{
+			delete connection;
+		}
+	}
 
 private:
 	data_T value_;
-	std::list<UndirectedConnection<data_T, size_t> * > connections_;
-
+	std::list<UndirectedEdge<data_T> * > edges_;
+	size_t vertex_id_{};
+	static size_t vertex_count_;
 };
 
+template <typename  data_T>
+size_t Vertex<data_T>::vertex_count_ = 0;
 
 
 #endif //GRAPH_H
---
+++ data_structures/source/trees/tree_node.h
@@ -0,0 +1,8 @@
+//
+// Created by andreas on 30.04.22.
+//
+
+#ifndef TREE_NODE_H
+#define TREE_NODE_H
+
+#endif //TREE_NODE_H
