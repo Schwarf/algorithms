23986a4ab28561f58f1857291753570b95448833
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-04-30 09:32:10 +0200

    Path sum from root to leaf.

---
+++ data_structures/source/trees/tree_has_path_sum.h
@@ -0,0 +1,24 @@
+//
+// Created by andreas on 30.04.22.
+//
+
+#ifndef TREE_ALGORITHMS_H
+#define TREE_ALGORITHMS_H
+
+
+// Given the root of a binary tree and an integer targetSum, return true if the tree has a
+// root-to-leaf path such that adding up all the values along the path equals targetSum
+#include "tree_node.h"
+bool has_path_sum(TreeNode<int> *root, int target)
+{
+	if (!root)
+		return false;
+	// has to be leave node
+	if (root->left == nullptr && root->right == nullptr && root->value == target)
+		return true;
+	target = target - root->value;
+	return has_path_sum(root->left, target) || has_path_sum(root->right, target);
+}
+
+
+#endif //TREE_ALGORITHMS_H
