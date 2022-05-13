286f59ab9b89051bb48c3d7fc99809fe34092f50
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-13 18:15:53 +0200

    Add test for inverted binary tree. Split up test-executables in CMakeLists.

--- data_structures/source/test/trees/binary_trees/test_invert_binary_tree.cpp
+++ data_structures/source/test/trees/binary_trees/test_invert_binary_tree.cpp
@@ -4,15 +4,15 @@
 
 #include "setup_binary_tree.h"
 #include "./../../../../trees/binary_trees/invert_binary_tree.h"
-//#include "./../../../../trees/binary_trees/post_order_traversal.h"
+#include "./../../../../trees/binary_trees/post_order_traversal.h"
 
 TEST_F(SetupBinaryTree, test_invert_binary_tree)
 {
 	auto root = SetupBinaryTree::binary_tree_root();
 	auto inverted = invert_binary_tree(root);
-//	auto expected_result = post_order_traversal(SetupBinaryTree::inverted_tree());
-//	auto inverted_result = post_order_traversal(inverted);
-//	bool comparison = expected_result == inverted_result;
-	EXPECT_TRUE(true);
+	auto expected_result = post_order_traversal(SetupBinaryTree::inverted_tree());
+	auto inverted_result = post_order_traversal(inverted);
+	bool comparison = expected_result == inverted_result;
+	EXPECT_TRUE(comparison);
 }
 
