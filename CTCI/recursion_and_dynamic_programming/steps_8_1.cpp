3442bbfc7c323b0e27947ed2cf2e9027325bd95f
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-28 19:22:15 +0200

    Fix bug and add recursive solution.

--- CTCI/recursion_and_dynamic_programming/steps_8_1.cpp
+++ CTCI/recursion_and_dynamic_programming/steps_8_1.cpp
@@ -5,20 +5,39 @@
 #include <vector>
 int64_t step_count(int steps)
 {
+	if(steps ==  0)
+		return 0;
+	if(steps == 1)
+		return 1;
+	if(steps == 2)
+		return 2;
+
     std::vector<int64_t> help(steps+1, 0);
-    help[0] = 0;
+	help[0] = 0;
     help[1] = 1;
     help[2] = 2;
-    help[3] = 4;
-    for(int i=4; i <= steps; ++i)
+
+	for(int i=3; i <= steps; ++i)
     {
         help[i] = help[i-1] + help[i-2] + help[i-3];
     }
     return help[steps];
 }
 
+int64_t step_count_recursive(int steps)
+{
+	if (steps <=0)
+		return 0;
+	if (steps ==1)
+		return 1;
+	if (steps ==2)
+		return 2;
+	return step_count_recursive(steps-1) + step_count_recursive(steps-2) + step_count_recursive(steps-3);
+}
+
 
 int main()
 {
-	std::cout << step_count(37) << std::endl;
+	std::cout << step_count(18) << std::endl;
+	std::cout << step_count_recursive(18) << std::endl;
 }
