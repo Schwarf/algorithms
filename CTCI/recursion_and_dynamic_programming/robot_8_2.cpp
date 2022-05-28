6365447355f238bf7c2a63f3ee7e711abcc6bcfc
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-28 19:52:19 +0200

    Add memo version of robot.

--- CTCI/recursion_and_dynamic_programming/robot_8_2.cpp
+++ CTCI/recursion_and_dynamic_programming/robot_8_2.cpp
@@ -4,6 +4,19 @@
 
 #include <vector>
 #include <iostream>
+#include <unordered_set>
+
+struct pair_hash
+{
+	template <class T1, class T2>
+	std::size_t operator () (std::pair<T1, T2> const &pair) const
+	{
+		std::size_t h1 = std::hash<T1>()(pair.first);
+		std::size_t h2 = std::hash<T2>()(pair.second);
+
+		return h1 ^ h2;
+	}
+};
 
 bool getPath(std::vector<std::vector<bool>> & maze, int row, int col, std::vector<std::pair<int, int>> & points)
 {
@@ -30,6 +43,36 @@ std::vector<std::pair<int, int>> getPath(std::vector<std::vector<bool>> & maze)
 
 
 
+
+bool getPath_memo(std::vector<std::vector<bool>> & maze, int row, int col, std::vector<std::pair<int, int>> & points, std::unordered_set<std::pair<int, int>, pair_hash> & memo)
+{
+	if(row < 0 || col < 0 || !maze[row][col])
+		return false;
+
+	if(memo.find(std::make_pair(row, col)) != memo.end() )
+		return false;
+
+	bool origin_reached = (row == 0) && (col == 0);
+	if(origin_reached || getPath_memo(maze, row-1, col, points, memo) || getPath_memo(maze, row, col-1, points, memo))
+	{
+		points.push_back(std::make_pair(row, col));
+		return true;
+	}
+	memo.insert(std::make_pair(row, col));
+	return false;
+}
+
+std::vector<std::pair<int, int>> getPath_memo(std::vector<std::vector<bool>> & maze)
+{
+	std::vector<std::pair<int, int>> points;
+	std::unordered_set<std::pair<int, int>, pair_hash> memo;
+	if(getPath_memo(maze, maze.size() - 1, maze[0].size() - 1, points, memo))
+		return points;
+	return {};
+}
+
+
+
 int main()
 {
 	auto maze = std::vector<std::vector<bool>>(3, std::vector<bool>(5, true));
@@ -41,6 +84,13 @@ int main()
 
 	auto path = getPath(maze);
 	for(const auto & pair: path)
+	{
+		std::cout << pair.first << "  " << pair.second << std::endl;
+	}
+
+	std::cout << "***************" << std::endl;
+	auto path2 = getPath(maze);
+	for(const auto & pair: path2)
 	{
 		std::cout << pair.first << "  " << pair.second << std::endl;
 	}
