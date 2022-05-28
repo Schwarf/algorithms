9a98aa2bb3e99de3884bba125a02f1ef3d44dec8
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-28 21:00:51 +0200

    Start magic index.

---
+++ CTCI/recursion_and_dynamic_programming/magic_index_8_3.cpp
@@ -0,0 +1,40 @@
+//
+// Created by andreas on 28.05.22.
+//
+#include <vector>
+#include <iostream>
+
+bool has_magic_index(std::vector<int> &A)
+{
+	if (A.empty())
+		return false;
+	int start = 0;
+	int end = A.size() - 1;
+	int mid{};
+	while (end > start) {
+		mid = start + (end - start) / 2;
+
+		if (A[mid] == mid)
+		{
+			std::cout << mid << std::endl;
+			return true;
+		}
+
+		else if (A[mid] > mid)
+			end = mid;
+		else if (A[mid] < mid)
+			start = mid + 1;
+	}
+	return false;
+
+}
+
+int main()
+{
+	auto v1 = std::vector < int > {-1, 0, 2, 4, 6, 110, 1200};
+	auto v2 = std::vector < int > {-2, -1, 1, 2, 3, 5, 8};
+	auto v3 = std::vector < int > {-3, -2, -1, 0, 10, 20, 30};
+	
+	return 0;
+
+}
