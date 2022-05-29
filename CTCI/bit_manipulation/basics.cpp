06537d6b51d279d16daabe3e829508e304f21cd6
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-29 14:22:46 +0200

    Implement has_bit function.

--- CTCI/bit_manipulation/basics.cpp
+++ CTCI/bit_manipulation/basics.cpp
@@ -14,11 +14,23 @@ std::bitset<bitset_size> convert(T & value)
 
 }
 
+template <typename T>
+bool has_bit(T number, int bit_position)
+{
+	static_assert(std::is_integral<T>::value, "Integral required.");
+	return (number & (1 << bit_position)) != 0;
+
+}
+
+
 
 
 int main()
 {
-	int x1{56};
+	int x1{29};
 	std::cout << convert<int, 6>(x1).to_string() << std::endl;
+	std::cout << "Bit 3 of 29 shall be set: " << has_bit<int>(x1, 3) << std::endl;
+	std::cout << "Bit 2 of 29 shall be set: " << has_bit<int>(x1, 2) << std::endl;
+	std::cout << "Bit 1 of 29 shall be NOT set: " << has_bit<int>(x1, 1) << std::endl;
 	return 0;
 }
