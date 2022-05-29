1cad56e6c77c37d534bcdd19c0ba5d8a2c6b080e
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-05-29 14:29:28 +0200

    Add and test set_bit.

--- CTCI/bit_manipulation/basics.cpp
+++ CTCI/bit_manipulation/basics.cpp
@@ -19,9 +19,15 @@ bool has_bit(T number, int bit_position)
 {
 	static_assert(std::is_integral<T>::value, "Integral required.");
 	return (number & (1 << bit_position)) != 0;
-
 }
 
+template <typename T>
+void set_bit(T &number, int bit_position)
+{
+	static_assert(std::is_integral<T>::value, "Integral required.");
+
+	number |= (1 << bit_position);
+}
 
 
 
@@ -29,8 +35,17 @@ int main()
 {
 	int x1{29};
 	std::cout << convert<int, 6>(x1).to_string() << std::endl;
+	std::cout << "Bit 5 of 29 shall be NOT set: " << has_bit<int>(x1, 5) << std::endl;
 	std::cout << "Bit 3 of 29 shall be set: " << has_bit<int>(x1, 3) << std::endl;
 	std::cout << "Bit 2 of 29 shall be set: " << has_bit<int>(x1, 2) << std::endl;
 	std::cout << "Bit 1 of 29 shall be NOT set: " << has_bit<int>(x1, 1) << std::endl;
+	std::cout << "Bit 0 of 29 shall be set: " << has_bit<int>(x1, 0) << std::endl;
+	std::cout << "Set 5 bit of 29."  << std::endl;
+	set_bit<int>(x1, 5);
+	std::cout << "Bit 5 of 29 (with 5th bit set) aka ("<< x1 << "=61) shall be set: " << has_bit<int>(x1, 5) << std::endl;
+	std::cout << "Set 1 bit of 61."  << std::endl;
+	set_bit<int>(x1, 1);
+	std::cout << "Bit 2 of 61 (with 2nd bit set) aka ("<< x1 << "=63) shall be set: " << has_bit<int>(x1, 1) << std::endl;
+
 	return 0;
 }
