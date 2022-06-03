a44e465ab9441894f09cb8ee833a03ba35fee150
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-06-03 11:34:39 +0200

    Update segment tree.

--- data_structures/source/trees/segment_trees/segment_tree.h
+++ data_structures/source/trees/segment_trees/segment_tree.h
@@ -5,7 +5,7 @@
 #ifndef SEGMENT_TREE_H
 #define SEGMENT_TREE_H
 #include <vector>
-
+// USed only for fixed size arrays/vectors. Adding of elements not POSSIBLE!!!
 template<typename T>
 class SegmentTree{
 public:
@@ -17,10 +17,28 @@ public:
 		tree_ = std::vector<T>(tree_size_);
 		build_tree(input);
 	}
-	void update(size_t index, T val)
+	void update(int index, T val)
 	{
-		
+		// input is stored in second half of tree
+		index += tree_size_/2;
+		// update leaf
+		tree_[index] = val;
+		// update parents
+		while(index >0)
+		{
+			int left = index;
+			int right = index;
+
+			if(index & 1)
+				left = index - 1;
+			else
+				right = index + 1;
+			tree_[index/2] = tree_[left] + tree_[right];
+			index /= 2;
+		}
 	}
+
+	
 
 private:
 	void build_tree(std::vector<T> & input)
@@ -37,7 +55,7 @@ private:
 	}
 
 	std::vector<T> tree_;
-	size_t tree_size_{};
+	int tree_size_{};
 };
 
 #endif //SEGMENT_TREE_H
