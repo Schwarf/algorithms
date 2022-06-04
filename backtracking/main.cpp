5636aa61b9c5a14bb8d549713b40226a87e66352
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-06-04 13:42:43 +0200

    Finish queen implementation.

--- backtracking/main.cpp
+++ backtracking/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "queens_problem.h"
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	auto r = solve_queen_problem(4);
+	for(const auto & element : r)
+	{
+		for(const auto & e : element)
+			std::
+	}
+
 	return 0;
 }
--- backtracking/queens_problem.h
+++ backtracking/queens_problem.h
@@ -19,18 +19,33 @@ void depth_first_search(std::vector<std::vector<std::string>> &result,
 		result.push_back(board);
 		return;
 	}
-	for(int col =0; col < board_size; ++col)
-	{
-		if(invalid_columns[col] || invalid_backward_diagonals[col + row ] || invalid_forward_diagonals[col - row + board_size -1])
+	for (int col = 0; col < board_size; ++col) {
+		if (invalid_columns[col] || invalid_backward_diagonals[col + row]
+			|| invalid_forward_diagonals[col - row + board_size - 1])
 			continue;
-		
+		board[row][col] = 'Q';
+		invalid_columns[col] = true;
+		invalid_backward_diagonals[col + row] = true;
+		invalid_forward_diagonals[col - row + board_size - 1] = true;
+		depth_first_search(result,
+						   std::move(board),
+						   row + 1,
+						   board_size,
+						   std::move(invalid_columns),
+						   std::move(invalid_forward_diagonals),
+						   std::move(invalid_backward_diagonals));
+		invalid_columns[col] = false;
+		invalid_backward_diagonals[col + row] = false;
+		invalid_forward_diagonals[col - row + board_size - 1] = false;
+		board[row][col] = '.';
+
 	}
 }
 
 std::vector<std::vector<std::string>> solve_queen_problem(int board_size)
 {
 	auto result = std::vector<std::vector<std::string>>();
-	auto board = std::vector<std::string>(board_size, std::string(board_size, '.'));
+	auto board = std::vector<std::string>(board_size, std::string(board_size, '-'));
 	depth_first_search(result,
 					   std::move(board),
 					   0,
