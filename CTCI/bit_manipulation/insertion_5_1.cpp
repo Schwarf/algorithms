8a7bdc5a5856c108aa0694454b1b89f1d5efb589
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-06-07 19:47:00 +0200

    Solution without while loop.

--- CTCI/bit_manipulation/insertion_5_1.cpp
+++ CTCI/bit_manipulation/insertion_5_1.cpp
@@ -14,18 +14,29 @@ int set_bit(int number, int index, bool value)
 
 int insert(int N, int M, int start, int end)
 {
+	if( start> end || start < 0 || end>= 32) return 0;
 	int N_index = start;
 	bool value{};
 	while(N_index <= end)
 	{
 		int M_index = N_index - start;
 		value = get_bit(M, M_index);
-		std::cout <<"Index: "<< M_index << ":  " << value << std::endl;
 		N = set_bit(N, N_index, value);
-		std::cout <<"N: "<< N << std::endl;
 		N_index++;
 	}
 	return N;
+}
+
+int master_solution(int n, int m, int i, int j)
+{
+	if( i> j || i < 0 || j>= 32) return 0;
+	int allOnes = ~0;
+	int left = j < 31 ? (allOnes << (j+1)) : 0;
+	int right = (1<<i) - 1;
+	int mask = left + right;
+	int n_cleared = n & mask;
+	int m_shifted = m << i;
+	return n_cleared | m_shifted;
 }
 
 int main()
@@ -35,8 +46,9 @@ int main()
 	std::cout << N << std::endl;
 	std::cout << M << std::endl;
 	auto result = insert(N,M, 2, 6);
-	auto expected = 2124;
+	auto expected = master_solution(N, M, 2, 6);
 
 	std::cout << result << std::endl;
+	std::cout << expected << std::endl;
 	return 0;
 }
