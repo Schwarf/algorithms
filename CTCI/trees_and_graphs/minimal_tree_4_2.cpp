8583a6a37a134711a407623b6b401543c5965e65
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-06-07 14:35:37 +0200

    Minimal tree.

---
+++ CTCI/trees_and_graphs/minimal_tree_4_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+template<typename T>
+
+struct Node
+{
+	T val;
+	Node *left = nullptr;
+	Node *right = nullptr;
+};
+
+template<typename T>
+Node<T> *create_minimal_tree(std::vector<T> &sorted, int start_index, int end_index)
+{
+	if (end_index < start_index)
+		return nullptr;
+	int middle = (start_index + end_index) / 2;
+	auto node = new Node<T>(sorted[middle]);
+	node->left = create_minimal_tree(sorted, start_index, middle - 1);
+	node->right = create_minimal_tree(sorted, middle + 1, end_index);
+	return node;
+}
+
+template<typename T>
+Node<T> *create_minimal_binary_search_tree(std::vector<T> &sorted)
+{
+	return create_minimal_tree(sorted, 0, sorted.size() - 1);
+}
+
+int main()
+{
+	std::vector<int> x{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+}
