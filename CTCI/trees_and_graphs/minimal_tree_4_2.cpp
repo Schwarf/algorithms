700e1c6740d15cc1126b9515b9ac73d8f727d285
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-06-07 15:14:48 +0200

    Add some iterative solution.

--- CTCI/trees_and_graphs/minimal_tree_4_2.cpp
+++ CTCI/trees_and_graphs/minimal_tree_4_2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 template<typename T>
 
@@ -29,11 +30,37 @@ Node<T> *create_minimal_binary_search_tree(std::vector<T> &sorted)
 	return create_minimal_tree(sorted, 0, sorted.size() - 1);
 }
 
+template<typename T>
+Node<T> *create_minimal_binary_search_tree_iteratively(std::vector<T> &sorted)
+{
+	int index = (sorted.size() -1)/ 2;
+	auto head = new Node<T>(sorted[index]);
+	auto queue = std::queue<std::pair<Node<T>*, int>>();
+	queue.push(std::make_pair(head, index));
+	while(!queue.empty() && ((index+index/2) < sorted.size()) && ((index-index/2) > -1))
+	{
+		auto node = queue.front().first;
+		index = queue.front().second;
+		queue.pop();
+		auto left_index = index - index/2;
+		auto right_index = index + index/2;
+		auto left = new Node<T>(sorted[left_index]);
+		auto right = new Node<T>(sorted[right_index]);
+		node->left = left;
+		node->right = right;
+		queue.push(std::make_pair(left, left_index));
+		queue.push(std::make_pair(right, right_index));
+	}
+	return head;
+}
+
 int main()
 {
 	std::vector<int> x{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 	auto head = create_minimal_binary_search_tree(x);
+	auto head2 = create_minimal_binary_search_tree_iteratively(x);
+
 	std::cout << head->value << std::endl;
 	std::cout << head->left->left->left->value << std::endl;
 	std::cout << head->right->value << std::endl;
