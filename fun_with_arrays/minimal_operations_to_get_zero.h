0ea014117a88bc5fd509775d3f839904ada3f156
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-06-12 14:17:56 +0200

    Fun with arrays again.

---
+++ fun_with_arrays/minimal_operations_to_get_zero.h
@@ -0,0 +1,48 @@
+//
+// Created by andreas on 12.06.22.
+//
+
+#ifndef MINIMAL_OPERATIONS_ARRAY_ELEMENTS_TO_GET_ZERO_TARGET_H
+#define MINIMAL_OPERATIONS_ARRAY_ELEMENTS_TO_GET_ZERO_TARGET_H
+#include <vector>
+
+// You are given an integer array nums and an integer x. In one operation, you can either remove the leftmost or the
+// rightmost element from the array nums and subtract its value from x. Note that
+// this modifies the array for future operations.
+// Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise, return -1.
+
+// Constraints:
+// 1 <= nums.length <= 10^5
+// 1 <= nums[i] <= 10^4
+// 1 <= x <= 10^9
+
+template<typename T>
+int minimal_operations_to_get_zero(std::vector<T> &input, T target)
+{
+	T sum{};
+	for(const auto & element: input)
+		sum += element;
+	if(sum == target)
+		return input.size();
+	if(sum < target)
+		return -1;
+
+	target = sum - target;
+	T current_sum{};
+	int start_index;
+	int count_operations{};
+	for(int i =0; i < input.size(); ++i)
+	{
+		current_sum += input[i];
+		while(current_sum > target)
+			current_sum -= input[start_index++];
+		if(current_sum == target)
+		{
+			count_operations = std::max(count_operations, i -start_index + 1);
+		}
+	}
+	return count_operations == 0 ? -1 : input.size() - count_operations;
+}
+
+
+#endif //MINIMAL_OPERATIONS_ARRAY_ELEMENTS_TO_GET_ZERO_TARGET_H
--- fun_with_arrays/sum_longest_subarray_with_unique_elements.h
+++ fun_with_arrays/sum_longest_subarray_with_unique_elements.h
@@ -7,6 +7,17 @@
 #include <unordered_map>
 #include <vector>
 
+// You are given an array of positive integers nums and want to erase a subarray containing unique elements.
+// The score you get by erasing the subarray is equal to the sum of its elements.
+// Return the maximum score you can get by erasing exactly one subarray.
+// An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is,
+// if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+// Constraints:
+
+// 1 <= nums.length <= 10^5
+// 1 <= nums[i] <= 10^4
+
 template <typename T>
 int sum_longest_subarray_with_unique_elements(std::vector<T> & input)
 {
