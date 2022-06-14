d8c4f52be19d807a45607350c3f8b8f20e2ab1c8
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-06-14 18:29:24 +0200

    Fix orting tests.

--- sorting_algorithms/source/test/sorting_algorithms/sorting_setup.h
+++ sorting_algorithms/source/test/sorting_algorithms/sorting_setup.h
@@ -14,7 +14,7 @@ public:
 		std::random_device random_device;
 		std::mt19937_64 mersenne_generator(random_device());
 		std::uniform_int_distribution<int64_t> uniform_distribution;
-		size_t N = 10000;
+		size_t N = 5;
 		for (size_t index = 0; index < N; ++index) {
 			auto number = uniform_distribution(mersenne_generator);
 			random_numbers.push_back(number);
@@ -23,10 +23,9 @@ public:
 		std::sort(std_sorted_reference.begin(), std_sorted_reference.end());
 
 		std::uniform_int_distribution<int64_t> uniform_distribution_in_range(lower_limit_, upper_limit_);
-		size_t range_N = 1000;
+		size_t range_N = 3;
 		for (size_t index = 0; index < range_N; ++index) {
 			auto number = uniform_distribution_in_range(mersenne_generator);
-			random_numbers.push_back(number);
 			std_sorted_numbers_in_range.push_back(number);
 		}
 		std::sort(std_sorted_numbers_in_range.begin(), std_sorted_numbers_in_range.end());
