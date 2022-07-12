4d47e155849aafb8b3866787ff87e29899637801
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-12 18:58:49 +0200

    Jump to last index.

--- dynamic_programming/min_costs_to_reach_end.cpp
+++ dynamic_programming/min_costs_to_reach_end.cpp
@@ -15,7 +15,7 @@ int min_cost_to_reach_end(std::vector<int> &cost)
 	if (cost.size() == 2)
 		return std::min(cost[0], cost[1]);
 	for (int i = 2; i <= cost.size(); ++i) {
-		int one_step = previous +  cost[i - 1] ;
+		int one_step = previous + cost[i - 1];
 		int two_step = before_previous + cost[i - 2];
 		before_previous = previous;
 		previous = std::min(one_step, two_step);
@@ -28,8 +28,9 @@ int main()
 	auto test1 = std::pair<std::vector<int>, int>{std::vector<int>{10, 15, 20}, 15};
 	auto test2 = std::pair<std::vector<int>, int>{std::vector<int>{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}, 6};
 	auto test3 =
-		std::pair<std::vector<int>, int>{std::vector<int>{1, 12, 419, 181, 17, 19, 0, 71, 26, 1, 2, 3, 4, 5, 6, 7}, 248};
-	std::cout << min_cost_to_reach_end(test1.first) << ", " <<  test1.second << std::endl;
+		std::pair<std::vector<int>, int>{std::vector<int>{1, 12, 419, 181, 17, 19, 0, 71, 26, 1, 2, 3, 4, 5, 6, 7},
+										 248};
+	std::cout << min_cost_to_reach_end(test1.first) << ", " << test1.second << std::endl;
 	std::cout << min_cost_to_reach_end(test2.first) << ", " << test2.second << std::endl;
-
+	std::cout << min_cost_to_reach_end(test3.first) << ", " << test3.second << std::endl;
 }
---
+++ dynamic_programming/min_jumps_to_last_index.cpp
@@ -0,0 +1,37 @@
+// Given an array of non-negative integers, you are initially positioned at the first index of the array.
+// Each element in the array represents your maximum jump length at that position.
+// Your goal is to reach the last index in the minimum number of jumps.
+// You can assume that you can always reach the last index.
+
+#include <iostream>
+#include <vector>
+
+int min_jumps_to_last_index(std::vector<int> &max_jump_lengths)
+{
+	int current_reach = 0;
+	int current_max = 0;
+	int jumps = 0;
+	for(int i = 0;i<max_jump_lengths.size()-1;i++){
+		if(i+max_jump_lengths[i] > current_max){
+			current_max = i +max_jump_lengths[i];
+		}
+		if(i==current_reach){
+			jumps++;
+			current_reach = current_max;
+		}
+	}
+	return jumps;
+}
+
+
+int main()
+{
+	auto test1 = std::pair<std::vector<int>, int>{std::vector<int>{2,3,1,1,4}, 2};
+	auto test2 = std::pair<std::vector<int>, int>{std::vector<int>{1,2,3,4,5,6,7,8,9,10}, 4};
+	auto test3 =
+	std::pair<std::vector<int>, int>{std::vector<int>{1,3,5,3,3,3,6,1,1,1,1,1,1}, 4};
+	std::cout << min_jumps_to_last_index(test1.first) << ", " << test1.second << std::endl;
+	std::cout << min_jumps_to_last_index(test2.first) << ", " << test2.second << std::endl;
+	std::cout << min_jumps_to_last_index(test3.first) << ", " << test3.second << std::endl;
+
+}
