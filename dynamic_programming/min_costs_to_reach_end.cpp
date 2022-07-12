7e468a475583be86d3b41356011ea6c33ed11798
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-12 18:16:38 +0200

    First dynmaic programming problem.

---
+++ dynamic_programming/min_costs_to_reach_end.cpp
@@ -0,0 +1,35 @@
+// You are given an integer array cost where cost[i] is the cost of ith step. Once you pay the cost, you can either
+// take one or two steps. You can either start from the step with index 0, or the step with index 1.
+// Return the minimum cost to reach the end of the array.
+// 2 <= cost.length <= 1000
+// 0 <= cost[i] <= 999
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int min_cost_to_reach_end(std::vector<int> &cost)
+{
+	int previous{};
+	int before_previous{};
+	if (cost.size() == 2)
+		return std::min(cost[0], cost[1]);
+	for (int i = 2; i <= cost.size(); ++i) {
+		int one_step = previous +  cost[i - 1] ;
+		int two_step = before_previous + cost[i - 2];
+		before_previous = previous;
+		previous = std::min(one_step, two_step);
+	}
+	return previous;
+}
+
+int main()
+{
+	auto test1 = std::pair<std::vector<int>, int>{std::vector<int>{10, 15, 20}, 15};
+	auto test2 = std::pair<std::vector<int>, int>{std::vector<int>{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}, 6};
+	auto test3 =
+		std::pair<std::vector<int>, int>{std::vector<int>{1, 12, 419, 181, 17, 19, 0, 71, 26, 1, 2, 3, 4, 5, 6, 7}, 248};
+	std::cout << min_cost_to_reach_end(test1.first) << ", " <<  test1.second << std::endl;
+	std::cout << min_cost_to_reach_end(test2.first) << ", " << test2.second << std::endl;
+
+}
