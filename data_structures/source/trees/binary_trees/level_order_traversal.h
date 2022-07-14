91a8a2f8b5014073adf5c3b3642c9c7243675d56
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-14 12:25:07 +0200

    Add level order traversal.

---
+++ data_structures/source/trees/binary_trees/level_order_traversal.h
@@ -0,0 +1,45 @@
+//
+// Created by andreas on 14.07.22.
+//
+
+#ifndef LEVEL_ORDER_TRAVERSAL_H
+#define LEVEL_ORDER_TRAVERSAL_H
+#include <vector>
+#include <queue>
+#include "tree_node.h"
+
+
+template <typename T>
+std::vector<std::vector<T>> levelOrder(TreeNode<T>* root) {
+	if(!root)
+		return {};
+	auto result = std::vector<std::vector<T>>();
+	std::queue<TreeNode<T>*> help;
+	help.push(root);
+	help.push(nullptr);
+	auto node = root;
+	while(!help.empty())
+	{
+		auto insert = std::vector<T>();
+		if(help.front() != nullptr)
+		{
+			while(help.front() != nullptr)
+			{
+				node = help.front();
+				insert.push_back(node->val);
+				help.pop();
+				if(node->left)
+					help.push(node->left);
+				if(node->right)
+					help.push(node->right);
+			}
+			result.push_back(insert);
+			help.push(nullptr);
+		}
+		else
+			help.pop();
+	}
+	return result;
+}
+
+#endif //LEVEL_ORDER_TRAVERSAL_H
