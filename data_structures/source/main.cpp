8c9f219ca69b621af572041c01b428fab913958e
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-14 11:09:35 +0200

    Construct binary tree from preorder and inorder.

--- data_structures/source/main.cpp
+++ data_structures/source/main.cpp
@@ -1,83 +1,29 @@
 #include <iostream>
-#include "./binary_search_tree/binary_search_tree.h"
-#include "./linked_lists/singly_linked_list.h"
-#include "stacks_and_queues/stack_linked_list_based.h"
-#include "stacks_and_queues/stack_array_based.h"
-#include "stacks_and_queues/queue_array_based.h"
-#include "heaps/binary_heap.h"
-#include "heaps/k_ary_heap.h"
 #include <vector>
-#include <algorithm>
-#include "hash_functions/division_hashing_for_strings.h"
-#include "hash_functions/multiplication_hashing_for_strings.h"
-#include "hash_functions/testing_hash_functions/test_if_hashes_uniformly_distributed.h"
-#include "miscellaneous/generate_prime_numbers_up_to.h"
-#include "trees/n-ary_tree.h"
-#include "tries/trie.h"
+#include "trees/binary_trees/tree_node.h"
+#include "trees/binary_trees/in_order_traversal.h"
+#include "trees/binary_trees/pre_order_traversal.h"
+#include "trees/binary_trees/construct_binary_tree_from_preorder_and_inorder.h"
 int main()
 {
-	constexpr size_t hash_table_size = 4L*4*4*4 * 4*4*4*4 ; // 2**16
-	auto multi_hashing = MultiplicationHashing<256, 65449>();
-	auto division_hashing = DivisionHashing<hash_table_size, 256>();
-	auto test = TestHashFunctions<hash_table_size>();
-	test.test(multi_hashing);
+	auto root = new TreeNode<int>(1);
+	root->add_left(2)->add_left(4)->add_left(6);
+	root->left->add_right(5);
+	root->add_right(3);
+	std::vector<int> pre_order, in_order;
+	in_order_traversal(in_order, root);
+	pre_order_traversal(pre_order, root);
+	std::cout << "Pre order:" << std::endl;
+	for(const auto & element: pre_order)
+		std::cout << element << ", " ;
+	std::cout << std::endl;
 
-	StackAB<int, 3> stack;
-	stack.push(1);
-	stack.push(2);
-	stack.push(3);
+	std::cout << "In order:" << std::endl;
+	for(const auto & element: in_order)
+		std::cout << element << ", " ;
 
-	std::cout << stack.pop() << std::endl;
-	std::cout << stack.top() << std::endl;
-	std::cout << stack.pop() << std::endl;
-	std::cout << stack.pop() << std::endl;
-	//std::cout << stack.top() <<std::endl;
-
-	QueueAB<int, 3> queue;
-	queue.enqueue(1);
-	queue.enqueue(2);
-	queue.enqueue(3);
-	std::cout << "Queue \n";
-
-	std::cout << queue.dequeue() << std::endl;
-	std::cout << queue.dequeue() << std::endl;
-	std::cout << queue.dequeue() << std::endl;
-
-	auto heap = new BinaryHeap<int, 8>();
-	heap->insert(1);
-	heap->insert(2);
-	heap->insert(3);
-	heap->insert(4);
-	heap->insert(5);
-	heap->insert(6);
-	heap->insert(7);
-	heap->get_maximum();
-	heap->print_array();
-	heap->insert(8);
-	heap->get_maximum();
-	heap->print_array();
-	std::vector<int> v{1, 2, 3, 4, 5, 6, 7};
-	std::make_heap(v.begin(), v.end());
-	for (const auto &element: v) {
-		std::cout << " std::heap = " << element << "\n";
-	}
-
-	auto tree = new N_aryTree<int, 2>(1);
-	tree->root()->add_child(2);
-	tree->root()->add_child(3);
-	tree->root()->children.at(0)->add_child(4);
-	tree->root()->children.at(0)->add_child(5);
-//	std::function<void(int)> print_function = print<int>;
-	tree->traverse_preorder(print<int>);
-	std::cout << "***************" << std::endl;
-	tree->traverse_postorder(print<int>);
-	std::cout << "***************" << std::endl;
-	tree->traverse_inorder(print<int>);
-	std::cout << "TRIE" << std::endl;
-	auto trie = Trie<26>();
-	trie.insert("apple");
-	std::cout << trie.search("apple") << std::endl;
-	std::cout << trie.prefix("app")<< std::endl;;
+	auto tree = construct_from_preorder_and_inorder(pre_order, in_order);
+	int x =1;
 	return 0;
 }
 
---
+++ data_structures/source/trees/binary_trees/construct_binary_tree_from_preorder_and_inorder.h
@@ -0,0 +1,74 @@
+//
+// Created by andreas on 14.07.22.
+//
+
+#ifndef CONSTRUCT_BINARY_TREE_FROM_PREORDER_AND_INORDER_H
+#define CONSTRUCT_BINARY_TREE_FROM_PREORDER_AND_INORDER_H
+#include <vector>
+#include "tree_node.h"
+#include <unordered_map>
+/*
+Intuition
+
+The two key observations are:
+Preorder traversal follows Root -> Left -> Right, therefore, given the preorder array preorder, we have easy access
+to the root which is preorder[0].
+Inorder traversal follows Left -> Root -> Right, therefore if we know the position of Root, we can recursively split
+the entire array into two subtrees.
+Now the idea should be clear enough. We will design a recursion function: it will set the first element of preorder
+as the root, and then construct the entire tree. To find the left and right subtrees, it will look for the root
+in inorder, so that everything on the left should be the left subtree, and everything on the right should be the
+right subtree. Both subtrees can be constructed by making another recursion call.
+
+It is worth noting that, while we recursively construct the subtrees, we should choose the next element in preorder to
+initialize as the new roots. This is because the current one has already been initialized to a parent node for the
+subtrees.
+
+Algorithm
+
+- Build a hashmap to record the relation of value -> index for inorder, so that we can find the position of root in constant time.
+- Initialize an integer variable preorderIndex to keep track of the element that will be used to construct the root.
+- Implement the recursion function arrayToTree which takes a range of inorder and returns the constructed binary tree:
+	- if the range is empty, return null;
+	- initialize the root with preorder[preorderIndex] and then increment preorderIndex;
+	- recursively use the left and right portions of inorder to construct the left and right subtrees.
+- Simply call the recursion function with the entire range of inorder.
+*/
+
+template<typename T>
+TreeNode<T> *_construct_tree(std::vector<T> &preorder,
+							 int left,
+							 int right,
+							 int &pre_order_index,
+							 std::unordered_map<T, int> &inorder_value_to_index)
+{
+	if (left > right)
+		return nullptr;
+	int root_value = preorder[pre_order_index++];
+	auto root = new TreeNode<T>(root_value);
+	root->left = _construct_tree(preorder,
+								 left,
+								 inorder_value_to_index[root_value] - 1,
+								 pre_order_index,
+								 inorder_value_to_index);
+	root->right = _construct_tree(preorder,
+								  inorder_value_to_index[root_value] + 1,
+								  right,
+								  pre_order_index,
+								  inorder_value_to_index);
+	return root;
+}
+
+template<typename T>
+TreeNode<T> *construct_from_preorder_and_inorder(std::vector<T> &pre_order, std::vector<int> &in_order)
+{
+	std::unordered_map<T, int> inorder_value_to_index;
+	int pre_order_index{};
+	for (int i = 0; i < in_order.size(); ++i)
+		inorder_value_to_index[in_order[i]] = i;
+
+	return _construct_tree(pre_order, 0, pre_order.size() - 1, pre_order_index, inorder_value_to_index);
+
+}
+
+#endif //CONSTRUCT_BINARY_TREE_FROM_PREORDER_AND_INORDER_H
