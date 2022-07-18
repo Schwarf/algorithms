643f29c5401ef6c0dc59cbf679c3ef44b591da88
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-07-18 18:49:43 +0200

    Add count ways in 2D grid.

---
+++ dynamic_programming/count_ways_2D_grid.h
@@ -0,0 +1,43 @@
+//
+// Created by andreas on 18.07.22.
+//
+
+#ifndef COUNT_WAYS_2D_GRID_H
+#define COUNT_WAYS_2D_GRID_H
+#include <unordered_map>
+
+// 2D grid. You start from upper left corner and have to reach lower right corner. How many ways are there to reach
+// the lower right corner.
+
+struct pair_hash
+{
+	template<class T1, class T2>
+	std::size_t operator()(const std::pair<T1, T2> &pair) const
+	{
+		return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+	}
+};
+
+int count_ways_down_and_right_in_2D_grid(int number_of_rows,
+										 int number_of_columns,
+										 std::unordered_map<std::pair<int, int>, int, pair_hash> &help)
+{
+	if (number_of_columns == 0 || number_of_rows == 0)
+		return 0;
+	auto pair = std::make_pair(number_of_rows, number_of_columns);
+
+	if (help.find(pair) != help.end())
+		return help[pair];
+	help[pair] = count_ways_down_and_right_in_2D_grid(number_of_rows - 1, number_of_columns, help)
+		+ count_ways_down_and_right_in_2D_grid(number_of_rows, number_of_columns - 1, help);
+
+	return help[pair];
+}
+
+int count_ways_down_and_right_in_2D_grid(int number_of_rows, int number_of_columns)
+{
+	std::unordered_map<std::pair<int, int>, int, pair_hash> row_column_number_of_ways;
+	return count_ways_down_and_right_in_2D_grid(number_of_rows, number_of_columns, row_column_number_of_ways);
+}
+
+#endif //COUNT_WAYS_2D_GRID_H
