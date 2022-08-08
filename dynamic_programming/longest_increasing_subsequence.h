0d786dedc472c194879709e35fd40650300bedcd
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-08 17:52:08 +0200

    Add longest increasing subsequence.

--- dynamic_programming/longest_increasing_subsequence.h
+++ dynamic_programming/longest_increasing_subsequence.h
@@ -4,6 +4,24 @@
 
 #ifndef LONGEST_INCREASING_SUBSEQUENCE_H
 #define LONGEST_INCREASING_SUBSEQUENCE_H
-
+#include <vector>
+#include <algorithm>
+// O(N^2)
+int size_of_longest_increasing_subsequence(const std::vector<int> &sequence)
+{
+	const int size = sequence.size();
+	std::vector<int> help(size, 1);
+	for(int i{1}; i < size; i++)
+	{
+		for(int j{0}; j < i; j++)
+		{
+			if(sequence[i] > sequence[j])
+			{
+				help[i] = std::max(help[i], help[j]+1);
+			}
+		}
+	}
+	return *std::max_element(help.begin(), help.end());
+}
 
 #endif //LONGEST_INCREASING_SUBSEQUENCE_H
