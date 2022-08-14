b8196d84ed84e38d15d0fc8eff0f2305751e0938
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-14 17:50:23 +0200

    Add unit test.

--- data_structures/source/test/trees/binary_trees/test_flatten_binary_search_tree_to_array.cpp
+++ data_structures/source/test/trees/binary_trees/test_flatten_binary_search_tree_to_array.cpp
@@ -16,3 +16,12 @@ TEST(flatten_binary_search_tree_to_array, first_attempt)
 	EXPECT_EQ(prep, result);
 }
 
+
+TEST(flatten_binary_search_tree_to_array, second_attempt)
+{
+	std::vector<int> prep{-1, 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 189, 2901, 1920910, 290192012};
+	auto input = binary_search_tree_from_sorted_array<int>(prep);
+
+	auto result = flatten_binary_search_tree<int>(input);
+	EXPECT_EQ(prep, result);
+}
