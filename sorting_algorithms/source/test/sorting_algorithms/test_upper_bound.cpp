f27a14d0f7b29e76527ee5b0b6043babf55f8bf4
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-08-16 19:40:16 +0200

    Add unit test for upper bound implementation.

---
+++ sorting_algorithms/source/test/sorting_algorithms/test_upper_bound.cpp
@@ -0,0 +1,30 @@
+//
+// Created by andreas on 16.08.22.
+//
+#include "gtest/gtest.h"
+#include <utility>
+#include "./../../algorithms/upper_lower_bounds/upper_bound.h"
+
+class SetupUpperbound: public testing::Test
+{
+protected:
+	std::vector<int64_t> sorted{-100, -18, -9, 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10};
+};
+
+
+TEST_F(SetupUpperbound, test_upper_bound1)
+{
+	auto value{7};
+	auto bound = my_upper_bound(sorted.begin(), sorted.end(), value);
+	auto expected_bound = std::upper_bound(sorted.begin(), sorted.end(), value);
+	EXPECT_EQ(*bound, *expected_bound);
+
+}
+TEST_F(SetupUpperbound, test_upper_bound2)
+{
+	auto value{-18};
+	auto bound = my_upper_bound(sorted.begin(), sorted.end(), value);
+	auto expected_bound = std::upper_bound(sorted.begin(), sorted.end(), value);
+	EXPECT_EQ(*bound, *expected_bound);
+
+}
