9d08dfb410afa792e4fba22835e7c84130d9ba6a
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-20 09:30:56 +0200

    Find same subarray in two arrays.

--- dynamic_programming/main.cpp
+++ dynamic_programming/main.cpp
@@ -2,9 +2,11 @@
 // Created by andreas on 18.07.22.
 //
 #include <iostream>
-
+#include "maximum_length_of_same_subarray_in_two_arrays.h"
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::vector<int> test1{1,2,2,3,4};
+	std::vector<int> test2{2,3,4,3,2,5};
+	std::cout << maximum_length(test1, test2) << std::endl;
 	return 0;
 }
