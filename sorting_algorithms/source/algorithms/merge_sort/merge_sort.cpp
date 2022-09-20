98d32bef9c14fb23b5d704387f8635be1d8d8d40
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-20 16:17:18 +0200

    Minor refactoring of merge-sort.

--- sorting_algorithms/source/algorithms/merge_sort/merge_sort.cpp
+++ sorting_algorithms/source/algorithms/merge_sort/merge_sort.cpp
@@ -6,32 +6,32 @@
 void MergeSort::sort(std::vector<int64_t> &array)
 {
 
-	std::vector<int64_t> help(array.size(), 0);
+	help_ = std::vector<int64_t>(array.size(), 0);
 	size_t start = 0;
 	size_t end = array.size() - 1;
 	if (non_recursive_version_is_enabled_)
-		sort_non_recursive_(array, help);
+		sort_non_recursive_(array);
 	else
-		sort_recursive_(array, help, start, end);
+		sort_recursive_(array, start, end);
 }
 
-void MergeSort::sort_recursive_(std::vector<int64_t> &array, std::vector<int64_t> &help, size_t start, size_t end)
+void MergeSort::sort_recursive_(std::vector<int64_t> &array, size_t start, size_t end)
 {
 	if (end <= start) {
 		return;
 	}
 	size_t middle = start + (end - start) / 2;
-	sort_recursive_(array, help, start, middle);
-	sort_recursive_(array, help, middle + 1, end);
-	merge_(array, help, start, middle, end);
+	sort_recursive_(array, start, middle);
+	sort_recursive_(array, middle + 1, end);
+	merge_(array, start, middle, end);
 }
 
-void MergeSort::sort_non_recursive_(std::vector<int64_t> &array, std::vector<int64_t> &help)
+void MergeSort::sort_non_recursive_(std::vector<int64_t> &array)
 {
 	size_t size = array.size();
 	for (size_t index = 1; index < size; index *= 2)
 		for (size_t index2 = 0; index2 < size - index; index2 += 2 * index)
-			merge_(array, help, index2, index2 + index - 1, std::min(index2 + index + index - 1, size - 1));
+			merge_(array, index2, index2 + index - 1, std::min(index2 + index + index - 1, size - 1));
 }
 
 void MergeSort::enable_non_recursive_version(bool enable = true)
@@ -39,19 +39,21 @@ void MergeSort::enable_non_recursive_version(bool enable = true)
 	non_recursive_version_is_enabled_ = enable;
 }
 
-void MergeSort::merge_(std::vector<int64_t> &array, std::vector<int64_t> &help, size_t start, size_t middle, size_t end)
+void MergeSort::merge_(std::vector<int64_t> &array, size_t start, size_t middle, size_t end)
 {
-	for (size_t index = start; index <= end; ++index) {
-		help[index] = array[index];
-	}
+	help_ = array;
 
 	size_t first_half_index = start;
 	size_t second_half_index = middle + 1;
 	for (size_t index = start; index <= end; ++index) {
-		if (first_half_index > middle) { array[index] = help[second_half_index++]; }
-		else if (second_half_index > end) { array[index] = help[first_half_index++]; }
-		else if (help[second_half_index] < help[first_half_index]) { array[index] = help[second_half_index++]; }
-		else { array[index] = help[first_half_index++]; }
+		if (first_half_index > middle)
+			array[index] = help_[second_half_index++];
+		else if (second_half_index > end)
+			array[index] = help_[first_half_index++];
+		else if (help_[second_half_index] < help_[first_half_index])
+			array[index] = help_[second_half_index++];
+		else
+			array[index] = help_[first_half_index++];
 	}
 
 }
--- sorting_algorithms/source/algorithms/merge_sort/merge_sort.h
+++ sorting_algorithms/source/algorithms/merge_sort/merge_sort.h
@@ -18,12 +18,12 @@ public:
 private:
 	bool non_recursive_version_is_enabled_{};
 	void static merge_(std::vector<int64_t> &array,
-					   std::vector<int64_t> &help,
 					   size_t start,
 					   size_t middle,
 					   size_t end);
-	void sort_recursive_(std::vector<int64_t> &array, std::vector<int64_t> &help, size_t start, size_t end);
-	void static sort_non_recursive_(std::vector<int64_t> &array, std::vector<int64_t> &help);
+	void sort_recursive_(std::vector<int64_t> &array, size_t start, size_t end);
+	void static sort_non_recursive_(std::vector<int64_t> &array);
+	std::vector<int64_t> help_;
 };
 
 
