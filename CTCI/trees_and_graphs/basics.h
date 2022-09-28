02261d72f949ce201a6c233ac2debb9fcb1d1778
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-09-28 19:59:54 +0200

    Undirected graph.

--- CTCI/trees_and_graphs/basics.h
+++ CTCI/trees_and_graphs/basics.h
@@ -12,12 +12,12 @@
 #include <fstream>
 
 
-template <typename T, typename std::enable_if<std::is_integral<T>::value, bool>::type = true>
-static inline T get_random(const T & lower_bound, const T & upper_bound)
+template<typename T, typename std::enable_if<std::is_integral<T>::value, bool>::type = true>
+static inline T get_random(const T &lower_bound, const T &upper_bound)
 {
 	auto int_distribution_ = std::uniform_int_distribution<T>(lower_bound, upper_bound);
 	std::random_device device;
-	auto generator = std::mt19937 (device());
+	auto generator = std::mt19937(device());
 	return int_distribution_(generator);
 }
 
@@ -33,46 +33,44 @@ struct Node
 };
 
 template<typename T>
-struct DirectedGraph
+struct Graph
 {
-	explicit DirectedGraph(std::string name, int number_of_vertices) :
-	name(std::move(name))
+	explicit Graph(std::string name, int number_of_vertices)
+		:
+		name(std::move(name))
 	{
-		for(int vertex{}; vertex < number_of_vertices; ++vertex)
+		for (int vertex{}; vertex < number_of_vertices; ++vertex)
 			nodes.push_back(new Node<int>(vertex));
 	}
 	std::vector<Node<T> *> nodes;
 	std::string name{};
 };
 
-template <typename T>
-void print_graph_in_DOT_language(DirectedGraph<T> & graph)
+template<typename T>
+void print_graph_in_DOT_language(Graph<T> &graph)
 {
-	auto file_name = graph.name + ".txt";
+	auto file_name = "/media/linux_data/projects/cpp/algorithms/CTCI/trees_and_graphs/" + graph.name + ".txt";
 	std::ofstream output_file(file_name);
 
-	if(output_file.is_open())
-	{
-		output_file << "digraph G {" << std::endl;
-		for(const auto & node : graph.nodes)
-		{
-			for(const auto & child: node->children)
-				output_file << node->value << " -> " << child->value << " ;" << std::endl;
+	if (output_file.is_open()) {
+		output_file << "graph G {" << std::endl;
+		for (const auto &node: graph.nodes) {
+			for (const auto &child: node->children)
+				output_file << node->value << " -- " << child->value << " ;" << std::endl;
 		}
 		output_file << "}" << std::endl;
 		output_file.close();
 	}
 }
 
-
-template <typename T >
-void create_edges_in_graph(DirectedGraph<T> & graph, int probability_for_edge_in_percent)
+template<typename T>
+void create_edges_in_graph(Graph<T> &graph, int probability_for_edge_in_percent)
 {
 	int probability{};
-	for(auto & node : graph.nodes) // here we call node and vertex the same
+	for (auto &node: graph.nodes) // here we call node and vertex the same
 	{
-		for(int vertex{}; vertex < graph.nodes.size(); ++vertex) {
-			if(vertex == node->value)
+		for (int vertex{}; vertex < graph.nodes.size(); ++vertex) {
+			if (vertex == node->value)
 				continue;
 			probability = get_random(0, 100);
 			if (probability <= probability_for_edge_in_percent) {
--- CTCI/trees_and_graphs/generate_random_graph.cpp
+++ CTCI/trees_and_graphs/generate_random_graph.cpp
@@ -7,8 +7,8 @@
 int main()
 {
 	constexpr int number_of_vertices = 10;
-	constexpr int probability_to_create_edge = 15; // percent
-	auto graph = DirectedGraph<int>("first_graph",number_of_vertices);
+	constexpr int probability_to_create_edge = 5; // percent
+	auto graph = Graph<int>("first_graph", number_of_vertices);
 	create_edges_in_graph(graph, probability_to_create_edge);
 	print_graph_in_DOT_language<int>(graph);
 	return 0;
