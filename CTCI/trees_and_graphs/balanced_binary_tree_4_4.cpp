fca3266551184e3aa46c2049b9ca3deb6b701351
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-10-08 16:25:07 +0200

    Start with algorithm.

--- CTCI/trees_and_graphs/balanced_binary_tree_4_4.cpp
+++ CTCI/trees_and_graphs/balanced_binary_tree_4_4.cpp
@@ -1,3 +1,35 @@
 //
 // Created by andreas on 04.10.22.
 //
+#include "basics.h"
+
+template <typename T>
+int depth_first_search_height(Node<T>* node)
+{
+		if(node==nullptr)
+			return 0;
+		auto left_height = depth_first_search_height(node->children[0]);
+		if(left_height ==-1)
+			return -1;
+		auto right_height = depth_first_search_height(node->children[1]);
+		if(right_height ==-1)
+			return -1;
+		if(std::abs(left_height - right_height) > 1)
+			return -1;
+		return 1 + std::max(left_height, right_height);
+};
+template<typename T>
+bool is_tree_balanced(Node<T>* root)
+{
+	if(!root)
+		return true;
+	return depth_first_search_height(root) != -1;
+}
+
+
+int main()
+{
+	auto root = generate_balanced_binary_tree_with_random_values<int>(0,10);
+	std::cout << is_tree_balanced(root) << std::endl;
+	return 0;
+}
