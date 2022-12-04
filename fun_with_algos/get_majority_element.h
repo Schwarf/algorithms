5c3f478deb2fb7d1d92fca2297ef3f9113a2ec6d
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-12-04 17:30:03 +0100

    Majority element version 1.

--- fun_with_algos/get_majority_element.h
+++ fun_with_algos/get_majority_element.h
@@ -2,8 +2,22 @@
 // Created by andreas on 04.12.22.
 //
 
+#include <unordered_map>
+#include <vector>
+
 #ifndef GET_MAJORITY_ELEMENT_H
 #define GET_MAJORITY_ELEMENT_H
+// The majority element in an array of length n appears at least n/2 times in the array. Find it
+int get_majority_element_hashmap(const std::vector<int>& input)
+{
+	std::unordered_map<int, int> counter;
+	for(const auto & element: input)
+	{
+		if(++counter[element] > input.size()/2)
+			return element;
+	}
+	return {};
+}
 
 
 #endif //GET_MAJORITY_ELEMENT_H
