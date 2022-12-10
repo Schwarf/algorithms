5ed9f6cb211910fbb10792251df17166cf18eeec
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-12-10 13:21:03 +0100

    Add first test brute force solution.

---
+++ data_structures/source/test/trees/binary_trees/test_maximum_difference_between_node_and_ancestor.cpp
@@ -0,0 +1,41 @@
+//
+// Created by andreas on 10.12.22.
+//
+#include "gtest/gtest.h"
+#include "./../../../trees/binary_trees/tree_node.h"
+#include "./../../../trees/binary_trees/maximum_difference_betwee_node_and_ancestor.h"
+class SetupTestTrees: public testing::Test
+{
+public:
+	SetupTestTrees() = default;
+protected:
+	static TreeNode<int> *first_tree_root()
+	{
+		auto root = new TreeNode<int>(8);
+		root->left = new TreeNode<int>(3);
+		root->right = new TreeNode<int>(10);
+		root->left->left = new TreeNode<int>(1);
+		root->left->right = new TreeNode<int>(6);
+		root->left->right->left = new TreeNode<int>(4);
+		root->left->right->right = new TreeNode<int>(7);
+		root->right->right = new TreeNode<int>(14);
+		root->right->right->left = new TreeNode<int>(13);
+		return root;
+	}
+	const int result1{7};
+};
+
+
+TEST_F(SetupTestTrees, first_tree_test_brute_force)
+{
+	auto root = SetupTestTrees::first_tree_root();
+	auto result = brute_force_solution(root);
+	EXPECT_EQ(result, result1);
+}
+
+TEST_F(SetupTestTrees, first_tree_one_recursion)
+{
+	auto root = SetupTestTrees::first_tree_root();
+	auto result = one_recursion_solution(root);
+	EXPECT_EQ(result, result1);
+}
