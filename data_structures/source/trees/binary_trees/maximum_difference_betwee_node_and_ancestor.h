557c1992b45f4b16a0fd55a2eecc6f176bd2f06a
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-12-10 12:26:54 +0100

    Add single recursion solution.

--- data_structures/source/trees/binary_trees/maximum_difference_betwee_node_and_ancestor.h
+++ data_structures/source/trees/binary_trees/maximum_difference_betwee_node_and_ancestor.h
@@ -7,6 +7,10 @@
 // Given the root of a binary tree, find the maximum valueue v for which there exist different nodes a and b
 // where v = |a.value - b.value| and a is an ancestor of b.
 // A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
+// The number of nodes in the tree is in the range [2, 5000].
+// 0 <= Node.val <= 10^5
+
+
 #include <algorithm>
 #include "tree_node.h"
 
@@ -34,11 +38,34 @@ void get_max_diff_for_node(TreeNode<int> *node, int &max)
 }
 
 // O(N^2)
-int brute_force(TreeNode<int> *root)
+int brute_force_solution(TreeNode<int> *root)
 {
 	int max{};
 	get_max_diff_for_node(root, max);
 	return max;
+}
+
+void helper(TreeNode<int> *node, int &current_max, int &current_min, int &result)
+{
+	if (!node)
+		return;
+	int local_result = std::max(std::abs(current_max - node->value), std::abs(current_min - node->value));
+	result = std::max(local_result, result);
+	current_max = std::max(current_max, node->value);
+	current_min = std::min(current_min, node->value);
+	helper(node->left, current_max, current_min, result);
+	helper(node->right, current_max, current_min, result);
+	return;
+}
+
+int one_recursion_solution(TreeNode<int> *root)
+{
+	if (!root)
+		return 0;
+	int result{};
+	helper(root, root->value, root->value, result);
+	return result;
+
 }
 
 #endif //MAXIMUM_DIFFERENCE_BETWEE_NODE_AND_ANCESTOR_H
