d5200ef5a7833ac6efef119e125d59a1e82f716d
Andreas Scharf <andreas.b.scharf@gmail.com>
2022-12-22 17:23:31 +0100

    Some work on LRU cache.

--- data_structures/source/stacks_and_queues/lru_cache.h
+++ data_structures/source/stacks_and_queues/lru_cache.h
@@ -18,12 +18,11 @@ public:
 	{
 	}
 
-	void put(const key_T & key, const value_T & value)
+	void put(const key_T &key, const value_T &value)
 	{
 		auto pair_iterator = cache_.find(key);
 		// cache hit, erase item in cache and list
-		if(pair_iterator != cache_.end())
-		{
+		if (pair_iterator != cache_.end()) {
 			key_value_list_.erase(pair_iterator->second);
 			cache_.erase(pair_iterator);
 		}
@@ -36,18 +35,31 @@ public:
 	value_T get(key_T key)
 	{
 		auto pair_iterator = cache_.find(key);
-		if(pair_iterator == cache_.end())
+		if (pair_iterator == cache_.end())
 			throw std::out_of_range("Element not in cache!");
 		key_value_list_.splice(key_value_list_.begin(), key_value_list_, pair_iterator->second);
 		return pair_iterator->second->second;
 	}
 
-	std::vector<value_T> get_all_values()
+	value_T get_most_recent_value() const
+	{
+		return key_value_list_.front().second;
+	}
+
+	std::vector<value_T> get_all_values() const
 	{
 		auto result = std::vector<value_T>();
-		for(const auto & element : key_value_list_)
-		{
+		for (const auto &element: key_value_list_) {
 			result.push_back(element.second);
+		}
+		return result;
+	}
+
+	std::vector<key_T> get_all_keys() const
+	{
+		auto result = std::vector<key_T>();
+		for (const auto &element: key_value_list_) {
+			result.push_back(element.first);
 		}
 		return result;
 	}
@@ -56,8 +68,7 @@ public:
 private:
 	void check_cache_size()
 	{
-		while(cache_.size() > cache_size_)
-		{
+		while (cache_.size() > cache_size_) {
 			auto last_key = key_value_list_.back().first;
 			key_value_list_.pop_back();
 			cache_.erase(last_key);
--- data_structures/source/test/stacks_and_queues/test_lru_cache.cpp
+++ data_structures/source/test/stacks_and_queues/test_lru_cache.cpp
@@ -8,8 +8,11 @@ TEST(test_lru_cache, test_lru_cache)
 {
 	auto cache = LRUCache<int, int>(3);
 	cache.put(1, 1);
+	EXPECT_EQ(cache.get_most_recent_value(), 1);
 	cache.put(2, 2);
+	EXPECT_EQ(cache.get_most_recent_value(), 2);
 	cache.put(3, 3);
+	EXPECT_EQ(cache.get_most_recent_value(), 3);
 	EXPECT_EQ(cache.get(1), 1);
 	EXPECT_THROW(cache.get(4), std::exception);
 }
@@ -18,9 +21,13 @@ TEST(test_lru_cache, test_lru_cache_values)
 {
 	auto cache = LRUCache<int, int>(3);
 	cache.put(1, 1);
+	EXPECT_EQ(cache.get_most_recent_value(), 1);
 	cache.put(2, 2);
+	EXPECT_EQ(cache.get_most_recent_value(), 2);
 	cache.put(3, 3);
+	EXPECT_EQ(cache.get_most_recent_value(), 3);;
 	cache.put(4, 4);
+	EXPECT_EQ(cache.get_most_recent_value(), 4);
 	auto expected_content = std::vector<int>{4, 3, 2};
 	auto content = cache.get_all_values();
 	for (int i = 0; i < content.size(); ++i) {
@@ -29,6 +36,32 @@ TEST(test_lru_cache, test_lru_cache_values)
 	expected_content = std::vector<int>{2, 4, 3};
 	cache.get(2);
 	content = cache.get_all_values();
+	for (int i = 0; i < content.size(); ++i) {
+		EXPECT_EQ(content[i], expected_content[i]);
+	}
+
+}
+
+
+TEST(test_lru_cache, test_lru_cache_keys)
+{
+	auto cache = LRUCache<int, int>(3);
+	cache.put(1, 1);
+	EXPECT_EQ(cache.get_most_recent_value(), 1);
+	cache.put(2, 2);
+	EXPECT_EQ(cache.get_most_recent_value(), 2);
+	cache.put(3, 3);
+	EXPECT_EQ(cache.get_most_recent_value(), 3);;
+	cache.put(4, 4);
+	EXPECT_EQ(cache.get_most_recent_value(), 4);
+	auto expected_content = std::vector<int>{4, 3, 2};
+	auto content = cache.get_all_keys();
+	for (int i = 0; i < content.size(); ++i) {
+		EXPECT_EQ(content[i], expected_content[i]);
+	}
+	expected_content = std::vector<int>{2, 4, 3};
+	cache.get(2);
+	content = cache.get_all_keys();
 	for (int i = 0; i < content.size(); ++i) {
 		EXPECT_EQ(content[i], expected_content[i]);
 	}
