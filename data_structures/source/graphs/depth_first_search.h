5115783b7a2e62a949650022add03140bf2cea63
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-01-04 12:44:09 +0100

    Fix template-argument bugs in DFS.

--- data_structures/source/graphs/depth_first_search.h
+++ data_structures/source/graphs/depth_first_search.h
@@ -9,19 +9,18 @@
 #include <vector>
 #include "graph.h"
 
-template <typename id_T, typename data_T>
-std::map<id_T, id_T> depth_first_search(BidirectionalGraph<id_T, data_T> & graph, const GraphNodePtr<id_t, data_T> & start_node)
+template<typename id_T, typename data_T>
+std::map<id_T, id_T> depth_first_search(BidirectionalGraph<id_T, data_T> &graph,
+										const GraphNodePtr<id_T, data_T> &start_node)
 {
 	std::stack<id_T> dfs_stack;
 	dfs_stack.push(start_node->id);
-	std::map<id_t, id_T> parents_map;
-	while(!dfs_stack.empty())
-	{
+	std::map<id_T, id_T> parents_map;
+	while (!dfs_stack.empty()) {
 		auto current_node_id = dfs_stack.top();
 		dfs_stack.pop();
 		graph.get_node_by_id(current_node_id)->discovered = true;
-		for(const auto & neighbor_id : graph.get_neighbors(current_node_id))
-		{
+		for (const auto &neighbor_id: graph.get_neighbors(current_node_id)) {
 			if (graph.get_node_by_id(neighbor_id)->discovered)
 				continue;
 			graph.get_node_by_id(neighbor_id)->discovered = true;
--- data_structures/source/test/graphs/test_depth_first_search.cpp
+++ data_structures/source/test/graphs/test_depth_first_search.cpp
@@ -3,11 +3,12 @@
 //
 #include "gtest/gtest.h"
 #include "./../../graphs/graph.h"
-#include "./../../graphs/breadth_first_search.h"
-class SetupBFSGraph: public testing::Test
+#include "./../../graphs/depth_first_search.h"
+
+class SetupDFSGraph: public testing::Test
 {
 public:
-	SetupBFSGraph() = default;
+	SetupDFSGraph() = default;
 protected:
 	int id1 = 1, data1 = 1;
 	int id2 = 2, data2 = 2;
@@ -16,7 +17,7 @@ protected:
 	int id5 = 5, data5 = 5;
 	int id6 = 6, data6 = 6;
 	int id7 = 7, data7 = 7;
-	BidirectionalGraph<int, int> get_graph(int start_node_id, GraphNodePtr<int,int> & start_node ) const
+	BidirectionalGraph<int, int> get_graph(int start_node_id, GraphNodePtr<int, int> &start_node) const
 	{
 		const std::shared_ptr<GraphNode<int, int>> node1(new GraphNode<int, int>(id1, data1));
 		const std::shared_ptr<GraphNode<int, int>> node2(new GraphNode<int, int>(id2, data2));
@@ -39,36 +40,13 @@ protected:
 	}
 };
 
-
-TEST_F(SetupBFSGraph, test_breadth_first_search_start_node1)
+TEST_F(SetupDFSGraph, test_depth_first_search_start_node1)
 {
 	GraphNodePtr<int, int> start_node = nullptr;
 	int start_node_id = 1;
 	auto graph = get_graph(start_node_id, start_node);
-	std::map<int, int> expected_parents {{2,1}, {3, 1}, {4, 3}, {5, 3}, {6, 2}, {7, 5}};
-	auto parents = breadth_first_search<int, int>(graph, start_node);
-	for(const auto & [child, parent] : expected_parents)
-		EXPECT_EQ(parent, parents[child]);
-}
-
-TEST_F(SetupBFSGraph, test_breadth_first_search_start_node2)
-{
-	GraphNodePtr<int, int> start_node = nullptr;
-	int start_node_id = 2;
-	auto graph = get_graph(start_node_id, start_node);
-	std::map<int, int> expected_parents {{1,2}, {3, 2}, {6, 2}, {4, 3}, {5, 3}, {7, 5}};
-	auto parents = breadth_first_search<int, int>(graph, start_node);
-	for(const auto & [child, parent] : expected_parents)
-		EXPECT_EQ(parent, parents[child]);
-}
-
-TEST_F(SetupBFSGraph, test_breadth_first_search_start_node7)
-{
-	GraphNodePtr<int, int> start_node = nullptr;
-	int start_node_id = 7;
-	auto graph = get_graph(start_node_id, start_node);
-	std::map<int, int> expected_parents {{5,7}, {3, 5}, {6, 5}, {4, 3}, {2, 3}, {1, 3}};
-	auto parents = breadth_first_search<int, int>(graph, start_node);
-	for(const auto & [child, parent] : expected_parents)
+	std::map<int, int> expected_parents{{2, 1}, {3, 2}, {4, 3}, {6, 2}, {5, 6}, {7, 5}};
+	auto parents = depth_first_search<int, int>(graph, start_node);
+	for (const auto &[child, parent]: expected_parents)
 		EXPECT_EQ(parent, parents[child]);
 }
