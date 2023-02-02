e2839d0a5a88c3b46b2819d061791f9ac9a5306c
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-02 17:52:19 +0100

    Split unit test/

--- data_structures/source/test/union_find_disjoint_set/test_quick_find.cpp
+++ data_structures/source/test/union_find_disjoint_set/test_quick_find.cpp
@@ -16,26 +16,32 @@ protected:
 
 };
 
-TEST_F(SetupDisjointSets, two_sets)
+TEST_F(SetupDisjointSets, single_set)
 {
 	auto quick_find = QuickFind(10);
+	for (int j{}; j < set1.size() - 1; ++j)
+		for (int i{j + 1}; i < set1.size(); ++i)
+			EXPECT_FALSE(quick_find.are_connected(set1[j], set1[i]));
+
 	for (int i{1}; i < set1.size(); ++i)
 		quick_find.union_set(set1[0], set1[i]);
 	for (int j{}; j < set1.size() - 1; ++j)
 		for (int i{j + 1}; i < set1.size(); ++i)
 			EXPECT_TRUE(quick_find.are_connected(set1[j], set1[i]));
 
+}
 
+TEST_F(SetupDisjointSets, two_sets)
+{
+	auto quick_find = QuickFind(10);
+	for (int i{1}; i < set1.size(); ++i)
+		quick_find.union_set(set1[0], set1[i]);
 	for (int i{1}; i < set2.size(); ++i)
 		quick_find.union_set(set2[0], set2[i]);
-	for (int j{}; j < set2.size() - 1; ++j)
-		for (int i{j + 1}; i < set2.size(); ++i)
-			EXPECT_TRUE(quick_find.are_connected(set2[j], set2[i]));
 
 	for (int i{}, j{}; i < set1.size(), j < set2.size(); ++i, ++j) {
 		EXPECT_FALSE(quick_find.are_connected(set1[i], set2[j]));
 	}
-
 	quick_find.union_set(set1[2], set2[3]);
 	for (int i{}, j{}; i < set1.size(), j < set2.size(); ++i, ++j) {
 		EXPECT_TRUE(quick_find.are_connected(set1[i], set2[j]));
