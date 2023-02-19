590db17d989da322f7ee5a459c50bd3520350a04
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-19 10:33:28 +0100

    Add tests for zigzag tree traversal.

--- data_structures/source/test/trees/binary_trees/test_level_order_traversal.cpp
+++ data_structures/source/test/trees/binary_trees/test_level_order_traversal.cpp
@@ -86,3 +86,62 @@ TEST_F(SetupBinaryTree, testconcise_level_order_traversal_empty)
 	EXPECT_TRUE(result.empty());
 
 }
+
+
+TEST_F(SetupBinaryTree, zigzag_level_order_traversal)
+{
+	auto root = SetupBinaryTree::binary_tree_root();
+	auto result = zigzag_level_order_traversal(root);
+	// We use the result for standard level traversal and read out out the sub-vectors depending if we
+	// read from left to right or vice versa
+	bool from_left_to_right{true};
+	for (int i = 0; i < result.size(); ++i) {
+		int size = result[i].size();
+		for (int j = 0; j < size; ++j) {
+			const int index = from_left_to_right ? j : size - 1 -j;
+			EXPECT_EQ(result[i][j], levelorder[i][index]);
+		}
+		from_left_to_right = !from_left_to_right;
+
+	}
+
+}
+TEST_F(SetupBinaryTree, zigzag_level_order_traversal_inverted)
+{
+	auto root = SetupBinaryTree::inverted_tree();
+	auto result = zigzag_level_order_traversal(root);
+	bool from_left_to_right{true};
+	for (int i = 0; i < result.size(); ++i) {
+		int size = result[i].size();
+		for (int j = 0; j < size; ++j) {
+			const int index = from_left_to_right ? j : size - 1 -j;
+			EXPECT_EQ(result[i][j], levelorder_inverted_tree[i][index]);
+		}
+		from_left_to_right = !from_left_to_right;
+
+	}
+
+}
+
+TEST_F(SetupBinaryTree, zigzag_level_order_traversal_small_tree)
+{
+	auto root = SetupBinaryTree::small_binary_tree_root();
+	auto result = zigzag_level_order_traversal(root);
+	bool from_left_to_right{true};
+	for (int i = 0; i < result.size(); ++i) {
+		int size = result[i].size();
+		for (int j = 0; j < size; ++j) {
+			const int index = from_left_to_right ? j : size - 1 - j;
+			EXPECT_EQ(result[i][j], levelorder_small_tree[i][index]);
+		}
+		from_left_to_right = !from_left_to_right;
+	}
+}
+
+TEST_F(SetupBinaryTree, zigzag_level_order_traversal_empty)
+{
+	auto root = nullptr;
+	auto result = zigzag_level_order_traversal<int>(root);
+	EXPECT_TRUE(result.empty());
+
+}
