d45a3664435d0ee92eaf4c34af0fb10d24d835f0
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-22 09:26:15 +0100

    Finish implementation.

--- binary_search/minimum_car_size_to_ship_packages.h
+++ binary_search/minimum_car_size_to_ship_packages.h
@@ -8,17 +8,16 @@
 // The ith package has a weight weights[i]
 // Return the least weight capacity of the car that will result in all the packages shipped within d days.
 #include <vector>
+#include <algorithm>
 
-template <typename T>
-bool is_valid_car_size(const std::vector<T> & weights, T max_load, int max_days)
+template<typename T>
+bool is_valid_car_size(const std::vector<T> &weights, T max_load, int max_days)
 {
 	int days{1};
 	T current_load{};
-	for(const auto & weight: weights)
-	{
+	for (const auto &weight: weights) {
 		current_load += weight;
-		if(current_load > max_load)
-		{
+		if (current_load > max_load) {
 			days++;
 			current_load = weight;
 		}
@@ -26,7 +25,26 @@ bool is_valid_car_size(const std::vector<T> & weights, T max_load, int max_days)
 	return days <= max_days;
 }
 
+template<typename T>
+T get_car_size_to_ship_in_days(const std::vector<T> &weights, int days)
+{
+	T max_weight{}, total_weight{};
+	for (const auto &weight: weights) {
+		max_weight = std::max(max_weight, weight);
+		total_weight += weight;
+	}
 
+	T left{max_weight}, right{total_weight};
+	while (left < right) {
+		auto mid = (left + right) / T{2};
+		if (is_valid_car_size(weights, mid, days))
+			right = mid;
+		else
+			left += T{1};
+	}
+	return left;
+
+}
 
 
 #endif //MINIMUM_CAR_SIZE_TO_SHIP_PACKAGES_H
