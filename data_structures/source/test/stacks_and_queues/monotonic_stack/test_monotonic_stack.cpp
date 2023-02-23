84960d3e4d5b59b8705a7c6bc493269545bc73fd
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-23 17:59:45 +0100

    Add tests for decreasing monotonic stack.

--- data_structures/source/test/stacks_and_queues/monotonic_stack/test_monotonic_stack.cpp
+++ data_structures/source/test/stacks_and_queues/monotonic_stack/test_monotonic_stack.cpp
@@ -46,3 +46,46 @@ TEST(monotonic_stack, simple_increasing3)
 		increasing_stack.pop();
 	}
 }
+
+
+TEST(monotonic_stack, simple_decreasing1)
+{
+	std::vector<int> input{6, 5, 4, 3, 2, 1};
+	auto decreasing_stack = MonotonicStack<int, false>();
+	for (const auto &element: input)
+		decreasing_stack.push(element);
+
+	while (!decreasing_stack.empty()) {
+		EXPECT_EQ(decreasing_stack.top(), input.back());
+		input.pop_back();
+		decreasing_stack.pop();
+	}
+}
+
+TEST(monotonic_stack, simple_decreasing2)
+{
+	std::vector<int> input{1, 4, 3, 4, 5, 6};
+	auto decreasing_stack = MonotonicStack<int, false>();
+	for (const auto &element: input)
+		decreasing_stack.push(element);
+	std::vector<int> expected_result{6};
+	while (!decreasing_stack.empty()) {
+		EXPECT_EQ(decreasing_stack.top(), expected_result.back());
+		expected_result.pop_back();
+		decreasing_stack.pop();
+	}
+}
+
+TEST(monotonic_stack, simple_decreasing3)
+{
+	std::vector<int> input{5, 4, 3, 4, 3, 2};
+	auto decreasing_stack = MonotonicStack<int, false>();
+	for (const auto &element: input)
+		decreasing_stack.push(element);
+	std::vector<int> expected_result{5, 4, 4, 3, 2};
+	while (!decreasing_stack.empty()) {
+		EXPECT_EQ(decreasing_stack.top(), expected_result.back());
+		expected_result.pop_back();
+		decreasing_stack.pop();
+	}
+}
