1e670f7a009c3de1a3574385eda7a040517826d9
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-28 16:32:04 +0100

    Update docu on segment tree.

--- data_structures/source/trees/segment_trees/segment_tree.h
+++ data_structures/source/trees/segment_trees/segment_tree.h
@@ -5,72 +5,69 @@
 #ifndef SEGMENT_TREE_H
 #define SEGMENT_TREE_H
 #include <vector>
-// Used only for fixed size arrays/vectors. Adding of elements not POSSIBLE!!!
-
-
+// A Segment Tree, also known as a statistic tree,
+// is a tree data structure used for storing information about intervals, or segments.
+// It allows querying which of the stored segments contain a given point.
+// It is, in principle, a static structure; that is, it’s a structure that cannot be modified once it’s built.
 
 template<typename T>
-class SegmentTree{
+class SegmentTree
+{
 public:
-	SegmentTree(std::vector<T> & input)
+	explicit SegmentTree(std::vector<T> &input)
 	{
-		if(input.empty())
+		if (input.empty())
 			return;
-		tree_size_ = 2*input.size();
+		tree_size_ = 2 * input.size();
 		tree_ = std::vector<T>(tree_size_);
 		build_tree(input);
 	}
 	void update(int index, T val)
 	{
 		// input is stored in second half of tree
-		index += tree_size_/2;
+		index += tree_size_ / 2;
 		// update leaf
 		tree_[index] = val;
 		// update parents
-		while(index >0)
-		{
+		while (index > 0) {
 			int left = index;
 			int right = index;
 
-			if(index & 1)
+			if (index & 1)
 				left--;
 			else
 				right++;
-			tree_[index/2] = tree_[left] + tree_[right];
+			tree_[index / 2] = tree_[left] + tree_[right];
 			index /= 2;
 		}
 	}
 
 	T sum_range(int left_index, int right_index)
 	{
-		left_index += tree_size_/2;
-		right_index += tree_size_/2;
+		left_index += tree_size_ / 2;
+		right_index += tree_size_ / 2;
 		T sum{};
-		while(left_index <= right_index)
-		{
-			if(left_index & 1)
+		while (left_index <= right_index) {
+			if (left_index & 1)
 				sum += tree_[left_index++];
-			if(!(right_index & 1))
+			if (!(right_index & 1))
 				sum += tree_[right_index--];
-			left_index /=2;
-			right_index /=2;
+			left_index /= 2;
+			right_index /= 2;
 		}
 		return sum;
 	}
 
 
-
 private:
-	void build_tree(std::vector<T> & input)
+	void build_tree(const std::vector<T> &input)
 	{
 		// fill second half of tree with input
-		for(int tree_index = tree_size_/2, input_index =0; tree_index < tree_size_; tree_index++, input_index++)
-		{
+		for (int tree_index = tree_size_ / 2, input_index = 0; tree_index < tree_size_; tree_index++, input_index++) {
 			tree_[tree_index] = input[input_index];
 		}
-		for(int index = tree_size_/2 - 1 ; index > 0; --index)
-		{
-			tree_[index] = tree_[2*index] + tree_[2*index +1];
+		for (int index = tree_size_ / 2 - 1; index > 0; --index) {
+			tree_[index] = tree_[2 * index] + tree_[2 * index + 1];
 		}
 	}
 
