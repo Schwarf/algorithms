80fa4cda33c86c4b99cf8f2a007a1958529d01fe
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-01 19:30:02 +0100

    Add quicksort-tests.

---
+++ sorting_algorithms/test/test_quick_sort.cpp
@@ -0,0 +1,43 @@
+//
+// Created by andreas on 01.03.23.
+//
+//
+// Created by andreas on 01.03.23.
+//
+#include "test_setup.h"
+#include "./../quick_sort.h"
+#include <algorithm>
+
+TEST_F(SetupSortingTests, quick_sort_int_vector)
+{
+	auto result = get_N_random_number_vector(-1000, 1000, 1000);
+	auto expected_result = result;
+	EXPECT_EQ(result, expected_result);
+	std::sort(expected_result.begin(), expected_result.end());
+	EXPECT_NE(result, expected_result);
+	quick_sort(result);
+	EXPECT_EQ(result, expected_result);
+}
+
+TEST_F(SetupSortingTests, quick_sort_int_array)
+{
+	auto result = get_N_random_number_array<100>(-1000, 1000);
+	auto expected_result = result;
+	EXPECT_EQ(result, expected_result);
+	std::sort(expected_result.begin(), expected_result.end());
+	EXPECT_NE(result, expected_result);
+	quick_sort(result);
+	EXPECT_EQ(result, expected_result);
+}
+
+
+TEST_F(SetupSortingTests, quick_sort_string)
+{
+	auto result = generate_random_string(128);
+	auto expected_result = result;
+	EXPECT_EQ(result, expected_result);
+	std::sort(expected_result.begin(), expected_result.end());
+	EXPECT_NE(result, expected_result);
+	quick_sort(result);
+	EXPECT_EQ(result, expected_result);
+}
