baaaa69ef9dc7d5855a38e2e61c3eecb6db4d6ba
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-07 19:31:44 +0100

    Add one more complex test.

--- binary_search/test/test_kth_missing_element.cpp
+++ binary_search/test/test_kth_missing_element.cpp
@@ -19,3 +19,18 @@ TEST(kthMissingElement, simple_first_missing_outside_vector)
 	const int k{1};
 	EXPECT_EQ(find_kth_missing_positive_element(input, k), expected_result);
 }
+
+TEST(kthMissingElement, simple_1st_2nd_3rd_4th_5th_and_13th_missing)
+{
+	const std::vector<unsigned int> input {2,3,4,7,11};
+	std::map<int, int> k_result;
+	k_result[1] = 1;
+	k_result[2] = 5;
+	k_result[3] = 6;
+	k_result[4] = 8;
+	k_result[5] = 9;
+	k_result[13] = 18;
+	for(const auto [k, expected_result] : k_result)
+		EXPECT_EQ(find_kth_missing_positive_element(input, k), expected_result);
+}
+
