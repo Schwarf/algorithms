c4bf3888235dac72599dde73fdd84c19bcaf70b0
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-08 19:39:01 +0100

    Finish bus-times for n trips binary search.

--- binary_search/minimum_time_to_schedule_n_bus_trips.h
+++ binary_search/minimum_time_to_schedule_n_bus_trips.h
@@ -5,6 +5,8 @@
 #ifndef MINIMUM_TIME_TO_SCHEDULE_N_BUS_TRIPS_H
 #define MINIMUM_TIME_TO_SCHEDULE_N_BUS_TRIPS_H
 #include <concepts>
+#include <ranges>
+#include <algorithm>
 
 template <typename Container>
 concept IndexedContainerWithUnsignedIntegralElements = requires(Container c)
@@ -20,9 +22,22 @@ concept UnsignedInteger64Bit = std::is_unsigned_v<T> && (sizeof(T) * 8 >= 64);
 
 template <typename Container, typename ReturnType>
 requires IndexedContainerWithUnsignedIntegralElements<Container> && UnsignedInteger64Bit<ReturnType>
-ReturnType minimum_time_to_schedule_n_bus_trips()
+ReturnType minimum_time_to_schedule_n_bus_trips(const Container & bus_times, int total_trips)
 {
-
+	ReturnType left{1};
+	auto right = static_cast<ReturnType>(*std::ranges::max_element(bus_times));
+	while(left <= right)
+	{
+		ReturnType trips{};
+		ReturnType mid = (left + right)/2;
+		for(const auto & bus_time : bus_times)
+			trips += mid/bus_time;
+		if(trips < total_trips)
+			left = mid +1;
+		else
+			right = mid -1;
+	}
+	return left;
 }
 
 
