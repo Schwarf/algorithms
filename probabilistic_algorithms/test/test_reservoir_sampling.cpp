084cf2e6dfcb4142334800d87e41e343a2aeff3a
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-10 20:13:46 +0100

    There is something I do not understand.

--- probabilistic_algorithms/test/test_reservoir_sampling.cpp
+++ probabilistic_algorithms/test/test_reservoir_sampling.cpp
@@ -26,15 +26,17 @@ TEST(reservoir_sampling, test_output)
 TEST(reservoir_sampling, test_distribution)
 {
 	std::vector<int> input(1000);
+	double input_size = static_cast<double>(input.size());
+
 	std::iota(input.begin(), input.end(), 1);
-	int sample_size{10};
+	int sample_size{100};
+	double sample_size_d = static_cast<double>(sample_size);
 	int draws{10000};
 	int count = draws;
-	double expected_frequency = static_cast<double>(sample_size) / static_cast<double>(input.size());
+	double expected_frequency = input_size/sample_size_d;
 	std::vector<int> frequencies(input.size()+1);
-	double one_sigma_error = std::sqrt(
-		1.0 / (static_cast<double>(input.size()) - 1.0) * (1.0 - 1.0 / static_cast<double>(input.size()))
-			/ sample_size);
+	double one_sigma_error = std::sqrt((1.0 / input_size) * (1.0 - 1.0/ input_size) / sample_size_d);
+
 	while (count--) {
 		auto sample = reservoir_sampling(input, sample_size);
 		for (const auto &sample_element: sample)
@@ -42,6 +44,7 @@ TEST(reservoir_sampling, test_distribution)
 	}
 	for (const auto &frequency: frequencies) {
 		double absolute_error = std::abs(static_cast<double>(frequency) / draws - expected_frequency);
+
 		EXPECT_TRUE(absolute_error < 3.0*one_sigma_error);
 	}
 }
