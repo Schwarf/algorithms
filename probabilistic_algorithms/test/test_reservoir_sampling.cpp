e6580ae5be996f8b17c2e37895841813916d9f5d
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-10 21:59:31 +0100

    Simplify test to total-mean ... thinking.

--- probabilistic_algorithms/test/test_reservoir_sampling.cpp
+++ probabilistic_algorithms/test/test_reservoir_sampling.cpp
@@ -25,26 +25,32 @@ TEST(reservoir_sampling, test_output)
 
 TEST(reservoir_sampling, test_distribution)
 {
-	std::vector<int> input(1000);
+	std::vector<int> input(200);
 	double input_size = static_cast<double>(input.size());
 
 	std::iota(input.begin(), input.end(), 1);
-	int sample_size{100};
+	int sample_size{30};
 	double sample_size_d = static_cast<double>(sample_size);
 	int draws{10000};
 	int count = draws;
-	double expected_frequency = input_size/sample_size_d;
 	std::vector<int> frequencies(input.size()+1);
-	double one_sigma_error = std::sqrt((1.0 / input_size) * (1.0 - 1.0/ input_size) / sample_size_d);
-
+	double mean = (input_size+1.0)/2.0;
+	double standard_deviation {};
+	for(const auto & element: input)
+		standard_deviation += (static_cast<double>(element) - mean)*(static_cast<double>(element) - mean);
+	standard_deviation *= 1.0/input_size;
+	standard_deviation = std::sqrt(standard_deviation);
+	double one_sigma_error = standard_deviation/std::sqrt(static_cast<double>(draws));
+	double mean_estimate {};
 	while (count--) {
 		auto sample = reservoir_sampling(input, sample_size);
 		for (const auto &sample_element: sample)
 			frequencies[sample_element]++;
 	}
-	for (const auto &frequency: frequencies) {
-		double absolute_error = std::abs(static_cast<double>(frequency) / draws - expected_frequency);
 
-		EXPECT_TRUE(absolute_error < 3.0*one_sigma_error);
-	}
+	for (int i{1} ; i < input_size+1 ; ++i)
+		mean_estimate += static_cast<double>(frequencies[i])* input[i-1];
+	mean_estimate = mean_estimate/ static_cast<double >(draws)/sample_size_d;
+
+	std::cout << mean_estimate <<  "  " << mean << "  " << one_sigma_error ;
 }
