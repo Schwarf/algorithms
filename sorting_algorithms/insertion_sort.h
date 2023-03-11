4fb391f265a744f5b50cd38ee4e9819ca7062e1e
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-11 20:46:25 +0100

    Move insertion_sort to C++20.

--- sorting_algorithms/insertion_sort.h
+++ sorting_algorithms/insertion_sort.h
@@ -4,5 +4,20 @@
 
 #ifndef INSERTION_SORT_H
 #define INSERTION_SORT_H
+#include <cstddef>
+#include "used_concepts.h"
+template<typename Container>
+requires IndexedContainer<Container> && NoPointerElement<typename Container::value_type>
+void insertion_sort(Container & container)
+{
+	auto size = container.size();
+	for (size_t index1 = 0; index1 < size; ++index1) {
+		for (size_t index2 = index1; index2 > 0; --index2) {
+			if (container[index2] < container[index2 - 1]) {
+				std::swap(container[index2], container[index2-1]);
+			}
+		}
+	}
+}
 
 #endif //INSERTION_SORT_H
