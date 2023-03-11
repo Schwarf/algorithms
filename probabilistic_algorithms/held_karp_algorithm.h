f675c6331feec0acdb7627bc118166ff85e30b79
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-11 19:40:32 +0100

    Enable more concept debug messages. Find the first error.

--- probabilistic_algorithms/held_karp_algorithm.h
+++ probabilistic_algorithms/held_karp_algorithm.h
@@ -9,12 +9,12 @@
 #include <limits>
 
 
-template<typename Coordinates>
-concept RequireCoordinates = requires(const Coordinates &coordinates1, const Coordinates &coordinates2)
+template<typename CoordinatesType>
+concept RequireCoordinates = requires(const CoordinatesType &coordinates1, const CoordinatesType &coordinates2)
 {
 	// coordinates1 & coordinates2 shall provide the index operator and return the value_type
-	{ coordinates1[0] } -> std::same_as<typename Coordinates::value_type &>;
-	{ coordinates2[0] } -> std::same_as<typename Coordinates::value_type &>;
+	{ coordinates1[0] } -> std::same_as<typename CoordinatesType::value_type const &>;
+	{ coordinates2[0] } -> std::same_as<typename CoordinatesType::value_type const &>;
 	// coordinates1 & coordinates2 shall provide the size method
 	{ coordinates1.size() } -> std::same_as<std::size_t>;
 	{ coordinates2.size() } -> std::same_as<std::size_t>;
@@ -25,28 +25,27 @@ concept RequireCoordinates = requires(const Coordinates &coordinates1, const Coo
 	{ static_cast<double> (coordinates2[0]) };
 };
 
-template<typename Container>
-concept RequireIndexedContainer = requires(const Container &container)
+template<typename ContainerType>
+concept RequireIndexedContainer = requires(const ContainerType &container)
 {
-	{ container[0] } -> std::same_as<typename Container::value_type &>;
+	{ container[0] } -> std::same_as<typename ContainerType::value_type const &>;
 	{ container.size() } -> std::same_as<std::size_t>;
 };
 
-template<typename Function, typename Container>
-concept RequireDistanceFunction = requires(Function function,
-										   const Container &container1,
-										   const Container &container2)
+template<typename FunctionType, typename ContainerType>
+concept RequireDistanceFunction = requires(FunctionType function,
+										   const ContainerType &container1,
+										   const ContainerType &container2)
 {
-	requires std::same_as<std::invoke_result_t<Function,
-											   typename Container::value_type,
-											   typename Container::value_type>, double>;
-	requires RequireCoordinates<Container>;
+	requires std::same_as< typename std::invoke_result_t<FunctionType,
+											   typename ContainerType::value_type,
+											   typename ContainerType::value_type>, double>;
+	requires RequireCoordinates<ContainerType>;
 
 };
-
-template<typename Coordinates>
-requires RequireCoordinates<Coordinates>
-double distance(const Coordinates &location1, const Coordinates &location2)
+template<typename CoordinatesType>
+requires RequireCoordinates<CoordinatesType>
+double distance(const CoordinatesType &location1, const CoordinatesType &location2)
 {
 	double distance{};
 	for (size_t index{}; index < location1.size(); index++) {
@@ -56,18 +55,40 @@ double distance(const Coordinates &location1, const Coordinates &location2)
 	return std::sqrt(distance);
 }
 
-template<typename Container, typename DistanceFunctionType>
-requires RequireIndexedContainer<Container> && RequireDistanceFunction<DistanceFunctionType, Container>
-double held_karp_algorithm(const Container &locations, DistanceFunctionType distance)
+template<typename ContainerType>
+requires RequireIndexedContainer<ContainerType>
+double held_karp_algorithm(const ContainerType &locations)
 {
 	const int number_of_locations = locations.size();
 	const int number_of_location_sets = 1 << number_of_locations;
 	std::vector<std::vector<double>> distance_memoization
-		(number_of_location_sets,std::vector<double>(number_of_locations, std::numeric_limits<double>::max));
+		(number_of_location_sets,std::vector<double>(number_of_locations, std::numeric_limits<double>::max()));
 
 	// The distance to the first city to itself is 0
 	distance_memoization[1][0] = 0.0;
+	for (int subset_bit_mask = 2; subset_bit_mask < number_of_location_sets; ++subset_bit_mask )
+	{
+		for(int location_index1{}; location_index1 < number_of_locations; ++location_index1)
+		{
+			if(subset_bit_mask & (1 << location_index1))
+			for(int location_index2{}; location_index2 < number_of_locations; ++location_index2)
+			{
+					if (location_index1 != location_index2 && (subset_bit_mask & (1 << location_index2))) {
+						distance_memoization[subset_bit_mask][location_index1] =
+							std::min(distance_memoization[subset_bit_mask][location_index1],
+									 distance_memoization[subset_bit_mask - (1 << location_index1)][location_index2]
+									 + distance(locations[location_index2], locations[location_index1]));
 
+				}
+			}
+		}
+	}
+	double minimal_distance = std::numeric_limits<double>::max();
+	for (int i = 1; i < number_of_locations; i++) {
+		minimal_distance = std::min(minimal_distance, distance_memoization[number_of_location_sets - 1][i] +
+						distance(locations[i], locations[0]));
+	}
+	return minimal_distance;
 }
 
 struct City
