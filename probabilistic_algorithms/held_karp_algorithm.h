3ad76d289e39cc50d1cc4badbf0ea448f4449a5a
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-11 16:17:40 +0100

    Start with concept for IndexedSameDimensionCoordinates.

--- probabilistic_algorithms/held_karp_algorithm.h
+++ probabilistic_algorithms/held_karp_algorithm.h
@@ -7,6 +7,48 @@
 #include <vector>
 #include <cmath>
 #include <limits>
+
+template <typename Container>
+concept IndexedContainer = requires(Container c)
+{
+	{ c[0] } -> std::same_as<typename Container::value_type&>;
+	{ c.size() } -> std::same_as<std::size_t>;
+};
+
+
+template <typename Coordinates>
+concept IndexedSameDimensionCoordinates = requires(const Coordinates & coordinates1, const Coordinates & coordinates2)
+{
+	{ coordinates1[0] } -> std::same_as<typename Coordinates::value_type&>;
+	{ coordinates2[0] } -> std::same_as<typename Coordinates::value_type&>;
+	{ coordinates1.size() } -> std::same_as<std::size_t>;
+	{ coordinates2.size() } -> std::same_as<std::size_t>;
+	coordinates1.size() == coordinates2.size();
+	{static_cast<double> (coordinates1[0])};
+	{static_cast<double> (coordinates2[0])};
+};
+
+
+template <typename Coordinates>
+requires IndexedSameDimensionCoordinates<Coordinates>
+double distance(const Coordinates & location1, const Coordinates & location2)
+{
+	double distance{};
+	for(size_t index{}; index < location1.size(); index++)
+	{
+		auto difference = static_cast<double>(location1[index] - location2[index]);
+		distance += difference*difference;
+	}
+	return std::sqrt(distance);
+}
+
+template <typename Coordinates>
+requires IndexedSameDimensionCoordinates<Coordinates> && IndexedContainer<IndexedSameDimensionCoordinates<Coordinates>>
+double held_karp_algorithm(const IndexedContainer<IndexedSameDimensionCoordinates<Coordinates>> & locations)
+{
+
+}
+
 struct City {
 	double x, y;
 };
