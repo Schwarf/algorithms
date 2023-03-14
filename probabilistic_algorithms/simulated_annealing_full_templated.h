9c9c2a3f2a7cd9b56d52d0467b1a7ca93b77b971
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-14 17:40:14 +0100

    Update fully templated version of simulated annelaing with new concepts.

--- probabilistic_algorithms/simulated_annealing_full_templated.h
+++ probabilistic_algorithms/simulated_annealing_full_templated.h
@@ -7,51 +7,64 @@
 #include <random>
 #include <algorithm>
 
-template<typename T,
-	template<typename...> class ContainerType,
+template<typename T, template<typename...> class ContainerType,
 	template<typename...> class InnerContainerType>
-double compute_tour_length(const ContainerType<InnerContainerType<T>> &container)
+class TravelingSalesmanObjectFunction
 {
-	auto distance_function = [](const auto &container1, const auto &container2)
+public:
+	double operator()(const ContainerType<InnerContainerType<T>> &container)
 	{
-		double distance{};
-		for (int i{}; i < container1.size(); ++i) {
-			double difference = container1[i] - container2[i];
-			distance += difference * difference;
-		}
-		return std::sqrt(distance);
-	};
-	const auto number_of_elements = container.size();
-	double tour_length{};
-	for (size_t i{}; i < number_of_elements - 1; ++i)
-		tour_length += distance_function(container[i], container[i + 1]);
-	tour_length += distance_function(container[number_of_elements - 1], container[0]);
-	return tour_length;
-}
+		auto distance_function = [](const auto &container1, const auto &container2)
+		{
+			double distance{};
+			for (size_t i{}; i < container1.size(); ++i) {
+				double difference = container1[i] - container2[i];
+				distance += difference * difference;
+			}
+			return std::sqrt(distance);
+		};
+		const auto number_of_elements = container.size();
+		double tour_length{};
+		for (size_t i{}; i < number_of_elements - 1; ++i)
+			tour_length += distance_function(container[i], container[i + 1]);
+		tour_length += distance_function(container[number_of_elements - 1], container[0]);
+		return tour_length;
+	}
+};
 
 template<typename T, template<typename...> class ContainerType,
 	template<typename...> class InnerContainerType>
-void perturbation(ContainerType<InnerContainerType<T>> &container,
-				  decltype(std::declval<std::mt19937>()) &random_generator)
+class TravelingSalesmanPerturbationFunction
 {
-	std::uniform_int_distribution<int> distribution(0, container.size() - 1);
-	int index1 = distribution(random_generator);
-	int index2 = distribution(random_generator);
-	std::swap(container[index1], container[index2]);
-}
+public:
+	void operator()(ContainerType<InnerContainerType<T>> &container,
+					decltype(std::declval<std::mt19937>()) &random_generator)
+	{
+		std::uniform_int_distribution<int> distribution(0, container.size() - 1);
+		index1_ = distribution(random_generator);
+		index2_ = distribution(random_generator);
+		std::swap(container[index1_], container[index2_]);
+	}
 
-template<typename T, typename ObjectFunctionType, typename PerturbationFunctionType,
+	void undo(ContainerType<InnerContainerType<T>> &container)
+	{
+		std::swap(container[index1_], container[index2_]);
+	}
+
+private:
+	size_t index1_{};
+	size_t index2_{};
+};
+
+template<typename T, typename ObjectFunctionType, typename PerturbationClass,
 	template<typename...> class ContainerType,
 	template<typename...> class InnerContainerType>
 requires ContainerTypeTemplate<T, ContainerType, InnerContainerType>
-	&& std::invocable<ObjectFunctionType, const ContainerType<InnerContainerType<T>> &>
-	&& std::invocable<PerturbationFunctionType,
-					  ContainerType<InnerContainerType<T>> &,
-					  decltype(std::declval<std::mt19937>()) &>
-	&& ObjectFunctionReturnTypeIsDouble<ObjectFunctionType, T, ContainerType, InnerContainerType>
+	&& PerturbationFunctor<PerturbationClass, T, ContainerType, InnerContainerType>
+	&& ObjectFunctionFunctor<ObjectFunctionType, T, ContainerType, InnerContainerType>
 double simulated_annealing_algorithm(ContainerType<InnerContainerType<T>> &container,
 									 ObjectFunctionType object_function,
-									 PerturbationFunctionType perturbation_function,
+									 PerturbationClass perturbation_function,
 									 double initial_temperature,
 									 double cooling_rate,
 									 double minimal_temperature)
@@ -67,6 +80,8 @@ double simulated_annealing_algorithm(ContainerType<InnerContainerType<T>> &conta
 		if (delta < 0 || std::exp(-delta / temperature) > distribution(random_generator)) {
 			current_value = new_value;
 		}
+		else
+			perturbation_function.undo(container);
 		temperature *= cooling_rate;
 	}
 	return current_value;
