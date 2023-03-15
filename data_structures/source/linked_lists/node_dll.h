a6d767119e201e7bdb449e0b87745b773cbf406f
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-15 20:30:23 +0100

    Add node for doubly linked list and implement algo remove_duplicates starting from head and tail.

---
+++ data_structures/source/linked_lists/node_dll.h
@@ -0,0 +1,21 @@
+//
+// Created by andreas on 15.03.23.
+//
+
+#ifndef NODE_DLL_H
+#define NODE_DLL_H
+// Node for double linked list
+template<typename T>
+struct NodeDLL
+{
+	explicit NodeDLL(T val)
+		:
+		value(val)
+	{
+	}
+	T value;
+	NodeDLL<T> *next{nullptr};
+	NodeDLL<T> *previous{nullptr};
+};
+
+#endif //NODE_DLL_H
---
+++ data_structures/source/linked_lists/remove_duplicates_dll.h
@@ -0,0 +1,65 @@
+//
+// Created by andreas on 15.03.23.
+//
+
+#ifndef REMOVE_DUPLICATES_DLL_H
+#define REMOVE_DUPLICATES_DLL_H
+#include "node_dll.h"
+#include <unordered_set>
+
+// Remove all duplicates from a doubly linked list using additional space, starting from head
+// Runtime complexity: O(N)
+// Space complexity: O(N)
+template<typename T>
+NodeDLL<T>* remove_duplicates(NodeDLL<T>* head)
+{
+	if(!head)
+		return head;
+	auto current = head;
+	NodeDLL<T> * previous = nullptr;
+	std::unordered_set<T> counter;
+	while(current)
+	{
+		if(counter.find(current->value) != counter.end()) {
+			previous->next = current->next;
+			current->next->previous = previous;
+		}
+		else {
+			previous = current;
+			counter.insert(current->value);
+		}
+		current = current->next;
+	}
+	return head;
+}
+
+
+// Remove all duplicates from a doubly linked list using additional space, starting from tail
+// Runtime complexity: O(N)
+// Space complexity: O(N)
+template<typename T>
+NodeDLL<T>* remove_duplicates_from_tail(NodeDLL<T>* tail)
+{
+	if(!tail)
+		return tail;
+	auto current = tail;
+	// This points behind tail
+	NodeDLL<T> * previous = nullptr;
+	std::unordered_set<T> counter;
+	while(current)
+	{
+		if(counter.find(current->value) != counter.end()) {
+			previous->previous = current->previous;
+			current->previous->next = previous;
+		}
+		else {
+			previous = current;
+			counter.insert(current->value);
+		}
+		current = current->previous;
+	}
+	return tail;
+}
+
+
+#endif //REMOVE_DUPLICATES_DLL_H
