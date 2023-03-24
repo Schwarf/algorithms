f561105c7e264e399c2f607f7f2bd19559130a34
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-24 16:33:58 +0100

    Add dfs implementation to find-smallest edge in sub-graph.

--- data_structures/source/graphs/find_smallest_edge_in_subgraph.h
+++ data_structures/source/graphs/find_smallest_edge_in_subgraph.h
@@ -7,6 +7,7 @@
 #include "used_concepts.h"
 #include <vector>
 #include <queue>
+#include <stack>
 #include <stdexcept>
 // Given a disconnected graph and two vertices. Find the smallest edge in the sub-graph that contains the two vertices.
 // Each edge is given as std::vector of size 3 with [vertex_1, vertex_2, edge-length].
@@ -41,6 +42,46 @@ VertexType find_smallest_edge_in_sub_graph_bfs(const std::vector<std::vector<Ver
 	while (!q.empty()) {
 		auto node = q.front();
 		q.pop();
+		for (const auto &edge: graph[node]) {
+			if (!visited[edge.first]) {
+				visited[edge.first] = true;
+				minimum = std::min(minimum, edge.second);
+				if (edge.first == end_vertex)
+					end_vertex_found = true;
+			}
+		}
+	}
+	return end_vertex_found ? minimum : not_connected_value;
+};
+
+
+template<typename VertexType>
+requires VertexRequirement<VertexType>
+VertexType find_smallest_edge_in_sub_graph_dfs(const std::vector<std::vector<VertexType>> &vertices,
+											   const VertexType &start_vertex,
+											   const VertexType &end_vertex,
+											   const VertexType &max_edge_value,
+											   const VertexType &not_connected_value)
+{
+
+	if (vertices.empty())
+		throw std::invalid_argument("Empty vector in find_smallest_edge_in_sub_graph_bfs!");
+	if (vertices.size() == 1)
+		return vertices[0][2];
+	// Transform into graph representation with adjacency list
+	std::vector<std::vector<std::pair<VertexType, VertexType>>> graph(vertices.size() + 1);
+	for (const auto &edge: vertices) {
+		graph[edge[0]].push_back({edge[1], edge[2]});
+		graph[edge[1]].push_back({edge[0], edge[2]});
+	}
+	std::stack<VertexType> s;
+	s.push(start_vertex);
+	VertexType minimum{max_edge_value};
+	std::vector<bool> visited(vertices.size() + 1);
+	bool end_vertex_found{};
+	while (!s.empty()) {
+		auto node = s.top();
+		s.pop();
 		for (const auto &edge: graph[node]) {
 			if (!visited[edge.first]) {
 				visited[edge.first] = true;
