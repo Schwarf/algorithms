d7be55e45580719763571aab4a16be0c20004858
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-26 14:57:06 +0200

    Update docu and make function template.

--- fun_with_algos/interval_merging.h
+++ fun_with_algos/interval_merging.h
@@ -5,16 +5,17 @@
 #include <algorithm>
 #ifndef INTERVAL_MERGING_H
 #define INTERVAL_MERGING_H
-// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
 //and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
-std::vector<std::vector<int>> merge_intervals(std::vector<std::vector<int>>& intervals) {
-	auto sorted = [](const std::vector<int> & input1,const std::vector<int> & input2)
+template <typename T>
+std::vector<std::vector<T>> merge_intervals(std::vector<std::vector<T>>& intervals) {
+	auto sorted = [](const std::vector<T> & input1,const std::vector<T> & input2)
 	{
 		return input1[0] < input2[0];
 	};
 	std::sort(intervals.begin(), intervals.end(), sorted);
-	std::vector<std::vector<int>> result;
+	std::vector<std::vector<T>> result;
 	result.push_back(intervals[0]);
 	for(int i{1}; i < intervals.size(); ++i)
 	{
