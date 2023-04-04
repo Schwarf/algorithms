b9e777b5b202da746405c990814fc9e57e4bebcc
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-04 21:13:13 +0200

    Check corner case and implement set_bit.

--- bit_manipulation/basic_bit_operations.h
+++ bit_manipulation/basic_bit_operations.h
@@ -9,18 +9,31 @@
 #include <bit>
 
 
-template <typename T, int maximum_bit_position>
+template<typename T, int maximum_bit_position>
 requires std::integral<T>
 bool has_bit(T number, int bit_position)
 {
+	static_assert(maximum_bit_position < 64);
 	if (bit_position > maximum_bit_position)
-		throw std::out_of_range("Bit position must be smaller than " + std::to_string(maximum_bit_position)+"!");
-	uint64_t bit = (1<<bit_position);
-	if(bit > static_cast<uint64_t>(std::numeric_limits<T>::max()))
+		throw std::out_of_range("Bit position must be smaller than " + std::to_string(maximum_bit_position) + "!");
+	uint64_t bit = (1 << bit_position);
+	if (bit >= static_cast<uint64_t>(std::numeric_limits<T>::max()))
 		throw std::out_of_range("Bit position overflows in provided type !");
 	return (number & 1 << bit_position) != 0;
-
-
 };
+
+template<typename T, int maximum_bit_position>
+requires std::integral<T>
+T set_bit(T number, int bit_position)
+{
+	static_assert(maximum_bit_position < 64);
+	if (bit_position > maximum_bit_position)
+		throw std::out_of_range("Bit position must be smaller than " + std::to_string(maximum_bit_position) + "!");
+	uint64_t bit = (1 << bit_position);
+	if (bit >= static_cast<uint64_t>(std::numeric_limits<T>::max()))
+		throw std::out_of_range("Bit position overflows in provided type !");
+	return number |= (1 << bit_position);
+};
+
 
 #endif //BASIC_BIT_OPERATIONS_H
--- bit_manipulation/main.cpp
+++ bit_manipulation/main.cpp
@@ -3,8 +3,9 @@
 
 int main()
 {
-	int x = 1 << 4;
-	int bit_position = 2;
-	std::cout << has_bit<int8_t,32>(x, bit_position) << std::endl;
+	uint64_t h{1};
+	uint64_t x = h << 63;
+	int bit_position = 63;
+	std::cout << has_bit<uint64_t, 63>(x, bit_position) << std::endl;
 	return 0;
 }
