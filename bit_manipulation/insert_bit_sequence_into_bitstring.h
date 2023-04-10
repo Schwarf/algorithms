6b0f3914ec270225fb4a58ad0a535e94c7afe84e
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-10 17:03:11 +0200

    Insert bit_sequence into bit_string.

---
+++ bit_manipulation/insert_bit_sequence_into_bitstring.h
@@ -0,0 +1,20 @@
+//
+// Created by andreas on 10.04.23.
+//
+
+#ifndef INSERT_BIT_SEQUENCE_INTO_BITSTRING_H
+#define INSERT_BIT_SEQUENCE_INTO_BITSTRING_H
+int insert_bit_sequence_into_bitstring(int bitstring, int bit_sequence, int left_start_position, int right_end_position)
+{
+	if(right_end_position > left_start_position || left_start_position >= 32 || right_end_position < 0)
+		return 0;
+	int all_bits_set = ~0;
+	int left = left_start_position < 31 ? (all_bits_set << (left_start_position+1)) : 0;
+	int right = (1 << right_end_position) -1;
+	int mask = left + right;
+	int clear_bitstring_between_left_and_right = bitstring & mask;
+	int shift_bit_sequence_in_position = bit_sequence << right_end_position;
+	return clear_bitstring_between_left_and_right | shift_bit_sequence_in_position;
+}
+
+#endif //INSERT_BIT_SEQUENCE_INTO_BITSTRING_H
--- bit_manipulation/main.cpp
+++ bit_manipulation/main.cpp
@@ -3,13 +3,6 @@
 
 int main()
 {
-	uint32_t h{1};
-	uint32_t x = h << 14;
-	int bit_position = 41;
-	//std::cout << has_bit(x, bit_position) << std::endl;
-	int y{};
-	set_bit(y, 3);
-	std::cout << y << std::endl;
 	return 0;
 
 }
