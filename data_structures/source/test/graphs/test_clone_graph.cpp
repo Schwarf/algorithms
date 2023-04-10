16ebf3f397587f5404e4949514d22abe858db954
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-10 16:37:19 +0200

    Add another test using DFS.

--- data_structures/source/test/graphs/test_clone_graph.cpp
+++ data_structures/source/test/graphs/test_clone_graph.cpp
@@ -18,10 +18,23 @@ protected:
 		node->neighbors.push_back(neighbor1_1);
 		node->neighbors.push_back(neighbor1_2);
 		neighbor1_1->neighbors.push_back(neighbor2_1);
+		neighbor1_1->neighbors.push_back(node);
 		neighbor1_2->neighbors.push_back(neighbor2_1);
+		neighbor1_2->neighbors.push_back(node);
 		return node;
 	}
 
+	bool is_node_in_graph(Node<int> *graph, Node<int> *node, std::vector<bool> &visited)
+	{
+		if (node == graph)
+			return true;
+		visited[graph->value] = true;
+		for (const auto &neighbor: graph->neighbors) {
+			if (!visited[neighbor->value])
+				is_node_in_graph(neighbor, node, visited);
+		}
+		return false;
+	}
 };
 
 
@@ -36,4 +49,17 @@ TEST_F(SetupCloneGraphTests, first_try)
 	EXPECT_EQ(new_node->neighbors[1]->value, node->neighbors[1]->value);
 	EXPECT_NE(new_node->neighbors[1], node->neighbors[1]);
 	EXPECT_NE(new_node->neighbors[0], node->neighbors[0]);
+}
+
+TEST_F(SetupCloneGraphTests, with_is_node_in_graph)
+{
+	auto node = sample_graph();
+	auto copy = node;
+	std::vector<bool> default_visited(5);
+	auto visited = default_visited;
+	EXPECT_TRUE(is_node_in_graph(node, copy, visited));
+	auto new_node = clone_graph(node);
+	visited = default_visited;
+	EXPECT_FALSE(is_node_in_graph(new_node, node, visited));
+	EXPECT_FALSE(is_node_in_graph(node, new_node, visited));
 }
