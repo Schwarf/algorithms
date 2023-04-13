1526476dde410b61b7aba6db62758e50f503099f
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-13 17:39:17 +0200

    Reformat.

--- fun_with_arrays/minimal_operations_to_get_zero.h
+++ fun_with_arrays/minimal_operations_to_get_zero.h
@@ -20,26 +20,23 @@ template<typename T>
 int minimal_operations_to_get_zero(std::vector<T> &input, T target)
 {
 	T sum{};
-	for(const auto & element: input)
+	for (const auto &element: input)
 		sum += element;
-	if(sum == target)
+	if (sum == target)
 		return input.size();
-	if(sum < target)
+	if (sum < target)
 		return -1;
 
 	target = sum - target;
 	T current_sum{};
 	int start_index{};
 	int count_operations{};
-	for(int i =0; i < input.size(); ++i)
-	{
+	for (int i = 0; i < input.size(); ++i) {
 		current_sum += input[i];
-		while(current_sum > target)
+		while (current_sum > target)
 			current_sum -= input[start_index++];
-		if(current_sum == target)
-		{
-			count_operations = std::max(count_operations, i -start_index + 1);
-		}
+		if (current_sum == target)
+			count_operations = std::max(count_operations, i - start_index + 1);
 	}
 	return count_operations == 0 ? -1 : input.size() - count_operations;
 }
