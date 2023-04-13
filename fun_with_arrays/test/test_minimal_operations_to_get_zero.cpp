d23f475d9f4af017ab215119e0072da095703e7b
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-13 17:39:41 +0200

    Add some tests for complicated algo

---
+++ fun_with_arrays/test/test_minimal_operations_to_get_zero.cpp
@@ -0,0 +1,29 @@
+//
+// Created by andreas on 13.04.23.
+//
+#include "gtest/gtest.h"
+#include "minimal_operations_to_get_zero.h"
+
+TEST(MinimalOperationsToGetZero, simple_result2)
+{
+	std::vector<int> input{1,1,4,2,3};
+	int target{5};
+	int expected_result{2};
+	EXPECT_EQ(expected_result, minimal_operations_to_get_zero(input, target));
+}
+
+TEST(MinimalOperationsToGetZero, simple_result_not_working)
+{
+	std::vector<int> input{5,6,7,8,9};
+	int target{4};
+	int expected_result{-1};
+	EXPECT_EQ(expected_result, minimal_operations_to_get_zero(input, target));
+}
+
+TEST(MinimalOperationsToGetZero, simple_result4)
+{
+	std::vector<int> input{3,2,20,1,1,3};
+	int target{10};
+	int expected_result{5};
+	EXPECT_EQ(expected_result, minimal_operations_to_get_zero(input, target));
+}
