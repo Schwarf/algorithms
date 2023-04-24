e4bc430daa45b87c6554cc1923b28b85fa36a446
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-24 18:02:22 +0200

    Add implementation for finding last stone using own heap structure.

--- data_structures/source/heaps/find_remaining_stone.h
+++ data_structures/source/heaps/find_remaining_stone.h
@@ -4,5 +4,30 @@
 
 #ifndef FIND_REMAINING_STONE_H
 #define FIND_REMAINING_STONE_H
+#include <vector>
+#include "binary_heap.h"
+// You are given an array of integers stones where stones[i] is the weight of the ith stone.
+// We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together.
+// Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+// If x == y, both stones are destroyed, and
+// If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+// At the end of the game, there is at most one stone left.
+// Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+template<typename T, size_t capacity>
+T find_remaining_stone(const std::vector<T> &stones)
+{
+	auto heap = BinaryHeap<int, capacity>();
+	for (const auto &stone: stones)
+		heap.insert(stone);
+	//binary_heap <int> p (stones.begin(), stones.end());
+	while (heap.size() > 1) {
+		auto stone1 = heap.pop_maximum();
+		auto stone2 = heap.pop_maximum();
+		if (stone1 != stone2)
+			heap.insert(stone1 - stone2);
+	}
+	return heap.is_empty() ? 0 : heap.pop_maximum();
+}
 
 #endif //FIND_REMAINING_STONE_H
