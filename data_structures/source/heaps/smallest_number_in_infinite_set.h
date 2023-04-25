3e36aca22203bd1ad60809835c94e698eb39bed6
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-25 18:00:12 +0200

    Add class for smallest number in infinite set.

---
+++ data_structures/source/heaps/smallest_number_in_infinite_set.h
@@ -0,0 +1,49 @@
+//
+// Created by andreas on 25.04.23.
+//
+
+#ifndef SMALLEST_NUMBER_IN_INFINITE_SET_H
+#define SMALLEST_NUMBER_IN_INFINITE_SET_H
+#include <concepts>
+#include <queue>
+#include <unordered_set>
+
+template<typename T> requires std::totally_ordered<T> && std::is_integral_v<T>
+class SmallestPositiveInfiniteSet
+{
+public:
+	explicit SmallestPositiveInfiniteSet(const T &initial_value)
+		: current_value(initial_value)
+	{
+	}
+	// pops the smallest element that has not been pooped yet and has not been re-added
+	T pop()
+	{
+		T answer{};
+		if (queue.empty()) {
+			answer = current_value;
+			current_value += 1;
+			return answer;
+		}
+		answer = queue.top();
+		is_present.erase(answer);
+		queue.pop();
+		return answer;
+	}
+
+	void add_back(T value)
+	{
+		if (current_value <= value || is_present.find(value) != is_present.end()) {
+			return;
+		}
+		queue.push(value);
+		is_present.insert(value);
+	}
+private:
+	std::priority_queue<T, std::vector<T>, std::greater<T>> queue;
+	std::unordered_set<T> is_present;
+	T current_value;
+};
+
+
+#endif //SMALLEST_NUMBER_IN_INFINITE_SET_H
