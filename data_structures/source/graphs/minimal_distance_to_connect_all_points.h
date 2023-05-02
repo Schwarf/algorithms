c66f25c33facc558c21b0ac0f7631a6971d671d3
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-02 20:06:50 +0200

    Work on algo.

--- data_structures/source/graphs/minimal_distance_to_connect_all_points.h
+++ data_structures/source/graphs/minimal_distance_to_connect_all_points.h
@@ -6,37 +6,52 @@
 #define MINIMAL_DISTANCE_TO_CONNECT_ALL_POINTS_H
 #include <vector>
 #include <concepts>
+#include <queue>
 
-template <typename T, template<typename...> typename CoordinateType>
-concept CoordinateTypeTemplate = requires(const CoordinateType<T> &coordinates)
-{
-	{ coordinates[0] } -> std::same_as<typename CoordinateType<T>::value_type const &>;
-	{ coordinates.size() } -> std::same_as<std::size_t>;
-};
-
-template<typename Type, typename Function, template<typename...> typename CoordinateType>
-concept DistanceFunction = requires(Function function, const CoordinateType<Type> & coordinates)
-{
-	requires CoordinateTypeTemplate<Type, CoordinateType>;
-	std::invocable<Function>;
-	{ function(coordinates, coordinates)} ->std::same_as<Type>;
-};
-
-template<typename T, typename DistanceFunctionType, template<typename...> typename CoordinateType >
-requires std::is_integral_v<T> && DistanceFunction<T, DistanceFunctionType, CoordinateType>
+template<typename T, typename ContainerType, typename DistanceFunctionType>
+requires std::is_integral_v<T> && std::same_as<T, typename ContainerType::value_type> &&
+	std::same_as<T, std::invoke_result_t<DistanceFunctionType, ContainerType, ContainerType>>
 T minimal_costs_to_connect_points(std::vector<std::vector<T>> points, DistanceFunctionType distance_function)
 {
 
-	std::vector<std::vector<std::pair<T, T>>> graph(points.size() + 1);
-	for(int i{} ; i < points.size(); ++i) {
-		for (int j{}; j < points.size(); j++)
-		{
-			if(i==j)
-				continue;
-			auto distance = distance_function(points[i], points[j]);
-			graph[i].push_back({j , })
+	auto comparator = [](const std::pair<T, T> &p1, const std::pair<T, T> &p2)
+	{
+		return p1.second > p2.second;
+	};
+
+	std::vector<std::priority_queue<std::pair<T, T>, std::vector<std::pair<T, T>>, decltype(comparator)>>
+		graph(points.size(),
+			  std::priority_queue<std::pair<T, T>, std::vector<std::pair<T, T>>, decltype(comparator)>(comparator));
+	std::unordered_map<T, T> id_weight;
+	for (int i{}; i < points.size(); ++i) {
+		for (int j{i + 1}; j < points.size(); ++j) {
+			T distance = distance_function(points[i], points[j]);
+			graph[i].push({j, distance});
+			graph[j].push({i, distance});
 		}
+		id_weight[i] = std::numeric_limits<T>::max();
 	}
+
+
+	std::vector<bool> visited(graph.size(), false);
+	int index{};
+	T total_distance{};
+	while (!visited[index]) {
+		visited[index] = true;
+		T distance{};
+		while (true) {
+			if (!visited[graph[index].top().first] && ) {
+				distance = graph[index].top().second;
+				break;
+			}
+			else if (graph[index].empty())
+				break;
+			graph[index].pop();
+		}
+		index = graph[index].top().first;
+		total_distance += distance;
+	}
+	return total_distance;
 }
 
 
