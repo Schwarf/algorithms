15be52f232ba95672fe093f7042c55d1ec8576ec
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-09 17:09:55 +0200

    This required.

---
+++ template_reminders/this_required.cpp
@@ -0,0 +1,29 @@
+//
+// Created by andreas on 09.05.23.
+//
+#include <iostream>
+template <typename T>
+class Base{
+public:
+	void bar(){
+		std::cout << "In bar" << std::endl;
+	};
+};
+
+template <typename T>
+class Derived : Base<T>{
+public:
+	void foo(){
+		// This is required here, does not compile without it.
+		this->bar();
+		// Alternative
+		Base<T>::bar();
+	};
+};
+
+
+int main()
+{
+	auto derived =Derived<int>();
+	return 0;
+}
