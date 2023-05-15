e446d65e45fef148cd81f6f0af67adf33e7f5b8d
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-15 18:52:04 +0200

    Add one test.

---
+++ data_structures/source/test/linked_lists/test_swapping_nodes.cpp
@@ -0,0 +1,21 @@
+//
+// Created by andreas on 15.05.23.
+//
+#include "gtest/gtest.h"
+#include "setup_singly_linked_lists.h"
+#include "./linked_lists/swapping_nodes.h"
+
+TEST_F(SetupSinglyLinkedLists, simple1)
+{
+	std::vector<int> input{1, 2, 3, 4, 5};
+	std::vector<int> expected_output{1, 4, 3, 2, 5};
+	constexpr int k{2};
+	auto head1 = get_single_list_from_vector(input);
+	auto expected = get_single_list_from_vector(expected_output);
+	auto result = swapping_nodes(head1, k);
+	while (result) {
+		EXPECT_EQ(result->value, expected->value);
+		expected = expected->next;
+		result = result->next;
+	}
+}
