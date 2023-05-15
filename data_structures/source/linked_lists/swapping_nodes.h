9d8b1d7d916e5cafafbac99d335cd159807e2f01
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-15 18:51:21 +0200

    Add swapping nodes.

---
+++ data_structures/source/linked_lists/swapping_nodes.h
@@ -0,0 +1,31 @@
+//
+// Created by andreas on 15.05.23.
+//
+
+#ifndef SWAPPING_NODES_H
+#define SWAPPING_NODES_H
+#include <algorithm>
+#include "node.h"
+
+// Given the head of a linked list, and an integer k.
+// Return the head of the linked list after swapping the values of the kth node from the beginning and
+// the kth node from the end (the list is 1-indexed).
+// k <= size of the list
+template<typename T>
+Node<T> *swapping_nodes(Node<T> *head, int k)
+{
+	if (!head)
+		return head;
+	auto right = head;
+	for (int i{0}; i < k - 1; ++i)
+		right = right->next;
+	auto first = right;
+	auto left = head;
+	while (right->next) {
+		left = left->next;
+		right = right->next;
+	}
+	std::swap(left->value, first->value);
+	return head;
+}
+#endif //SWAPPING_NODES_H
