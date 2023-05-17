39ba8c4cd93608cd1d4412e7fca0c38fbaedd917
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-17 18:10:15 +0200

    Add stack solution.

--- data_structures/source/linked_lists/maximum_twin_sum.h
+++ data_structures/source/linked_lists/maximum_twin_sum.h
@@ -6,6 +6,7 @@
 #define MAXIMUM_TWIN_SUM_H
 #include "node.h"
 #include <vector>
+#include <stack>
 // In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
 // For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
 // The twin sum is defined as the sum of a node and its twin.
@@ -26,5 +27,24 @@ T maximum_twin_sum_with_vector(Node<T> *head)
 	return max;
 }
 
+template<typename T>
+T maximum_twin_sum_with_stack(Node<T> *head)
+{
+	std::stack<T> help;
+	auto node = head;
+	while (node) {
+		help.push(head->value);
+		node = node->next;
+	}
+	int half_size = help.size() >> 1;
+	int count{};
+	T max{};
+	while (count++ <= half_size) {
+		max = std::max(head->value + help.top(), max);
+		help.pop();
+	}
+
+	return max;
+}
 
 #endif //MAXIMUM_TWIN_SUM_H
