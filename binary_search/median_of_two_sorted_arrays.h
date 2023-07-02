efdfc83f2a0ce4c516107fb702e142fadc16b6db
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-02 19:56:21 +0200

    Add more efficient solution.

--- binary_search/median_of_two_sorted_arrays.h
+++ binary_search/median_of_two_sorted_arrays.h
@@ -6,7 +6,7 @@
 #define MEDIAN_OF_TWO_SORTED_ARRAYS_H
 #include <concepts>
 #include <vector>
-
+#include <limits>
 
 template<typename T>
 int find_median(const std::vector<T> &input1,
@@ -53,7 +53,7 @@ double median_of_sorted_arrays(const std::vector<T> &input1, const std::vector<T
 	int size2 = input2.size() - 1;
 
 	int size = size1 + size2 + 2;
-	if(size ==0)
+	if (size == 0)
 		return -1.0;
 	else if (size & 1)
 		return find_median(input1, input2, size / 2, 0, size1, 0, size2);
@@ -62,5 +62,46 @@ double median_of_sorted_arrays(const std::vector<T> &input1, const std::vector<T
 			+ find_median(input1, input2, size / 2, 0, size1, 0, size2)) / 2.0;
 }
 
+template<typename T>
+requires std::is_arithmetic_v<T>
+double median_of_sorted_arrays2(const std::vector<T> &input1, const std::vector<T> &input2)
+{
+	if (input1.size() > input2.size()) {
+		return median_of_sorted_arrays2(input2, input1);
+	}
+
+	int m = input1.size(), n = input2.size();
+	int left = 0, right = m;
+
+	while (left <= right) {
+		int partitionA = (left + right) / 2;
+		int partitionB = (m + n + 1) / 2 - partitionA;
+
+		T maxLeftA = (partitionA == 0) ? std::numeric_limits<T>::min() : input1[partitionA - 1];
+		T minRightA = (partitionA == m) ? std::numeric_limits<T>::max() : input1[partitionA];
+		T maxLeftB = (partitionB == 0) ? std::numeric_limits<T>::min() : input2[partitionB - 1];
+		T minRightB = (partitionB == n) ? std::numeric_limits<T>::max() : input2[partitionB];
+
+		if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+			if ((m + n) % 2 == 0) {
+				return (std::max(maxLeftA, maxLeftB) + std::min(minRightA, minRightB)) / 2.0;
+			}
+			else {
+				return std::max(maxLeftA, maxLeftB);
+			}
+		}
+		else if (maxLeftA > minRightB) {
+			right = partitionA - 1;
+		}
+		else {
+			left = partitionA + 1;
+		}
+	}
+
+	return 0.0;
+}
+
+
 #endif //MEDIAN_OF_TWO_SORTED_ARRAYS_H
+
 
