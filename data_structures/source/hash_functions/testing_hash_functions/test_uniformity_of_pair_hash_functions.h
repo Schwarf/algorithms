da7ebd016d21522e26fad7a2dd0e6018cacb6153
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-15 21:09:59 +0200

    Some handwaving test for pair hash functions.

--- data_structures/source/hash_functions/testing_hash_functions/test_uniformity_of_pair_hash_functions.h
+++ data_structures/source/hash_functions/testing_hash_functions/test_uniformity_of_pair_hash_functions.h
@@ -6,6 +6,7 @@
 #define TEST_UNIFORMITY_OF_PAIR_HASH_FUNCTIONS_H
 
 #include <vector>
+#include <unordered_map>
 template<typename Functor, typename FirstType, typename SecondType>
 concept PairHashFunction = requires(Functor functor, const std::pair<FirstType, SecondType> &argument)
 {
@@ -18,7 +19,18 @@ double test_uniformity_of_pair_hash_functions(HashFunction hash_function,
 											  const std::vector<std::pair<FirstType, SecondType>> &pairs,
 											  int number_of_buckets)
 {
-
+	std::unordered_map<int, int> histogram;
+	for (const auto &pair: pairs) {
+		int bucket = hash_function(pair) % number_of_buckets;
+		histogram[bucket]++;
+	}
+	auto expected_count = static_cast<double>(pairs.size()) / number_of_buckets;
+	double chi_square{};
+	for (const auto &[key, value]: histogram) {
+		double difference = static_cast<double>(value) - expected_count;
+		chi_square += difference * difference / expected_count;
+	}
+	return chi_square;
 }
 
 #endif //TEST_UNIFORMITY_OF_PAIR_HASH_FUNCTIONS_H
