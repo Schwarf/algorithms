3c337e9ea7262c215e269d40b25ad820eb41c719
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-15 15:18:44 +0200

    Refactor recursive implementation. Add top-down implementation for knapsack problem using memoization.

--- dynamic_programming/knapsack_problem.h
+++ dynamic_programming/knapsack_problem.h
@@ -6,6 +6,9 @@
 #define KNAPSACK_PROBLEM_H
 #include <concepts>
 #include <vector>
+#include <unordered_map>
+#include "./../data_structures/source/hash_functions/hash_function_for_pairs.h"
+
 template<typename ValueType, typename WeightType> requires std::is_arithmetic_v<ValueType>
 	&& std::is_arithmetic_v<WeightType>
 struct Item
@@ -17,7 +20,7 @@ struct Item
 template<typename ValueType, typename WeightType>
 requires std::is_arithmetic_v<ValueType>
 	&& std::is_arithmetic_v<WeightType>
-int knapsack_problem_recursive(std::vector<Item<ValueType, WeightType>> &items,
+int knapsack_problem_recursive(const std::vector<Item<ValueType, WeightType>> &items,
 							   WeightType knapsack_capacity,
 							   int number_of_items = -1)
 {
@@ -25,13 +28,63 @@ int knapsack_problem_recursive(std::vector<Item<ValueType, WeightType>> &items,
 		number_of_items = items.size();
 	if (knapsack_capacity == WeightType{} || number_of_items == 0)
 		return 0;
+	// Exclude if item weight is larger than remaining knapsack capacity
 	if (items[number_of_items - 1].weight > knapsack_capacity)
 		return knapsack_problem_recursive(items, knapsack_capacity, number_of_items - 1);
-	return std::max(items[number_of_items - 1].value + knapsack_problem_recursive(items,
-																				  knapsack_capacity
-																					  - items[number_of_items - 1]
-																						  .weight,
-																				  number_of_items - 1),
-					knapsack_problem_recursive(items, knapsack_capacity, number_of_items - 1));
+
+	auto remaining_capacity = knapsack_capacity - items[number_of_items - 1].weight;
+	ValueType include_item = items[number_of_items - 1].value + knapsack_problem_recursive(items, remaining_capacity,
+																						   number_of_items - 1);
+	ValueType exclude_item = knapsack_problem_recursive(items, knapsack_capacity, number_of_items - 1);
+
+	return std::max(include_item, exclude_item);
 }
+
+template<typename ValueType, typename WeightType>
+requires std::is_arithmetic_v<ValueType>
+	&& std::is_arithmetic_v<WeightType>
+int memoization(std::vector<Item<ValueType, WeightType>> &items,
+				WeightType knapsack_capacity,
+				std::unordered_map<std::pair<int, WeightType>,
+								   ValueType,
+								   non_commutative_pair_hash<int, WeightType>> &memo,
+				int number_of_items = -1)
+{
+	if (number_of_items == -1)
+		number_of_items = items.size();
+	if (knapsack_capacity == WeightType{} || number_of_items == 0)
+		return 0;
+	// Check if this capacity with this number of items was computed before
+	std::pair<int, WeightType> key = std::make_pair<int, WeightType>(number_of_items, knapsack_capacity);
+	if (memo.find(key) != memo.end())
+		return memo[key];
+
+	if (items[number_of_items - 1].weight > knapsack_capacity)
+		return memoization(items, knapsack_capacity, memo, number_of_items - 1);
+
+	auto remaining_capacity = knapsack_capacity - items[number_of_items - 1].weight;
+
+	ValueType include_item = items[number_of_items - 1].value + memoization(items, remaining_capacity, memo,
+																			number_of_items - 1);
+
+	ValueType exclude_item = memoization(items, knapsack_capacity, memo, number_of_items - 1);
+
+	auto result = std::max(include_item, exclude_item);
+	memo[key] = result;
+
+	return result;
+
+}
+
+template<typename ValueType, typename WeightType>
+requires std::is_arithmetic_v<ValueType>
+	&& std::is_arithmetic_v<WeightType>
+int knapsack_problem_top_down(const std::vector<Item<ValueType, WeightType>> &items, WeightType knapsack_capacity)
+{
+	std::unordered_map<std::pair<int, WeightType>, ValueType, non_commutative_pair_hash<int, WeightType>>
+		memo;
+	return memoization(items, knapsack_capacity, memo);
+}
+
+
 #endif //KNAPSACK_PROBLEM_H
--- dynamic_programming/test/test_knapsack_problem.cpp
+++ dynamic_programming/test/test_knapsack_problem.cpp
@@ -9,6 +9,9 @@ class SetupKnapsackProblem: public testing::Test
 protected:
 	std::vector<Item<int, int>> items1{{6, 1}, {10, 2}, {12, 3}};
 	int knapsack1{5};
+	std::vector<Item<int, int>> items2{{3, 2}, {4, 3}, {5, 4}, {6, 5}};
+	int knapsack2{8};
+
 };
 
 
@@ -16,5 +19,12 @@ TEST_F(SetupKnapsackProblem, knapsack_recursive1)
 {
 	const auto result = knapsack_problem_recursive(items1, knapsack1);
 	constexpr int expected_result{22};
+	EXPECT_EQ(result, expected_result);
+}
+
+TEST_F(SetupKnapsackProblem, knapsack_recursive2)
+{
+	const auto result = knapsack_problem_recursive(items2, knapsack2);
+	constexpr int expected_result{10};
 	EXPECT_EQ(result, expected_result);
 }
