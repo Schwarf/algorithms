a63aeea642bd9c724f7a23beb08ca47efa22d5ee
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-15 14:08:05 +0200

    Add pair hash function using the golden ratio.

--- data_structures/source/hash_functions/hash_function_for_pairs.h
+++ data_structures/source/hash_functions/hash_function_for_pairs.h
@@ -18,4 +18,24 @@ struct commutative_pair_hash
 	}
 };
 
+// Strong hash function that uses the fractional part of the golden ratio \phi = 1.61803398875
+// The fractional part of the golden ratio is (sqrt(5)-1)/2 ~= 0.61803398875. If this number is
+// multiplied with 2^32 and round down to integer you get 2654435769 which is '0x9e3779b9' in hexadecimal
+// representation. Since the golden ratio is irrational and has a decimal part that goes on forever
+// a multiplication with it (or the fractional) part spreads out values in 'random' way. In addition, \phi
+// has 'poor rational approximation', that means it's hard to approximate with a simple fraction
+// which makes it less likely that different inputs will, by coincidence, produce the same output,
+// reducing the chance of hash collisions.
+struct non_commutative_pair_hash
+{
+	template<class T1, class T2>
+	std::size_t operator()(const std::pair<T1, T2> &pair) const
+	{
+		std::size_t seed{};
+		seed ^= std::hash<T1>{}(pair.first) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		seed ^= std::hash<T2>{}(pair.second) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+		return seed;
+	}
+};
+
 #endif //HASH_FUNCTION_FOR_PAIRS_H
