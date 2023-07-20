a4b07ead7e2ad932c1d937644926a27733746bac
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-20 18:16:34 +0200

    Add some more unit tests.

--- data_structures/source/test/stacks_and_queues/test_asteroids_survival.cpp
+++ data_structures/source/test/stacks_and_queues/test_asteroids_survival.cpp
@@ -5,23 +5,38 @@
 #include "gtest/gtest.h"
 #include "stacks_and_queues/asteroids_survival.h"
 
-TEST(TestAsteroidsSurvival, simple1)
-{
+TEST(TestAsteroidsSurvival, simple1) {
     const std::vector<int> asteroids{5, 10, -5};
     const std::vector<int> expected_results{5, 10};
     EXPECT_EQ(expected_results, survived_asteroids(asteroids));
 }
 
-TEST(TestAsteroidsSurvival, simple2)
-{
+TEST(TestAsteroidsSurvival, simple2) {
     const std::vector<int> asteroids{8, -8};
     const std::vector<int> expected_results{};
     EXPECT_EQ(expected_results, survived_asteroids(asteroids));
 }
 
-TEST(TestAsteroidsSurvival, simple3)
-{
+TEST(TestAsteroidsSurvival, simple3) {
     const std::vector<int> asteroids{10, 2, -5};
     const std::vector<int> expected_results{10};
+    EXPECT_EQ(expected_results, survived_asteroids(asteroids));
+}
+
+TEST(TestAsteroidsSurvival, simple4) {
+    const std::vector<int> asteroids{-2, 5, 5, 5, 5, 5, -6};
+    const std::vector<int> expected_results{-2, -6};
+    EXPECT_EQ(expected_results, survived_asteroids(asteroids));
+}
+
+TEST(TestAsteroidsSurvival, simple5) {
+    const std::vector<int> asteroids{20,10, -15, 12, -9, -21, 17};
+    const std::vector<int> expected_results{-21, 17};
+    EXPECT_EQ(expected_results, survived_asteroids(asteroids));
+}
+
+TEST(TestAsteroidsSurvival, simple6) {
+    const std::vector<int> asteroids{5,10, -8, -10, 2, 3, 8, 9};
+    const std::vector<int> expected_results{5,2,3,8,9};
     EXPECT_EQ(expected_results, survived_asteroids(asteroids));
 }
