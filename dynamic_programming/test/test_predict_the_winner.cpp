8903d03c4f7c9a61d4e9162f3ef2eef57f2196f4
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-07-28 12:23:56 +0200

    Add unit tests.

---
+++ dynamic_programming/test/test_predict_the_winner.cpp
@@ -0,0 +1,36 @@
+//
+// Created by andreas on 28.07.23.
+//
+#include "gtest/gtest.h"
+
+#include "predict_the_winner.h"
+
+TEST(PredictTheWinner, simple1)
+{
+	std::vector<int> input{1, 5, 2};
+	EXPECT_FALSE(predict_the_winner_recursion(input));
+}
+
+TEST(PredictTheWinner, simple2)
+{
+	std::vector<int> input{1, 5, 233, 7};
+	EXPECT_TRUE(predict_the_winner_recursion(input));
+}
+
+TEST(PredictTheWinner, simple3)
+{
+	std::vector<int> input{0, 0, 7, 6, 5, 6, 1};
+	EXPECT_FALSE(predict_the_winner_recursion(input));
+}
+
+TEST(PredictTheWinner, simple4)
+{
+	std::vector<double> input{1.12718};
+	EXPECT_TRUE(predict_the_winner_recursion(input));
+}
+
+TEST(PredictTheWinner, simple5)
+{
+	std::vector<short> input{10, 10, 2, 2, 454, 2, 2, 3};
+	EXPECT_TRUE(predict_the_winner_recursion(input));
+}
