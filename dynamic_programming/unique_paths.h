01b947b2e5b9923e93141a25864416af3e203faf
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-08-13 11:13:55 +0200

    Add unique path top down

---
+++ dynamic_programming/unique_paths.h
@@ -0,0 +1,28 @@
+//
+// Created by andreas on 13.08.23.
+//
+
+#ifndef UNIQUE_PATHS_H
+#define UNIQUE_PATHS_H
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]).
+// The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+// The robot can only move either down or right at any point in time.
+// Given the two integers m and n, return the number of possible unique paths that the robot can
+// take to reach the bottom-right corner.
+//The test cases are generated so that the answer will be less than or equal to 2 * 109.
+#include <vector>
+
+int unique_paths(int rows, int columns)
+{
+	// We initialize with 1 because at least one path exists from each position
+	std::vector<std::vector<int>> dp(rows, std::vector<int>(columns, 1));
+	for (int row{1}; row < rows; ++row) {
+		for (int col{1}; col < columns; ++col) {
+			dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+		}
+	}
+	return dp[rows - 1][columns - 1];
+}
+
+
+#endif //UNIQUE_PATHS_H
