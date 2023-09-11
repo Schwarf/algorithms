2a5177c2ada7c7efdc592775412b32bf651c51bc
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-11 14:37:00 +0200

    Add some more tests.

--- data_structures/source/test/linked_lists/test_deep_copy_linked_list_with_random_pointer.cpp
+++ data_structures/source/test/linked_lists/test_deep_copy_linked_list_with_random_pointer.cpp
@@ -62,3 +62,42 @@ TEST_F(SetupSinglyLinkedLists, deep_copy_with_pointer3)
 		original_head = original_head->next;
 	}
 }
+
+TEST_F(SetupSinglyLinkedLists, deep_copy_with_pointer4)
+{
+	std::vector<int> input{1, 2, 3, 4, 5, 6};
+	std::unordered_map<int, int> from_to{{2, 1}, {1, 2}, {3, 4}};
+	auto original_head = get_single_list_with_random_pointer_from_vector_and_value_pairs(input, from_to);
+	auto result_head = deep_copy_linked_list_with_random_pointer(original_head);
+
+	while (result_head && original_head) {
+		EXPECT_NE(original_head, result_head);
+		EXPECT_EQ(original_head->value, result_head->value);
+		if (original_head->random)
+			EXPECT_EQ(original_head->random->value, result_head->random->value);
+		else
+			EXPECT_EQ(result_head->random, nullptr);
+		result_head = result_head->next;
+		original_head = original_head->next;
+	}
+}
+
+
+TEST_F(SetupSinglyLinkedLists, deep_copy_with_pointer5)
+{
+	std::vector<int> input{1, 2, 3, 4, 5, 6};
+	std::unordered_map<int, int> from_to{{3, 1}, {2, 1}, {4, 1}, {5, 1}};
+	auto original_head = get_single_list_with_random_pointer_from_vector_and_value_pairs(input, from_to);
+	auto result_head = deep_copy_linked_list_with_random_pointer(original_head);
+
+	while (result_head && original_head) {
+		EXPECT_NE(original_head, result_head);
+		EXPECT_EQ(original_head->value, result_head->value);
+		if (original_head->random)
+			EXPECT_EQ(original_head->random->value, result_head->random->value);
+		else
+			EXPECT_EQ(result_head->random, nullptr);
+		result_head = result_head->next;
+		original_head = original_head->next;
+	}
+}
