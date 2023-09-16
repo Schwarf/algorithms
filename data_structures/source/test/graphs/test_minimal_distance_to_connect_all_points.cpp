4a452919b9a640adea9c9672fc038d64a75159de
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-16 16:18:39 +0200

    Add some simple unit tests for version 1 with greedy algo.

--- data_structures/source/test/graphs/test_minimal_distance_to_connect_all_points.cpp
+++ data_structures/source/test/graphs/test_minimal_distance_to_connect_all_points.cpp
@@ -4,18 +4,26 @@
 #include "gtest/gtest.h"
 #include "graphs/minimal_distance_to_connect_all_points.h"
 
-TEST(TestMinimalDistanceToConnectAllPooints, five_points_manhatten_distance) {
-    std::vector<std::vector<int>> input{{0, 0},
-                                        {2, 2},
-                                        {3, 10},
-                                        {5, 2},
-                                        {7, 0}};
-    int expected_result{20};
-    auto distance_function = [](const std::vector<int> &v1, const std::vector<int> &v2) {
-        return std::abs(v1[0] - v2[0]) + std::abs(v1[1] - v2[1]);
-    };
-    auto result =
-            minimal_costs_to_connect_points<int, std::vector<int>, decltype(distance_function)>(input,
-                                                                                                distance_function);
-    EXPECT_EQ(expected_result, result);
+TEST(TestMinimalDistanceToConnectAllPooints, five_points_manhatten_distance)
+{
+	std::vector<std::vector<int>> input{{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}};
+	int expected_result{20};
+	auto result = minimal_costs_to_connect_points<int, std::vector<int>>(input);
+	EXPECT_EQ(expected_result, result);
+}
+
+TEST(TestMinimalDistanceToConnectAllPooints, three_points_manhatten_distance)
+{
+	std::vector<std::vector<int>> input{{3, 12}, {-2, 5}, {-4, 1}};
+	int expected_result{18};
+	auto result = minimal_costs_to_connect_points<int, std::vector<int>>(input);
+	EXPECT_EQ(expected_result, result);
+}
+
+TEST(TestMinimalDistanceToConnectAllPooints, one_point_manhatten_distance)
+{
+	std::vector<std::vector<int>> input{{3, 12}};
+	int expected_result{0};
+	auto result = minimal_costs_to_connect_points<int, std::vector<int>>(input);
+	EXPECT_EQ(expected_result, result);
 }
