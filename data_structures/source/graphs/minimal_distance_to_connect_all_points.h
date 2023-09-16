c21fac6e1ffae77a4f8d8f310cadcc1bd6af0780
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-16 15:31:30 +0200

    Fix algo for minimal distance to connect all points using a greedy approach.

--- data_structures/source/graphs/minimal_distance_to_connect_all_points.h
+++ data_structures/source/graphs/minimal_distance_to_connect_all_points.h
@@ -7,52 +7,37 @@
 #include <vector>
 #include <concepts>
 #include <queue>
-
+#include <limits>
+// You are given an array points representing integer coordinates of some points on a 2D-plane,
+// where points[i] = [xi, yi].
+// The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance between them:
+// |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
+// Return the minimum cost to make all points connected. All points are connected if
+// there is exactly one simple path between any two points.
 
 template<typename T, typename ContainerType, typename DistanceFunctionType>
 requires std::is_integral_v<T> && std::same_as<T, typename ContainerType::value_type> &&
 	std::same_as<T, std::invoke_result_t<DistanceFunctionType, ContainerType, ContainerType>>
 T minimal_costs_to_connect_points(std::vector<std::vector<T>> points, DistanceFunctionType distance_function)
 {
-
-	auto comparator = [](const std::pair<T, T> &p1, const std::pair<T, T> &p2) -> bool
-	{
-		return p1.second > p2.second;
-	};
-
-	std::vector<std::priority_queue<std::pair<T, T>, std::vector<std::pair<T, T>>, bool>>
-		graph(points.size(),
-			  std::priority_queue<std::pair<T, T>, std::vector<std::pair<T, T>>, bool>);
-	std::unordered_map<T, T> id_weight;
-	for (int i{}; i < points.size(); ++i) {
+	std::vector<T> distances(points.size(), std::numeric_limits<T>::max());
+	T answer{};
+	for (int i{}; i < points.size() - 1; ++i) {
 		for (int j{i + 1}; j < points.size(); ++j) {
-			T distance = distance_function(points[i], points[j]);
-			graph[i].push({j, distance});
-			graph[j].push({i, distance});
-		}
-		id_weight[i] = std::numeric_limits<T>::max();
-	}
-
-
-	std::vector<bool> visited(graph.size(), false);
-	int index{};
-	T total_distance{};
-	while (!visited[index]) {
-		visited[index] = true;
-		T distance{};
-		while (true) {
-			if (!visited[graph[index].top().first]) {
-				distance = graph[index].top().second;
-				break;
+			// We find the minimum distances from point i to all points (not yet considered j = i+1...max_index)
+			distances[j] =
+				std::min(distances[j], std::abs(points[i][0] - points[j][0]) + std::abs(points[i][1] - points[j][1]));
+			// the point j that has the minimum distance with point i shall be considered next, so we swap the points
+			// j (the one we want) with one currently coming next (i+1)
+			if (distances[j] < distances[i + 1]) {
+				std::swap(points[j], points[i + 1]);
+				std::swap(distances[j], distances[i + 1]);
 			}
-			else if (graph[index].empty())
-				break;
-			graph[index].pop();
 		}
-		index = graph[index].top().first;
-		total_distance += distance;
+		// we add the distance from i to i+1 to the answer
+		answer += distances[i + 1];
 	}
-	return total_distance;
+	return answer;
 }
 
 
