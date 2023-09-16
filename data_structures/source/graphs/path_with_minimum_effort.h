ea70e2033496e1b3dd0f44d34adfe4d080ac3887
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-16 18:38:19 +0200

    Add Djikstra solution for this problem.

--- data_structures/source/graphs/path_with_minimum_effort.h
+++ data_structures/source/graphs/path_with_minimum_effort.h
@@ -8,6 +8,7 @@
 #include <queue>
 #include <concepts>
 #include <stack>
+#include <limits>
 template<typename T>
 requires std::is_signed_v<T>
 bool bfs_algo_reaches_right_lower_end(const std::vector<std::vector<T>> &heights, T threshold)
@@ -100,5 +101,61 @@ T path_with_minimum_effort(const std::vector<std::vector<T>> &heights, GraphFunc
 
 	return left;
 }
+
+// Using Djikstra algorithm
+template<typename T>
+requires std::is_signed_v<T>
+T path_with_minimum_effort2(std::vector<std::vector<T>> &heights)
+{
+	int rows = heights.size();
+	int cols = heights[0].size();
+
+	// Initialize a 2D vector to store the minimum effort required to reach each cell
+	std::vector<std::vector<T>> minEffort(rows, vector<T>(cols, std::numeric_limits<T>::max()));
+
+	// Priority queue for Dijkstra's algorithm
+	std::priority_queue<std::tuple<T, int, int>,
+						std::vector<std::tuple<T, int, int>>,
+						std::greater<std::tuple<T, int, int>>> pq;
+
+	// Start from the top-left cell
+	pq.push(std::make_tuple(0, 0, 0));
+	minEffort[0][0] = 0;
+
+	// Define possible directions to move (up, down, left, right)
+	std::vector<std::pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+	while (!pq.empty()) {
+		auto [currentEffort, currentRow, currentCol] = pq.top();
+		pq.pop();
+
+		// If we have reached the bottom-right cell, return the minimum effort
+		if (currentRow == rows - 1 && currentCol == cols - 1) {
+			return currentEffort;
+		}
+
+		for (const auto &dir: directions) {
+			int newRow = currentRow + dir.first;
+			int newCol = currentCol + dir.second;
+
+			// Check if the new cell is within bounds
+			if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+				// Calculate the effort to move to the new cell
+				int newEffort = max(currentEffort, abs(heights[newRow][newCol] - heights[currentRow][currentCol]));
+
+				// If the new effort is less than the minimum effort to reach the new cell,
+				// update the minimum effort and add the new cell to the priority queue
+				if (newEffort < minEffort[newRow][newCol]) {
+					minEffort[newRow][newCol] = newEffort;
+					pq.push(std::make_tuple(newEffort, newRow, newCol));
+				}
+			}
+		}
+	}
+
+	// If we reach this point, it means we couldn't reach the bottom-right cell (should not happen in this context)
+	return {};
+}
+
 
 #endif //PATH_WITH_MINIMUM_EFFORT_H
