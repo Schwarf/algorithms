8cff1080bbb24dd73c09b0e5d59c316d5d639b40
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-24 10:32:30 +0200

    Reimplement finding unique path in 2D grid.

--- dynamic_programming/count_ways_2D_grid.h
+++ dynamic_programming/count_ways_2D_grid.h
@@ -8,7 +8,7 @@
 
 // 2D grid. You start from upper left corner and have to reach lower right corner. How many ways are there to reach
 // the lower right corner.
-
+// This is a ridiculous implementation:
 struct pair_hash
 {
 	template<class T1, class T2>
@@ -40,6 +40,29 @@ int count_ways_down_and_right_in_2D_grid(int number_of_rows, int number_of_colum
 	// The termination condition for the recursive call is stored in the unordered_map
 	row_column_number_of_ways[std::make_pair(1, 1)] = 1;
 	return count_ways_down_and_right_in_2D_grid(number_of_rows, number_of_columns, row_column_number_of_ways);
+}
+// This is a ridiculous implementation END
+
+int count_ways_in_2D_grid(int number_of_rows, int number_of_columns)
+{
+	std::vector<std::vector<int>> dp(number_of_rows, std::vector<int>(number_of_columns));
+	dp[0][0] = 1;
+
+	for (int i{0}; i < number_of_rows; ++i) {
+		for (int j{0}; j < number_of_columns; ++j) {
+			if (i == 0 && j == 0)
+				continue;
+			else if (j == 0)
+				dp[i][0] = dp[i - 1][0];
+			else if (i == 0)
+				dp[0][j] = dp[0][j - 1];
+			else
+				dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+
+		}
+	}
+	return dp[number_of_rows - 1][number_of_columns - 1];
+
 }
 
 #endif //COUNT_WAYS_2D_GRID_H
--- dynamic_programming/test/test_count_ways_in_2D_grid.cpp
+++ dynamic_programming/test/test_count_ways_in_2D_grid.cpp
@@ -49,3 +49,38 @@ TEST_F(SetupCountWaysIn2DGrid, test_scenario4)
 	auto result4 = count_ways_down_and_right_in_2D_grid(number_of_rows4, number_of_columns4);
 	EXPECT_EQ(result4, ways4);
 }
+
+
+TEST_F(SetupCountWaysIn2DGrid, dynamic_programming1)
+{
+	auto result1 = count_ways_in_2D_grid(number_of_rows1, number_of_columns1);
+	EXPECT_EQ(result1, ways1);
+}
+
+TEST_F(SetupCountWaysIn2DGrid, dynamic_programming2)
+{
+	auto result2 = count_ways_in_2D_grid(number_of_rows2, number_of_columns2);
+	EXPECT_EQ(result2, ways2);
+}
+
+TEST_F(SetupCountWaysIn2DGrid, dynamic_programming3)
+{
+	auto result3 = count_ways_in_2D_grid(number_of_rows3, number_of_columns3);
+	EXPECT_EQ(result3, ways3);
+}
+
+TEST_F(SetupCountWaysIn2DGrid, dynamic_programming4)
+{
+	auto result4 = count_ways_in_2D_grid(number_of_rows4, number_of_columns4);
+	EXPECT_EQ(result4, ways4);
+}
+
+TEST_F(SetupCountWaysIn2DGrid, dynamic_programming5)
+{
+	constexpr int number_of_rows{10};
+	constexpr int number_of_columns{34};
+	constexpr int expected_result{445891810};
+	EXPECT_EQ(count_ways_in_2D_grid(number_of_rows, number_of_columns), expected_result);
+	EXPECT_EQ(count_ways_in_2D_grid(number_of_columns, number_of_rows), expected_result);
+}
+
