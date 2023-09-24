6c3305ac97cdb089b09d054533db8d798b004df2
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-24 09:59:39 +0200

    Renaming.

--- dynamic_programming/can_reach_last_index.h
+++ dynamic_programming/can_reach_last_index.h
@@ -1,21 +1,20 @@
 // Given an array of non-negative integers, you are initially positioned at the first index of the array.
 // Each element in the array represents your maximum jump length at that position.
-// Your goal is to reach the last index in the minimum number of jumps.
-// You can assume that you can always reach the last index.
+// Can the last index be reached from the 0-index?
 
 #include <iostream>
 #include <vector>
 
 bool can_reach_last_index(const std::vector<int> &jump_distances)
 {
-	int back = jump_distances.size() - 1;
-	if (back == 0)
+	int last_index = jump_distances.size() - 1;
+	if (last_index == 0)
 		return true;
-	if (jump_distances[0] == back)
+	if (jump_distances[0] == last_index)
 		return true;
 	if (jump_distances[0] == 0)
 		return false;
-	int current_position = back;
+	int current_position = last_index;
 	for (int i{current_position - 1}; i > -1; --i) {
 		if (i + jump_distances[i] >= current_position)
 			current_position = i;
