2a5ed83528a18781ce0fc045d9a6658ee1c6bdb9
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-26 10:37:54 +0200

    Run first test and fix some CMake issues.

--- fun_with_strings/test/test_remove_duplicates.cpp
+++ fun_with_strings/test/test_remove_duplicates.cpp
@@ -1,3 +1,11 @@
 //
 // Created by andreas on 26.09.23.
 //
+#include "gtest/gtest.h"
+#include "./../remove_duplicate_letters.h"
+TEST(TestRemoveDuplicates, simple1)
+{
+	const std::string input{"bcabc"};
+	const std::string expected_output{"abc"};
+	EXPECT_EQ(remove_duplicate_letters(input), expected_output);
+}
