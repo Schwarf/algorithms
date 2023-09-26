e79af5356f6e993550cb101e2e1aabe5eb5479a6
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-26 10:41:30 +0200

    Add more tests.

--- fun_with_strings/test/test_remove_duplicates.cpp
+++ fun_with_strings/test/test_remove_duplicates.cpp
@@ -3,9 +3,33 @@
 //
 #include "gtest/gtest.h"
 #include "./../remove_duplicate_letters.h"
+
 TEST(TestRemoveDuplicates, simple1)
 {
 	const std::string input{"bcabc"};
 	const std::string expected_output{"abc"};
+	EXPECT_EQ(remove_duplicate_letters(input), expected_output);
+}
+
+TEST(TestRemoveDuplicates, simple2)
+{
+	const std::string input{"cbacdcbc"};
+	const std::string expected_output{"acdb"};
+	EXPECT_EQ(remove_duplicate_letters(input), expected_output);
+}
+
+TEST(TestRemoveDuplicates, medium1)
+{
+	const std::string input{"thesqtitxyetpxloeevdeqifkz"};
+	const std::string expected_output{"hesitxyplovdqfkz"};
+	EXPECT_EQ(remove_duplicate_letters(input), expected_output);
+}
+
+
+TEST(TestRemoveDuplicates, complex1)
+{
+	const std::string input
+		{"peymrzknlxtrutjiybqemquchgvtmmtpjvunvekszrkatctcirxwuqknrycpdtcuadblzkkleduezgspoxhhssoipbmdgrqggpfdsanolzczpaggwxrlaleaqtnzxclmxwjucnujsptnbmmjzzjhypnlsoxjveywsufegzlfnyvkcnfevkshbckfropoydkdlblppllgefagjgpajsplvxknvtlgtjyhmnwxcpjjzcizihycvsnhnnmqohivekitxzuo"};
+	const std::string expected_output{"abcefghkrdjlmnwpiysqovtxzu"};
 	EXPECT_EQ(remove_duplicate_letters(input), expected_output);
 }
