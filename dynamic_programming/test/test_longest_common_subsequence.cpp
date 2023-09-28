2996f140ab6451c3a61aac8e572274b11bfa650d
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-28 12:09:44 +0200

    Add some simple tests for longest common subsequence.

---
+++ dynamic_programming/test/test_longest_common_subsequence.cpp
@@ -0,0 +1,29 @@
+//
+// Created by andreas on 28.09.23.
+//
+#include "gtest/gtest.h"
+#include "./../longest_common_subsequence.h"
+
+TEST(TestLongesCommonSqubSequence, simple1)
+{
+	const std::string input1{"ABCBDAB"};
+	const std::string input2{"BDCAB"};
+	constexpr int expected_result{4};
+	EXPECT_EQ(expected_result, longest_common_sub_sequence(input1, input2));
+}
+
+TEST(TestLongesCommonSqubSequence, simple2)
+{
+	const std::string input1{"ABC"};
+	const std::string input2{"BC"};
+	constexpr int expected_result{2};
+	EXPECT_EQ(expected_result, longest_common_sub_sequence(input1, input2));
+}
+
+TEST(TestLongesCommonSqubSequence, simple3)
+{
+	const std::string input1{"CBA"};
+	const std::string input2{"CDA"};
+	constexpr int expected_result{2};
+	EXPECT_EQ(expected_result, longest_common_sub_sequence(input1, input2));
+}
