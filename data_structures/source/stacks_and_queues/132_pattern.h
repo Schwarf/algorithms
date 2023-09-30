d440d91b41acbd4054d9c0a20d91ea6f5363a131
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-30 09:52:01 +0200

    Add 132 pattern.

---
+++ data_structures/source/stacks_and_queues/132_pattern.h
@@ -0,0 +1,31 @@
+//
+// Created by andreas on 30.09.23.
+//
+
+#ifndef INC_132_PATTERN_H
+#define INC_132_PATTERN_H
+#include <concepts>
+#include <vector>
+#include <stack>
+// Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k]
+// such that i < j < k and nums[i] < nums[k] < nums[j].
+// Return true if there is a 132 pattern in nums, otherwise, return false.
+template<typename T>
+requires std::is_signed_v<T>
+bool find132pattern(std::vector<T> &nums)
+{
+	std::stack<T> s;
+	int third = std::numeric_limits<T>::min();
+
+	for (int i = nums.size() - 1; i >= 0; i--) {
+		if (nums[i] < third) return true;
+		while (!s.empty() && s.top() < nums[i]) {
+			third = s.top();
+			s.pop();
+		}
+		s.push(nums[i]);
+	}
+	return false;
+}
+
+#endif //INC_132_PATTERN_H
