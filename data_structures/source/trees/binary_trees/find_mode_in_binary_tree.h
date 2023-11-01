8458193218cd939e5be33380bcc47993050f8865
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-11-01 12:47:39 +0100

    Add iterative DFS to find mode in BST.

--- data_structures/source/trees/binary_trees/find_mode_in_binary_tree.h
+++ data_structures/source/trees/binary_trees/find_mode_in_binary_tree.h
@@ -9,6 +9,8 @@
 #include <type_traits>
 #include <unordered_map>
 #include <vector>
+#include <stack>
+#include <queue>
 #include "tree_node.h"
 template<typename T>
 requires std::is_arithmetic_v<T>
@@ -39,5 +41,35 @@ std::vector<T> find_mode_dfs_recursive(TreeNode<T> *root)
 	return result;
 }
 
+template<typename T>
+requires std::is_arithmetic_v<T>
+std::vector<T> find_mode_dfs_iterative(TreeNode<T> *root)
+{
+	std::unordered_map<T, int> count;
+	std::stack<TreeNode<T> *> s{{root}};
+
+	while (!s.empty()) {
+		auto current = s.top();
+		s.pop();
+		if (current->left)
+			s.push(current->left);
+		if (current->right)
+			s.push(current->right);
+		count[current->value]++;
+	}
+
+	int max{};
+	for (const auto &[value, frequency]: count) {
+		max = std::max(max, frequency);
+	}
+	std::vector<T> result;
+	for (const auto &[value, frequency]: count) {
+		if (frequency == max)
+			result.push_back(value);
+	}
+	return result;
+}
+
 
 #endif //FIND_MODE_IN_BINARY_TREE_H
+
