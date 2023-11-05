3acde4316569c7def3b4b883f39680d909d4c824
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-11-05 17:30:12 +0100

    Add tests for find all subarray with product less than value.

--- fun_with_algos/subarray_product_less_than.h
+++ fun_with_algos/subarray_product_less_than.h
@@ -5,17 +5,19 @@
 #ifndef SUBARRAY_PRODUCT_LESS_THAN_H
 #define SUBARRAY_PRODUCT_LESS_THAN_H
 // Given an array 'input' and a value 'max',
-// return the number of contiguous subarrays where the product of all the elements in the subarray is
+// return the number of contiguous sub-arrays where the product of all the elements in the subarray is
 // strictly less than 'max'.
 #include <vector>
 #include <concepts>
-template<typename T, typename std::is_arithmetic<T>>
-int number_of_subarrays_less_than(const std::vector<T> &input, T max_value)
+template<typename T, typename = typename std::enable_if<std::is_integral_v<T>>>
+T number_of_subarrays_less_than(const std::vector<T> &input, const T max_value)
 {
-	int left{};
-	T product{};
-	int result{};
-	for (int right{}; right < input.size(); ++right) {
+	if(max_value < 2)
+		return 0;
+	T left{};
+	T product{1};
+	T result{};
+	for (T right{}; right < input.size(); ++right) {
 		product *= input[right];
 		while (product >= max_value)
 			product /= input[left++];
