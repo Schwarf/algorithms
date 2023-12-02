601b9e168813501795f3897a74a80aafc6b60f90
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-12-02 12:17:02 +0100

    Add some tests.

---
+++ fun_with_strings/test/test_count_beautiful_substrings.cpp
@@ -0,0 +1,38 @@
+//
+// Created by andreas on 02.12.23.
+//
+#include "gtest/gtest.h"
+#include "../count_beautiful_substrings.h"
+
+TEST(CountBeautifulSubStrings, simple1)
+{
+	const std::string input{"baeyh"};
+	constexpr int k{2};
+	constexpr int expected_result{2};
+	EXPECT_EQ(expected_result, count_beautiful_substrings(input, k));
+}
+
+TEST(CountBeautifulSubStrings, simple2)
+{
+	const std::string input{"abba"};
+	constexpr int k{1};
+	constexpr int expected_result{3};
+	EXPECT_EQ(expected_result, count_beautiful_substrings(input, k));
+}
+
+TEST(CountBeautifulSubStrings, simple3)
+{
+	const std::string input{"bcdf"};
+	constexpr int k{1};
+	constexpr int expected_result{0};
+	EXPECT_EQ(expected_result, count_beautiful_substrings(input, k));
+}
+
+TEST(CountBeautifulSubStrings, complex1)
+{
+	const std::string input
+		{"auooubignuoyoaiyoetrezuuucuocemaeefldjqwoioizoyoputaaixrasvuseichoikpsjgeevbwebeoxuejagyoxiuqhgmoapiketuzjnmaaoamceejuujguqafquzaqwhnuiuiofuebaiiwiavetqiiearkuoovukwouauuhipauiguaadojcaaeiicbwaozouihusgzalacbiqbffsdeousaecsvqudjouixoojettoiizaaumeforosgiiiewiouuoouoaqfooyhgacawpeeeowpouhieogstoghvutljmuuvkeenliyyueaeeiooiwhiesamwaowmoehhshedslidnosbnojjrmdoijtaebavgkaizeaeeolcgooyapaofeiuugfzoeuwydouiuoqnaexruhzoyobhujwupoiipaaqwokioooaodfufouuoexcobliunbirebbxqlsuevtntdreooeueedfraqlcoreeilqoeuyczxnadiejvehieiebiykkvpiijeozignatgevuauiihioorqrnlhmzmyoiasiuajlrerinuuxoudbweeoiaajibtikko"};
+	constexpr int k{135};
+	constexpr int expected_result{112};
+	EXPECT_EQ(expected_result, count_beautiful_substrings(input, k));
+}
