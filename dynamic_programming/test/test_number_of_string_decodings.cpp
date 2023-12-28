77cd3f88f083bec48f6a70fcd161b4b28d354ea5
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-12-28 14:46:23 +0100

    Add some first tests.

---
+++ dynamic_programming/test/test_number_of_string_decodings.cpp
@@ -0,0 +1,34 @@
+//
+// Created by andreas on 28.12.23.
+//
+
+#include "gtest/gtest.h"
+#include "../number_of_string_decodings.h"
+
+TEST(TestNumberOfStringDecodings, simple1)
+{
+	const std::string input{"12"};
+	constexpr int expected_result{2};
+	EXPECT_EQ(expected_result, number_of_decodings_bottom_up(input));
+}
+
+TEST(TestNumberOfStringDecodings, simple2)
+{
+	const std::string input{"226"};
+	constexpr int expected_result{3};
+	EXPECT_EQ(expected_result, number_of_decodings_bottom_up(input));
+}
+
+TEST(TestNumberOfStringDecodings, simple3)
+{
+	const std::string input{"06"};
+	constexpr int expected_result{0};
+	EXPECT_EQ(expected_result, number_of_decodings_bottom_up(input));
+}
+
+TEST(TestNumberOfStringDecodings, simple4)
+{
+	const std::string input{"1372189472892"};
+	constexpr int expected_result{6};
+	EXPECT_EQ(expected_result, number_of_decodings_bottom_up(input));
+}
