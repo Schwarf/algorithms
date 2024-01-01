2b22fc3f6c56733acfaf04337e83cc516381bc9f
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-01 18:42:08 +0100

    WIP

--- data_structures/source/test/caches/test_lru_cache.cpp
+++ data_structures/source/test/caches/test_lru_cache.cpp
@@ -9,8 +9,25 @@ TEST(TestLRUCache, test_constructor_capacity)
 {
 	LRUCache<std::string, int> cache(1);
 	cache.put("1", 1);
-	EXPECT_EQ(cache.get("1"), 1);
+	EXPECT_EQ(cache.get("1").value(), 1);
 	cache.put("2", 2);
 	EXPECT_EQ(cache.get("1"), std::nullopt);
-	EXPECT_EQ(cache.get("2"), 2);
+	EXPECT_EQ(cache.get("2").value(), 2);
+}
+
+TEST(TestLRUCache, test_put)
+{
+	LRUCache<int, int> cache(3);
+	cache.put(1, -1);
+	cache.put(2, -2);
+	cache.put(3, -3);
+	auto return_value = cache.get(1);
+	EXPECT_TRUE(return_value.has_value());
+	EXPECT_EQ(return_value.value(), -1);
+	return_value = cache.get(2);
+	EXPECT_TRUE(return_value.has_value());
+	EXPECT_EQ(return_value.value(), -2);
+	return_value = cache.get(3);
+	EXPECT_TRUE(return_value.has_value());
+	EXPECT_EQ(return_value.value(), -3);
 }
