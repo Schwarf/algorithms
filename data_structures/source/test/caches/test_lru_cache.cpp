f8e766ac2c5561fce2e2125afed48b10607657fa
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-02 16:43:15 +0100

    Add additional test for eviction.

--- data_structures/source/test/caches/test_lru_cache.cpp
+++ data_structures/source/test/caches/test_lru_cache.cpp
@@ -5,7 +5,7 @@
 #include "caches/lru_cache.h"
 
 
-TEST(TestLRUCache, test_constructor_capacity)
+TEST(TestLRUCache, test_eviction_policy1)
 {
 	LRUCache<std::string, int> cache(1);
 	cache.put("1", 1);
@@ -14,6 +14,32 @@ TEST(TestLRUCache, test_constructor_capacity)
 	EXPECT_EQ(cache.get("1"), std::nullopt);
 	EXPECT_EQ(cache.get("2").value(), 2);
 }
+
+TEST(TestLRUCache, test_eviction_policy2)
+{
+	LRUCache<int, int> cache(5);
+	constexpr int value_conversion_factor{100};
+	std::vector<int> keys1{1, 2, 3, 4, 5};
+	for (const auto &key: keys1) {
+		cache.put(key, key * value_conversion_factor);
+	}
+	std::vector<int> getKeys{1, 1, 2, 3, 1, 2, 4, 5};
+	for (const auto &key: getKeys) {
+		EXPECT_EQ(cache.get(key), key * value_conversion_factor);
+	}
+	std::vector<int> keys2{6, 7, 8, 9, 10};
+	cache.put(keys2[0], keys2[0] * value_conversion_factor);
+	EXPECT_FALSE(cache.get(3).has_value());
+	cache.put(keys2[1], keys2[1] * value_conversion_factor);
+	EXPECT_FALSE(cache.get(1).has_value());
+	cache.put(keys2[2], keys2[2] * value_conversion_factor);
+	EXPECT_FALSE(cache.get(2).has_value());
+	cache.put(keys2[3], keys2[3] * value_conversion_factor);
+	EXPECT_FALSE(cache.get(4).has_value());
+	cache.put(keys2[4], keys2[4] * value_conversion_factor);
+	EXPECT_FALSE(cache.get(5).has_value());
+}
+
 
 TEST(TestLRUCache, test_put)
 {
