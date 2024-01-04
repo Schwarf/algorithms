6f053666fee0e9b41a1a78f46a8f9d815286ea2a
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-04 19:00:24 +0100

    Add Prim's algorithm.

---
+++ data_structures/source/graphs/minimum_spanning_tree_algorithms.h
@@ -0,0 +1,54 @@
+//
+// Created by andreas on 04.01.24.
+//
+
+#ifndef MINIMUM_SPANNING_TREE_ALGORITHMS_H
+#define MINIMUM_SPANNING_TREE_ALGORITHMS_H
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <tuple>
+
+
+template<typename VertexType, typename DistanceType>
+requires std::is_unsigned_v<VertexType> && std::is_arithmetic_v<DistanceType>
+std::tuple<std::vector<VertexType>, std::vector<DistanceType>> minimum_spanning_tree_prim(VertexType start_vertex,
+																						  const std::vector<std::vector<
+																							  std::pair<VertexType,
+																										DistanceType>>> &graph)
+{
+	if (start_vertex < graph.size())
+		throw std::out_of_range("The start_vertex id is large than the graph size.");
+
+	auto number_of_vertices = graph.size();
+	std::vector<DistanceType> weights(number_of_vertices, std::numeric_limits<DistanceType>::max());
+	std::vector<VertexType> spanning_tree_parents(number_of_vertices, std::numeric_limits<VertexType>::max());
+	std::vector<bool> visited_vertices(number_of_vertices, false);
+	std::priority_queue<std::pair<DistanceType, VertexType>,
+						std::vector<std::pair<DistanceType, VertexType>>,
+						std::greater<>> pq;
+	pq.push(std::make_pair({static_cast<DistanceType>(0), start_vertex}));
+
+	weights[start_vertex] = static_cast<DistanceType>(0);
+	while (!pq.empty()) {
+		VertexType current_vertex = pq.top().second;
+		pq.pop();
+		if (visited_vertices[current_vertex])
+			continue;
+		visited_vertices[current_vertex] = true;
+		for (const auto &edge: graph[current_vertex]) {
+			auto next_vertex = edge.first;
+			auto weight = edge.second;
+			if (!visited_vertices[next_vertex] && weights[next_vertex] > weight) {
+				weights[next_vertex] = weight;
+				pq.push({weight, next_vertex});
+				spanning_tree_parents[next_vertex] = current_vertex;
+
+			}
+		}
+	}
+	return std::make_tuple(spanning_tree_parents, weights);
+
+}
+#endif //MINIMUM_SPANNING_TREE_ALGORITHMS_H
