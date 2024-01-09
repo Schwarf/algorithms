07973893242aad39110ef1fb3314b3ee56cf6a26
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-09 19:13:16 +0100

    Finish tests.

--- dynamic_programming/test/test_count_unique_binary_search_trees.cpp
+++ dynamic_programming/test/test_count_unique_binary_search_trees.cpp
@@ -31,3 +31,59 @@ TEST(TestCountUniqueBinarySearchTrees, nineteen_recursive)
 	constexpr int expected_result{1767263190};
 	EXPECT_EQ(expected_result, count_unique_BST_recursive(n));
 }
+
+TEST(TestCountUniqueBinarySearchTrees, one_top_down)
+{
+	constexpr int n{1};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, count_unique_BST_top_down(n));
+}
+
+TEST(TestCountUniqueBinarySearchTrees, three_top_down)
+{
+	constexpr int n{3};
+	constexpr int expected_result{5};
+	EXPECT_EQ(expected_result, count_unique_BST_top_down(n));
+}
+
+TEST(TestCountUniqueBinarySearchTrees, ten_top_down)
+{
+	constexpr int n{10};
+	constexpr int expected_result{16796};
+	EXPECT_EQ(expected_result, count_unique_BST_top_down(n));
+}
+
+TEST(TestCountUniqueBinarySearchTrees, nineteen_top_down)
+{
+	constexpr int n{19};
+	constexpr int expected_result{1767263190};
+	EXPECT_EQ(expected_result, count_unique_BST_top_down(n));
+}
+
+TEST(TestCountUniqueBinarySearchTrees, one_bottom_up)
+{
+	constexpr int n{1};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, count_unique_BST_bottom_up(n));
+}
+
+TEST(TestCountUniqueBinarySearchTrees, three_bottom_up)
+{
+	constexpr int n{3};
+	constexpr int expected_result{5};
+	EXPECT_EQ(expected_result, count_unique_BST_bottom_up(n));
+}
+
+TEST(TestCountUniqueBinarySearchTrees, ten_bottom_up)
+{
+	constexpr int n{10};
+	constexpr int expected_result{16796};
+	EXPECT_EQ(expected_result, count_unique_BST_bottom_up(n));
+}
+
+TEST(TestCountUniqueBinarySearchTrees, nineteen_bottom_up)
+{
+	constexpr int n{19};
+	constexpr int expected_result{1767263190};
+	EXPECT_EQ(expected_result, count_unique_BST_bottom_up(n));
+}
