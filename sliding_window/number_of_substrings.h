d1f8880a9f72729bcc6c060535eb77708a95294a
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-12 17:41:41 +0100

    Add first sliding window algorithm with tests.

---
+++ sliding_window/number_of_substrings.h
@@ -0,0 +1,32 @@
+//
+// Created by andreas on 12.01.24.
+//
+
+#ifndef NUMBER_OF_SUBSTRINGS_H
+#define NUMBER_OF_SUBSTRINGS_H
+// Given a string s consisting only of characters a, b and c.
+// Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+#include <string>
+#include <unordered_map>
+int number_of_substrings(const std::string &s)
+{
+	int n = s.size();
+	std::unordered_map<char, int> frequency;
+	int counter{};
+	int left{};
+	int right{};
+
+	while (right < n) {
+		frequency[s[right]]++;
+		while (frequency['a'] > 0 && frequency['b'] > 0 && frequency['c'] > 0) {
+			counter += (n - right);
+			frequency[s[left]]--;
+			left++;
+
+		}
+		right++;
+	}
+	return counter;
+}
+
+#endif //NUMBER_OF_SUBSTRINGS_H
