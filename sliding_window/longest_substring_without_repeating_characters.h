c18f78b4fbb5337b53d94c096c912d4463f12ceb
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-12 18:36:43 +0100

    Add some tests.

--- sliding_window/longest_substring_without_repeating_characters.h
+++ sliding_window/longest_substring_without_repeating_characters.h
@@ -4,5 +4,31 @@
 
 #ifndef LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS_H
 #define LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS_H
+#include <string>
+#include <unordered_set>
+int longest_substring_without_repeating_character(const std::string & s)
+{
+	if(s.empty())
+		return 0;
 
+	std::unordered_set<char> help;
+	int left = 0;
+	int max_length = 0;
+
+	for (int right = 0; right < s.size(); ++right) {
+		// If s[right] is in the set, remove characters from the set until it's removed
+		while(help.find(s[right]) != help.end()) {
+			help.erase(s[left]);
+			left++;
+		}
+		// Add the current character to the set
+		help.insert(s[right]);
+
+		// Update the maximum length
+		max_length = std::max(max_length, right - left + 1);
+	}
+
+	return max_length;
+
+}
 #endif //LONGEST_SUBSTRING_WITHOUT_REPEATING_CHARACTERS_H
---
+++ sliding_window/test/test_longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,41 @@
+//
+// Created by andreas on 12.01.24.
+//
+
+#include "gtest/gtest.h"
+#include "longest_substring_without_repeating_characters.h"
+
+TEST(TestLongestSubstringWithoutRepeatingCharacters, simple1)
+{
+	const std::string input{"acabnhjkbahb"};
+	constexpr int expected_result{6};
+	EXPECT_EQ(expected_result, longest_substring_without_repeating_character(input));
+}
+
+TEST(TestLongestSubstringWithoutRepeatingCharacters, simple2)
+{
+const std::string input{"bbbbb"};
+constexpr int expected_result{1};
+EXPECT_EQ(expected_result, longest_substring_without_repeating_character(input));
+}
+
+TEST(TestLongestSubstringWithoutRepeatingCharacters, simple3)
+{
+	const std::string input{""};
+	constexpr int expected_result{0};
+	EXPECT_EQ(expected_result, longest_substring_without_repeating_character(input));
+}
+
+TEST(TestLongestSubstringWithoutRepeatingCharacters, simple4)
+{
+	const std::string input{"pwwkew"};
+	constexpr int expected_result{3};
+	EXPECT_EQ(expected_result, longest_substring_without_repeating_character(input));
+}
+
+TEST(TestLongestSubstringWithoutRepeatingCharacters, simple5)
+{
+	const std::string input{"aab"};
+	constexpr int expected_result{2};
+	EXPECT_EQ(expected_result, longest_substring_without_repeating_character(input));
+}
