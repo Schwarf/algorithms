d914bdfb7735ac788232d8598d5d6299f2eeb23e
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-14 18:45:52 +0100

    Add Sedgewick sequence for shellsort and tests.

--- sorting_algorithms/shell_sort.h
+++ sorting_algorithms/shell_sort.h
@@ -32,6 +32,40 @@ std::vector<size_t> simple_gaps(size_t container_size)
 	return result;
 }
 
+constexpr std::size_t constexpr_pow(std::size_t base, std::size_t exponent) {
+	std::size_t result = 1;
+	while (exponent > 0) {
+		if (exponent % 2 == 1) {
+			result *= base;
+		}
+		exponent /= 2;
+		base *= base;
+	}
+	return result;
+}
+
+constexpr std::size_t calculate_sedgewick_element(std::size_t i) {
+	if (i % 2 == 0) {
+
+		return 9 * constexpr_pow(4, i) - 9 * constexpr_pow(2, i) + 1;
+	} else {
+		// For odd i: 8 * 4^i - 6 * 2^i + 1
+		return 8 * constexpr_pow(4, i) - 6 * constexpr_pow(2, i) + 1;
+	}
+}
+
+
+
+std::vector<size_t> sedgewick_sequence(size_t container_size) {
+	std::vector<size_t> sequence{1};
+	while(sequence.back() < container_size) {
+		sequence.push_back(calculate_sedgewick_element(sequence.back()));
+	}
+	std::reverse(sequence.begin(), sequence.end());
+	return sequence;
+}
+
+
 
 template <typename InputType, typename SequenceFunction, template <typename ...>  class ContainerType>
 requires IndexedContainer<ContainerType<InputType>> && SequenceFunctionTemplate<SequenceFunction>
--- sorting_algorithms/test/test_shell_sort.cpp
+++ sorting_algorithms/test/test_shell_sort.cpp
@@ -5,7 +5,7 @@
 #include "shell_sort.h"
 #include <algorithm>
 
-TEST_F(SetupSortingTests, ShellSort1)
+TEST_F(SetupSortingTests, ShellSortSimpleSequence1)
 {
 	auto input = get_N_random_number_vector(0, 1000, 100);
 	auto expected_output{input};
@@ -14,7 +14,7 @@ TEST_F(SetupSortingTests, ShellSort1)
 	EXPECT_EQ(input, expected_output);
 }
 
-TEST_F(SetupSortingTests, ShellSort2)
+TEST_F(SetupSortingTests, ShellSortSimpleSequence2)
 {
 	auto input = get_N_random_number_vector(0, 10000, 1000);
 	auto expected_output{input};
@@ -23,7 +23,7 @@ TEST_F(SetupSortingTests, ShellSort2)
 	EXPECT_EQ(input, expected_output);
 }
 
-TEST_F(SetupSortingTests, ShellSort3)
+TEST_F(SetupSortingTests, ShellSortSimpleSequence3)
 {
 	auto input = get_N_random_number_vector(-1000, 1000, 523);
 	auto expected_output{input};
@@ -32,7 +32,43 @@ TEST_F(SetupSortingTests, ShellSort3)
 	EXPECT_EQ(input, expected_output);
 }
 
-TEST_F(SetupSortingTests, ShellSort4)
+TEST_F(SetupSortingTests, ShellSortSimpleSequence4)
+{
+	auto input = get_N_random_number_vector(-1000, 1000, 5689);
+	auto expected_output{input};
+	std::sort(expected_output.begin(), expected_output.end());
+	shell_sort(input, simple_gaps);
+	EXPECT_EQ(input, expected_output);
+}
+
+TEST_F(SetupSortingTests, ShellSortSedgewickSequence1)
+{
+	auto input = get_N_random_number_vector(0, 1000, 100);
+	auto expected_output{input};
+	std::sort(expected_output.begin(), expected_output.end());
+	shell_sort(input, sedgewick_sequence);
+	EXPECT_EQ(input, expected_output);
+}
+
+TEST_F(SetupSortingTests, ShellSortSedgewickSequence2)
+{
+	auto input = get_N_random_number_vector(0, 10000, 1000);
+	auto expected_output{input};
+	std::sort(expected_output.begin(), expected_output.end());
+	shell_sort(input, simple_gaps);
+	EXPECT_EQ(input, expected_output);
+}
+
+TEST_F(SetupSortingTests, ShellSortSedgewickSequence3)
+{
+	auto input = get_N_random_number_vector(-1000, 1000, 523);
+	auto expected_output{input};
+	std::sort(expected_output.begin(), expected_output.end());
+	shell_sort(input, simple_gaps);
+	EXPECT_EQ(input, expected_output);
+}
+
+TEST_F(SetupSortingTests, ShellSortSedgewickSequence4)
 {
 	auto input = get_N_random_number_vector(-1000, 1000, 5689);
 	auto expected_output{input};
