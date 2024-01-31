27cb0b0b9450b2192494bd371e145979578dcb0c
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-01-31 15:03:03 +0100

    Some cases covered by tests.

--- fun_with_algos/interval_map.h
+++ fun_with_algos/interval_map.h
@@ -5,61 +5,95 @@
 #ifndef INTERVAL_MAP_H
 #define INTERVAL_MAP_H
 #include <map>
+#include <iterator>
+#include <utility>
 template <typename KeyType, typename ValueType>
 class IntervalMap{
-private:
-	std::map<KeyType, ValueType> interval_map_;
-public:
-	IntervalMap() = default;
 
+public:
+	std::map<KeyType, ValueType> interval_map_;
+	IntervalMap()
+	{
+		//interval_map_.insert(interval_map_.end(), std::make_pair<KeyType,ValueType >(std::numeric_limits<KeyType>::min(), ValueType{}));
+	};
+
+	ValueType const & operator[](const KeyType & key) const
+	{
+		return (--interval_map_.upper_bound(key))->second;
+	}
 	void add(const KeyType & intervalBegin, const KeyType & intervalEnd, const ValueType & mappedValue)
 	{
 		// invalid interval
 		if(intervalEnd <= intervalBegin)
 			return;
 
-		auto greaterEqualIntervalBeginIterator = interval_map_.lower_bound(intervalBegin);
-		// Add new interval
-		if(greaterEqualIntervalBeginIterator != interval_map_.begin())
-		{
-			auto previousIterator = std::prev(greaterEqualIntervalBeginIterator);
-			if((*previousIterator).first < intervalBegin) {
-				interval_map_.insert(previousIterator, std::make_pair(intervalBegin, std::prev(previousIterator)->second));
-				interval_map_[intervalEnd] = previousIterator->second;
-			}
-		}
+		auto begin = interval_map_.find(intervalBegin);
+		auto end = interval_map_.find(intervalEnd);
 
-		// Merge intervals
-		auto currentIterator = greaterEqualIntervalBeginIterator;
-		while(currentIterator != interval_map_.end() && (*currentIterator).first < intervalEnd)
+		// Found both 'begin' and 'end' or endInterval is beyond last element
+		if(begin != interval_map_.end() && (end != interval_map_.end() || intervalEnd > std::prev(interval_map_.end())->first))
 		{
-			// Case: Current interval has same mappedValue as incoming mappedValue
-			if(currentIterator->second == mappedValue)
+			std::cout << intervalEnd <<  ", " <<std::prev(interval_map_.end())->first << std::endl;
+			// Overwrite all intervals that are between intervalBegin and intervalEnd
+			while(begin != end)
 			{
-				currentIterator++;
+				interval_map_.erase(begin++);
+			}
+			interval_map_[intervalBegin] = mappedValue;
+		}
+		// Found 'begin' but not 'end', but 'end' is below the highest stored value
+		else if(begin != interval_map_.end() && end == interval_map_.end())
+		{
+			auto oldValue = interval_map_[intervalBegin];
+			while(begin->first < intervalEnd)
+			{
+				interval_map_.erase(begin++);
+				if(begin->first < intervalEnd)
+					oldValue = begin->second;
+			}
+			interval_map_[intervalEnd] = oldValue;
+			interval_map_[intervalBegin] = mappedValue;
+
+		}
+		// Found 'end' but not 'begin'
+		else if(begin == interval_map_.end() && end != interval_map_.end())
+		{
+			begin = interval_map_.lower_bound(intervalBegin);
+			interval_map_[intervalBegin] = mappedValue;
+			while(begin != end)
+			{
+				interval_map_.erase(begin++);
+			}
+
+		}
+		// container empty
+		else if (interval_map_.empty()) {
+			interval_map_[intervalBegin] = mappedValue;
+		}
+		// both are not found
+		else {
+			begin = interval_map_.lower_bound(intervalBegin);
+			end = interval_map_.lower_bound(intervalEnd);
+
+			if (begin == end) {
+				interval_map_[intervalEnd] = std::prev(begin)->second;
+				interval_map_[intervalBegin] = mappedValue;
 			}
 			else
-			{   // Given  begin = 3, end = 8 and [1,3,5,7,9] --> need to erase 5 and 7
-				// we start at 3
-				auto nextIterator = std::next(currentIterator);
-				if(nextIterator != interval_map_.end() && nextIterator->first <= intervalEnd)
+			{
+				interval_map_[intervalEnd] = std::prev(end)->second;
+				interval_map_[intervalBegin] = mappedValue;
+				while(begin != end)
 				{
-					interval_map_.erase(currentIterator);
-					currentIterator = nextIterator;
-				}
-				else if(currentIterator->first < intervalEnd)
-				{
-					interval_map_[intervalEnd] = currentIterator->second;
+					interval_map_.erase(begin++);
 				}
 			}
+
 		}
-		interval_map_[intervalBegin] = mappedValue;
+
 	}
 
-	ValueType const & operator[](const KeyType & key) const
-	{
-		return (--interval_map_.upper_bound(key))->second;
-	}
+
 };
 
 #endif //INTERVAL_MAP_H
--- fun_with_algos/test/test_interval_map.cpp
+++ fun_with_algos/test/test_interval_map.cpp
@@ -4,31 +4,93 @@
 #include "gtest/gtest.h"
 #include "./../interval_map.h"
 
-//TEST(TestIntervalMap, simple1)
-//{
-//	constexpr int start{1};
-//	constexpr int end{4};
-//	constexpr float value{1.f};
-//	auto map = IntervalMap<int, float>();
-//	map.add(start, end, value);
-//	EXPECT_EQ(map[2], value);
-//	EXPECT_EQ(map[0], value);
-//	EXPECT_EQ(map[1000], value);
-//}
-//
-//
-//TEST(TestIntervalMap, simple2)
-//{
-//	const std::vector<int> start{1 , 3, 6 ,9 , 18};
-//	const std::vector<int> end{2 , 4, 7 ,15 , 21};
-//	const std::vector<double> value{1., 2., 3., 4., 5.};
-//	auto map = IntervalMap<int, double>();
-//	for(int i{}; i < start.size(); ++i)
-//		map.add(start[i], end[i], value[i]);
-//	EXPECT_EQ(map[2], value[0]);
-//	EXPECT_EQ(map[10], value[3]);
-//	EXPECT_EQ(map[20], value[4]);
-//}
+TEST(TestIntervalMap, simple1)
+{
+	constexpr int start{1};
+	constexpr int end{4};
+	constexpr float value{1.f};
+	auto map = IntervalMap<int, float>();
+	map.add(start, end, value);
+	EXPECT_EQ(map[2], value);
+	EXPECT_EQ(map[0], value);
+	EXPECT_EQ(map[1000], value);
+}
+
+
+TEST(TestIntervalMap, simple2)
+{
+	const std::vector<int> start{1 , 3, 6 ,9 , 18};
+	const std::vector<int> end{2 , 4, 7 ,15 , 21};
+	const std::vector<double> value{1., 2., 3., 4., 5.};
+	auto map = IntervalMap<int, double>();
+	for(int i{}; i < start.size(); ++i)
+		map.add(start[i], end[i], value[i]);
+	EXPECT_EQ(map[2], value[0]);
+	EXPECT_EQ(map[10], value[3]);
+	EXPECT_EQ(map[20], value[4]);
+}
+
+
+TEST(TestIntervalMap, internalOverlap1)
+{
+	const std::vector<int> starts{1, 6, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+	EXPECT_EQ(map[2], values[0]);
+	EXPECT_EQ(map[3], values[0]);
+	EXPECT_EQ(map[4], values[0]);
+	EXPECT_EQ(map[5], values[0]);
+	EXPECT_EQ(map[6], values[1]);
+
+	constexpr int start{3};
+	constexpr int end{4};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[2], values[0]);
+	EXPECT_EQ(map[3], value);
+	EXPECT_EQ(map[4], values[0]);
+	EXPECT_EQ(map[5], values[0]);
+	EXPECT_EQ(map[6], values[1]);
+}
+
+TEST(TestIntervalMap, internalOverlap2)
+{
+	const std::vector<int> starts{1};
+	const std::vector<int> ends{5};
+	const std::vector<char> values{'a'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+	EXPECT_EQ(map[-1], values[0]);
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[3], values[0]);
+	EXPECT_EQ(map[4], values[0]);
+	EXPECT_EQ(map[5], values[0]);
+
+}
+
+
+
+TEST(TestIntervalMap, internalOverlapEndEqual)
+{
+	const std::vector<int> starts{1, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size() - 1; ++i)
+		map.add(starts[i], ends[i], values[i]);
+	constexpr int start{3};
+	constexpr int end{5};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[3], value);
+	EXPECT_EQ(map[4], value);
+	EXPECT_EQ(map[5], values[1]);
+	EXPECT_EQ(map[6], values[1]);
+}
 
 TEST(TestIntervalMap, overlap1)
 {
@@ -46,6 +108,8 @@ TEST(TestIntervalMap, overlap1)
 }
 
 
+
+
 TEST(TestIntervalMap, overlap2)
 {
 	const std::vector<int> start{1, 3, 5, 7, 3};
@@ -56,6 +120,271 @@ TEST(TestIntervalMap, overlap2)
 		map.add(start[i], end[i], value[i]);
 	EXPECT_EQ(map[2], value[0]);
 	EXPECT_EQ(map[4], value[4]);
-	EXPECT_EQ(map[8], value[0]);
+	EXPECT_EQ(map[8], value[3]);
 	EXPECT_EQ(map[9], value[3]);
+}
+
+TEST(TestIntervalMap, overwriteExistingIntervals1)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{0};
+	constexpr int end{5};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[1], value);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+TEST(TestIntervalMap, overwriteExistingIntervals2)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{5};
+	constexpr int end{10};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], value);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+TEST(TestIntervalMap, overwriteExistingIntervals3)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{0};
+	constexpr int end{10};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[1], value);
+	EXPECT_EQ(map[7], value);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+TEST(TestIntervalMap, overwriteExistingIntervals4)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{0};
+	constexpr int end{15};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[1], value);
+	EXPECT_EQ(map[7], value);
+	EXPECT_EQ(map[11], value);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+TEST(TestIntervalMap, overwriteExistingIntervals5)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{0};
+	constexpr int end{1000};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[1], value);
+	EXPECT_EQ(map[7], value);
+	EXPECT_EQ(map[11], value);
+	EXPECT_EQ(map[20], value);
+
+}
+
+
+TEST(TestIntervalMap, beginIsIn1)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{5};
+	constexpr int end{8};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[5], value);
+	EXPECT_EQ(map[7], value);
+	EXPECT_EQ(map[8], values[1]);
+	EXPECT_EQ(map[9], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+
+TEST(TestIntervalMap, beginIsIn2)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{5};
+	constexpr int end{12};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[5], value);
+	EXPECT_EQ(map[7], value);
+	EXPECT_EQ(map[8], value);
+	EXPECT_EQ(map[9], value);
+	EXPECT_EQ(map[11], value);
+	EXPECT_EQ(map[12], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+TEST(TestIntervalMap, beginIsIn3)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{0};
+	constexpr int end{14};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[0], value);
+	EXPECT_EQ(map[1], value);
+	EXPECT_EQ(map[5], value);
+	EXPECT_EQ(map[7], value);
+	EXPECT_EQ(map[8], value);
+	EXPECT_EQ(map[9], value);
+	EXPECT_EQ(map[11], value);
+	EXPECT_EQ(map[12], value);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+
+TEST(TestIntervalMap, endIsIn1)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{2};
+	constexpr int end{5};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[0], values[0]);
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[2], value);
+	EXPECT_EQ(map[3], value);
+	EXPECT_EQ(map[5], values[1]);
+	EXPECT_EQ(map[9], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[12], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+
+}
+
+
+TEST(TestIntervalMap, endIsIn2)
+{
+	const std::vector<int> starts{0, 5, 10, 15};
+	const std::vector<int> ends{5, 10, 15, 1000};
+	const std::vector<char> values{'a', 'b', 'c', 'd'};
+	auto map = IntervalMap<int, char>();
+	for (int i{}; i < starts.size(); ++i)
+		map.add(starts[i], ends[i], values[i]);
+
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[7], values[1]);
+	EXPECT_EQ(map[11], values[2]);
+	EXPECT_EQ(map[20], values[3]);
+	constexpr int start{2};
+	constexpr int end{15};
+	constexpr char value{'x'};
+	map.add(start, end, value);
+	EXPECT_EQ(map[0], values[0]);
+	EXPECT_EQ(map[1], values[0]);
+	EXPECT_EQ(map[2], value);
+	EXPECT_EQ(map[3], value);
+	EXPECT_EQ(map[5], value);
+	EXPECT_EQ(map[9], value);
+	EXPECT_EQ(map[11], value);
+	EXPECT_EQ(map[12], value);
+	EXPECT_EQ(map[20], values[3]);
 }
