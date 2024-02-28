88383bc15f1dc5cb9a4963dda63098384b12053b
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-02-28 19:27:17 +0100

    Add algo for finding bootom left node.

---
+++ data_structures/source/trees/binary_trees/find_bottom_left_tree_value.h
@@ -0,0 +1,40 @@
+//
+// Created by andreas on 28.02.24.
+//
+
+#ifndef FIND_BOTTOM_LEFT_TREE_VALUE_H
+#define FIND_BOTTOM_LEFT_TREE_VALUE_H
+// Given the root of a binary tree, return the leftmost value in the last row of the tree.
+#include <queue>
+#include "tree_node.h"
+
+template<typename T>
+T findBottomLeftValue(TreeNode<T> *root)
+{
+
+	std::queue<TreeNode<T> *> q;
+	q.push(root);
+	int candidate{};
+	while (!q.empty()) {
+		q.push(nullptr);
+		bool first{true};
+		while (q.front()) {
+			if (first && q.front()->left == nullptr) {
+				candidate = q.front()->val;
+				first = false;
+			}
+			if (q.front()->left) {
+				q.push(q.front()->left);
+			}
+			if (q.front()->right) {
+				q.push(q.front()->right);
+			}
+			q.pop();
+		}
+		q.pop();
+	}
+	return candidate;
+
+}
+
+#endif //FIND_BOTTOM_LEFT_TREE_VALUE_H
