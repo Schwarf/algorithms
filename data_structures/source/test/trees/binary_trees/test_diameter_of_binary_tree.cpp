a48d78ca82744c42c39d172b436679721e148d0a
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-02-28 19:01:21 +0100

    Add some tests.

---
+++ data_structures/source/test/trees/binary_trees/test_diameter_of_binary_tree.cpp
@@ -0,0 +1,58 @@
+//
+// Created by andreas on 28.02.24.
+//
+#include "setup_binary_tree.h"
+#include "trees/binary_trees/diameter_of_binary_tree.h"
+
+
+TEST_F(SetupBinaryTree, simple1)
+{
+	auto tree = small_binary_tree_root();
+	constexpr int expected_diameter{3};
+	EXPECT_EQ(expected_diameter, diameter_of_tree(tree));
+}
+
+TEST_F(SetupBinaryTree, simple2)
+{
+	auto tree = binary_tree_root();
+	constexpr int expected_diameter{6};
+	EXPECT_EQ(expected_diameter, diameter_of_tree(tree));
+}
+
+TEST_F(SetupBinaryTree, simple3)
+{
+	auto tree = medium_tree_with_subtree_duplicates1();
+	constexpr int expected_diameter{5};
+	EXPECT_EQ(expected_diameter, diameter_of_tree(tree));
+}
+
+TEST_F(SetupBinaryTree, simple5)
+{
+	auto tree = new TreeNode<float>{0.f};
+	constexpr int expected_diameter{0};
+	EXPECT_EQ(expected_diameter, diameter_of_tree(tree));
+}
+
+TEST_F(SetupBinaryTree, simple6)
+{
+	auto tree = new TreeNode<float>{0.f};
+	tree->left = new TreeNode<float>{2.f};
+	constexpr int expected_diameter{1};
+	EXPECT_EQ(expected_diameter, diameter_of_tree(tree));
+}
+
+TEST_F(SetupBinaryTree, medium1)
+{
+	auto tree = new TreeNode<float>{0.f};
+	tree->left = new TreeNode<float>{2.f};
+	tree->right = new TreeNode<float>{2.f};
+	tree->left->right = new TreeNode<float>{2.f};
+	tree->left->left = new TreeNode<float>{2.f};
+	tree->left->right->left = new TreeNode<float>{2.f};
+	tree->left->left->right = new TreeNode<float>{2.f};
+	tree->left->right->left->right = new TreeNode<float>{2.f};
+	tree->left->left->right->left = new TreeNode<float>{2.f};
+	constexpr int expected_diameter{6};
+	EXPECT_EQ(expected_diameter, diameter_of_tree(tree));
+}
+
--- data_structures/source/trees/binary_trees/diameter_of_binary_tree.h
+++ data_structures/source/trees/binary_trees/diameter_of_binary_tree.h
@@ -39,12 +39,18 @@ T diameter_of_tree(TreeNode<T> *root)
 	if (root->left && root->right)
 		max = std::max(max, 2 + bfs_help(root->right) + bfs_help(root->left));
 	else
-		max = std::max(max, bfs(root));
+		max = std::max(max, bfs_help(root));
 	if (root->right)
 		max = std::max(max, diameter_of_tree(root->right));
 	if (root->left)
 		max = std::max(max, diameter_of_tree(root->left));
 	return max;
+}
+
+template<typename T>
+T diameter_of_tree_optimal(TreeNode<T> *root)
+{
+
 }
 
 #endif //DIAMETER_OF_BINARY_TREE_H
