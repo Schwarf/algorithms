1f5c67075c2f640c7d2a931a194a841093498e97
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-08 16:39:30 +0100

    Add some docu.

--- binary_search/find_element_index_in_rotated_array.h
+++ binary_search/find_element_index_in_rotated_array.h
@@ -6,7 +6,12 @@
 #define FIND_ELEMENT_INDEX_IN_ROTATED_ARRAY_H
 #include <concepts>
 #include <vector>
-
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k(1 <= k < nums.length)
+// such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+// For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
 template<typename T>
 requires std::is_arithmetic_v<T>
 int find_index_with_binary_search(const std::vector<T> &input, int left_boundary, int right_boundary, T target)
