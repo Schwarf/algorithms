dd200caf33583ba4139fef1ccde6137ed0b40d81
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-12 19:42:34 +0100

    Add O(N^2) time complexity O(1) space complexity algo for removing zero sum consecutive nodes.

---
+++ data_structures/source/linked_lists/remove_zero_sum_consecutive_nodes.h
@@ -0,0 +1,33 @@
+//
+// Created by andreas on 12.03.24.
+//
+
+#ifndef REMOVE_ZERO_SUM_CONSECUTIVE_NODES_H
+#define REMOVE_ZERO_SUM_CONSECUTIVE_NODES_H
+// Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that
+// sum to 0 until there are no such sequences.
+// After doing so, return the head of the final linked list.  You may return any such answer.
+#include "node.h"
+
+template<typename T>
+requires std::is_arithmetic_v<T>
+Node<T> *remove_zero_sum_consecutive_nodes(Node<T> *head)
+{
+	auto front = new Node<T>(T{}, head);
+	auto left = front;
+	while (left) {
+		// we start with head (since left==front previous head)
+		auto right = left->next;
+		int sum{};
+		while (right) {
+			sum += right->val;
+			if (sum == 0) {
+				left->next = right->next;
+			}
+			right = right->next;
+		}
+		left = left->next;
+	}
+	return front->next;
+}
+#endif //REMOVE_ZERO_SUM_CONSECUTIVE_NODES_H
---
+++ data_structures/source/test/linked_lists/test_remove_zero_sum_consecutive_nodes.cpp
@@ -0,0 +1,6 @@
+//
+// Created by andreas on 12.03.24.
+//
+
+#include "gtest/gtest.h"
+#include "linked_lists/remove_zero_sum_consecutive_nodes.h"
