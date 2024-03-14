89dddb7bc3234efbf009c12a1ec5b7ebe7db4525
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-14 18:30:09 +0100

    Add algo to remove consecutive elements in linked list that sum to zero.

--- data_structures/source/linked_lists/remove_zero_sum_consecutive_nodes.h
+++ data_structures/source/linked_lists/remove_zero_sum_consecutive_nodes.h
@@ -8,6 +8,7 @@
 // sum to 0 until there are no such sequences.
 // After doing so, return the head of the final linked list.  You may return any such answer.
 #include "node.h"
+#include <unordered_map>
 
 // O(N^2) time, O(1) space
 template<typename T>
@@ -34,5 +35,39 @@ Node<T> *remove_zero_sum_consecutive_nodes(Node<T> *head)
 }
 
 // O(N) time, O(N) space is missing
+template<typename T>
+Node<T> *remove_zero_sum_consecutive_nodes_optimized(Node<T> *head)
+{
+	auto front = new Node<T>();
+	front->next = head;
+	auto current = front;
+	int prefix_sum = 0;
+	std::unordered_map<T, Node<T> *> prefix_sum_to_node;
+	prefix_sum_to_node[0] = front;
+
+	// Calculate the prefix sum for each node and add to the hashmap
+	// Each prefix_sum that appears multiple times e.g. two times at index i and index j means
+	// that the sum between the two indices is zero (i+1 ... j)
+	// [1,2,−3,3,−2] yields prefix-sum [1,3,0,3,1]
+	while (current != nullptr) {
+		prefix_sum += current->value;
+		prefix_sum_to_node[prefix_sum] = current;
+		current = current->next;
+	}
+
+	// Reset prefix sum and current
+	prefix_sum = 0;
+	current = front;
+
+	// Delete zero sum consecutive sequences
+	// by setting node before sequence to node after
+	while (current != nullptr) {
+		prefix_sum += current->value;
+		// Here we replace to the latest appearance of prefix_sum in prefix_sum_to_node.
+		current->next = prefix_sum_to_node[prefix_sum]->next;
+		current = current->next;
+	}
+	return front->next;
+}
 
 #endif //REMOVE_ZERO_SUM_CONSECUTIVE_NODES_H
