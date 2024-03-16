a68cabc2de26856ffedb804f330643635da098ca
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-16 18:51:46 +0100

    Add tests for algo to find longest contiguous array consisting of 1s and 0s.

--- prefix_sums/contiguous_array.h
+++ prefix_sums/contiguous_array.h
@@ -10,14 +10,14 @@
 int find_max_contiguous_array(const std::vector<int> & input)
 {
 	std::unordered_map<int, int> help;
-	help[0] = -1;
+	help[0] = -1; // Set to -1 to account for 0-indexed nature of std::vector.
 	int max_length{};
 	int counter{};
 	for(int i{}; i < input.size(); ++i)
 	{
 		counter += input[i] ? 1 : -1;
-		// The algorithm is base on the idea that between equal prefix sums the number of 0s and 1s is equal.
-		// So whenever we encounter a prefix-sum a second time we look up the index when it appeared first.
+		// The algorithm is based on the idea that between equal prefix sums the number of 0s and 1s is equal.
+		// So whenever we encounter a prefix-sum a second, third, fourth.. time we look up the index when it appeared first.
 		// The appearance of a prefix sum is never updated (only for the first time see 'else'-branch).
 		if(help.find(counter) != help.end())
 			max_length = std::max(max_length, i - help[counter]);
--- prefix_sums/test/test_contiguous_array.cpp
+++ prefix_sums/test/test_contiguous_array.cpp
@@ -3,3 +3,52 @@
 //
 #include "gtest/gtest.h"
 #include "contiguous_array.h"
+
+TEST(TestContiguousArray, simple1)
+{
+	const std::vector<int> input{0, 1};
+	constexpr int expected_answer{2};
+	EXPECT_EQ(find_max_contiguous_array(input), expected_answer);
+}
+
+TEST(TestContiguousArray, simple2)
+{
+	const std::vector<int> input{0, 1, 1};
+	constexpr int expected_answer{2};
+	EXPECT_EQ(find_max_contiguous_array(input), expected_answer);
+}
+
+TEST(TestContiguousArray, simple3)
+{
+	const std::vector<int> input{0, 1, 0};
+	constexpr int expected_answer{2};
+	EXPECT_EQ(find_max_contiguous_array(input), expected_answer);
+}
+
+TEST(TestContiguousArray, simple4)
+{
+	const std::vector<int> input{0, 1, 1, 1, 1, 1, 0};
+	constexpr int expected_answer{2};
+	EXPECT_EQ(find_max_contiguous_array(input), expected_answer);
+}
+
+TEST(TestContiguousArray, simple5)
+{
+	const std::vector<int> input{1, 1, 1, 1, 1, 1, 0, 0, 0, 0};
+	constexpr int expected_answer{8};
+	EXPECT_EQ(find_max_contiguous_array(input), expected_answer);
+}
+
+TEST(TestContiguousArray, simple6)
+{
+	const std::vector<int> input{0, 0, 1, 1, 0, 0};
+	constexpr int expected_answer{4};
+	EXPECT_EQ(find_max_contiguous_array(input), expected_answer);
+}
+
+TEST(TestContiguousArray, simple7)
+{
+	const std::vector<int> input{1, 1, 1, 0, 0};
+	constexpr int expected_answer{4};
+	EXPECT_EQ(find_max_contiguous_array(input), expected_answer);
+}
