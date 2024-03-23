ef0aadca7b08922b829d61129e0c4fd962e0d9cc
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-23 15:16:57 +0100

    Code cleanup in hash functions.

--- data_structures/source/hash_functions/testing_hash_functions/test_hash_function_for_pairs.cpp
+++ data_structures/source/hash_functions/testing_hash_functions/test_hash_function_for_pairs.cpp
@@ -3,17 +3,19 @@
 //
 #include <random>
 #include "gtest/gtest.h"
-#include "hash_function_for_pairs.h"
+#include "./../hash_function_for_pairs.h"
 #include "uniformity_of_pair_hash_functions.h"
 
 TEST(TestUniformityOfPairHashes, integer_integer_pairs_commutative)
 {
 	std::random_device random_device;
 	std::mt19937 generator(random_device());
-	std::uniform_int_distribution<int> distribution(1, 1000000000);
+	constexpr int lower_bound{1};
+	constexpr int upper_bound{1000000000};
+	std::uniform_int_distribution<int> distribution(lower_bound, upper_bound);
 	std::vector<std::pair<int, int>> input;
-	constexpr int number_of_samples = 1000000;
-	constexpr int number_of_buckets = 1000;
+	constexpr int number_of_samples{1000000};
+	constexpr int number_of_buckets{1000};
 	for (int i{}; i < number_of_samples; ++i) {
 		auto first = distribution(generator);
 		auto second = distribution(generator);
@@ -24,6 +26,7 @@ TEST(TestUniformityOfPairHashes, integer_integer_pairs_commutative)
 			commutative_pair_hash<int, int>(),
 			input,
 			number_of_buckets);
+	// 999 degrees of freedom = buckets -1
 	constexpr double chi_square{1074.3};
 	std::cout << test << std::endl;
 	EXPECT_TRUE(test < chi_square);
@@ -33,10 +36,12 @@ TEST(TestUniformityOfPairHashes, integer_integer_pairs_non_commutative)
 {
 	std::random_device random_device;
 	std::mt19937 generator(random_device());
-	std::uniform_int_distribution<int> distribution(1, 1000000000);
+	constexpr int lower_bound{1};
+	constexpr int upper_bound{1000000000};
+	std::uniform_int_distribution<int> distribution(lower_bound, upper_bound);
 	std::vector<std::pair<int, int>> input;
-	constexpr int number_of_samples = 1000000;
-	constexpr int number_of_buckets = 1000;
+	constexpr int number_of_samples{1000000};
+	constexpr int number_of_buckets{1000};
 	for (int i{}; i < number_of_samples; ++i) {
 		auto first = distribution(generator);
 		auto second = distribution(generator);
@@ -58,11 +63,15 @@ TEST(TestUniformityOfPairHashes, integer_double_pairs_commutative)
 {
 	std::random_device random_device;
 	std::mt19937 generator(random_device());
-	std::uniform_int_distribution<size_t> int_distribution(1, 1000000000);
-	std::uniform_real_distribution<double> double_distribution(0.0, 1.0);
+	constexpr int int_lower_bound{1};
+	constexpr int int_upper_bound{1000000000};
+	constexpr double double_lower_bound{0.0};
+	constexpr double double_upper_bound{1.0};
+	std::uniform_int_distribution<int> int_distribution(int_lower_bound, int_upper_bound);
+	std::uniform_real_distribution<double> double_distribution(double_lower_bound, double_upper_bound);
 	std::vector<std::pair<int, double>> input;
-	constexpr int number_of_samples = 1000000;
-	constexpr int number_of_buckets = 1000;
+	constexpr int number_of_samples{1000000};
+	constexpr int number_of_buckets{1000};
 	for (int i{}; i < number_of_samples; ++i) {
 		auto first = int_distribution(generator);
 		auto second = double_distribution(generator);
@@ -85,11 +94,15 @@ TEST(TestUniformityOfPairHashes, integer_double_pairs_non_commutative)
 {
 	std::random_device random_device;
 	std::mt19937 generator(random_device());
-	std::uniform_int_distribution<size_t> int_distribution(1, 1000000000);
-	std::uniform_real_distribution<double> double_distribution(0.0, 1.0);
+	constexpr int int_lower_bound{1};
+	constexpr int int_upper_bound{1000000000};
+	constexpr double double_lower_bound{0.0};
+	constexpr double double_upper_bound{1.0};
+	std::uniform_int_distribution<int> int_distribution(int_lower_bound, int_upper_bound);
+	std::uniform_real_distribution<double> double_distribution(double_lower_bound, double_upper_bound);
 	std::vector<std::pair<int, double>> input;
-	constexpr int number_of_samples = 1000000;
-	constexpr int number_of_buckets = 1000;
+	constexpr int number_of_samples{1000000};
+	constexpr int number_of_buckets{1000};
 	for (int i{}; i < number_of_samples; ++i) {
 		auto first = int_distribution(generator);
 		auto second = double_distribution(generator);
