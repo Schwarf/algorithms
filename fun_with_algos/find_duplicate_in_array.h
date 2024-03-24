871bb2845c3479c24b55fffded0ca09a4fc56b2b
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-24 08:51:43 +0100

    Add algo for finding duplicate in array.

---
+++ fun_with_algos/find_duplicate_in_array.h
@@ -0,0 +1,29 @@
+//
+// Created by andreas on 24.03.24.
+//
+
+#ifndef FIND_DUPLICATE_IN_ARRAY_H
+#define FIND_DUPLICATE_IN_ARRAY_H
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+#include <vector>
+// Easy-peasy: Sort and find the duplicate. Here we concentrate on O(N) solutions.
+
+int find_duplicate(std::vector<int>& input) {
+	for(int i{}; i < input.size(); ++i)
+	{
+		// We compute the index and negate the value at the index.
+		int index = std::abs(i);
+		// If the value at the index is already negative, we return the index because it has been visited once before/
+		if(input[index] < 0)
+			return index;
+		// HERE we negate
+		input[index] = -input[index];
+	}
+	return -1;
+}
+
+
+#endif //FIND_DUPLICATE_IN_ARRAY_H
