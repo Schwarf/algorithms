16de8a2fca0888a1145d0d17b9629317aef5e50c
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-24 09:05:57 +0100

    Add 2nd solution using linked list algo to find a cycle (and the cycles beginning).

--- fun_with_algos/find_duplicate_in_array.h
+++ fun_with_algos/find_duplicate_in_array.h
@@ -5,9 +5,9 @@
 #ifndef FIND_DUPLICATE_IN_ARRAY_H
 #define FIND_DUPLICATE_IN_ARRAY_H
 
-// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-// There is only one repeated number in nums, return this repeated number.
-// You must solve the problem without modifying the array nums and uses only constant extra space.
+// Given an array of integers input containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in input, return this repeated number.
+// You must solve the problem without modifying the array input and uses only constant extra space.
 #include <vector>
 // Easy-peasy: Sort and find the duplicate. Here we concentrate on O(N) solutions.
 
@@ -24,6 +24,31 @@ int find_duplicate(std::vector<int>& input) {
 	}
 	return -1;
 }
+
+// Due to the given problem structure (n+1) numbers in [1, n] with a duplicate, we can approach this problem like
+// a linked list. We use slow and fast runner in linked list and use them to find the begining of a cycle in
+// linked list.
+
+int find_duplicate_linked_list(const std::vector<int>& input) {
+	int slow = input[0];
+	int fast = input[0];
+	// Find the intersection point of the two pointers
+	do {
+		slow = input[slow];
+		fast = input[input[fast]];
+	} while (slow != fast);
+
+
+	// Find the entrance of the cycle
+	slow = input[0];
+	while (slow != fast) {
+		slow = input[slow];
+		fast = input[fast];
+	}
+	return slow;
+}
+
+
 
 
 #endif //FIND_DUPLICATE_IN_ARRAY_H
