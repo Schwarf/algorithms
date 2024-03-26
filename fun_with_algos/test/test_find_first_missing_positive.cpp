952d58a272468cb5e84c42de40da643d0a0032f5
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-26 17:47:44 +0100

    Add tests for optimized algo.

--- fun_with_algos/test/test_find_first_missing_positive.cpp
+++ fun_with_algos/test/test_find_first_missing_positive.cpp
@@ -54,3 +54,53 @@ TEST(TestFindFirstMissingPositive, simple7)
 	EXPECT_EQ(expected_result, find_first_missing_positive(input));
 }
 
+
+TEST(TestFindFirstMissingPositiveOptimized, simple1)
+{
+	std::vector<int> input{1, 2, 0};
+	constexpr int expected_result{3};
+	EXPECT_EQ(expected_result, find_first_missing_positive_optimized(input));
+}
+
+TEST(TestFindFirstMissingPositiveOptimized, simple2)
+{
+	std::vector<int> input{1, 2, -2, -3};
+	constexpr int expected_result{3};
+	EXPECT_EQ(expected_result, find_first_missing_positive_optimized(input));
+}
+
+TEST(TestFindFirstMissingPositiveOptimized, simple3)
+{
+	std::vector<int> input{0, -1, -2,};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, find_first_missing_positive_optimized(input));
+}
+
+TEST(TestFindFirstMissingPositiveOptimized, simple4)
+{
+	std::vector<int> input{1, 2, 3, 4, 5};
+	constexpr int expected_result{6};
+	EXPECT_EQ(expected_result, find_first_missing_positive_optimized(input));
+}
+
+TEST(TestFindFirstMissingPositiveOptimized, simple5)
+{
+	std::vector<int> input{0, 0, 0, 0, 0};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, find_first_missing_positive_optimized(input));
+}
+
+
+TEST(TestFindFirstMissingPositiveOptimized, simple6)
+{
+	std::vector<int> input{5, 6, 7, 8};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, find_first_missing_positive_optimized(input));
+}
+
+TEST(TestFindFirstMissingPositiveOptimized, simple7)
+{
+	std::vector<int> input{1, 1, 1};
+	constexpr int expected_result{2};
+	EXPECT_EQ(expected_result, find_first_missing_positive_optimized(input));
+}
