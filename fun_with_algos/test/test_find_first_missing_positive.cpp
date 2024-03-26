5b90563a12cb2220cfe6f51fd404dd732bfdbe0a
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-26 17:47:23 +0100

    Add tests for algo.

---
+++ fun_with_algos/test/test_find_first_missing_positive.cpp
@@ -0,0 +1,56 @@
+//
+// Created by andreas on 26.03.24.
+//
+#include "gtest/gtest.h"
+#include "./../find_first_missing_positive.h"
+
+TEST(TestFindFirstMissingPositive, simple1)
+{
+	const std::vector<int> input{1, 2, 0};
+	constexpr int expected_result{3};
+	EXPECT_EQ(expected_result, find_first_missing_positive(input));
+}
+
+TEST(TestFindFirstMissingPositive, simple2)
+{
+	const std::vector<int> input{1, 2, -2, -3};
+	constexpr int expected_result{3};
+	EXPECT_EQ(expected_result, find_first_missing_positive(input));
+}
+
+TEST(TestFindFirstMissingPositive, simple3)
+{
+	const std::vector<int> input{0, -1, -2,};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, find_first_missing_positive(input));
+}
+
+TEST(TestFindFirstMissingPositive, simple4)
+{
+	const std::vector<int> input{1, 2, 3, 4, 5};
+	constexpr int expected_result{6};
+	EXPECT_EQ(expected_result, find_first_missing_positive(input));
+}
+
+TEST(TestFindFirstMissingPositive, simple5)
+{
+	const std::vector<int> input{0, 0, 0, 0, 0};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, find_first_missing_positive(input));
+}
+
+
+TEST(TestFindFirstMissingPositive, simple6)
+{
+	const std::vector<int> input{5, 6, 7, 8};
+	constexpr int expected_result{1};
+	EXPECT_EQ(expected_result, find_first_missing_positive(input));
+}
+
+TEST(TestFindFirstMissingPositive, simple7)
+{
+	const std::vector<int> input{1, 1, 1};
+	constexpr int expected_result{2};
+	EXPECT_EQ(expected_result, find_first_missing_positive(input));
+}
+
