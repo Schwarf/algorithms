2559a3b7535f0b05bafcc0b435cca946846f7b6b
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-26 17:46:12 +0100

    Add oprimized O(1) space and O(N) time complexity algo to find first missing positive integer.

--- fun_with_algos/find_first_missing_positive.h
+++ fun_with_algos/find_first_missing_positive.h
@@ -23,4 +23,48 @@ int find_first_missing_positive(const std::vector<int>& nums) {
 	return nums.size();
 }
 
+int find_first_missing_positive_optimized(std::vector<int>& nums) {
+	int n = nums.size();
+	bool contains_value_1{};
+
+	// Check if array conatin the value 1. Set all non-positive values to 1.
+	for (int i {}; i < n; i++) {
+		// Check whether 1 is in the original array
+		if (nums[i] == 1) {
+			contains_value_1 = true;
+		}
+		if (nums[i] <= 0 || nums[i] > n) {
+			nums[i] = 1;
+		}
+	}
+	// Does not contain, return 1 as first missing positive integer.
+	if (!contains_value_1)
+		return 1;
+
+	// Mark whether integers 1 to n are in nums
+	// Use index as a hash key and negative sign as a presence detector.
+	for (int i{}; i < n; i++) {
+		int index = abs(nums[i]);
+		// since 0-index is not use, use it for index=n
+		if (index == n) {
+			nums[0] = - abs(nums[0]);
+		} else {
+			nums[index] = - abs(nums[index]);
+		}
+	}
+
+	// We negated all values at the appearing indices in the array. The first value greater 0
+	// has as index the first missing positive integer
+	for (int i{1}; i < n; i++) {
+		if (nums[i] > 0)
+			return i;
+	}
+
+	// nums[0] stores whether n is in nums
+	if (nums[0] > 0) {
+		return n;
+	}
+	// If all former value were negative, we return n+1 as the first missing integer
+	return n + 1;
+}
 #endif //FIND_FIRST_MISSING_POSITIVE_H
