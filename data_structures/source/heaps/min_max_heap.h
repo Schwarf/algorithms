8ac885809635951753b9bf47e543411c6bb8e71f
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-03 20:22:35 +0200

    WIP.

--- data_structures/source/heaps/min_max_heap.h
+++ data_structures/source/heaps/min_max_heap.h
@@ -23,20 +23,56 @@ class min_max_heap
 
 
 private:
-	// Compute the level for index i in the min-max-heap by using floor(log2int(i))
-	unsigned int log2int(unsigned int n)
+	// In min-max heaps, the required ordering must be established between an element, its children, and its grandchildren.
+	void trickle_down(int index)
 	{
-		if (n == 0U)
+		const auto level = log2int(1 + index); // We add 1 since we can not deal with the zero (min) level in the log
+		if (level & 1) {
+			trickle_down_min(index);
+		}
+		else {
+			trickle_down_max(index);
+		}
+	}
+
+	int left_child(int index)
+	{
+		return 2 * index + 1;
+	}
+
+	int right_child(int index)
+	{
+		return 2 * index + 2;
+	}
+
+	void trickle_down_min(int index)
+	{
+		if (index >= heap_.size()) {
+			throw std::invalid_argument("Index value " + std::to_string(index) + "does not exist!");
+		}
+		auto leftChild = left_child(index);
+		if(leftChild < heap_.size() && leftChild+1 < heap_.size() && )
+	}
+
+	void trickle_down_max(int index)
+	{
+
+	}
+
+	// Compute the level for index i in the min-max-heap by using floor(log2int(i))
+	int log2int(int n)
+	{
+		if (n == 0)
 			throw std::domain_error("log2 of 0 is undefined!");
-		unsigned int result{0U};
-		while (n > 1U) {
+		int result{0};
+		while (n > 1) {
 			n >>= 1;
 			result++;
 		}
 		return result;
 	}
 
-	std::vector<T> elements_;
+	std::vector<T> heap_;
 };
 
 #endif //MIN_MAX_HEAP_H
