8c40a9ae5ec18ff8db2d577ae1b84db2d0b0960c
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-07 10:32:26 +0200

    Add tests for optimized version.

--- data_structures/source/test/stacks_and_queues/test_valid_parnethesis_string_with_wildcard.cpp
+++ data_structures/source/test/stacks_and_queues/test_valid_parnethesis_string_with_wildcard.cpp
@@ -77,3 +77,77 @@ TEST(TestValidParenthesisStringWithWildcard, complex2)
 		input{"((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()"};
 	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid(input));
 }
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple1)
+{
+	std::string input{"()"};
+	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple2)
+{
+	std::string input{"(*)"};
+	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple3)
+{
+	std::string input{"(*))"};
+	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple4)
+{
+	std::string input{"("};
+	EXPECT_FALSE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple5)
+{
+	std::string input{")"};
+	EXPECT_FALSE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple6)
+{
+	std::string input{"**"};
+	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple7)
+{
+	std::string input{"*(()"};
+	EXPECT_FALSE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple8)
+{
+	std::string input{"**()"};
+	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple9)
+{
+	std::string input{"**(*)(*"};
+	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, simple10)
+{
+	std::string input{"**(*)(*("};
+	EXPECT_FALSE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, complex1)
+{
+	std::string
+		input{"(((((*(()((((*((**(((()()*)()()()*((((**)())*)*)))))))(())(()))())((*()()(((()((()*(())*(()**)()(())"};
+	EXPECT_FALSE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
+
+TEST(TestValidParenthesisStringWithWildcardOptimized, complex2)
+{
+	std::string
+		input{"((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()"};
+	EXPECT_TRUE(is_parenthesis_string_with_wildcard_valid_optimized(input));
+}
