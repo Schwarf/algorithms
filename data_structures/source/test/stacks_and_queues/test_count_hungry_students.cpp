63798a1e4c76ac58d38a5eb7a1d65d2dd940dc28
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-08 17:23:11 +0200

    Add tests.

--- data_structures/source/test/stacks_and_queues/test_count_hungry_students.cpp
+++ data_structures/source/test/stacks_and_queues/test_count_hungry_students.cpp
@@ -47,7 +47,6 @@ TEST(TestCountHungryStudents, simple6) {
     EXPECT_EQ(expected_result, count_hungry_students(students, dishes));
 }
 
-
 TEST(TestCountHungryStudents, simple7) {
     const std::vector<int> students{0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
                                     0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0};
@@ -58,3 +57,54 @@ TEST(TestCountHungryStudents, simple7) {
 }
 
 
+TEST(TestCountHungryStudentsOptimized, simple1) {
+    const std::vector<int> students{1, 1, 0, 0};
+    const std::vector<int> dishes{1, 0, 1, 0};
+    constexpr int expected_result{};
+    EXPECT_EQ(expected_result, count_hungry_students_optimized(students, dishes));
+}
+
+TEST(TestCountHungryStudentsOptimized, simple2) {
+    const std::vector<int> students{1, 1, 0, 0};
+    const std::vector<int> dishes{0, 1, 0, 1};
+    constexpr int expected_result{};
+    EXPECT_EQ(expected_result, count_hungry_students_optimized(students, dishes));
+}
+
+TEST(TestCountHungryStudentsOptimized, simple3) {
+    const std::vector<int> students{1, 1, 1, 0, 0, 1};
+    const std::vector<int> dishes{1, 0, 0, 0, 1, 1};
+    constexpr int expected_result{3};
+    EXPECT_EQ(expected_result, count_hungry_students_optimized(students, dishes));
+}
+
+TEST(TestCountHungryStudentsOptimized, simple4) {
+    const std::vector<int> students{0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1};
+    const std::vector<int> dishes{0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0};
+    constexpr int expected_result{5};
+    EXPECT_EQ(expected_result, count_hungry_students_optimized(students, dishes));
+}
+
+
+TEST(TestCountHungryStudentsOptimized, simple5) {
+    const std::vector<int> students{0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0};
+    const std::vector<int> dishes{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0};
+    constexpr int expected_result{5};
+    EXPECT_EQ(expected_result, count_hungry_students_optimized(students, dishes));
+}
+
+TEST(TestCountHungryStudentsOptimized, simple6) {
+    const std::vector<int> students{0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0};
+    const std::vector<int> dishes{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0};
+    constexpr int expected_result{5};
+    EXPECT_EQ(expected_result, count_hungry_students_optimized(students, dishes));
+}
+
+TEST(TestCountHungryStudentsOptimized, simple7) {
+    const std::vector<int> students{0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+                                    0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0};
+    const std::vector<int> dishes{1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+                                  1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0};
+    constexpr int expected_result{16};
+    EXPECT_EQ(expected_result, count_hungry_students_optimized(students, dishes));
+}
