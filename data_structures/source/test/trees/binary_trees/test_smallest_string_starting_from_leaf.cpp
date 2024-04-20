04e886e964b93e160853c79c402eef15443e119b
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-20 09:45:14 +0200

    Add first test.

---
+++ data_structures/source/test/trees/binary_trees/test_smallest_string_starting_from_leaf.cpp
@@ -0,0 +1,13 @@
+//
+// Created by andreas on 20.04.24.
+//
+#include "setup_binary_tree.h"
+#include "trees/binary_trees/smallest_string_starting_from_leaf.h"
+
+
+TEST_F(SetupBinaryTree, TestSmallestStringStartingFromLeaf) {
+    auto input_tree = medium_tree_with_subtree_duplicates2();
+    const std::string expected_result{"cba"};
+    std::string result;
+    smallest_string_starting_from_leaf(input_tree, result);
+}
--- data_structures/source/trees/binary_trees/smallest_string_starting_from_leaf.h
+++ data_structures/source/trees/binary_trees/smallest_string_starting_from_leaf.h
@@ -6,6 +6,7 @@
 #define DATA_STRUCTURES_SMALLEST_STRING_STARTING_FROM_LEAF_H
 
 #include <string>
+#include <concepts>
 #include "tree_node.h"
 
 // You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters
@@ -14,13 +15,15 @@
 // As a reminder, any shorter prefix of a string is lexicographically smaller.
 // For example, "ab" is lexicographically smaller than "aba".
 // A leaf of a node is a node that has no children.
+template<typename T>
+requires std::is_unsigned_v<T>
 void
-smallest_string_starting_from_leaf(TreeNode<int> *root, std::string &smallest_string, std::string current_string = "") {
+smallest_string_starting_from_leaf(TreeNode<T> *root, std::string &smallest_string, std::string current_string = "") {
     if (!root)
         return;
     current_string = static_cast<char>(root->value + 'a') + current_string;
     if (!root->left && !root->right) {
-        if (smallest_string == "" || smallest_string > current_string)
+        if (smallest_string.empty() || smallest_string > current_string)
             smallest_string = current_string;
     }
 
