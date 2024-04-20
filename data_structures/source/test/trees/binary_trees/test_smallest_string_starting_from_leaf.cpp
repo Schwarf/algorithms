7ba1ccee68d561ae9c3a213ce55eb41c90f5e4ce
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-20 09:52:36 +0200

    Finish tests.

--- data_structures/source/test/trees/binary_trees/test_smallest_string_starting_from_leaf.cpp
+++ data_structures/source/test/trees/binary_trees/test_smallest_string_starting_from_leaf.cpp
@@ -5,9 +5,25 @@
 #include "trees/binary_trees/smallest_string_starting_from_leaf.h"
 
 
-TEST_F(SetupBinaryTree, TestSmallestStringStartingFromLeaf) {
+TEST_F(SetupBinaryTree, TestSmallestStringStartingFromLeaf1) {
     auto input_tree = medium_tree_with_subtree_duplicates2();
     const std::string expected_result{"cba"};
+    std::string result;
+    smallest_string_starting_from_leaf(input_tree, result);
+}
+
+TEST_F(SetupBinaryTree, TestSmallestStringStartingFromLeaf2) {
+    auto input_tree = medium_tree_with_subtree_duplicates1();
+    const std::string expected_result{"ecb"};
+    std::string result;
+    smallest_string_starting_from_leaf(input_tree, result);
+}
+
+
+TEST_F(SetupBinaryTree, TestSmallestStringStartingFromLeaf3) {
+    auto input_tree = medium_tree_with_subtree_duplicates1();
+    input_tree->value = 25;
+    const std::string expected_result{"ecdz"};
     std::string result;
     smallest_string_starting_from_leaf(input_tree, result);
 }
