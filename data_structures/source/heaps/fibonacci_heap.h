195df7937bb9716f4fdc950a366a411506da72ac
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-23 18:34:36 +0200

    Start with fibonacci heap.

--- data_structures/source/heaps/fibonacci_heap.h
+++ data_structures/source/heaps/fibonacci_heap.h
@@ -5,4 +5,27 @@
 #ifndef DATA_STRUCTURES_FIBONACCI_HEAP_H
 #define DATA_STRUCTURES_FIBONACCI_HEAP_H
 
+#include <concepts>
+#include <compare>
+
+template<typename T>
+concept strong_ordered = requires(T a, T b) {
+    { a <=> b } -> std::same_as<std::strong_ordering>;
+};
+
+template<typename KeyType, typename ValueType> requires strong_ordered<KeyType>
+struct Node {
+    KeyType key;
+    ValueType value;
+    Node *child{nullptr};
+    Node *parent{nullptr};
+    Node *left{this};
+    Node *right{this};
+    bool is_marked{};
+    int number_of_children{};
+
+    Node(KeyType key, ValueType value) : key(key), value(value) {}
+};
+
+
 #endif //DATA_STRUCTURES_FIBONACCI_HEAP_H
