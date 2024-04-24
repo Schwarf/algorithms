977641fac72416868f876c66133ae26d91a3d0f6
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-24 19:49:04 +0200

    Finish  operation to merge two nodes next to each other.

--- data_structures/source/heaps/fibonacci_heap.h
+++ data_structures/source/heaps/fibonacci_heap.h
@@ -42,7 +42,6 @@ private:
     void _insert(Node<KeyType, ValueType> *node) {
         _minimum_node = _merge(_minimum_node, node);
         _number_of_nodes++;
-
     }
 
     void _merge(Node<KeyType, ValueType> *node1, Node<KeyType, ValueType> *node2) {
@@ -50,11 +49,20 @@ private:
             return node2;
         if (!node2)
             return node1;
-
+        // HERE STD::GREATER
         if (node1->key > node2->key)
             std::swap(node1, node2);
-        
 
+        // Inserting node1 before node2
+        // Remember original right and left nodes
+        auto node1_right = node1->right;
+        auto node2_left = node2->left;
+        node1->right = node2;
+        node2->left = node1;
+
+        node1_right->left = node2_left;
+        node2_left->right = node1_right;
+        return node1;
     }
 
     int _number_of_nodes{};
