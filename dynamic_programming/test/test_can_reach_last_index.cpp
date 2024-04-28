1717299c5d49d3ac87ba0cdfbcfb51130176e7ae
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-04-28 19:09:47 +0200

    Make code more const correct.

--- dynamic_programming/test/test_can_reach_last_index.cpp
+++ dynamic_programming/test/test_can_reach_last_index.cpp
@@ -6,34 +6,29 @@
 #include "can_reach_last_index.h"
 #include <vector>
 
-TEST(CanReachLastIndex, test_scenario1)
-{
-	std::vector<int> jump_distance{2, 3, 1, 1, 4};
-	EXPECT_TRUE(can_reach_last_index(jump_distance));
+TEST(CanReachLastIndex, test_scenario1) {
+    const std::vector<int> jump_distance{2, 3, 1, 1, 4};
+    EXPECT_TRUE(can_reach_last_index(jump_distance));
 }
 
-TEST(CanReachLastIndex, test_scenario2)
-{
-	std::vector<int> jump_distance{3, 2, 1, 0, 4};
-	EXPECT_FALSE(can_reach_last_index(jump_distance));
+TEST(CanReachLastIndex, test_scenario2) {
+    const std::vector<int> jump_distance{3, 2, 1, 0, 4};
+    EXPECT_FALSE(can_reach_last_index(jump_distance));
 }
 
-TEST(CanReachLastIndex, test_scenario3)
-{
-	std::vector<int> jump_distance{0, 3, 1, 1, 4};
-	EXPECT_FALSE(can_reach_last_index(jump_distance));
+TEST(CanReachLastIndex, test_scenario3) {
+    const std::vector<int> jump_distance{0, 3, 1, 1, 4};
+    EXPECT_FALSE(can_reach_last_index(jump_distance));
 }
 
-TEST(CanReachLastIndex, test_scenario4)
-{
-	std::vector<int> jump_distance{4, 0, 0, 0, 0};
-	EXPECT_TRUE(can_reach_last_index(jump_distance));
+TEST(CanReachLastIndex, test_scenario4) {
+    const std::vector<int> jump_distance{4, 0, 0, 0, 0};
+    EXPECT_TRUE(can_reach_last_index(jump_distance));
 }
 
-TEST(CanReachLastIndex, test_scenario5)
-{
-	std::vector<int> jump_distance{0};
-	EXPECT_TRUE(can_reach_last_index(jump_distance));
+TEST(CanReachLastIndex, test_scenario5) {
+    const std::vector<int> jump_distance{0};
+    EXPECT_TRUE(can_reach_last_index(jump_distance));
 }
 
 
