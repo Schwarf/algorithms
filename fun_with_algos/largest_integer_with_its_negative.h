73f0bd5a97f729e28103e337a192266d62175339
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-04 18:27:37 +0200

    Add algo with bitset.

--- fun_with_algos/largest_integer_with_its_negative.h
+++ fun_with_algos/largest_integer_with_its_negative.h
@@ -7,19 +7,24 @@
 
 #include <vector>
 #include <unordered_set>
+#include <bitset>
+// Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+// Return the positive integer k. If there is no such integer, return -1.
+// Restrictions
+// 1 <= nums.length <= 1000
+// -1000 <= nums[i] <= 1000
+// nums[i] != 0
 
-int largest_integer_with_its_negative(std::vector<int> & input)
-{
+ int largest_integer_with_its_negative(std::vector<int> &input) {
     std::sort(input.begin(), input.end());
     int left{};
-    int right = input.size()-1;
-    while(left !=right)
-    {
-        if(input[left]/input[right] > 0)
+    int right = input.size() - 1;
+    while (left != right) {
+        if (input[left] / input[right] > 0)
             return -1;
-        if(std::abs(input[left]) == input[right])
+        if (std::abs(input[left]) == input[right])
             return input[right];
-        else if(std::abs(input[left]) > input[right])
+        else if (std::abs(input[left]) > input[right])
             left++;
         else
             right--;
@@ -27,16 +32,26 @@ int largest_integer_with_its_negative(std::vector<int> & input)
     return -1;
 }
 
-int largest_integer_with_its_negative_hashset(std::vector<int> & input)
-{
+int largest_integer_with_its_negative_hashset(std::vector<int> &input) {
     int result{-1};
     std::unordered_set<int> seen;
-    for(const auto element: input)
-    {
+    for (const auto element: input) {
         auto abs = std::abs(element);
-        if(abs > result && seen.contains(-element))
+        if (abs > result && seen.contains(-element))
             result = abs;
         seen.insert(element);
+    }
+    return result;
+}
+
+int largest_integer_with_its_negative_bitset(std::vector<int> &input) {
+    int result{-1};
+    std::bitset<2048> seen;
+    for (const auto element: input) {
+        auto abs = std::abs(element);
+        if (abs > result && seen[-element + 1024])
+            result = abs;
+        seen[element + 1024] = true;
     }
     return result;
 }
