bab7cb1f2590846fbefed9025e49f7af2269d049
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-04 17:59:02 +0200

    Add algo using a hashset.

--- fun_with_algos/largest_integer_with_its_negative.h
+++ fun_with_algos/largest_integer_with_its_negative.h
@@ -6,6 +6,8 @@
 #define FUN_WITH_ALGOS_LARGEST_INTEGER_WITH_ITS_NEGATIVE_H
 
 #include <vector>
+#include <unordered_set>
+
 int largest_integer_with_its_negative(std::vector<int> & input)
 {
     std::sort(input.begin(), input.end());
@@ -23,7 +25,22 @@ int largest_integer_with_its_negative(std::vector<int> & input)
             right--;
     }
     return -1;
-
 }
 
+int largest_integer_with_its_negative_hashset(std::vector<int> & input)
+{
+    int result{-1};
+    std::unordered_set<int> seen;
+    for(const auto element: input)
+    {
+        auto abs = std::abs(element);
+        if(abs > result && seen.contains(-element))
+            result = abs;
+        seen.insert(element);
+    }
+    return result;
+}
+
+
 #endif //FUN_WITH_ALGOS_LARGEST_INTEGER_WITH_ITS_NEGATIVE_H
+
