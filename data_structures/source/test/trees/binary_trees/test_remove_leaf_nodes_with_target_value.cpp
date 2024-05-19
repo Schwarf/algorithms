3de18222012aacb1d5af7491829aaaac6b73608a
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-19 16:12:15 +0200

    Add tests for iterative solution.

--- data_structures/source/test/trees/binary_trees/test_remove_leaf_nodes_with_target_value.cpp
+++ data_structures/source/test/trees/binary_trees/test_remove_leaf_nodes_with_target_value.cpp
@@ -67,3 +67,66 @@ TEST_F(SetupBinaryTree, RemoveLeafNodesSimple5) {
     auto result_tree = remove_leaf_nodes(input, target);
     EXPECT_EQ(result_tree, nullptr);
 }
+
+TEST_F(SetupBinaryTree, RemoveLeafNodesIterativeSimple1) {
+    auto input = medium_tree_with_subtree_duplicates3();
+    constexpr unsigned int target{2U};
+    auto result_tree = remove_leaf_nodes_iterative(input, target);
+    auto expected_result_tree = new TreeNode<unsigned int>(2);
+    expected_result_tree->left = new TreeNode<unsigned int>(1);
+    expected_result_tree->right = new TreeNode<unsigned int>(1);
+    expected_result_tree->left->right = new TreeNode<unsigned int>(3);
+    expected_result_tree->right->left = new TreeNode<unsigned int>(4);
+    expected_result_tree->right->right = new TreeNode<unsigned int>(5);
+    auto result = pre_order_traversal(result_tree);
+    auto expected_result = pre_order_traversal(expected_result_tree);
+    EXPECT_EQ(result, expected_result);
+}
+
+TEST_F(SetupBinaryTree, RemoveLeafNodesIterativeSimple2) {
+    auto input = medium_tree_with_subtree_duplicates2();
+    constexpr unsigned int target{3U};
+    auto result_tree = remove_leaf_nodes_iterative(input, target);
+    auto expected_result_tree = new TreeNode<unsigned int>(1);
+    expected_result_tree->left = new TreeNode<unsigned int>(2);
+    expected_result_tree->right = new TreeNode<unsigned int>(2);
+    auto result = pre_order_traversal(result_tree);
+    auto expected_result = pre_order_traversal(expected_result_tree);
+    EXPECT_EQ(result, expected_result);
+}
+
+TEST_F(SetupBinaryTree, RemoveLeafNodesIterativeSimple3) {
+    auto input = medium_tree_with_subtree_duplicates1();
+    constexpr unsigned int target{4U};
+    auto result_tree = remove_leaf_nodes_iterative(input, target);
+    auto expected_result_tree = new TreeNode<unsigned int>(1);
+    expected_result_tree->left = new TreeNode<unsigned int>(2);
+    expected_result_tree->right = new TreeNode<unsigned int>(3);
+    expected_result_tree->right->left = new TreeNode<unsigned int>(2);
+    auto result = pre_order_traversal(result_tree);
+    auto expected_result = pre_order_traversal(expected_result_tree);
+    EXPECT_EQ(result, expected_result);
+}
+
+TEST_F(SetupBinaryTree, RemoveLeafNodesIterativeSimple4) {
+    auto input = medium_tree_with_subtree_duplicates1();
+    input->left->left = new TreeNode(4U);
+    constexpr unsigned int target{4U};
+    auto result_tree = remove_leaf_nodes_iterative(input, target);
+    auto expected_result_tree = new TreeNode<unsigned int>(1);
+    expected_result_tree->left = new TreeNode<unsigned int>(2);
+    expected_result_tree->right = new TreeNode<unsigned int>(3);
+    expected_result_tree->right->left = new TreeNode<unsigned int>(2);
+    auto result = pre_order_traversal(result_tree);
+    auto expected_result = pre_order_traversal(expected_result_tree);
+    EXPECT_EQ(result, expected_result);
+}
+
+
+TEST_F(SetupBinaryTree, RemoveLeafNodesIterativeSimple5) {
+    auto input = small_tree_with_subtree_duplicates();
+    input->value = 2U;
+    constexpr unsigned int target{2U};
+    auto result_tree = remove_leaf_nodes_iterative(input, target);
+    EXPECT_EQ(result_tree, nullptr);
+}
