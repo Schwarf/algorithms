ea312383f7627c05a5096905be70ec46a2bbffb6
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-19 17:01:27 +0200

    Add tests.

---
+++ data_structures/source/test/trees/binary_trees/test_evaluate_boolean_binary_tree.cpp
@@ -0,0 +1,34 @@
+//
+// Created by andreas on 19.05.24.
+//
+#include "trees/binary_trees/evaluate_boolean_binary_tree.h"
+#include "gtest/gtest.h"
+
+
+TEST(TestEvaluateBinaryTree, simple1) {
+    auto root = new Node(2);
+    root->left = new Node(1);
+    root->right = new Node(3);
+    root->right->left = new Node(1);
+    root->right->right = new Node(0);
+    EXPECT_TRUE(evaluate_binary_tree(root));
+}
+
+TEST(TestEvaluateBinaryTree, simple2) {
+    auto root = new Node(3);
+    root->left = new Node(2);
+    root->right = new Node(0);
+    root->left->left = new Node(1);
+    root->left->right = new Node(0);
+    EXPECT_FALSE(evaluate_binary_tree(root));
+}
+
+TEST(TestEvaluateBinaryTree, simple3) {
+    auto root = new Node(0);
+    EXPECT_FALSE(evaluate_binary_tree(root));
+}
+
+TEST(TestEvaluateBinaryTree, simple4) {
+    auto root = new Node(1);
+    EXPECT_TRUE(evaluate_binary_tree(root));
+}
