ab7e311637fda1d3aed2ec93df52eabe9ae217d1
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-19 15:13:03 +0200

    Add test executables per subfolder in data-structures and debug tests.

--- data_structures/source/test/trees/binary_trees/test_find_bottom_left_tree_value.cpp
+++ data_structures/source/test/trees/binary_trees/test_find_bottom_left_tree_value.cpp
@@ -4,78 +4,68 @@
 #include "setup_binary_tree.h"
 #include "trees/binary_trees/find_bottom_left_tree_value.h"
 
-TEST_F(SetupBinaryTree, simple1)
-{
-	auto tree = small_binary_tree_root();
-	constexpr unsigned int expected_value{15};
-	EXPECT_EQ(expected_value, find_bottom_left_value(tree));
+TEST_F(SetupBinaryTree, FindBottomSimple1) {
+    auto tree = small_binary_tree_root();
+    constexpr unsigned int expected_value{15};
+    EXPECT_EQ(expected_value, find_bottom_left_value(tree));
 }
 
-TEST_F(SetupBinaryTree, simple2)
-{
-	auto tree = binary_tree_root();
-	constexpr unsigned int expected_value{4};
-	EXPECT_EQ(expected_value, find_bottom_left_value(tree));
-}
-
-
-TEST_F(SetupBinaryTree, simple3)
-{
-	auto tree = inverted_tree();
-	constexpr unsigned int expected_value{90};
-	EXPECT_EQ(expected_value, find_bottom_left_value(tree));
-}
-
-TEST_F(SetupBinaryTree, simple4)
-{
-	auto tree = imbalanced_right_tree();
-	constexpr unsigned int expected_value{12};
-	EXPECT_EQ(expected_value, find_bottom_left_value(tree));
+TEST_F(SetupBinaryTree, FindBottomSimple2) {
+    auto tree = binary_tree_root();
+    constexpr unsigned int expected_value{4};
+    EXPECT_EQ(expected_value, find_bottom_left_value(tree));
 }
 
 
-TEST_F(SetupBinaryTree, simple5)
-{
-	auto tree = palindromic_tree2();
-	constexpr unsigned int expected_value{1};
-	EXPECT_EQ(expected_value, find_bottom_left_value(tree));
+TEST_F(SetupBinaryTree, FindBottomSimple3) {
+    auto tree = inverted_tree();
+    constexpr unsigned int expected_value{90};
+    EXPECT_EQ(expected_value, find_bottom_left_value(tree));
+}
+
+TEST_F(SetupBinaryTree, FindBottomSimple4) {
+    auto tree = imbalanced_right_tree();
+    constexpr unsigned int expected_value{12};
+    EXPECT_EQ(expected_value, find_bottom_left_value(tree));
 }
 
 
-TEST_F(SetupBinaryTree, simple_bfs1)
-{
-	auto tree = small_binary_tree_root();
-	constexpr unsigned int expected_value{15};
-	EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
+TEST_F(SetupBinaryTree, FindBottomSimple5) {
+    auto tree = palindromic_tree2();
+    constexpr unsigned int expected_value{1};
+    EXPECT_EQ(expected_value, find_bottom_left_value(tree));
 }
 
 
-TEST_F(SetupBinaryTree, simple_bfs2)
-{
-	auto tree = binary_tree_root();
-	constexpr unsigned int expected_value{4};
-	EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
+TEST_F(SetupBinaryTree, simple_bfs1) {
+    auto tree = small_binary_tree_root();
+    constexpr unsigned int expected_value{15};
+    EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
 }
 
 
-TEST_F(SetupBinaryTree, simple_bfs3)
-{
-	auto tree = inverted_tree();
-	constexpr unsigned int expected_value{90};
-	EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
-}
-
-TEST_F(SetupBinaryTree, simple_bfs4)
-{
-	auto tree = imbalanced_right_tree();
-	constexpr unsigned int expected_value{12};
-	EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
+TEST_F(SetupBinaryTree, simple_bfs2) {
+    auto tree = binary_tree_root();
+    constexpr unsigned int expected_value{4};
+    EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
 }
 
 
-TEST_F(SetupBinaryTree, simple_bfs5)
-{
-	auto tree = palindromic_tree2();
-	constexpr unsigned int expected_value{1};
-	EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
+TEST_F(SetupBinaryTree, simple_bfs3) {
+    auto tree = inverted_tree();
+    constexpr unsigned int expected_value{90};
+    EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
+}
+
+TEST_F(SetupBinaryTree, simple_bfs4) {
+    auto tree = imbalanced_right_tree();
+    constexpr unsigned int expected_value{12};
+    EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
+}
+
+
+TEST_F(SetupBinaryTree, simple_bfs5) {
+    auto tree = palindromic_tree2();
+    constexpr unsigned int expected_value{1};
+    EXPECT_EQ(expected_value, find_bottom_left_value_bfs(tree));
 }
--- data_structures/source/test/trees/binary_trees/test_is_tree_even_odd_tree.cpp
+++ data_structures/source/test/trees/binary_trees/test_is_tree_even_odd_tree.cpp
@@ -5,32 +5,27 @@
 #include "setup_binary_tree.h"
 #include "trees/binary_trees/is_tree_even_odd_tree.h"
 
-TEST_F(SetupBinaryTree, simple1)
-{
-	auto tree = even_odd_tree1();
-	EXPECT_TRUE(is_even_odd_tree(tree));
+TEST_F(SetupBinaryTree, IsEvenOddTreeSimple1) {
+    auto tree = even_odd_tree1();
+    EXPECT_TRUE(is_even_odd_tree(tree));
 }
 
-TEST_F(SetupBinaryTree, simple2)
-{
-	auto tree = new TreeNode<short int>(1);
-	EXPECT_TRUE(is_even_odd_tree(tree));
+TEST_F(SetupBinaryTree, IsEvenOddTreeSimple2) {
+    auto tree = new TreeNode<short int>(1);
+    EXPECT_TRUE(is_even_odd_tree(tree));
 }
 
-TEST_F(SetupBinaryTree, simple3)
-{
-	auto tree = new TreeNode<short int>(2);
-	EXPECT_FALSE(is_even_odd_tree(tree));
+TEST_F(SetupBinaryTree, IsEvenOddTreeSimple3) {
+    auto tree = new TreeNode<short int>(2);
+    EXPECT_FALSE(is_even_odd_tree(tree));
 }
 
-TEST_F(SetupBinaryTree, simple4)
-{
-	auto tree = infected_tree1();
-	EXPECT_FALSE(is_even_odd_tree(tree));
+TEST_F(SetupBinaryTree, IsEvenOddTreeSimple4) {
+    auto tree = infected_tree1();
+    EXPECT_FALSE(is_even_odd_tree(tree));
 }
 
-TEST_F(SetupBinaryTree, simple5)
-{
-	auto tree = even_odd_tree2();
-	EXPECT_TRUE(is_even_odd_tree(tree));
+TEST_F(SetupBinaryTree, IsEvenOddTreeSimple5) {
+    auto tree = even_odd_tree2();
+    EXPECT_TRUE(is_even_odd_tree(tree));
 }
