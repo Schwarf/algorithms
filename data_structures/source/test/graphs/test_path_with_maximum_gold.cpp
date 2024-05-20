1ec58f860d6e1f01b57dd85d0db052a040d53085
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-20 16:13:04 +0200

    Add two more tests.

--- data_structures/source/test/graphs/test_path_with_maximum_gold.cpp
+++ data_structures/source/test/graphs/test_path_with_maximum_gold.cpp
@@ -11,3 +11,30 @@ TEST(TestPathWithMaximumGold, simple1) {
     constexpr float expected_result{24.f};
     EXPECT_EQ(path_with_maximum_gold(input), expected_result);
 }
+
+TEST(TestPathWithMaximumGold, simple2) {
+    std::vector<std::vector<int>> input{{1, 0, 7},
+                                        {2, 0, 6},
+                                        {3, 4, 5},
+                                        {0, 3, 0},
+                                        {9, 0, 20}};
+    constexpr int expected_result{28};
+    EXPECT_EQ(path_with_maximum_gold(input), expected_result);
+}
+
+TEST(TestPathWithMaximumGold, simple3) {
+    std::vector<std::vector<short>> input{{0, 0, 0,  3, 0, 0,  17, 0,  0, 0,  0,  0},
+                                          {0, 0, 0,  0, 0, 0,  0,  0,  0, 0,  0,  0},
+                                          {0, 0, 0,  0, 0, 0,  0,  8,  5, 15, 0,  0},
+                                          {0, 0, 0,  0, 0, 17, 0,  17, 0, 0,  12, 0},
+                                          {0, 0, 0,  4, 0, 0,  20, 0,  0, 0,  0,  0},
+                                          {0, 0, 0,  0, 0, 7,  13, 0,  0, 0,  0,  0},
+                                          {0, 0, 0,  0, 0, 0,  0,  0,  0, 0,  16, 20},
+                                          {2, 0, 0,  4, 0, 0,  0,  16, 0, 4,  0,  0},
+                                          {0, 0, 11, 0, 0, 10, 0,  0,  5, 0,  0,  18},
+                                          {2, 0, 12, 0, 0, 0,  0,  0,  0, 0,  0,  0},
+                                          {0, 0, 0,  0, 0, 0,  0,  0,  0, 0,  3,  0},
+                                          {0, 0, 0,  0, 0, 0,  0,  0,  0, 0,  0,  0}};
+    constexpr short expected_result{45};
+    EXPECT_EQ(path_with_maximum_gold(input), expected_result);
+}
