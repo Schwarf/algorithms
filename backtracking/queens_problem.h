89e33994ad595d691be0bdb2df36b5373345f06c
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-28 20:03:49 +0200

    Fix algo and tests for N-queens problem.

--- backtracking/queens_problem.h
+++ backtracking/queens_problem.h
@@ -4,58 +4,62 @@
 
 #ifndef QUEENS_PROBLEM_H
 #define QUEENS_PROBLEM_H
+
 #include <vector>
 #include <string>
 
-void depth_first_search(std::vector<std::vector<int>> &board,
-						std::vector <int> &queens,
-						int current_row,
-						int board_size,
-						std::vector<bool> &invalid_columns,
-						std::vector<bool> &invalid_forward_diagonals,
-						std::vector<bool> &invalid_backward_diagonals)
-{
-	if (current_row == board_size) {
-		board.push_back(queens);
-		return;
-	}
-	for (int col{}; col < board_size; ++col) {
-		if (invalid_columns[col] || invalid_backward_diagonals[col + current_row]
-			|| invalid_forward_diagonals[col - current_row + board_size - 1])
-			continue;
-		queens[current_row] = col;
-		invalid_columns[col] = true;
-		invalid_backward_diagonals[col + current_row] = true;
-		invalid_forward_diagonals[col - current_row + board_size - 1] = true;
-		depth_first_search(board,
+void depth_first_search(std::vector<std::vector<std::vector<int>>> &solutions,
+                        std::vector<int> &queens,
+                        int current_row,
+                        int board_size,
+                        std::vector<bool> &invalid_columns,
+                        std::vector<bool> &invalid_forward_diagonals,
+                        std::vector<bool> &invalid_backward_diagonals) {
+    if (current_row == board_size) {
+        std::vector<std::vector<int>> board(board_size, std::vector<int>(board_size, 0));
+        for (int i = 0; i < board_size; ++i) {
+            board[i][queens[i]] = 1;
+        }
+        solutions.push_back(board);
+        return;
+    }
+    for (int col{}; col < board_size; ++col) {
+        if (invalid_columns[col] || invalid_backward_diagonals[col + current_row]
+            || invalid_forward_diagonals[col - current_row + board_size - 1])
+            continue;
+        queens[current_row] = col;
+        invalid_columns[col] = true;
+        invalid_backward_diagonals[col + current_row] = true;
+        invalid_forward_diagonals[col - current_row + board_size - 1] = true;
+        depth_first_search(solutions,
                            queens,
                            current_row + 1,
                            board_size,
                            invalid_columns,
                            invalid_forward_diagonals,
                            invalid_backward_diagonals);
-		invalid_columns[col] = false;
-		invalid_backward_diagonals[col + current_row] = false;
-		invalid_forward_diagonals[col - current_row + board_size - 1] = false;
-	}
+        queens[current_row] = -1;
+        invalid_columns[col] = false;
+        invalid_backward_diagonals[col + current_row] = false;
+        invalid_forward_diagonals[col - current_row + board_size - 1] = false;
+    }
 }
 
-std::vector<std::vector<int>> solve_queen_problem(int board_size)
-{
-	auto board = std::vector<std::vector<int>>();
-	auto queens = std::vector<int>(board_size,  -1);
+std::vector<std::vector<std::vector<int>>> solve_queen_problem(int board_size) {
+    auto solutions = std::vector<std::vector<std::vector<int>>>();
+    auto queens = std::vector<int>(board_size, -1);
     auto invalid_columns = std::vector<bool>(board_size);
-    auto invalid_forward_diagonals =  std::vector<bool>(2 * board_size - 1);
+    auto invalid_forward_diagonals = std::vector<bool>(2 * board_size - 1);
     auto invalid_backward_diagonals = std::vector<bool>(2 * board_size - 1);
-	depth_first_search(board,
-					   queens,
-					   0,
-					   board_size,
+    depth_first_search(solutions,
+                       queens,
+                       0,
+                       board_size,
                        invalid_columns,
                        invalid_forward_diagonals,
                        invalid_backward_diagonals
-);
-	return board;
+    );
+    return solutions;
 
 }
 
--- backtracking/test/test_queens_problem.cpp
+++ backtracking/test/test_queens_problem.cpp
@@ -25,9 +25,66 @@ void check_solution_is_valid(const std::vector<std::vector<int>> &board) {
     }
 }
 
+TEST(NQueensTest, OneQueens) {
+    int board_size = 1;
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 1);  // There are 2 solutions for 4-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
+}
+
+TEST(NQueensTest, TwoQueens) {
+    int board_size = 2;
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 0);  // There are 2 solutions for 4-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
+}
+
+TEST(NQueensTest, ThreeQueens) {
+    int board_size = 3;
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 0);  // There are 2 solutions for 4-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
+}
+
 TEST(NQueensTest, FourQueens) {
     int board_size = 4;
-    auto solution = solve_queen_problem(board_size);
-    EXPECT_EQ(solution.size(), 2);  // There are 2 solutions for 4-queens
-    check_solution_is_valid(solution);
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 2);  // There are 2 solutions for 4-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
+}
+
+TEST(NQueensTest, FiveQueens) {
+    int board_size = 5;
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 10);  // There are 10 solutions for 5-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
+}
+
+TEST(NQueensTest, SixQueens) {
+    int board_size = 6;
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 4);  // There are 10 solutions for 5-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
+}
+
+TEST(NQueensTest, SevenQueens) {
+    int board_size = 7;
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 40);  // There are 10 solutions for 5-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
+}
+
+TEST(NQueensTest, EightQueens) {
+    int board_size = 8;
+    auto solutions = solve_queen_problem(board_size);
+    EXPECT_EQ(solutions.size(), 92);  // There are 10 solutions for 5-queens
+    for(const auto & solution : solutions)
+        check_solution_is_valid(solution);
 }
