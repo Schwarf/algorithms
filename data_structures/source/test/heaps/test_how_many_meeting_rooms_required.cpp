22fc0d8ac07d1834a709897b6147d22cb548eec0
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-30 18:44:13 +0200

    Add tests.

--- data_structures/source/test/heaps/test_how_many_meeting_rooms_required.cpp
+++ data_structures/source/test/heaps/test_how_many_meeting_rooms_required.cpp
@@ -3,9 +3,39 @@
 //
 
 #include "gtest/gtest.h"
-#include "./../how_many_meeting_rooms_required.h"
+#include "heaps/how_many_meeting_rooms_required.h"
 
-TEST(SimpleHowManyMeetingRooms, simple1)
-{
-    std::vector<std::vector<int>> meetings
+TEST(SimpleHowManyMeetingRooms, simple1) {
+    std::vector<std::vector<int>> meetings{{1, 3},
+                                           {4, 6},
+                                           {1, 5}};
+    constexpr int expected_result{2};
+    EXPECT_EQ(expected_result, how_many_meeting_rooms_required(meetings));
+}
+
+TEST(SimpleHowManyMeetingRooms, simple2) {
+    std::vector<std::vector<int>> meetings{{1, 3},
+                                           {4, 6},
+                                           {1, 5},
+                                           {1, 2}};
+    constexpr int expected_result{3};
+    EXPECT_EQ(expected_result, how_many_meeting_rooms_required(meetings));
+}
+
+TEST(SimpleHowManyMeetingRooms, simple3) {
+    std::vector<std::vector<int>> meetings{{1, 3},
+                                           {4, 6},
+                                           {6, 7},
+                                           {0, 1}};
+    constexpr int expected_result{1};
+    EXPECT_EQ(expected_result, how_many_meeting_rooms_required(meetings));
+}
+
+TEST(SimpleHowManyMeetingRooms, simple4) {
+    std::vector<std::vector<int>> meetings{{1, 3},
+                                           {2, 4},
+                                           {3, 5},
+                                           {4, 6}};
+    constexpr int expected_result{2};
+    EXPECT_EQ(expected_result, how_many_meeting_rooms_required(meetings));
 }
