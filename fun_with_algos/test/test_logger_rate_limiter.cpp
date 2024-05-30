f2b2f8100c1717e588e306012f3c6f38edf973ba
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-05-30 21:26:09 +0200

    Add tests.

--- fun_with_algos/test/test_logger_rate_limiter.cpp
+++ fun_with_algos/test/test_logger_rate_limiter.cpp
@@ -43,3 +43,43 @@ TEST(LoggerRateLimiter, simple4)
     EXPECT_TRUE(logger.shall_print_message(1, "foo3"));
     EXPECT_TRUE(logger.shall_print_message(1, "foo4"));
 }
+
+TEST(LoggerRateLimiter10Seconds, simple1)
+{
+    auto logger = LoggerOnly10Seconds();
+    EXPECT_TRUE(logger.shall_print_message(1, "foo"));
+    EXPECT_TRUE(logger.shall_print_message(2, "bar"));
+    EXPECT_FALSE(logger.shall_print_message(3, "foo"));
+    EXPECT_FALSE(logger.shall_print_message(8, "bar"));
+    EXPECT_FALSE(logger.shall_print_message(10, "foo"));
+    EXPECT_TRUE(logger.shall_print_message(11, "foo"));
+}
+
+
+TEST(LoggerRateLimiter10Seconds, simple2)
+{
+    auto logger = LoggerOnly10Seconds();
+    EXPECT_TRUE(logger.shall_print_message(1, "foo"));
+    EXPECT_TRUE(logger.shall_print_message(1, "bar"));
+    EXPECT_FALSE(logger.shall_print_message(1, "foo"));
+    EXPECT_FALSE(logger.shall_print_message(1, "bar"));
+    EXPECT_FALSE(logger.shall_print_message(2, "foo"));
+    EXPECT_TRUE(logger.shall_print_message(21, "foo"));
+}
+
+TEST(LoggerRateLimiter10Seconds, simple3)
+{
+    auto logger = LoggerOnly10Seconds();
+    EXPECT_TRUE(logger.shall_print_message(1, "foo"));
+    EXPECT_TRUE(logger.shall_print_message(11, "foo"));
+    EXPECT_TRUE(logger.shall_print_message(22, "foo"));
+}
+
+TEST(LoggerRateLimiter10Seconds, simple4)
+{
+    auto logger = LoggerOnly10Seconds();
+    EXPECT_TRUE(logger.shall_print_message(1, "foo1"));
+    EXPECT_TRUE(logger.shall_print_message(1, "foo2"));
+    EXPECT_TRUE(logger.shall_print_message(1, "foo3"));
+    EXPECT_TRUE(logger.shall_print_message(1, "foo4"));
+}
