d55400a67074305877e1daf7491708d3708c6100
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-06-16 10:37:17 +0200

    Add tests fro algo to maximize capital.

--- data_structures/source/heaps/maximize_capital_with_k_projects.h
+++ data_structures/source/heaps/maximize_capital_with_k_projects.h
@@ -25,7 +25,7 @@ int maximize_capital(int k, int start_capital, std::vector<int> &profits, std::v
     std::sort(projects.begin(), projects.end());
     std::priority_queue<int> q;
     int index{};
-    for (int i{}; i < number_of_projects; ++i) {
+    for (int i{}; i < k; ++i) {
         while (index < number_of_projects && projects[index].first <= start_capital) {
             q.push(projects[index].second);
             index++;
