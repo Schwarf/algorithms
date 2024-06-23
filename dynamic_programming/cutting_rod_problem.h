863bc87c8afc831490d782d99fe6af495aade4a9
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-06-23 17:41:16 +0200

    Add solution for memoization including tests.

--- dynamic_programming/cutting_rod_problem.h
+++ dynamic_programming/cutting_rod_problem.h
@@ -26,4 +26,25 @@ int rod_cutting(std::vector<int> &prices, int rod_length) {
     return cutting(prices, rod_length);
 }
 
+
+int cutting_memo(std::vector<int> &prices, std::vector<int> &memo, int length) {
+    if (length == 0)
+        return 0;
+    if (memo[length] != -1)
+        return memo[length];
+    int value{};
+    for (int i{1}; i <= length; ++i) {
+        int price = (i < prices.size()) ? prices[i] : 0;
+        value = std::max(value, price + cutting_memo(prices, memo, length - i));
+    }
+    memo[length] = value;
+    return value;
+}
+
+int rod_cutting_memo(std::vector<int> &prices, int rod_length) {
+    int n = prices.size();
+    std::vector<int> memo(rod_length + 1, -1);
+    return cutting_memo(prices, memo, rod_length);
+}
+
 #endif //DYNAMIC_PROGRAMMING_SAMPLES_CUTTING_ROD_PROBLEM_H
--- dynamic_programming/test/test_cutting_rod_problem.cpp
+++ dynamic_programming/test/test_cutting_rod_problem.cpp
@@ -32,3 +32,67 @@ TEST(TestCuttingRodProblem, simple4) {
     constexpr int expected_result{43};
     EXPECT_EQ(expected_result, rod_cutting(prices, length));
 }
+
+TEST(TestCuttingRodProblem, simple5) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{20};
+    constexpr int expected_result{60};
+    EXPECT_EQ(expected_result, rod_cutting(prices, length));
+}
+
+//TEST(TestCuttingRodProblem, simple6) {
+//    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+//    constexpr int length{30};
+//    constexpr int expected_result{90};
+//    EXPECT_EQ(expected_result, rod_cutting(prices, length));
+//}
+
+
+TEST(TestCuttingRodProblemMemo, simple1) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{8};
+    constexpr int expected_result{22};
+    EXPECT_EQ(expected_result, rod_cutting_memo(prices, length));
+}
+
+TEST(TestCuttingRodProblemMemo, simple2) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{10};
+    constexpr int expected_result{30};
+    EXPECT_EQ(expected_result, rod_cutting_memo(prices, length));
+}
+
+TEST(TestCuttingRodProblemMemo, simple3) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{9};
+    constexpr int expected_result{25};
+    EXPECT_EQ(expected_result, rod_cutting_memo(prices, length));
+}
+
+TEST(TestCuttingRodProblemMemo, simple4) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{15};
+    constexpr int expected_result{43};
+    EXPECT_EQ(expected_result, rod_cutting_memo(prices, length));
+}
+
+TEST(TestCuttingRodProblemMemo, simple5) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{20};
+    constexpr int expected_result{60};
+    EXPECT_EQ(expected_result, rod_cutting_memo(prices, length));
+}
+
+TEST(TestCuttingRodProblemMemo, simple6) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{30};
+    constexpr int expected_result{90};
+    EXPECT_EQ(expected_result, rod_cutting_memo(prices, length));
+}
+
+TEST(TestCuttingRodProblemMemo, simple7) {
+    std::vector<int> prices{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+    constexpr int length{100};
+    constexpr int expected_result{300};
+    EXPECT_EQ(expected_result, rod_cutting_memo(prices, length));
+}
