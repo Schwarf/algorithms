02a6db86460ee3c3f0cdfa22b6d96d6a1a0885a3
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-06-23 18:53:27 +0200

    Add alternative implementation of coin combinations and tests.

--- dynamic_programming/coin_combinations.h
+++ dynamic_programming/coin_combinations.h
@@ -12,50 +12,60 @@
 // The answer is guaranteed to fit into a signed 32-bit integer.
 #include <vector>
 #include <limits>
-int coin_combinations_recursive(const std::vector<int> &coins, int amount, int index = 0)
-{
-	if (amount == 0) {
-		return 1;
-	}
-	if (amount < 0 || index >= coins.size()) {
-		return 0;
-	}
-	int include_current_coin = coin_combinations_recursive(coins, amount - coins[index], index);
-	int exclude_current_coin = coin_combinations_recursive(coins, amount, index + 1);
-	return include_current_coin + exclude_current_coin;
+
+int coin_combinations_recursive(const std::vector<int> &coins, int amount, int index = 0) {
+    if (amount == 0) {
+        return 1;
+    }
+    if (amount < 0 || index >= coins.size()) {
+        return 0;
+    }
+    int include_current_coin = coin_combinations_recursive(coins, amount - coins[index], index);
+    int exclude_current_coin = coin_combinations_recursive(coins, amount, index + 1);
+    return include_current_coin + exclude_current_coin;
 }
 
-int memoization(const std::vector<int> &coins, int amount, std::vector<std::vector<int>> &memo, int index)
-{
-	if (amount < 0 || index >= coins.size()) {
-		return 0;
-	}
-	if (memo[index][amount] != -1)
-		return memo[index][amount];
-	if (amount == 0)
-		return 1;
-	int include_current_coin = memoization(coins, amount - coins[index], memo, index);
-	int exclude_current_coin = memoization(coins, amount, memo, index + 1);
-	memo[index][amount] = include_current_coin + exclude_current_coin;
-	return memo[index][amount];
+int coin_combinations_recursive2(const std::vector<int> &coins, int amount, int index = 0) {
+    if (amount < 0)
+        return 0;
+    if (amount == 0)
+        return 1;
+    int result{};
+    for (int i{index}; i < coins.size(); ++i) {
+        result += coin_combinations_recursive2(coins, amount - coins[i], i);
+    }
+    return result;
 }
 
-int coin_combinations_top_down(const std::vector<int> &coins, int amount, int index = 0)
-{
-	std::vector<std::vector<int>> memo(coins.size(), std::vector<int>(amount + 1, -1));
-	return memoization(coins, amount, memo, index);
+
+int memoization(const std::vector<int> &coins, int amount, std::vector<std::vector<int>> &memo, int index) {
+    if (amount < 0 || index >= coins.size()) {
+        return 0;
+    }
+    if (memo[index][amount] != -1)
+        return memo[index][amount];
+    if (amount == 0)
+        return 1;
+    int include_current_coin = memoization(coins, amount - coins[index], memo, index);
+    int exclude_current_coin = memoization(coins, amount, memo, index + 1);
+    memo[index][amount] = include_current_coin + exclude_current_coin;
+    return memo[index][amount];
 }
 
-int coin_combinations_bottom_up(const std::vector<int> &coins, int amount)
-{
-	std::vector<int> dp(amount + 1, 0);
-	dp[0] = 1;
-	for (const auto &coin: coins) {
-		for (int i = coin; i <= amount; ++i) {
-			dp[i] += dp[i - coin];
-		}
-	}
-	return dp[amount];
+int coin_combinations_top_down(const std::vector<int> &coins, int amount, int index = 0) {
+    std::vector<std::vector<int>> memo(coins.size(), std::vector<int>(amount + 1, -1));
+    return memoization(coins, amount, memo, index);
+}
+
+int coin_combinations_bottom_up(const std::vector<int> &coins, int amount) {
+    std::vector<int> dp(amount + 1, 0);
+    dp[0] = 1;
+    for (const auto &coin: coins) {
+        for (int i = coin; i <= amount; ++i) {
+            dp[i] += dp[i - coin];
+        }
+    }
+    return dp[amount];
 }
 
 
--- dynamic_programming/test/test_coin_combinations.cpp
+++ dynamic_programming/test/test_coin_combinations.cpp
@@ -6,37 +6,64 @@
 #include "gtest/gtest.h"
 #include "./../coin_combinations.h"
 
-TEST(CoinCombinationsRecursive, simple1)
-{
-	const std::vector<int> coins{1, 2, 5};
-	constexpr int amount{5};
-	constexpr int expected_number_of_combinations{4};
-	EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsRecursive, simple1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{5};
+    constexpr int expected_number_of_combinations{4};
+    EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
 }
 
-TEST(CoinCombinationsRecursive, simple2)
-{
-	const std::vector<int> coins{5};
-	constexpr int amount{4};
-	constexpr int expected_number_of_combinations{0};
-	EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsRecursive, simple2) {
+    const std::vector<int> coins{5};
+    constexpr int amount{4};
+    constexpr int expected_number_of_combinations{0};
+    EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
 }
 
-TEST(CoinCombinationsRecursive, simple3)
-{
-	const std::vector<int> coins{1};
-	constexpr int amount{7};
-	constexpr int expected_number_of_combinations{1};
-	EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsRecursive, simple3) {
+    const std::vector<int> coins{1};
+    constexpr int amount{7};
+    constexpr int expected_number_of_combinations{1};
+    EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
 }
 
 
-TEST(CoinCombinationsRecursive, complex1)
-{
-	const std::vector<int> coins{1, 2, 5};
-	constexpr int amount{500};
-	constexpr int expected_number_of_combinations{12701};
-	EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsRecursive, complex1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{500};
+    constexpr int expected_number_of_combinations{12701};
+    EXPECT_EQ(coin_combinations_recursive(coins, amount), expected_number_of_combinations);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+TEST(CoinCombinationsRecursive2, simple1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{5};
+    constexpr int expected_number_of_combinations{4};
+    EXPECT_EQ(coin_combinations_recursive2(coins, amount), expected_number_of_combinations);
+}
+
+TEST(CoinCombinationsRecursive2, simple2) {
+    const std::vector<int> coins{5};
+    constexpr int amount{4};
+    constexpr int expected_number_of_combinations{0};
+    EXPECT_EQ(coin_combinations_recursive2(coins, amount), expected_number_of_combinations);
+}
+
+TEST(CoinCombinationsRecursive2, simple3) {
+    const std::vector<int> coins{1};
+    constexpr int amount{7};
+    constexpr int expected_number_of_combinations{1};
+    EXPECT_EQ(coin_combinations_recursive2(coins, amount), expected_number_of_combinations);
+}
+
+
+TEST(CoinCombinationsRecursive2, complex1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{500};
+    constexpr int expected_number_of_combinations{12701};
+    EXPECT_EQ(coin_combinations_recursive2(coins, amount), expected_number_of_combinations);
 }
 
 // Recursive run takes long
@@ -51,85 +78,75 @@ TEST(CoinCombinationsRecursive, complex2)
 */
 
 
-TEST(CoinCombinationsTopDown, simple1)
-{
-	const std::vector<int> coins{1, 2, 5};
-	constexpr int amount{5};
-	constexpr int expected_number_of_combinations{4};
-	EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsTopDown, simple1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{5};
+    constexpr int expected_number_of_combinations{4};
+    EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
 }
 
-TEST(CoinCombinationsTopDown, simple2)
-{
-	const std::vector<int> coins{5};
-	constexpr int amount{4};
-	constexpr int expected_number_of_combinations{0};
-	EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsTopDown, simple2) {
+    const std::vector<int> coins{5};
+    constexpr int amount{4};
+    constexpr int expected_number_of_combinations{0};
+    EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
 }
 
-TEST(CoinCombinationsTopDown, simple3)
-{
-	const std::vector<int> coins{1};
-	constexpr int amount{7};
-	constexpr int expected_number_of_combinations{1};
-	EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsTopDown, simple3) {
+    const std::vector<int> coins{1};
+    constexpr int amount{7};
+    constexpr int expected_number_of_combinations{1};
+    EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
 }
 
 
-TEST(CoinCombinationsTopDown, complex1)
-{
-	const std::vector<int> coins{1, 2, 5};
-	constexpr int amount{500};
-	constexpr int expected_number_of_combinations{12701};
-	EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsTopDown, complex1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{500};
+    constexpr int expected_number_of_combinations{12701};
+    EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
 }
 
-TEST(CoinCombinationsTopDown, complex2)
-{
-	const std::vector<int> coins{3, 5, 7, 8, 9, 10, 11};
-	constexpr int amount{1000};
-	constexpr int expected_number_of_combinations{1952879228};
-	EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
-}
-
-
-TEST(CoinCombinationsBottomUp, simple1)
-{
-	const std::vector<int> coins{1, 2, 5};
-	constexpr int amount{5};
-	constexpr int expected_number_of_combinations{4};
-	EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
-}
-
-TEST(CoinCombinationsBottomUp, simple2)
-{
-	const std::vector<int> coins{5};
-	constexpr int amount{4};
-	constexpr int expected_number_of_combinations{0};
-	EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
-}
-
-TEST(CoinCombinationsBottomUp, simple3)
-{
-	const std::vector<int> coins{1};
-	constexpr int amount{7};
-	constexpr int expected_number_of_combinations{1};
-	EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsTopDown, complex2) {
+    const std::vector<int> coins{3, 5, 7, 8, 9, 10, 11};
+    constexpr int amount{1000};
+    constexpr int expected_number_of_combinations{1952879228};
+    EXPECT_EQ(coin_combinations_top_down(coins, amount), expected_number_of_combinations);
 }
 
 
-TEST(CoinCombinationsBottomUp, complex1)
-{
-	const std::vector<int> coins{1, 2, 5};
-	constexpr int amount{500};
-	constexpr int expected_number_of_combinations{12701};
-	EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsBottomUp, simple1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{5};
+    constexpr int expected_number_of_combinations{4};
+    EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
 }
 
-TEST(CoinCombinationsBottomUp, complex2)
-{
-	const std::vector<int> coins{3, 5, 7, 8, 9, 10, 11};
-	constexpr int amount{1000};
-	constexpr int expected_number_of_combinations{1952879228};
-	EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
+TEST(CoinCombinationsBottomUp, simple2) {
+    const std::vector<int> coins{5};
+    constexpr int amount{4};
+    constexpr int expected_number_of_combinations{0};
+    EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
+}
+
+TEST(CoinCombinationsBottomUp, simple3) {
+    const std::vector<int> coins{1};
+    constexpr int amount{7};
+    constexpr int expected_number_of_combinations{1};
+    EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
+}
+
+
+TEST(CoinCombinationsBottomUp, complex1) {
+    const std::vector<int> coins{1, 2, 5};
+    constexpr int amount{500};
+    constexpr int expected_number_of_combinations{12701};
+    EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
+}
+
+TEST(CoinCombinationsBottomUp, complex2) {
+    const std::vector<int> coins{3, 5, 7, 8, 9, 10, 11};
+    constexpr int amount{1000};
+    constexpr int expected_number_of_combinations{1952879228};
+    EXPECT_EQ(coin_combinations_bottom_up(coins, amount), expected_number_of_combinations);
 }
