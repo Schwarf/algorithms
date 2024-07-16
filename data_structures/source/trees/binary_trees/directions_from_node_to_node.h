b81c34b8b11548825e4645d22091d8a509755e78
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-16 16:22:56 +0200

    Add solution with lowest common ancestor.

--- data_structures/source/trees/binary_trees/directions_from_node_to_node.h
+++ data_structures/source/trees/binary_trees/directions_from_node_to_node.h
@@ -25,9 +25,10 @@
 
 
 // ---------------------------------------------------------------------------------------------------------------------
-// solution without back tracking
+// solution with back tracking
 // we build a graph, then we bfs through the graph. Startting from start_node we track all nodes (strings) in 
-// a hashmap until we reach end_node. The we use the tracking-hashmap to construct the direction-string.
+// a hashmap until we reach end_node. Then we use the tracking-hashmap to construct the direction-string and reverse
+// it to get the correct answer. Very complicated approach
 template<typename T>
 requires std::is_integral_v<T>
 TreeNode<T> *find_start_node(TreeNode<T> *node, T start_value) {
@@ -121,6 +122,71 @@ std::string get_directions(TreeNode<T> *root, T start_value, T end_value) {
 // end solution without back tracking
 // ---------------------------------------------------------------------------------------------------------------------
 
+
+
+// another solution is to find the lowest common ancestor (LCA)
+
+template<typename T>
+TreeNode<T> *find_lca(TreeNode<T> *node, T value1, T value2) {
+    if (!node)
+        return nullptr;
+
+    if (node->value == value1 || node->value == value2)
+        return node;
+
+    auto left_lca = find_lca(node->left, value1, value2);
+    auto right_lca = find_lca(node->right, value1, value2);
+
+    if (!left_lca)
+        return right_lca;
+    else if (!right_lca)
+        return left_lca;
+    // Both
+    return node;
+
+}
+
+template<typename T>
+bool find_path(TreeNode<T> *node, int value, std::string path) {
+    if (!node)
+        return false;
+    if (node->value == value)
+        return true;
+    // go down left tree
+    path.push_back('L');
+    if (find_path(node->left, value, path))
+        return true;
+    // backtrack, remove left tree if not found
+    path.pop_back();
+    // go down right tree
+
+    path.push_back('R');
+    if (find_path(node->left, value, path))
+        return true;
+    // backtrack, remove right tree if not found
+    path.pop_back();
+
+    return false;
+}
+
+template<typename T>
+std::string get_directions_lca(TreeNode<T> *root, T start_value, T end_value) {
+    auto lowest_common_ancestor = find_lca(root, start_value, end_value);
+
+    std::string path_to_start{};
+    std::string path_to_end{};
+
+    find_path(lowest_common_ancestor, start_value, path_to_start);
+    find_path(lowest_common_ancestor, end_value, path_to_end);
+
+    std::string directions{};
+    // we use the number of down-steps to start and reverse them ([L,R] --> U)
+    directions.append(path_to_start.length(), 'U');
+
+    directions.append(path_to_end);
+
+    return directions;
+}
 
 
 #endif //DATA_STRUCTURES_DIRECTIONS_FROM_NODE_TO_NODE_H
