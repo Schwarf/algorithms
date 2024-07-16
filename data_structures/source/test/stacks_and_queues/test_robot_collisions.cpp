b0f6d592f5a52efe0e1e054f396e14bc20b62c97
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-16 12:14:39 +0200

    Add complex test cases for robot collisions.

--- data_structures/source/test/stacks_and_queues/test_robot_collisions.cpp
+++ data_structures/source/test/stacks_and_queues/test_robot_collisions.cpp
@@ -69,3 +69,29 @@ TEST(TestRobotCollissions, medium3) {
     std::vector<int> expected_result{39, 33, 46, 2, 18, 24, 44, 10, 32};
     EXPECT_EQ(survivedRobotsHealths(positions, healths, directions), expected_result);
 }
+
+TEST(TestRobotCollissions, complex1) {
+    std::vector<int> positions{7, 20, 78, 41, 59, 39, 88, 44, 66, 46, 84, 16, 92, 14, 82, 19, 69, 5, 56, 13, 11, 85, 3,
+                               28, 71, 9, 34, 47, 30, 54, 91, 68, 93, 23, 79, 77, 53, 4, 45, 75, 37, 26, 42, 76, 50, 73,
+                               99, 97, 55, 48, 10};
+    std::vector<int> healths{232, 384, 466, 561, 603, 568, 515, 349, 99, 247, 726, 311, 781, 545, 567, 270, 10, 506,
+                             908, 798, 432, 647, 866, 576, 76, 810, 666, 85, 355, 155, 891, 381, 412, 65, 809, 60, 304,
+                             745, 456, 228, 559, 959, 231, 322, 366, 878, 549, 216, 932, 978, 367};
+    std::string directions("RRLLLRLRRLLRLRLRLLLLLLLRRRLRLRLRLLLLRLLLLRLLLRLLRRL");
+    std::vector<int> expected_result{232, 383, 564, 311, 545, 270, 506, 866, 807, 85, 155, 304, 745, 957, 925, 977};
+    EXPECT_EQ(survivedRobotsHealths(positions, healths, directions), expected_result);
+}
+
+TEST(TestRobotCollissions, complex2) {
+    std::vector<int> positions{7, 50, 31, 41, 27, 87, 98, 63, 18, 94, 85, 60, 73, 79, 53, 100, 32, 26, 30, 43, 22, 69,
+                               89, 40, 45, 25, 24, 65, 97, 83, 81, 37, 52, 58, 61, 55, 66, 3, 80, 51, 21, 57, 44, 49,
+                               77, 86, 47, 6, 67, 39, 64, 19, 17, 75, 33, 71, 11, 20, 10, 74, 46, 70, 54, 16, 28, 56,
+                               76, 78, 5, 35, 68};
+    std::vector<int> healths{587, 807, 457, 92, 509, 657, 233, 299, 681, 361, 193, 614, 628, 877, 130, 344, 975, 763,
+                             172, 835, 768, 444, 236, 620, 659, 445, 847, 547, 218, 800, 964, 211, 89, 502, 194, 25,
+                             136, 557, 480, 558, 461, 625, 814, 199, 32, 413, 833, 985, 821, 496, 302, 694, 135, 630,
+                             759, 839, 927, 332, 999, 174, 920, 783, 411, 526, 258, 464, 40, 745, 406, 760, 104};
+    std::string directions("LRLLLLLRLRRRRRLRRRLLLRRLRLLRLRRRLRLLRLRRLRLRRRRRRRRRRRRLRLRRLLLRLRRLLLL");
+    std::vector<int> expected_result{359, 877, 344, 969, 759, 236, 799, 964, 557, 480, 831, 984, 923, 999, 406};
+    EXPECT_EQ(survivedRobotsHealths(positions, healths, directions), expected_result);
+}
