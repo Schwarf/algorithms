70bca76a7663e1d2d03357d8f13bffc5312eee16
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-16 12:06:03 +0200

    Add first tests for robot collisions.

--- data_structures/source/test/stacks_and_queues/test_robot_collisions.cpp
+++ data_structures/source/test/stacks_and_queues/test_robot_collisions.cpp
@@ -2,3 +2,28 @@
 // Created by andreas on 16.07.24.
 //
 #include "stacks_and_queues/robot_collisions.h"
+#include "gtest/gtest.h"
+
+TEST(TestRobotCollissions, simple1) {
+    std::vector<int> positions{5, 4, 3, 2, 1};
+    std::vector<int> healths{2, 17, 9, 15, 10};
+    std::string directions("RRRRR");
+    std::vector<int> expected_result{2, 17, 9, 15, 10};
+    EXPECT_EQ(survivedRobotsHealths(positions, healths, directions), expected_result);
+}
+
+TEST(TestRobotCollissions, simple2) {
+    std::vector<int> positions{3, 5, 2, 6};
+    std::vector<int> healths{10, 10, 15, 12};
+    std::string directions("RLRL");
+    std::vector<int> expected_result{14};
+    EXPECT_EQ(survivedRobotsHealths(positions, healths, directions), expected_result);
+}
+
+TEST(TestRobotCollissions, simple3) {
+    std::vector<int> positions{3, 5, 2, 6};
+    std::vector<int> healths{10, 10, 11, 11};
+    std::string directions("RLRL");
+    std::vector<int> expected_result{};
+    EXPECT_EQ(survivedRobotsHealths(positions, healths, directions), expected_result);
+}
