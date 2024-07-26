6c01c4603ed0d3baa149bc072d412d9dc0dc2380
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-26 18:46:05 +0200

    Add algo using shortest path first.

--- data_structures/source/graphs/city_with_smallest_number_of_neighbors_in_distance.h
+++ data_structures/source/graphs/city_with_smallest_number_of_neighbors_in_distance.h
@@ -13,6 +13,7 @@
 #include <vector>
 #include <queue>
 #include <limits>
+#include <iostream>
 
 void
 dijkstra(int n, const std::vector<std::vector<std::pair<int, int>>> &graph, std::vector<int> &shortest_path_distances,
@@ -57,6 +58,7 @@ void bellman_ford(int n, const std::vector<std::vector<int>> &edges,
         }
     }
 }
+
 
 int
 city_with_smallest_number_of_neighbors_dijkstra(int n, std::vector<std::vector<int>> &edges, int max_allowed_distance) {
@@ -126,7 +128,7 @@ city_with_smallest_number_of_neighbors_bellman_ford(int n, std::vector<std::vect
     }
     return result_city;
 }
-    
+
 void floyd_warshall(int n, std::vector<std::vector<int>> &distance_matrix) {
     for (int transition_city{}; transition_city < n; ++transition_city) {
         for (int start_city{}; start_city < n; ++start_city) {
@@ -172,8 +174,66 @@ city_with_smallest_number_of_neighbors_floyd_warshall(int n, std::vector<std::ve
         }
     }
     return result_city;
-
 }
 
+void shortest_path_first(int n, const std::vector<std::vector<std::pair<int, int>>> &graph,
+                         std::vector<int> &shortest_path_distances,
+                         int start) {
+    std::queue<int> q;
+    std::vector<int> update_count(n);
+    q.push(start);
+    std::fill(shortest_path_distances.begin(), shortest_path_distances.end(), std::numeric_limits<int>::max());
+    shortest_path_distances[start] = 0;
+
+    while (!q.empty()) {
+        int current_city = q.front();
+        q.pop();
+        for (const auto &[neighbor_city, distance]: graph[current_city]) {
+            if (shortest_path_distances[neighbor_city] > shortest_path_distances[current_city] + distance) {
+                shortest_path_distances[neighbor_city] = shortest_path_distances[current_city] + distance;
+                update_count[neighbor_city]++;
+                q.push(neighbor_city);
+                if (update_count[neighbor_city] > 0) // Should not happen here but to see the algo's work.
+                    std::cerr << "Negative weight cycle detected" << std::endl;
+            }
+        }
+
+    }
+}
+
+int
+city_with_smallest_number_of_neighbors_shortest_path_first(int n, std::vector<std::vector<int>> &edges,
+                                                           int max_allowed_distance) {
+    std::vector<std::vector<std::pair<int, int>>> graph(n);
+    for (const auto &edge: edges) {
+        graph[edge[0]].emplace_back(edge[1], edge[2]);
+        graph[edge[1]].emplace_back(edge[0], edge[2]);
+    }
+    std::vector<std::vector<int>> shortest_paths(n, std::vector<int>(n, std::numeric_limits<int>::max()));
+    for (int i{}; i < n; ++i) {
+        shortest_paths[i][i] = 0;
+    }
+    for (int city{}; city < n; ++city)
+        shortest_path_first(n, graph, shortest_paths[city], city);
+
+    // get city with the fewest number of reachable cities given max_allowed_distance constraint
+    int result_city{-1};
+    int count{n};
+    for (int city1{}; city1 < n; ++city1) {
+        int reachable_count{};
+        for (int city2{}; city2 < n; ++city2) {
+            if (city2 == city1)
+                continue;
+            if (shortest_paths[city1][city2] <= max_allowed_distance) {
+                reachable_count++;
+            }
+        }
+        if (reachable_count <= count) {
+            count = reachable_count;
+            result_city = city1;
+        }
+    }
+    return result_city;
+}
 
 #endif //DATA_STRUCTURES_CITY_WITH_SMALLEST_NUMBER_OF_NEIGHBORS_IN_DISTANCE_H
