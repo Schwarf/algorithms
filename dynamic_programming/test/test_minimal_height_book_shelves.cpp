1572d92c3e41886ef5949180a02fdd2dc6c823f6
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-07-31 18:15:54 +0200

    Add one test.

--- dynamic_programming/test/test_minimal_height_book_shelves.cpp
+++ dynamic_programming/test/test_minimal_height_book_shelves.cpp
@@ -1,3 +1,18 @@
 //
 // Created by andreas on 31.07.24.
 //
+#include "./../minimal_height_book_shelves.h"
+#include "gtest/gtest.h"
+
+TEST(MinimalHeightBookShelvesTopDownDP, simple1) {
+    std::vector<std::vector<int>> books{{1, 1},
+                                        {2, 3},
+                                        {2, 3},
+                                        {1, 1},
+                                        {1, 1},
+                                        {1, 1},
+                                        {1, 2}};
+    constexpr int shelf_width{4};
+    constexpr int expected_result{6};
+    EXPECT_EQ(minimal_height_shelves(books, shelf_width), expected_result);
+}
