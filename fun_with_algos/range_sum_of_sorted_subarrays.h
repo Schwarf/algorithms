bc3552d133a84895caa4e99393439a208260800f
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-04 14:54:45 +0200

    Add very fancy algo that i have not completely understood.

--- fun_with_algos/range_sum_of_sorted_subarrays.h
+++ fun_with_algos/range_sum_of_sorted_subarrays.h
@@ -10,14 +10,15 @@
 // inclusive ('left' and 'right' are 1-indexed).
 #include <vector>
 #include <algorithm>
+#include <numeric>
 
-int range_sum_brute_force(std::vector<int> &nums, int n, int left, int right) {
+int range_sum_brute_force(std::vector<int> &input, int n, int left, int right) {
     std::vector<int> sub_sums;
-    for (int i = 0; i < nums.size(); i++) {
+    for (int i = 0; i < input.size(); i++) {
         int sum = 0;
         // Iterate through all indices ahead of the current index.
-        for (int j = i; j < nums.size(); j++) {
-            sum += nums[j];
+        for (int j = i; j < input.size(); j++) {
+            sum += input[j];
             sub_sums.push_back(sum);
         }
     }
@@ -29,5 +30,49 @@ int range_sum_brute_force(std::vector<int> &nums, int n, int left, int right) {
     }
     return result;
 }
+// This function computes the count and the sum of all subarray-sums 's' where each 's' is lower than 'target'.
+std::pair<int, long long> count_subarrays_sums_below_target(std::vector<int> &input, int n, int target) {
+    int count = 0;
+    long long current_sum{};
+    long long total_sum{};
+    long long window_sum{};
+    for (int j = 0, i = 0; j < n; ++j) {
+        current_sum += input[j];
+        // The window_sum computes the contribution of input[j] to all subarray-sums from [i..j].
+        window_sum += input[j] * (j - i + 1);
+        while (current_sum > target) {
+            window_sum -= current_sum;
+            current_sum -= input[i++];
+        }
+        count += j - i + 1;
+        total_sum += window_sum;
+    }
+    return {count, total_sum};
+}
+
+long long sum_of_first_k(std::vector<int>& nums, int n, int k) {
+    int min_sum = *min_element(nums.begin(), nums.end());
+    int max_sum = std::accumulate(nums.begin(), nums.end(), 0);
+    int left = min_sum;
+    int right = max_sum;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (count_subarrays_sums_below_target(nums, n, mid).first >= k)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    auto [count, sum] = count_subarrays_sums_below_target(nums, n, left);
+    // There can be more subarrays with the same sum of left.
+    return sum - left * (count - k);
+}
+
+int range_sum_binary_search_sliding_window(std::vector<int> &nums, int n, int left, int right) {
+    int mod{1000000007};
+    long long result = (sum_of_first_k(nums, n, right) - sum_of_first_k(nums, n, left - 1)) % mod;
+    return (result + mod) % mod;
+}
+
 
 #endif //FUN_WITH_ALGOS_RANGE_SUM_OF_SORTED_SUBARRAYS_H
