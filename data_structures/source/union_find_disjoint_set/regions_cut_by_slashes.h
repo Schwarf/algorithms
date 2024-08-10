0c8128ae399521612188850754518ef69daf4d61
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-10 13:48:28 +0200

    Add some documentation.

--- data_structures/source/union_find_disjoint_set/regions_cut_by_slashes.h
+++ data_structures/source/union_find_disjoint_set/regions_cut_by_slashes.h
@@ -14,16 +14,16 @@
 
 // We use union-find and for that we decompose each grid cell into four triangles:
 // -----------
-// |\1 /|\  /|
-// | \/2| \/ |
-// |4/\ | /\ |
-// |/ 3\|/  \|
+// |\0 /|\ 0/|              The triangles in the upper-left cell have indices: 0,1,2,3
+// | \/1|3\/1|              The triangles in the upper-right cell have indices: 4,5,6,7
+// |3/\ | /\ |              The triangles in the lower-left cell have indices: 8,9,10,11
+// |/ 2\|/ 2\|              The triangles in the lower-right cell have indices: 12,13,14,15
 // -----------
-// |\1 /|\  /|
-// | \/2| \/ |
-// |4/\ | /\ |
-// |/ 3\|/  \|
-// -----------
+// |\0 /|\ 0/|              Connecting upper-triangle 8 with lower-triangle 2 and upper-triangle 12 with lower-triangle 6 yields the formula:
+// | \/1|3\/1|              union_set(upper-triangle-index (=grid_index + 0), upper-triangle-index - 4*n + 2 (=grid_index + 0 -4*n +2)) with n = 2, since 2 x 2 grid.
+// |3/\ | /\ |
+// |/ 2\|/ 3\|              Connecting left-triangle 7  with right-triangle 1 and left-triangle 15 with right-triangle 9 yields the formula:
+// -----------              union_set(left-triangle-index (=grid_index + 3), right-triangle-index+1(=grid_index-3))
 
 class UnionFind {
 
@@ -63,8 +63,54 @@ public:
 private:
     std::vector<int> parents;
     std::vector<int> rank;
-
-
 };
+
+int count_regions_by_slahes(std::vector<std::string> &grid) {
+    int n = grid.size();
+    int number_of_triangles = n * n * 4;
+    UnionFind union_find(number_of_triangles);
+
+    for (int r = 0; r < n; ++r) {
+        for (int c = 0; c < n; ++c) {
+            int grid_index = 4 * (r * n + c);
+            char val = grid[r][c];
+
+            if (val == ' ') {
+                // Connect all four triangles if it's a blank space
+                union_find.union_set(grid_index, grid_index + 1);
+                union_find.union_set(grid_index + 1, grid_index + 2);
+                union_find.union_set(grid_index + 2, grid_index + 3);
+            } else if (val == '/') {
+                // Connect top left to bottom right
+                union_find.union_set(grid_index, grid_index + 3);
+                union_find.union_set(grid_index + 1, grid_index + 2);
+            } else if (val == '\\') {
+                // Connect top right to bottom left
+                union_find.union_set(grid_index, grid_index + 1);
+                union_find.union_set(grid_index + 2, grid_index + 3);
+            }
+
+            // Connect with the adjacent cells
+            if (r > 0) {
+                union_find.union_set(grid_index,
+                                     grid_index - 4 * n +
+                                     2); // Connect top triangle to the bottom triangle of above cell
+            }
+            if (c > 0) {
+                union_find.union_set(grid_index + 3,
+                                     grid_index - 3); // Connect left triangle to the right triangle of the left cell
+            }
+        }
+    }
+
+    // Count unique parents
+    int count{};
+    for (int i = 0; i < number_of_triangles; ++i) {
+        if (union_find.find_parent(i) == i) {
+            count++;
+        }
+    }
+    return count;
+}
 
 #endif //DATA_STRUCTURES_REGIONS_CUT_BY_SLASHES_H
