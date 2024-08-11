b03fea7b0f74a8e6b47459609fe4a2aea6439140
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-11 15:07:57 +0200

    Cleaning up old code.

--- data_structures/source/linked_lists/does_linked_list_have_loop.h
+++ data_structures/source/linked_lists/does_linked_list_have_loop.h
@@ -4,33 +4,34 @@
 
 #ifndef DOES_LINKED_LIST_HAVE_LOOP_H
 #define DOES_LINKED_LIST_HAVE_LOOP_H
+
 #include "node.h"
 
 template<typename T>
-bool does_linked_list_have_loop(Node<T> *head, Node<T> *&node)
-{
-	node = nullptr;
-	if (!head)
-		return false;
-	auto fast = head;
-	auto slow = head;
-	while (fast && fast->next) {
-		slow = slow->next;
-		fast = fast->next->next;
-		if (fast == slow)
-			break;
-	}
-	if (!fast || !fast->next)
-		return false;
+bool does_linked_list_have_loop(Node<T> *head, Node<T> *&node) {
+    node = nullptr;
+    if (!head)
+        return false;
+    auto fast = head;
+    auto slow = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+            break;
+    }
+    if (!fast || !fast->next)
+        return false;
 
-	fast = head;
-	while (slow != fast) {
-		slow = slow->next;
-		fast = fast->next;
-	}
-	node = fast;
-	return true;
+    fast = head;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    node = fast;
+    return true;
 }
+
 
 #endif //DOES_LINKED_LIST_HAVE_LOOP_H
 
--- data_structures/source/linked_lists/partition_linked_list_for_element.h
+++ data_structures/source/linked_lists/partition_linked_list_for_element.h
@@ -4,71 +4,69 @@
 
 #ifndef PARTITION_LINKED_LIST_FOR_ELEMENT_H
 #define PARTITION_LINKED_LIST_FOR_ELEMENT_H
+
 #include "node.h"
+
 template<typename T>
-Node<T> *partition_linked_list_for_element(Node<T> *head, const T &element)
-{
-	if (!head)
-		return head;
+Node<T> *partition_linked_list_for_element(Node<T> *head, const T &element) {
+    if (!head)
+        return head;
 
-	Node<T> *new_head = head;
-	Node<T> *new_tail = head;
-	while (head) {
-		auto next = head->next;
-		if (head->value < element) {
-			head->next = new_head;
-			new_head = head;
-		}
-		else {
-			new_tail->next = head;
-			new_tail = head;
-		}
-		head = next;
+    Node<T> *new_head = head;
+    Node<T> *new_tail = head;
+    while (head) {
+        auto next = head->next;
+        if (head->value < element) {
+            head->next = new_head;
+            new_head = head;
+        } else {
+            new_tail->next = head;
+            new_tail = head;
+        }
+        head = next;
 
-	}
-	new_tail->next = nullptr;
-	return new_head;
+    }
+    new_tail->next = nullptr;
+    return new_head;
 }
 
 // Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes
 // greater than or equal to x.
 // You should preserve the original relative order of the nodes in each of the two partitions.
 template<typename T>
-Node<T> *partition_linked_list_for_element_and_keep_order(Node<T> *head, const T &element)
-{
-	if (!head)
-		return nullptr;
-	auto first_greater_than_x = head;
-	Node<T> *prev_first = nullptr;
-	while (first_greater_than_x) {
-		if (first_greater_than_x->value >= element)
-			break;
-		prev_first = first_greater_than_x;
-		first_greater_than_x = first_greater_than_x->next;
-	}
+Node<T> *partition_linked_list_for_element_and_keep_order(Node<T> *head, const T &element) {
+    if (!head)
+        return nullptr;
+    auto first_greater_than_element = head;
+    Node<T> *prev_first = nullptr;
+    while (first_greater_than_element) {
+        if (first_greater_than_element->value >= element)
+            break;
+        prev_first = first_greater_than_element;
+        first_greater_than_element = first_greater_than_element->next;
+    }
 
-	if (!first_greater_than_x)
-		return head;
-	auto node = first_greater_than_x;
-	auto prev_node = prev_first;
-	while (node) {
-		if (node->value < element) {
-			prev_node->next = node->next;
-			if (prev_first)
-				prev_first->next = node;
-			else
-				head = node;
+    if (!first_greater_than_element)
+        return head;
+    auto node = first_greater_than_element;
+    auto prev_node = prev_first;
+    while (node) {
+        if (node->value < element) {
+            prev_node->next = node->next;
+            if (prev_first)
+                prev_first->next = node;
+            else
+                head = node;
 
-			node->next = first_greater_than_x;
-			prev_first = node;
-			node = prev_node->next;
-		}
-		else {
-			prev_node = node;
-			node = node->next;
-		}
-	}
-	return head;
+            node->next = first_greater_than_element;
+            prev_first = node;
+            node = prev_node->next;
+        } else {
+            prev_node = node;
+            node = node->next;
+        }
+    }
+    return head;
 }
 
 
