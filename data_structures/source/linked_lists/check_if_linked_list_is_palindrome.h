471ab3e5aaab3ff7729c51e38e7b160b9703533a
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-11 18:08:21 +0200

    Minor refactoring.

--- data_structures/source/linked_lists/check_if_linked_list_is_palindrome.h
+++ data_structures/source/linked_lists/check_if_linked_list_is_palindrome.h
@@ -7,61 +7,55 @@
 
 #include "node.h"
 #include <stack>
-template <typename T>
-Node<T> * revert(Node<T> * head)
-{
-	Node<T> * new_head = nullptr;
-	while(head)
-	{
-		auto node = new Node<T>(head->value);
-		node->next = new_head;
-		new_head =node;
-		head =head->next;
-	}
-	return new_head;
+
+template<typename T>
+Node<T> *revert_and_clone(Node<T> *head) {
+    Node<T> *new_head = nullptr;
+    while (head) {
+        auto node = new Node<T>(head->value);
+        node->next = new_head;
+        new_head = node;
+        head = head->next;
+    }
+    return new_head;
 }
 
 
-template <typename T>
-bool is_palindrome(Node<T> * head)
-{
-	if(!head)
-		return true;
-	auto new_head = revert(head);
-	while(head)
-	{
-		if(new_head->value != head->value)
-			return false;
-		head = head->next;
-		new_head = new_head->next;
-	}
-	return true;
+template<typename T>
+bool is_palindrome(Node<T> *head) {
+    if (!head)
+        return true;
+    auto new_head = revert_and_clone(head);
+    while (head) {
+        if (new_head->value != head->value)
+            return false;
+        head = head->next;
+        new_head = new_head->next;
+    }
+    return true;
 }
 
-template <typename T>
-bool is_palindrome_with_stack(Node<T> * head)
-{
-	if(!head)
-		return true;
-	auto slow = head;
-	auto fast = head;
-	std::stack<T> s;
-	while(fast && fast->next)
-	{
-		s.push(slow->value);
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-	if(fast)
-		slow = slow->next;
-	while(slow)
-	{
-		if(slow->value != s.top())
-			return false;
-		slow = slow->next;
-		s.pop();
-	}
-	return true;
+template<typename T>
+bool is_palindrome_with_stack(Node<T> *head) {
+    if (!head)
+        return true;
+    auto slow = head;
+    auto fast = head;
+    std::stack<T> s;
+    while (fast && fast->next) {
+        s.push(slow->value);
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (fast)
+        slow = slow->next;
+    while (slow) {
+        if (slow->value != s.top())
+            return false;
+        slow = slow->next;
+        s.pop();
+    }
+    return true;
 }
 
 
