466582c36f74a63c59adaf218f9453261b69eefa
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-11 14:48:49 +0200

    Start cleaning up linked list stuff.

--- data_structures/source/linked_lists/delete_nodes.h
+++ data_structures/source/linked_lists/delete_nodes.h
@@ -4,37 +4,38 @@
 
 #ifndef DELETE_NODES_H
 #define DELETE_NODES_H
+
 #include "node.h"
 
+// Here we have no access to head but only to the middle node, the one that shall be deleted
+// We do that by just by moving all the values forward by one node
 template<typename T>
-void delete_given_node_but_not_last_node(Node<T> *node_to_delete)
-{
-	if (!node_to_delete) {
-		return;
-	}
-	auto next = node_to_delete->next;
-	node_to_delete->value = next->value;
-	node_to_delete->next = next->next;
-	delete next;
+void delete_given_node_but_not_last_node(Node<T> *node_to_delete) {
+    if (!node_to_delete) {
+        return;
+    }
+    auto next = node_to_delete->next;
+    node_to_delete->value = next->value;
+    node_to_delete->next = next->next;
+    delete next;
 
 }
 
-// Here we hae no acces to head but only to the middle node, the one that shall be deleted
+// Here we just use the two runner technique
 template<typename T>
-void delete_middle_node_in_list_with_ate_least_two_elements(Node<T> *head)
-{
-	Node<T> *previous = nullptr;
-	auto slow = head;
-	auto fast = head;
-	while (fast->next && fast->next->next) {
-		previous = slow;
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-	if (previous) {
-		previous->next = slow->next;
-	}
-	delete slow;
+void delete_middle_node_in_list_with_at_least_two_elements(Node<T> *head) {
+    Node<T> *previous = nullptr;
+    auto slow = head;
+    auto fast = head;
+    while (fast->next && fast->next->next) {
+        previous = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (previous) {
+        previous->next = slow->next;
+    }
+    delete slow;
 
 }
 
--- data_structures/source/test/linked_lists/test_delete_nodes.cpp
+++ data_structures/source/test/linked_lists/test_delete_nodes.cpp
@@ -5,86 +5,80 @@
 #include "linked_lists/delete_nodes.h"
 #include <vector>
 
-TEST_F(SetupSinglyLinkedLists, delete_nodes1)
-{
-	std::vector<int> input{1, 2, 3, 4, 5};
-	auto head = get_single_list_from_vector(input);
-	auto node_to_delete = head->next->next;
-	delete_given_node_but_not_last_node(node_to_delete);
-	std::vector<int> expected_values{1, 2, 4, 5};
-	size_t index{};
-	while (head) {
-		EXPECT_EQ(head->value, expected_values[index++]);
-		head = head->next;
-	}
+TEST_F(SetupSinglyLinkedLists, delete_nodes1) {
+    std::vector<int> input{1, 2, 3, 4, 5};
+    auto head = get_single_list_from_vector(input);
+    auto node_to_delete = head->next->next;
+    delete_given_node_but_not_last_node(node_to_delete);
+    std::vector<int> expected_values{1, 2, 4, 5};
+    size_t index{};
+    while (head) {
+        EXPECT_EQ(head->value, expected_values[index++]);
+        head = head->next;
+    }
 }
 
 
-TEST_F(SetupSinglyLinkedLists, delete_nodes2)
-{
-	std::vector<int> input{1, 2, 3, 4, 5};
-	auto head = get_single_list_from_vector(input);
-	auto node_to_delete = head;
-	delete_given_node_but_not_last_node(node_to_delete);
-	std::vector<int> expected_values{2, 3, 4, 5};
-	size_t index{};
-	while (head) {
-		EXPECT_EQ(head->value, expected_values[index++]);
-		head = head->next;
-	}
+TEST_F(SetupSinglyLinkedLists, delete_nodes2) {
+    std::vector<int> input{1, 2, 3, 4, 5};
+    auto head = get_single_list_from_vector(input);
+    auto node_to_delete = head;
+    delete_given_node_but_not_last_node(node_to_delete);
+    std::vector<int> expected_values{2, 3, 4, 5};
+    size_t index{};
+    while (head) {
+        EXPECT_EQ(head->value, expected_values[index++]);
+        head = head->next;
+    }
 }
 
 
-TEST_F(SetupSinglyLinkedLists, delete_nodes3)
-{
-	std::vector<int> input{1, 2, 3, 4, 5};
-	auto head = get_single_list_from_vector(input);
-	auto node_to_delete = head->next->next->next;
-	delete_given_node_but_not_last_node(node_to_delete);
-	std::vector<int> expected_values{1, 2, 3, 5};
-	size_t index{};
-	while (head) {
-		EXPECT_EQ(head->value, expected_values[index++]);
-		head = head->next;
-	}
+TEST_F(SetupSinglyLinkedLists, delete_nodes3) {
+    std::vector<int> input{1, 2, 3, 4, 5};
+    auto head = get_single_list_from_vector(input);
+    auto node_to_delete = head->next->next->next;
+    delete_given_node_but_not_last_node(node_to_delete);
+    std::vector<int> expected_values{1, 2, 3, 5};
+    size_t index{};
+    while (head) {
+        EXPECT_EQ(head->value, expected_values[index++]);
+        head = head->next;
+    }
 }
 
-TEST_F(SetupSinglyLinkedLists, delete_middle_nodes1)
-{
-	std::vector<int> input{1, 2, 3, 4, 5};
-	auto head = get_single_list_from_vector(input);
-	delete_middle_node_in_list_with_ate_least_two_elements(head);
-	std::vector<int> expected_values{1, 2, 4, 5};
-	size_t index{};
-	while (head) {
-		EXPECT_EQ(head->value, expected_values[index++]);
-		head = head->next;
-	}
+TEST_F(SetupSinglyLinkedLists, delete_middle_nodes1) {
+    std::vector<int> input{1, 2, 3, 4, 5};
+    auto head = get_single_list_from_vector(input);
+    delete_middle_node_in_list_with_at_least_two_elements(head);
+    std::vector<int> expected_values{1, 2, 4, 5};
+    size_t index{};
+    while (head) {
+        EXPECT_EQ(head->value, expected_values[index++]);
+        head = head->next;
+    }
 }
 
-TEST_F(SetupSinglyLinkedLists, delete_middle_nodes2)
-{
-	std::vector<int> input{1, 2};
-	auto head = get_single_list_from_vector(input);
-	delete_middle_node_in_list_with_ate_least_two_elements(head);
-	std::vector<int> expected_values{2};
-	size_t index{};
-	while (head) {
-		EXPECT_EQ(head->value, expected_values[index++]);
-		head = head->next;
-	}
+TEST_F(SetupSinglyLinkedLists, delete_middle_nodes2) {
+    std::vector<int> input{1, 2};
+    auto head = get_single_list_from_vector(input);
+    delete_middle_node_in_list_with_at_least_two_elements(head);
+    std::vector<int> expected_values{2};
+    size_t index{};
+    while (head) {
+        EXPECT_EQ(head->value, expected_values[index++]);
+        head = head->next;
+    }
 }
 
 
-TEST_F(SetupSinglyLinkedLists, delete_middle_nodes3)
-{
-	std::vector<int> input{1, 2, 3, 4};
-	auto head = get_single_list_from_vector(input);
-	delete_middle_node_in_list_with_ate_least_two_elements(head);
-	std::vector<int> expected_values{1, 3, 4};
-	size_t index{};
-	while (head) {
-		EXPECT_EQ(head->value, expected_values[index++]);
-		head = head->next;
-	}
+TEST_F(SetupSinglyLinkedLists, delete_middle_nodes3) {
+    std::vector<int> input{1, 2, 3, 4};
+    auto head = get_single_list_from_vector(input);
+    delete_middle_node_in_list_with_at_least_two_elements(head);
+    std::vector<int> expected_values{1, 3, 4};
+    size_t index{};
+    while (head) {
+        EXPECT_EQ(head->value, expected_values[index++]);
+        head = head->next;
+    }
 }
