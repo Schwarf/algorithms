acf5fb35ff7acaa3d5cc4096984267dc25612378
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-17 14:17:39 +0200

    Add improved dp algo.

--- dynamic_programming/maximum_number_of_points_with_costs.h
+++ dynamic_programming/maximum_number_of_points_with_costs.h
@@ -11,7 +11,7 @@
 // and the column chosen in the row before.
 #include <vector>
 
-// O(N^2 *M) N is number of columns, M number of rows
+// O(N^2 *M) N is number of columns, M number of rows, Space-complexity O(N*M)
 long long get_maximum_cost_dp_brute_force(std::vector<std::vector<int>> &matrix) {
     int rows = matrix.size();
     int columns = matrix[0].size();
@@ -31,5 +31,42 @@ long long get_maximum_cost_dp_brute_force(std::vector<std::vector<int>> &matrix)
     return *std::max_element(dp[rows - 1].begin(), dp[rows - 1].end());
 }
 
+
+// O(N*M) N is number of columns, M number of rows, Space-complexity O(N*M
+long long get_maximum_cost_dp(std::vector<std::vector<int>> &matrix) {
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+    std::vector<long long> previous_row(columns);
+    for (int col{}; col < columns; ++col) {
+        previous_row[col] = matrix[0][col];
+    }
+    for (int row{}; row < rows - 1; ++row) {
+        std::vector<long long> left_maxima(columns);
+        std::vector<long long> right_maxima(columns);
+        std::vector<long long> current_row(columns);
+
+        left_maxima[0] = previous_row[0];
+        // We compute all the maxima left of the col-index. If the value in the previous row is the largest
+        // it is chosen. If the any value left of the current index is the largest (including the subtraction) we find it.
+        for (int col{1}; col < columns; ++col) {
+            left_maxima[col] = std::max(left_maxima[col - 1] - 1, previous_row[col]);
+        }
+        // Now the same for right
+        right_maxima[columns-1] = previous_row[columns-1];
+        for (int col{columns-2}; col > -1; --col) {
+            right_maxima[col] = std::max(right_maxima[col + 1] - 1, previous_row[col]);
+        }
+        // Now we find the maximum for the current row
+        for (int col{}; col < columns; ++col) {
+            current_row[col] = matrix[row + 1][col] + std::max(left_maxima[col], right_maxima[col]);
+        }
+        previous_row = current_row;
+    }
+    return *std::max_element(previous_row.begin(), previous_row.end());
+}
+
+}
+
 #endif //DYNAMIC_PROGRAMMING_SAMPLES_MAXIMUM_NUMBER_OF_POINTS_WITH_COSTS_H
+
 
