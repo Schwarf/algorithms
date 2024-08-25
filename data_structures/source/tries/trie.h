d731739d7cda80e33321ab50adf36a206e72565f
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-25 10:47:54 +0200

    Fix tests in tries.

--- data_structures/source/tries/trie.h
+++ data_structures/source/tries/trie.h
@@ -4,106 +4,99 @@
 
 #ifndef TRIE_H
 #define TRIE_H
+
 #include <memory>
 #include <string>
 // https://leetcode.com/problems/word-search-ii/
 
 
 template<size_t alphabet_size>
-struct TrieNode
-{
-	TrieNode *children[alphabet_size];
-	bool is_end_of_word{};
+struct TrieNode {
+    TrieNode *children[alphabet_size];
+    bool is_end_of_word{};
 };
 
 template<size_t alphabet_size>
-class Trie
-{
+class Trie {
 public:
-	Trie()
-	{
-		root_ = get_new_node();
-	}
-	void insert(const std::string &key)
-	{
-		auto node = root_;
-		for (const auto &character: key) {
-			int trie_index = character;
-			if (!node->children[trie_index])
-				node->children[trie_index] = get_new_node();
-			node = node->children[trie_index];
-		}
-		node->is_end_of_word = true;
-	}
+    Trie() {
+        root_ = get_new_node();
+    }
 
-	void remove(const std::string &key)
-	{
-		remove(root_, key, 0);
-	}
+    void insert(const std::string &key) {
+        auto node = root_;
+        for (const auto &character: key) {
+            int trie_index = character;
+            if (!node->children[trie_index])
+                node->children[trie_index] = get_new_node();
+            node = node->children[trie_index];
+        }
+        node->is_end_of_word = true;
+    }
 
-	bool search(const std::string &key)
-	{
-		auto node = root_;
-		for (const auto &character: key) {
-			auto trie_index = character;
-			if (!node->children[trie_index])
-				return false;
-			node = node->children[trie_index];
-		}
-		return node->is_end_of_word;
-	}
+    void remove(const std::string &key) {
+        remove(root_, key, 0);
+    }
 
-	bool prefix(const std::string &key)
-	{
-		auto node = root_;
-		for (const auto &character: key) {
-			auto trie_index = character;
-			if (!node->children[trie_index])
-				return false;
-			node = node->children[trie_index];
-		}
-		return true;
-	}
+    bool search(const std::string &key) {
+        auto node = root_;
+        for (const auto &character: key) {
+            auto trie_index = static_cast<int>(character);
+            if (!node->children[trie_index])
+                return false;
+            node = node->children[trie_index];
+        }
+        return node->is_end_of_word;
+    }
+
+    bool prefix(const std::string &key) {
+        auto node = root_;
+        for (const auto &character: key) {
+            auto trie_index = character;
+            if (!node->children[trie_index])
+                return false;
+            node = node->children[trie_index];
+        }
+        return true;
+    }
 
 private:
-	bool has_node_children(TrieNode<alphabet_size> *node)
-	{
-		for (int i{}; i < alphabet_size; i++)
-			if (node->children[i])
-				return true;
-		return false;
-	}
+    bool has_node_children(TrieNode<alphabet_size> *node) {
+        for (int i{}; i < alphabet_size; i++)
+            if (node->children[i])
+                return true;
+        return false;
+    }
 
-	TrieNode<alphabet_size> *remove(TrieNode<alphabet_size> *node, const std::string &key, size_t depth)
-	{
-		if (!node)
-			return nullptr;
-		if (depth == key.size()) {
-			if (node->is_end_of_word)
-				node->is_end_of_word = false;
-			if (has_node_children(node) == false) {
-				delete (node);
-				node = nullptr;
-			}
-			return node;
-		}
-		auto trie_index = static_cast<size_t>(key[depth]);
-		node->children[trie_index] = remove(node->children[trie_index], key, depth + 1);
-		if (has_node_children(node) == false && node->is_end_of_word == false) {
-			delete node;
-			node = nullptr;
-		}
-		return node;
-	}
+    TrieNode<alphabet_size> *remove(TrieNode<alphabet_size> *node, const std::string &key, size_t depth) {
+        if (!node)
+            return nullptr;
+        if (depth == key.size()) {
+            if (node->is_end_of_word)
+                node->is_end_of_word = false;
+            if (has_node_children(node) == false) {
+                delete (node);
+                node = nullptr;
+            }
+            return node;
+        }
+        auto trie_index = static_cast<size_t>(key[depth]);
+        node->children[trie_index] = remove(node->children[trie_index], key, depth + 1);
+        if (has_node_children(node) == false && node->is_end_of_word == false) {
+            delete node;
+            node = nullptr;
+        }
+        return node;
+    }
 
-	TrieNode<alphabet_size> *get_new_node()
-	{
-		auto new_node = new TrieNode<alphabet_size>();
-		for (size_t index = 0; index < alphabet_size; ++index)
-			new_node->children[index] = nullptr;
-		return new_node;
-	}
-	TrieNode<alphabet_size> *root_;
+    TrieNode<alphabet_size> *get_new_node() {
+        auto new_node = new TrieNode<alphabet_size>();
+        for (size_t index = 0; index < alphabet_size; ++index)
+            new_node->children[index] = nullptr;
+        return new_node;
+    }
+
+    TrieNode<alphabet_size> *root_;
 };
 
 
