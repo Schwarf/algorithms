542fc55d2c29cab59aaf4f87466719fdd37b14ec
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-31 16:34:40 +0200

    Add algo for dfs.

--- data_structures/source/graphs/count_sub_islands.h
+++ data_structures/source/graphs/count_sub_islands.h
@@ -63,4 +63,55 @@ int count_sub_islands_bfs(std::vector<std::vector<int>> &grid1, std::vector<std:
     return count;
 }
 
+
+bool dfs(std::vector<std::vector<int>> &grid1, std::vector<std::vector<int>> &grid2,
+         std::vector<std::vector<bool>> &visited, int row, int col) {
+    int rows = grid1.size();
+    int columns = grid1[0].size();
+    int directions[4][2] = {{0,  1},
+                            {1,  0},
+                            {0,  -1},
+                            {-1, 0}};
+    visited[row][col] = true;
+    std::stack<std::pair<int, int>> q;
+    q.emplace(row, col);
+    bool answer = true;
+    while (!q.empty()) {
+        auto [c_row, c_col] = q.top();
+        q.pop();
+        if (!grid1[c_row][c_col])
+            answer = false;
+
+        for (auto &direction: directions) {
+            int x = c_row + direction[0];
+            int y = c_col + direction[1];
+            if (x > -1 && y > -1 && x < rows && y < columns &&
+                !visited[x][y] && grid2[x][y]) {
+                q.emplace(x, y);
+                visited[x][y] = true;
+            }
+        }
+
+    }
+    return answer;
+}
+
+
+int count_sub_islands_dfs(std::vector<std::vector<int>> &grid1, std::vector<std::vector<int>> &grid2) {
+    int rows = grid1.size();
+    int columns = grid1[0].size();
+    int count{};
+    std::vector<std::vector<bool>> visited(rows, std::vector<bool>(columns));
+    for (int row{}; row < rows; ++row) {
+        for (int col{}; col < columns; ++col) {
+            if (visited[row][col])
+                continue;
+            if (grid2[row][col] && dfs(grid1, grid2, visited, row, col)) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 #endif //DATA_STRUCTURES_COUNT_SUB_ISLANDS_H
