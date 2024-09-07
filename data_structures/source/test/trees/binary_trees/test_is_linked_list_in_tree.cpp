eceeacdab126f91e02dade811665d970df33bca9
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-09-07 20:54:43 +0200

    Add tests.

--- data_structures/source/test/trees/binary_trees/test_is_linked_list_in_tree.cpp
+++ data_structures/source/test/trees/binary_trees/test_is_linked_list_in_tree.cpp
@@ -24,6 +24,29 @@ TEST_F(SetupBinaryTreesAndLinkedLists, TestIsLinkedListInTreeSimple1) {
     auto list_head = get_single_list_from_vector(std::vector<unsigned int>{15, 10, 12});
     auto tree = binary_tree_root();
     EXPECT_TRUE(is_linked_list_in_tree(list_head, tree));
+}
+
+TEST_F(SetupBinaryTreesAndLinkedLists, TestIsLinkedListInTreeSimple2) {
+    auto list_head = get_single_list_from_vector(std::vector<unsigned int>{15, 11, 12});
+    auto tree = binary_tree_root();
+    EXPECT_FALSE(is_linked_list_in_tree(list_head, tree));
+}
+
+TEST_F(SetupBinaryTreesAndLinkedLists, TestIsLinkedListInTreeSimple3) {
+    auto list_head = get_single_list_from_vector(std::vector<int>{1, 2, 5, 10});
+    auto tree = infected_tree2();
+    EXPECT_TRUE(is_linked_list_in_tree(list_head, tree));
+}
 
 
+TEST_F(SetupBinaryTreesAndLinkedLists, TestIsLinkedListInTreeSimple4) {
+    Node<int> *list_head = nullptr;
+    auto tree = infected_tree2();
+    EXPECT_TRUE(is_linked_list_in_tree(list_head, tree));
+}
+
+TEST_F(SetupBinaryTreesAndLinkedLists, TestIsLinkedListInTreeSimple5) {
+    auto list_head = get_single_list_from_vector(std::vector<int>{1, 2, 5, 10});;
+    TreeNode<int> *tree = nullptr;
+    EXPECT_FALSE(is_linked_list_in_tree(list_head, tree));
 }
