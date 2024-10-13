12e178d5a90406ac3c5c0fe6670065ff53beab83
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-13 11:17:13 +0200

    Add algo to find largest min-max-gap in array using two pointers.

--- fun_with_algos/largest_min_max_index_gap .h
+++ fun_with_algos/largest_min_max_index_gap .h
@@ -14,29 +14,52 @@
 #include <numeric>
 
 template<typename T>
-T largest_min_max_index_gap_sort(std::vector<T> & input)
-{
+T largest_min_max_index_gap_sort(std::vector<T> &input) {
     int n = input.size();
-    if( n < 2)
+    if (n < 2)
         return 0;
     std::vector<int> indices(n);
     std::iota(indices.begin(), indices.end(), 0);
     // Sort indices based on corresponding values in input.
     // Indices are reordered so that input-elements are arranged in increasing order
-    std::sort(indices.begin(), indices.end(), [&](int i, int j)
-    {
+    std::sort(indices.begin(), indices.end(), [&](int i, int j) {
         return input[i] != input[j] ? input[i] < input[j] : i < j;
     });
 
     int min_index_so_far = n;
     int max_gap = 0;
-    for(int i{}; i < n; ++i)
-    {
+    for (int i{}; i < n; ++i) {
         max_gap = std::max(max_gap, indices[i] - min_index_so_far);
         min_index_so_far = std::min(min_index_so_far, indices[i]);
     }
     return max_gap;
+}
 
+template<typename T>
+T largest_min_max_index_gap_two_pointers(std::vector<T> &input) {
+    int n = input.size();
+    if (n < 2)
+        return 0;
+    std::vector<T> right_maximums(n);
+    right_maximums[n-1] = input[n-1];
+    for(int i{n-2}; i>=0; i--)
+    {
+        right_maximums[i] = std::max(right_maximums[i+1], input[i]);
+    }
+    int left{};
+    int right{};
+    int max_gap{};
+    while(right < n)
+    {
+        while(left < right && input[left] > right_maximums[right])
+        {
+            left++;
+        }
+        max_gap = std::max(max_gap, right -left);
+        right++;
+    }
+
+    return max_gap;
 }
 
 
