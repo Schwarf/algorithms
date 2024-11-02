48e8885458777fdfcb1518378e21c695a4fde9f1
Schwarf <andreas.b.scharf@gmail.com>
2024-11-02 11:15:49 +0100

    First benchmarks.

--- sorting_algorithms/performance_measurements/benchmark/benchmark.cpp
+++ sorting_algorithms/performance_measurements/benchmark/benchmark.cpp
@@ -6,6 +6,11 @@
 #include <algorithm>
 #include <functional>
 #include <random>
+#include "./../../heap_sort.h"
+#include "./../../insertion_sort.h"
+#include "./../../merge_sort.h"
+#include "./../../quick_sort.h"
+
 
 template <typename T>
 requires std::is_arithmetic_v<T>
@@ -24,12 +29,47 @@ static void generic_sorting_benchmark(benchmark::State& state, const std::functi
     }
 }
 
-void std_sort(std::vector<int>& v) { std::sort(v.begin(), v.end()); }
-void std_stable_sort(std::vector<int>& v) { std::stable_sort(v.begin(), v.end()); }
+template <typename T>
+requires std::is_arithmetic_v<T>
+void std_sort(std::vector<T>& v) { std::sort(v.begin(), v.end()); }
+
+template <typename T>
+requires std::is_arithmetic_v<T>
+void std_stable_sort(std::vector<T>& v) { std::stable_sort(v.begin(), v.end()); }
 
 
-BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_sort, std_sort)->Arg(1000);
-BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_stable_sort, std_stable_sort)->Arg(1000);
+template <typename T>
+requires std::is_arithmetic_v<T>
+void my_heap_sort(std::vector<T>& v) { heap_sort(v); }
 
+
+template <typename T>
+requires std::is_arithmetic_v<T>
+void my_insertion_sort(std::vector<T>& v) { insertion_sort(v); }
+
+template <typename T>
+requires std::is_arithmetic_v<T>
+void my_merge_sort(std::vector<T>& v) { merge_sort(v); }
+
+template <typename T>
+requires std::is_arithmetic_v<T>
+void my_quick_sort(std::vector<T>& v) { quick_sort(v); }
+
+
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_sort, std_sort<int>)->Arg(1000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_stable_sort, std_stable_sort<int>)->Arg(1000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_heap_sort, my_heap_sort<int>)->Arg(1000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_insertion_sort, my_insertion_sort<int>)->Arg(1000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_merge_sort, my_merge_sort<int>)->Arg(1000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_quick_sort, my_quick_sort<int>)->Arg(1000);
+
+
+
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_sort, std_sort<int>)->Arg(10000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_stable_sort, std_stable_sort<int>)->Arg(10000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_heap_sort, my_heap_sort<int>)->Arg(10000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_insertion_sort, my_insertion_sort<int>)->Arg(10000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_merge_sort, my_merge_sort<int>)->Arg(10000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, my_quick_sort, my_quick_sort<int>)->Arg(10000);
 
 BENCHMARK_MAIN();
