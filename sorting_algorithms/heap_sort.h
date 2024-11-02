71370f502a3c53cf5bfe62d08fbed363d213dc37
Schwarf <andreas.b.scharf@gmail.com>
2024-11-02 19:20:44 +0100

    Using comparator in heap sort.

--- sorting_algorithms/heap_sort.h
+++ sorting_algorithms/heap_sort.h
@@ -7,86 +7,85 @@
 
 #include "used_concepts.h"
 
-template<typename Container>
-requires IndexedContainer<Container> && NoPointerElement<typename Container::value_type> && Sortable<typename Container::value_type>
-void heapify(Container &container, int n, int index) {
+#include <concepts>
+#include <functional>
+#include <utility>
+#include <vector>
+#include <algorithm>
 
+// Assuming IndexedContainer and NoPointerElement concepts are defined elsewhere
+// IndexedContainer checks if the container is indexable, and NoPointerElement checks if elements are not pointers
+
+template<typename Container, typename Comparator>
+requires IndexedContainer<Container> && NoPointerElement<typename Container::value_type> && Sortable<typename Container::value_type>
+void heapify(Container &container, int n, int index, Comparator comparator) {
     int index_largest_element = index; // index is root element and shall be largest of 3 (root, left child, right child)
     int index_left_child = 2 * index + 1;
     int index_right_child = 2 * index + 2;
 
-    if (index_left_child < n && container[index_left_child] > container[index_largest_element])
+    if (index_left_child < n && comparator(container[index_largest_element], container[index_left_child]))
         index_largest_element = index_left_child;
 
-    if (index_right_child < n && container[index_right_child] > container[index_largest_element])
+    if (index_right_child < n && comparator(container[index_largest_element], container[index_right_child]))
         index_largest_element = index_right_child;
 
     if (index_largest_element != index) {
         std::swap(container[index], container[index_largest_element]);
-        heapify<Container>(container, n, index_largest_element);
+        heapify(container, n, index_largest_element, comparator);
     }
-
 }
 
-// heapify for container with pointers
-template<typename Container>
+// heapify function with comparator for pointer elements
+template<typename Container, typename Comparator>
 requires IndexedContainer<Container> && (!NoPointerElement<typename Container::value_type>)
-void heapify(Container &container, int n, int index) {
-
+void heapify(Container &container, int n, int index, Comparator comparator) {
     int index_largest_element = index; // index is root element and shall be largest of 3 (root, left, right child)
     int index_left_child = 2 * index + 1;
     int index_right_child = 2 * index + 2;
 
-    if (index_left_child < n && *container[index_left_child] > *container[index_largest_element])
+    if (index_left_child < n && comparator(*container[index_largest_element], *container[index_left_child]))
         index_largest_element = index_left_child;
 
-    if (index_right_child < n && *container[index_right_child] > *container[index_largest_element])
+    if (index_right_child < n && comparator(*container[index_largest_element], *container[index_right_child]))
         index_largest_element = index_right_child;
 
     if (index_largest_element != index) {
         std::swap(*container[index], *container[index_largest_element]);
-        heapify<Container>(container, n, index_largest_element);
+        heapify(container, n, index_largest_element, comparator);
     }
-
 }
 
-
-template<typename Container>
+// heap_sort function with comparator for general elements
+template<typename Container, typename Comparator = std::less<>>
 requires IndexedContainer<Container> && NoPointerElement<typename Container::value_type>
-void heap_sort(Container &container) {
+void heap_sort(Container &container, Comparator comparator = Comparator()) {
     if (!container.size())
         return;
     int n = container.size();
-    // First we heapify the array, that means we have find the maximum element in the array and propagate it to index 0
-    // We do that starting from index= n/2-1 (with children at 2*index+1 and 2*index+2) and go all the way down to 0.
+
     for (int index = n / 2 - 1; index >= 0; --index)
-        heapify(container, n, index);
+        heapify(container, n, index, comparator);
 
     for (int index = n - 1; index > 0; --index) {
-        // we swap the current maximum to the end
         std::swap(container[0], container[index]);
-        // we heapify the remaining array
-        heapify<Container>(container, index, 0);
+        heapify(container, index, 0, comparator);
     }
 }
 
-// heap sort for container with pointers
-template<typename Container>
+// heap_sort function with comparator for pointer elements
+template<typename Container, typename Comparator = std::less<>>
 requires IndexedContainer<Container> && (!NoPointerElement<typename Container::value_type>)
-void heap_sort(Container &container) {
+void heap_sort(Container &container, Comparator comparator = Comparator()) {
     if (!container.size())
         return;
     int n = container.size();
-    // First we heapify the array, that means we have find the maximum element in the array and propagate it to index 0
-    // We do that starting from index= n/2-1 (with children at 2*index+1 and 2*index+2) and go all the way down to 0.
+
     for (int index = n / 2 - 1; index >= 0; --index)
-        heapify(container, n, index);
+        heapify(container, n, index, comparator);
 
     for (int index = n - 1; index > 0; --index) {
-        // we swap the current maximum to the end
-        std::swap(container[0], container[index]);
-        // we heapify the remaining array
-        heapify<Container>(container, index, 0);
+        std::swap(*container[0], *container[index]);
+        heapify(container, index, 0, comparator);
     }
 }
 
