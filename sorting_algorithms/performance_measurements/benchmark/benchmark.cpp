3e7e323b8ccf6b3e1e2d8a78b91436670e667192
Schwarf <andreas.b.scharf@gmail.com>
2024-11-02 10:31:23 +0100

    Add first working example using std::sort's.

--- sorting_algorithms/performance_measurements/benchmark/benchmark.cpp
+++ sorting_algorithms/performance_measurements/benchmark/benchmark.cpp
@@ -7,13 +7,15 @@
 #include <functional>
 #include <random>
 
-static void generic_sorting_benchmark(benchmark::State& state, const std::function<void(std::vector<int>&)>& sort_func) {
-    std::vector<int> data(state.range(0));
-    std::mt19937 generator(42);
+template <typename T>
+requires std::is_arithmetic_v<T>
+static void generic_sorting_benchmark(benchmark::State& state, const std::function<void(std::vector<T>&)>& sort_func) {
+    std::vector<T> data(state.range(0));
+    std::mt19937 generator(427891);
     std::generate(data.begin(), data.end(), generator);
 
     for (auto _ : state) {
-        std::vector<int> temp = data;
+        auto temp = data;
         benchmark::DoNotOptimize(temp);
 
         sort_func(temp); // Call the provided sorting function
@@ -22,4 +24,12 @@ static void generic_sorting_benchmark(benchmark::State& state, const std::functi
     }
 }
 
+void std_sort(std::vector<int>& v) { std::sort(v.begin(), v.end()); }
+void std_stable_sort(std::vector<int>& v) { std::stable_sort(v.begin(), v.end()); }
 
+
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_sort, std_sort)->Arg(1000);
+BENCHMARK_CAPTURE(generic_sorting_benchmark<int>, std_stable_sort, std_stable_sort)->Arg(1000);
+
+
+BENCHMARK_MAIN();
