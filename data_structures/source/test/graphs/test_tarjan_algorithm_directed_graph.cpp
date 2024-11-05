deff93437e308abc435bab8309f20196e5bb62d4
Schwarf <andreas.b.scharf@gmail.com>
2024-11-05 20:07:57 +0100

    Add one more test for SSC Tarjan test.

--- data_structures/source/test/graphs/test_tarjan_algorithm_directed_graph.cpp
+++ data_structures/source/test/graphs/test_tarjan_algorithm_directed_graph.cpp
@@ -32,3 +32,21 @@ TEST(TestTarjanAlgoSSC, simple2)
     std::set<std::set<int>> expected_sscs{first_SSC, second_SSC, third_SSC, fourth_SSC};
     EXPECT_EQ(ssc_sets, expected_sscs);
 }
+
+TEST(TestTarjanAlgoSSC, simple3)
+{
+    DirectedGraph<int> digraph{
+            {1, 2, 3, 4, 5, 6}, {{2, 1}, {2, 4}, {3, 1}, {3, 4}, {3, 6}, {4, 1}, {4, 2}}
+    };
+
+    auto ssc_sets = strongly_connected_components_tarjan(digraph);
+    constexpr int expected_number_of_strongly_connected_components{5};
+    EXPECT_EQ(ssc_sets.size(), expected_number_of_strongly_connected_components);
+    const std::set<int> first_SSC{1};
+    const std::set<int> second_SSC{2, 4};
+    const std::set<int> third_SSC{3};
+    const std::set<int> fourth_SSC{5};
+    const std::set<int> fifth_SSC{6};
+    std::set<std::set<int>> expected_sscs{first_SSC, second_SSC, third_SSC, fourth_SSC, fifth_SSC};
+    EXPECT_EQ(ssc_sets, expected_sscs);
+}
