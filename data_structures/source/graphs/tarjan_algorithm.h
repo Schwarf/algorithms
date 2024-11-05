96089c0f9a5b1a2ae0a8fdc2fafd47b688ceb569
Schwarf <andreas.b.scharf@gmail.com>
2024-11-05 17:37:19 +0100

    Work on Tarjan algo.

--- data_structures/source/graphs/tarjan_algorithm.h
+++ data_structures/source/graphs/tarjan_algorithm.h
@@ -10,34 +10,48 @@
 #include "used_concepts.h"
 #include <vector>
 
+#include <iostream>  // For debugging output
+
 template <typename T>
-void dfs_tarjan(std::vector<std::vector<T>>& graph, T node, std::vector<bool>& visited, std::vector<T>& discovered,
-                std::vector<T>& ancestor_reachability_value, std::vector<T>& parent, int time)
+void dfs_tarjan_undirected(std::vector<std::vector<T>>& graph, T node, std::vector<bool>& visited, std::vector<T>& discovery_time,
+                std::vector<T>& ancestor_reachability_value, std::vector<T>& parent, int & time)
 {
     visited[node] = true;
-    discovered[node] = ancestor_reachability_value[node] = ++time;
+    discovery_time[node] = ancestor_reachability_value[node] = ++time;
+
+    std::cout << "Visiting node " << node << " | discovery_time: " << discovery_time[node]
+              << " | ancestor_reachability_value: " << ancestor_reachability_value[node] << std::endl;
+
     for (auto neighbor_node : graph[node])
     {
         if (!visited[neighbor_node])
         {
-            //expand the dfs tree
+            // Expand the DFS tree
             parent[neighbor_node] = node;
-            dfs_tarjan(graph, neighbor_node, visited, discovered, ancestor_reachability_value, parent, time);
+            dfs_tarjan(graph, neighbor_node, visited, discovery_time, ancestor_reachability_value, parent, time);
+
+            // Update ancestor reachability based on DFS child
             ancestor_reachability_value[node] = std::min(ancestor_reachability_value[node],
                                                          ancestor_reachability_value[neighbor_node]);
+
+            std::cout << "Backtracking to node " << node << " from child " << neighbor_node
+                      << " | Updated ancestor_reachability_value: " << ancestor_reachability_value[node] << std::endl;
+
         }
         else if (neighbor_node != parent[node])
         {
-            // we found a back-edge because if neighbor_node is not a parent of node it must be an ancestor of node
-            // that has been visited earlier
-            ancestor_reachability_value[node] = std::min(ancestor_reachability_value[node], discovered[neighbor_node]);
+            // Update low value for back edges
+            ancestor_reachability_value[node] = std::min(ancestor_reachability_value[node], discovery_time[neighbor_node]);
+
+            std::cout << "Found back edge from node " << node << " to ancestor " << neighbor_node
+                      << " | Updated ancestor_reachability_value: " << ancestor_reachability_value[node] << std::endl;
         }
     }
 }
 
 
 template <typename T>
-std::pair<std::vector<T>, std::vector<T>> tarjan_algorithm(std::vector<std::vector<T>>& edges, int number_of_nodes)
+std::pair<std::vector<T>, std::vector<T>> tarjan_algorithm_undirected(std::vector<std::vector<T>>& edges, int number_of_nodes)
 {
     std::vector<std::vector<T>> graph(number_of_nodes);
     for (const auto& edge : edges)
@@ -48,18 +62,24 @@ std::pair<std::vector<T>, std::vector<T>> tarjan_algorithm(std::vector<std::vect
     T invalid = -1;
 
     std::vector visited(number_of_nodes, false);
-    std::vector<T> discovered(number_of_nodes, invalid);
+    std::vector<T> discovery_time(number_of_nodes, invalid);
     std::vector<T> ancestor_reachability_value(number_of_nodes, invalid);
     std::vector<T> parent(number_of_nodes, invalid);
     int time{};
 
     for (T node{}; node < number_of_nodes; ++node)
     {
-        if (!visited[node])
-            dfs_tarjan(graph, node, visited, discovered, ancestor_reachability_value, parent, time);
+        if (!visited[node]) {
+            std::cout << "\nStarting DFS at node " << node << std::endl;
+            dfs_tarjan(graph, node, visited, discovery_time, ancestor_reachability_value, parent, time);
+        }
     }
-    return std::make_pair(ancestor_reachability_value, discovered);
+    return std::make_pair(ancestor_reachability_value, discovery_time);
 }
 
 
+
+
 #endif //LR_PLANARITY_TEST_H
+
+
