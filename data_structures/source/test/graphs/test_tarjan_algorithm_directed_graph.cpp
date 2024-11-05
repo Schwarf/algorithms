a555fd37c6f113639f7e17fb9db53cbc386dfb7d
Schwarf <andreas.b.scharf@gmail.com>
2024-11-05 21:41:27 +0100

    Add more tests for tarjan algo.

--- data_structures/source/test/graphs/test_tarjan_algorithm_directed_graph.cpp
+++ data_structures/source/test/graphs/test_tarjan_algorithm_directed_graph.cpp
@@ -7,7 +7,9 @@
 TEST(TestTarjanAlgoSSC, simple1)
 {
     DirectedGraph<int> digraph{
-        {1, 2}, {2, 3}, {2, 8}, {3, 4}, {3, 7}, {4, 5}, {5, 3}, {5, 6}, {7, 4}, {7, 6}, {8, 1}, {8, 7}
+        {1, 2, 3, 4, 5, 6, 7, 8}, {
+            {1, 2}, {2, 3}, {2, 8}, {3, 4}, {3, 7}, {4, 5}, {5, 3}, {5, 6}, {7, 4}, {7, 6}, {8, 1}, {8, 7}
+        }
     };
     auto ssc_sets = strongly_connected_components_tarjan(digraph);
     constexpr int expected_number_of_strongly_connected_components{3};
@@ -21,7 +23,7 @@ TEST(TestTarjanAlgoSSC, simple1)
 
 TEST(TestTarjanAlgoSSC, simple2)
 {
-    DirectedGraph<int> digraph{{2 ,  1}, { 2 ,  3}, { 3 ,  4}, { 3 ,  5}, { 4 ,  2}, { 4 ,  5}, { 6 ,  4}};
+    DirectedGraph<int> digraph{{1, 2, 3, 4, 5, 6}, {{2, 1}, {2, 3}, {3, 4}, {3, 5}, {4, 2}, {4, 5}, {6, 4}}};
     auto ssc_sets = strongly_connected_components_tarjan(digraph);
     constexpr int expected_number_of_strongly_connected_components{4};
     EXPECT_EQ(ssc_sets.size(), expected_number_of_strongly_connected_components);
@@ -36,7 +38,7 @@ TEST(TestTarjanAlgoSSC, simple2)
 TEST(TestTarjanAlgoSSC, simple3)
 {
     DirectedGraph<int> digraph{
-            {1, 2, 3, 4, 5, 6}, {{2, 1}, {2, 4}, {3, 1}, {3, 4}, {3, 6}, {4, 1}, {4, 2}}
+        {1, 2, 3, 4, 5, 6}, {{2, 1}, {2, 4}, {3, 1}, {3, 4}, {3, 6}, {4, 1}, {4, 2}}
     };
 
     auto ssc_sets = strongly_connected_components_tarjan(digraph);
@@ -48,5 +50,58 @@ TEST(TestTarjanAlgoSSC, simple3)
     const std::set<int> fourth_SSC{5};
     const std::set<int> fifth_SSC{6};
     std::set<std::set<int>> expected_sscs{first_SSC, second_SSC, third_SSC, fourth_SSC, fifth_SSC};
+    EXPECT_EQ(ssc_sets, expected_sscs);
+}
+
+
+TEST(TestTarjanAlgoSSC, medium1)
+{
+    DirectedGraph<int> digraph{
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+        {
+            {1, 11}, {1, 15}, {3, 10}, {4, 1}, {4, 15}, {5, 14}, {6, 8},
+            {7, 3}, {8, 4}, {8, 7}, {9, 2}, {9, 8}, {9, 11}, {11, 12},
+            {12, 4}, {12, 10}, {13, 9}, {13, 11}, {14, 2}, {15, 5}
+        }
+    };
+
+    auto ssc_sets = strongly_connected_components_tarjan(digraph);
+    constexpr int expected_number_of_strongly_connected_components{12};
+    EXPECT_EQ(ssc_sets.size(), expected_number_of_strongly_connected_components);
+    const std::set<int> first_SSC{1, 4, 1, 112};
+    const std::set<int> second_SSC{2, 4};
+    const std::set<int> third_SSC{3};
+    const std::set<int> fourth_SSC{5};
+    const std::set<int> fifth_SSC{6};
+    std::set<std::set<int>> expected_sscs{{2}, {14}, {5}, {15}, {10}, {1, 4, 11, 12}, {3}, {7}, {8}, {6}, {9}, {13}};
+    EXPECT_EQ(ssc_sets, expected_sscs);
+}
+
+
+TEST(TestTarjanAlgoSSC, medium2)
+{
+    DirectedGraph<int> digraph{
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
+        {{1, 18}, {2, 9}, {2, 10}, {3, 9},
+            {4, 9}, {4, 18}, {5, 1},{5, 13}, {6, 1}, {6, 2},
+            {6, 3}, {6, 8}, {6, 13},{7, 4}, {7, 9}, {7, 15},
+            {8, 16}, {8, 19}, {9, 5},{9, 11}, {10, 12}, {11, 7},
+            {12, 11}, {12, 20}, {13, 15},{15, 11}, {17, 1}, {17, 3},
+            {17, 18}, {18, 17}
+        }
+    };
+
+    auto ssc_sets = strongly_connected_components_tarjan(digraph);
+    constexpr int expected_number_of_strongly_connected_components{10};
+    EXPECT_EQ(ssc_sets.size(), expected_number_of_strongly_connected_components);
+    const std::set<int> first_SSC{1, 4, 1, 112};
+    const std::set<int> second_SSC{2, 4};
+    const std::set<int> third_SSC{3};
+    const std::set<int> fourth_SSC{5};
+    const std::set<int> fifth_SSC{6};
+    std::set<std::set<int>> expected_sscs{
+        {1, 3, 4, 5, 7, 9, 11, 13, 15, 17, 18},
+        {20}, {12}, {10}, {2}, {16}, {19}, {8}, {6}, {14}
+    };
     EXPECT_EQ(ssc_sets, expected_sscs);
 }
