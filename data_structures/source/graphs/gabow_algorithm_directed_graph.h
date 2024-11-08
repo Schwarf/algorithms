4cc1f016d44e98ec8196c5df92c17f4c43ef950c
Schwarf <andreas.b.scharf@gmail.com>
2024-11-08 18:18:34 +0100

    Minor reformat and docu.

--- data_structures/source/graphs/gabow_algorithm_directed_graph.h
+++ data_structures/source/graphs/gabow_algorithm_directed_graph.h
@@ -11,34 +11,42 @@ template <typename NodeType>
 void gabow_dfs(NodeType current_node, const DirectedGraph<NodeType>& graph,
                std::unordered_map<NodeType, int>& dicover_order,
                std::stack<NodeType>& path_stack, std::stack<int>& boundary_stack,
-               std::unordered_set<NodeType>& identified,
-               std::set<std::set<NodeType>>& SCCs, int& current_index) {
-
+               std::unordered_set<NodeType>& identified_scc_nodes,
+               std::set<std::set<NodeType>>& SCCs, int& current_index)
+{
     dicover_order[current_node] = current_index++;
     path_stack.push(current_node);
     boundary_stack.push(dicover_order[current_node]);
 
-    for (NodeType neighbor : graph.get_neighbors(current_node)) {
-        if (!dicover_order.contains(neighbor)) {
-            gabow_dfs(neighbor, graph, dicover_order, path_stack, boundary_stack, identified, SCCs, current_index);
+    for (auto& neighbor : graph.get_neighbors(current_node))
+    {
+        if (!dicover_order.contains(neighbor))
+        {
+            gabow_dfs(neighbor, graph, dicover_order, path_stack, boundary_stack, identified_scc_nodes, SCCs,
+                      current_index);
         }
-        if (!identified.contains(neighbor)) {
-            while (boundary_stack.top() > dicover_order[neighbor]) {
+        if (!identified_scc_nodes.contains(neighbor))
+        {
+            while (boundary_stack.top() > dicover_order[neighbor])
+            {
                 boundary_stack.pop();
             }
         }
     }
 
-    if (boundary_stack.top() == dicover_order[current_node]) {
+    if (boundary_stack.top() == dicover_order[current_node])
+    {
         boundary_stack.pop();
         std::set<NodeType> scc;
         NodeType node;
-        do {
+        do
+        {
             node = path_stack.top();
             path_stack.pop();
-            identified.insert(node);
+            identified_scc_nodes.insert(node);
             scc.insert(node);
-        } while (node != current_node);
+        }
+        while (node != current_node);
         SCCs.insert(scc);
     }
 }
@@ -50,13 +58,16 @@ std::set<std::set<NodeType>> strongly_connected_components_gabow(const DirectedG
     std::unordered_map<NodeType, int> discover_index;
     std::stack<NodeType> path_stack;
     std::stack<int> boundary_stack;
-    std::unordered_set<NodeType> identified;
+    std::unordered_set<NodeType> identified_scc_nodes;
     std::set<std::set<NodeType>> strongly_connected_components;
     int current_index{};
 
-    for (NodeType node : graph.get_all_nodes()) {
-        if (!discover_index.contains(node)) {
-            gabow_dfs(node, graph, discover_index, path_stack, boundary_stack, identified, strongly_connected_components, current_index);
+    for (auto& node : graph.get_all_nodes())
+    {
+        if (!discover_index.contains(node))
+        {
+            gabow_dfs(node, graph, discover_index, path_stack, boundary_stack, identified_scc_nodes,
+                      strongly_connected_components, current_index);
         }
     }
     return strongly_connected_components;
