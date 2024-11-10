3189576501a91531db7c9b38b8bc6cbbfd79fe89
Schwarf <andreas.b.scharf@gmail.com>
2024-11-10 10:13:11 +0100

    Add alterantive for

--- fun_with_strings/are_strings_equal_upto_rotation.h
+++ fun_with_strings/are_strings_equal_upto_rotation.h
@@ -10,19 +10,29 @@
 #include <algorithm>
 #include <string>
 
-inline bool are_strings_equal_brute_force(std::string & input1, const std::string & input2)
+inline bool are_strings_equal_brute_force(std::string & input1, const std::string & target_string)
 {
-    if (input1.length() != input2.length()) return false;
+    if (input1.length() != target_string.length()) return false;
     int length = input1.length();
 
     // Try all possible rotations of the string
     for (int rotationCount = 0; rotationCount < length; ++rotationCount) {
         // Perform one rotation
         std::rotate(input1.begin(), input1.begin() + 1, input1.end());
-        if (input1 == input2)
+        if (input1 == target_string)
             return true;
     }
     return false;
+}
+
+// TRICK: If we double input1 we have a string that contains all rotations of input1
+// In that way we just need to search for the target_string in the double_string
+inline bool are_strings_equal_double_string(std::string & input1, const std::string & target_string)
+{
+    if (input1.length() != target_string.length())
+        return false;
+    auto double_string = input1 + input1;
+    return double_string.find(target_string) < double_string.length();
 }
 
 
--- fun_with_strings/test/test_are_strings_equal_upto_rotation.cpp
+++ fun_with_strings/test/test_are_strings_equal_upto_rotation.cpp
@@ -11,29 +11,61 @@
 TEST(AreStringsRotateEqualBruteForce, simple1)
 {
     std::string input1{"abcdef"};
-    std::string input2{"bcdefg"};
-    EXPECT_FALSE(are_strings_equal_brute_force(input1, input2));
+    std::string target_string{"bcdefg"};
+    EXPECT_FALSE(are_strings_equal_brute_force(input1, target_string));
 }
 
 TEST(AreStringsRotateEqualBruteForce, simple2)
 {
     std::string input1{"a"};
-    std::string input2{"bas"};
-    EXPECT_FALSE(are_strings_equal_brute_force(input1, input2));
+    std::string target_string{"bas"};
+    EXPECT_FALSE(are_strings_equal_brute_force(input1, target_string));
 }
 
 TEST(AreStringsRotateEqualBruteForce, simple3)
 {
     std::string input1{"asb"};
-    std::string input2{"bas"};
-    EXPECT_TRUE(are_strings_equal_brute_force(input1, input2));
+    std::string target_string{"bas"};
+    EXPECT_TRUE(are_strings_equal_brute_force(input1, target_string));
 }
 
 TEST(AreStringsRotateEqualBruteForce, simple4)
 {
     std::string input1{"asb"};
-    std::string input2{"bas"};
-    EXPECT_TRUE(are_strings_equal_brute_force(input1, input2));
+    std::string target_string{"bas"};
+    EXPECT_TRUE(are_strings_equal_brute_force(input1, target_string));
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(AreStringsRotateEqualDoubleString, simple1)
+{
+    std::string input1{"abcdef"};
+    std::string target_string{"bcdefg"};
+    EXPECT_FALSE(are_strings_equal_double_string(input1, target_string));
+}
+
+TEST(AreStringsRotateEqualDoubleString, simple2)
+{
+    std::string input1{"a"};
+    std::string target_string{"bas"};
+    EXPECT_FALSE(are_strings_equal_double_string(input1, target_string));
+}
+
+TEST(AreStringsRotateEqualDoubleString, simple3)
+{
+    std::string input1{"asb"};
+    std::string target_string{"bas"};
+    EXPECT_TRUE(are_strings_equal_double_string(input1, target_string));
+}
+
+TEST(AreStringsRotateEqualDoubleString, simple4)
+{
+    std::string input1{"asb"};
+    std::string target_string{"bas"};
+    EXPECT_TRUE(are_strings_equal_double_string(input1, target_string));
 }
 
 #endif //TEST_ARE_STRINGS_EQUAL_UPTO_ROTATION_H
