36aa150165f758f90581fd06402508ab9254d28d
Schwarf <andreas.b.scharf@gmail.com>
2024-11-15 18:32:29 +0100

    Work on some ToDo's.

--- fun_with_strings/test/test_sort_string_by_character_frequency.cpp
+++ fun_with_strings/test/test_sort_string_by_character_frequency.cpp
@@ -5,23 +5,33 @@
 #include "gtest/gtest.h"
 #include "./../sort_string_by_character_frequency.h"
 
+bool has_same_character_frequencies(const std::string &input, const std::string &output) {
+	std::unordered_map<char, int> input_freq, output_freq;
+	for (char c : input) input_freq[c]++;
+	for (char c : output) output_freq[c]++;
+	return input_freq == output_freq;
+}
+
 TEST(SortStringByCharacterFrequency, simple1)
 {
 	const std::string input{"tree"};
-	const std::string expected_output{"eetr"};
-	EXPECT_EQ(expected_output, sorted_by_character_frequency(input));
+	const std::string output = sorted_by_character_frequency(input);
+	EXPECT_TRUE(has_same_character_frequencies(input, output));
+	EXPECT_EQ(output.size(), input.size());
 }
 
 TEST(SortStringByCharacterFrequency, simple2)
 {
 	const std::string input{"aaaccc"};
-	const std::string expected_output{"cccaaa"};
-	EXPECT_EQ(expected_output, sorted_by_character_frequency(input));
+	const std::string output = sorted_by_character_frequency(input);
+	EXPECT_TRUE(has_same_character_frequencies(input, output));
+	EXPECT_EQ(output.size(), input.size());
 }
 
 TEST(SortStringByCharacterFrequency, simple3)
 {
 	const std::string input{"abbcccdddd"};
-	const std::string expected_output{"ddddcccbba"};
-	EXPECT_EQ(expected_output, sorted_by_character_frequency(input));
-}
+	const std::string output = sorted_by_character_frequency(input);
+	EXPECT_TRUE(has_same_character_frequencies(input, output));
+	EXPECT_EQ(output.size(), input.size());
+	}
