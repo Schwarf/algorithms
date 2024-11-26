8eef072e9d680bef8ab2312df202d9312fc48e8f
Schwarf <andreas.b.scharf@gmail.com>
2024-11-26 19:08:54 +0100

    Add insertion sort.

--- sorting_algorithms/optimized_sort.h
+++ sorting_algorithms/optimized_sort.h
@@ -5,28 +5,60 @@
 #ifndef OPTIMIZED_SORT_H
 #define OPTIMIZED_SORT_H
 #include <algorithm>
+#include <limits>
 
-template<typename T, typename Comparator>
-constexpr void sort_net2(T& x, T& y, Comparator comparator) {
-    if (comparator(y, x)) {
+
+template <std::random_access_iterator RandomAccessIterator, typename Comparator>
+bool insertion_sort(RandomAccessIterator begin, RandomAccessIterator end, Comparator comp,
+                    const int max_distance = std::numeric_limits<int>::max())
+{
+    for (auto iterator = begin + 1; iterator < end; ++iterator)
+    {
+        auto element = std::move(*iterator);
+        decltype(iterator) other_iterator;
+        int distance{};
+
+        for (other_iterator = iterator; other_iterator > begin && comp(element, *(other_iterator - 1)); --
+             other_iterator)
+        {
+            *other_iterator = std::move(*(other_iterator - 1));
+            if (++distance > max_distance)
+                return false;
+        }
+
+        *other_iterator = element;
+    }
+
+    return true;
+}
+
+
+template <typename T, typename Comparator>
+constexpr void sort_net2(T& x, T& y, Comparator comparator)
+{
+    if (comparator(y, x))
+    {
         std::swap(x, y);
     }
 }
 
-template<typename T, typename Comparator>
-constexpr void sort2(T &a, T &b, Comparator comparator) {
+template <typename T, typename Comparator>
+constexpr void sort2(T& a, T& b, Comparator comparator)
+{
     sort_net2(a, b, comparator);
 };
 
-template<typename T, typename Comparator>
-constexpr void sort3(T &a, T &b, T &c, Comparator comparator) {
+template <typename T, typename Comparator>
+constexpr void sort3(T& a, T& b, T& c, Comparator comparator)
+{
     sort_net2(b, c, comparator);
     sort_net2(a, c, comparator);
     sort_net2(a, b, comparator);
 };
 
-template<typename T, typename Comparator>
-constexpr void sort4(T &a, T &b, T &c, T &d, Comparator comparator) {
+template <typename T, typename Comparator>
+constexpr void sort4(T& a, T& b, T& c, T& d, Comparator comparator)
+{
     sort_net2(a, b, comparator);
     sort_net2(c, d, comparator);
     sort_net2(a, c, comparator);
@@ -34,8 +66,9 @@ constexpr void sort4(T &a, T &b, T &c, T &d, Comparator comparator) {
     sort_net2(b, c, comparator);
 };
 
-template<typename T, typename Comparator>
-constexpr void sort5(T &a, T &b, T &c, T &d, T &e, Comparator comparator) {
+template <typename T, typename Comparator>
+constexpr void sort5(T& a, T& b, T& c, T& d, T& e, Comparator comparator)
+{
     sort_net2(a, b, comparator);
     sort_net2(d, e, comparator);
     sort_net2(c, e, comparator);
@@ -47,8 +80,9 @@ constexpr void sort5(T &a, T &b, T &c, T &d, T &e, Comparator comparator) {
     sort_net2(b, c, comparator);
 };
 
-template<typename T, typename Comparator>
-constexpr void sort6(T &a, T &b, T &c, T &d, T &e, T &f, Comparator comparator) {
+template <typename T, typename Comparator>
+constexpr void sort6(T& a, T& b, T& c, T& d, T& e, T& f, Comparator comparator)
+{
     sort_net2(b, c, comparator);
     sort_net2(e, f, comparator);
     sort_net2(a, c, comparator);
