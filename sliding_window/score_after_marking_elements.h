a355e6a1230cfa1f60b28ad5a9e2e85d6ac99ce2
Schwarf <andreas.b.scharf@gmail.com>
2024-12-19 16:49:35 +0100

    Add sliding windows algo to get score for marking all elements.

--- sliding_window/score_after_marking_elements.h
+++ sliding_window/score_after_marking_elements.h
@@ -16,29 +16,51 @@
 // Return the score you get after applying the above algorithm.
 #include<vector>
 #include<queue>
-long long find_score_using_heap(std::vector<int>& input) {
+
+long long find_score_using_heap(std::vector<int>& input)
+{
     std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> q;
     int n = input.size();
-    for(int i {}; i < n; ++i)
+    for (int i{}; i < n; ++i)
     {
         q.emplace(input[i], i);
     }
     std::vector<unsigned char> marked(n);
     long long score{};
-    while(!q.empty())
+    while (!q.empty())
     {
         auto [value, index] = q.top();
         q.pop();
-        if(!marked[index])
+        if (!marked[index])
         {
             marked[index] = 1;
-            if(index -1 >= 0)
-                marked[index-1] = 1;
-            if(index +1 < n)
-                marked[index+1] = 1;
+            if (index - 1 >= 0)
+                marked[index - 1] = 1;
+            if (index + 1 < n)
+                marked[index + 1] = 1;
             score += value;
         }
     }
     return score;
 }
+
+long long find_score_using_sliding_window(std::vector<int>& input)
+{
+    long long result{};
+    int n = input.size();
+    for (int i{}; i < n; i += 2)
+    {
+        int left = i;
+        while (i + 1 < n && input[i + 1] < input[i])
+        {
+            i++;
+        }
+        for (int right = i; right >= left; right -= 2)
+        {
+            result += input[right];
+        }
+    }
+    return result;
+}
+
 #endif //SCORE_AFTER_MARKING_ELEMENTS_H
