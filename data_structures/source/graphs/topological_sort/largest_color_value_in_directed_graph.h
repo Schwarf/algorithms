0b2296c06a08981095ca7de7aa594fc0030f16ac
Schwarf <andreas.b.scharf@gmail.com>
2024-12-28 15:02:50 +0100

    Format code.

--- data_structures/source/graphs/topological_sort/largest_color_value_in_directed_graph.h
+++ data_structures/source/graphs/topological_sort/largest_color_value_in_directed_graph.h
@@ -12,45 +12,48 @@
 // in this graph (0-indexed). You are also given a 2D array edges where edges[j] = [aj, bj] indicates that there
 // is a directed edge from node aj to node bj.
 // A valid path in the graph is a sequence of nodes x1 -> x2 -> x3 -> ... -> xk such that there is a directed edge
-// from xi to xi+1 for every 1 <= i < k. The color value of the path is the number of nodes that are colored the most frequently occurring color along that path.
-// Return the largest color value of any valid path in the given graph, or -1 if the graph contains a cycle.
+// from xi to xi+1 for every 1 <= i < k. The color value of the path is the number of nodes that are colored the most
+// frequently occurring color along that path. Return the largest color value of any valid path in the
+// given graph, or -1 if the graph contains a cycle.
 
 // TAG: Topological sort
-int largest_color_value_in_directed_graph(std::string & colors, std::vector<std::vector<int>> & edges)
+int largest_color_value_in_directed_graph(std::string& colors, std::vector<std::vector<int>>& edges)
 {
     int n = colors.size();
     std::vector<std::vector<int>> graph(n);
-    std::vector<int> indegree(n);
-    for(const auto & edge : edges)
+    std::vector<int> in_degree(n);
+    for (const auto& edge : edges)
     {
         graph[edge[0]].push_back(edge[1]);
-        indegree[edge[1]]++;
+        in_degree[edge[1]]++;
     }
 
-    std::vector<std::vector<int>> count(n, std::vector<int>(26) );
+    std::vector<std::vector<int>> count(n, std::vector<int>(26));
     std::queue<int> q;
-    for(int i{}; i < n; i++)
+    for (int i{}; i < n; i++)
     {
-        if(indegree[i] == 0)
+        if (in_degree[i] == 0)
             q.push(i);
     }
     int result{};
     int node_visited{};
-    while(!q.empty())
+    while (!q.empty())
     {
-        auto current = q.front();
+        const auto current = q.front();
         q.pop();
         result = std::max(result, ++count[current][colors[current] - 'a']);
         node_visited++;
-        for(const auto neighbor : graph[current])
+        for (const auto neighbor : graph[current])
         {
-            for(int i{}; i < 26; i++){
+            for (int i{}; i < 26; i++)
+            {
                 // Try to update the frequency of colors for neighbor to include paths
-                    // that use node->neighbor edge.
-                        count[neighbor][i] = std::max(count[neighbor][i], count[current][i]);
+                // that use node->neighbor edge.
+                count[neighbor][i] = std::max(count[neighbor][i], count[current][i]);
             }
-            indegree[neighbor]--;
-            if (indegree[neighbor] == 0) {
+            in_degree[neighbor]--;
+            if (in_degree[neighbor] == 0)
+            {
                 q.push(neighbor);
             }
         }
