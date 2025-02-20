d47cdba40b070b51da77da1a99e34d5837743062
Schwarf <andreas.b.scharf@gmail.com>
2025-02-20 19:16:40 +0100

    Add tests.

--- backtracking/test/test_construct_lexicographically_largest_sequence.cpp
+++ backtracking/test/test_construct_lexicographically_largest_sequence.cpp
@@ -1,8 +1,34 @@
 //
-// Created by andreas on 20.02.25.
+// Created by andreas on 15.02.25.
 //
 #include "gtest/gtest.h"
 #include "./../construct_lexicographically_largest_sequence.h"
 
 
-TEST(   )
+TEST(TestConstructLexicographicallyLargestSequence, simple1)
+{
+    constexpr int n{2};
+    const std::vector<int> expected_result{2, 1, 2};
+    EXPECT_EQ(expected_result, construct_distanced_sequence(n));
+}
+
+TEST(TestConstructLexicographicallyLargestSequence, simple2)
+{
+    constexpr int n{3};
+    const std::vector<int> expected_result{3, 1, 2, 3, 2};
+    EXPECT_EQ(expected_result, construct_distanced_sequence(n));
+}
+
+TEST(TestConstructLexicographicallyLargestSequence, simple3)
+{
+    constexpr int n{5};
+    const std::vector<int> expected_result{5, 3, 1, 4, 3, 5, 2, 4, 2};
+    EXPECT_EQ(expected_result, construct_distanced_sequence(n));
+}
+
+TEST(TestConstructLexicographicallyLargestSequence, simple4)
+{
+    constexpr int n{10};
+    const std::vector<int> expected_result{10, 8, 6, 9, 3, 1, 7, 3, 6, 8, 10, 5, 9, 7, 4, 2, 5, 2, 4};
+    EXPECT_EQ(expected_result, construct_distanced_sequence(n));
+}
