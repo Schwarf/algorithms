37ef967790fb66681421c3f77697510287b05343
Schwarf <andreas.b.scharf@gmail.com>
2025-02-25 17:08:18 +0100

    Add prefix version.

--- prefix_sums/count_subarrays_with_odd_sum.h
+++ prefix_sums/count_subarrays_with_odd_sum.h
@@ -26,4 +26,30 @@ int count_odd_subarray_sums_brute_force(const std::vector<int>& input)
 }
 
 
+
+int count_odd_subarray_sums(std::vector<int>& input) {
+    constexpr int mod = 1e9 + 7;
+    int count{};
+    int prefix_sum{};
+    int oddCount{};
+    int evenCount{1};
+
+    for (const auto value : input) {
+        prefix_sum += value;
+        // If current prefix sum is odd, add the number of even
+        // subarrays
+        if ( prefix_sum & 1) {
+            count += evenCount;
+            oddCount++;
+        } else {
+        // If current prefix sum is even, add the number of odd subarrays
+            count += oddCount;
+            evenCount++;
+        }
+
+        count %= mod;
+    }
+
+    return count;
+}
 #endif //COUNT_SUBARRAYS_WITH_ODD_SUM_H
