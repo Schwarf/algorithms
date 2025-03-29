97233eee7f2d5df83a2af6b278f9495e81284976
Schwarf <andreas.b.scharf@gmail.com>
2025-03-29 08:28:16 +0100

    Add tests.

---
+++ data_structures/source/test/graphs/test_shortest_path_in_grid_with_obstacles.cpp
@@ -0,0 +1,55 @@
+//
+// Created by andreas on 10.01.25.
+//
+#include "gtest/gtest.h"
+#include "graphs/shortest_path_in_grid_with_obstacles.h"
+
+TEST(TestShortestPathINGridWithObstacles, simple1)
+{
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    constexpr int k{1};
+    constexpr int expected_result{6};
+    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles(grid, k));
+}
+
+TEST(TestShortestPathINGridWithObstacles, simple2)
+{
+    std::vector<std::vector<int>> grid = {{0, 1, 1}, {1, 1, 1}, {1, 0, 0}};
+    constexpr int k{1};
+    constexpr int expected_result{-1};
+    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles(grid, k));
+}
+
+TEST(TestShortestPathINGridWithObstacles, simple3)
+{
+    std::vector<std::vector<int>> grid = {{0, 1, 1}, {1, 1, 1}, {1, 0, 0}};
+    constexpr int k{2};
+    constexpr int expected_result{4};
+    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles(grid, k));
+}
+
+TEST(TestShortestPathINGridWithObstacles, simple4)
+{
+    std::vector<std::vector<int>> grid = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+    };
+    constexpr int k{1};
+    constexpr int expected_result{20};
+    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles(grid, k));
+}
+
+TEST(TestShortestPathINGridWithObstacles, simple5)
+{
+    std::vector<std::vector<int>> grid = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+    };
+    constexpr int k{5};
+    constexpr int expected_result{20};
+    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles(grid, k));
+}
