8beffe362ba4613ac16669e7354e51388651da32
Schwarf <andreas.b.scharf@gmail.com>
2025-03-29 11:44:22 +0100

    Add tests for AStar in grid with obstacles.

--- data_structures/source/test/graphs/test_shortest_path_in_grid_with_obstacles.cpp
+++ data_structures/source/test/graphs/test_shortest_path_in_grid_with_obstacles.cpp
@@ -8,50 +8,84 @@ TEST(TestShortestPathINGridWithObstacles, simple1)
 {
     std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
     std::pair<int, int> source = {0, 0};
-    std::pair<int, int> target = {2, 4};
+    std::pair<int, int> target = {4, 2};
     constexpr int expected_result{10};
     EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
 }
-/*
+
 TEST(TestShortestPathINGridWithObstacles, simple2)
 {
-    std::vector<std::vector<int>> grid = {{0, 1, 1}, {1, 1, 1}, {1, 0, 0}};
-    constexpr int k{1};
-    constexpr int expected_result{-1};
-    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles_removed(grid, k));
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 0, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {4, 2};
+    constexpr int expected_result{8};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
 }
+
 
 TEST(TestShortestPathINGridWithObstacles, simple3)
 {
-    std::vector<std::vector<int>> grid = {{0, 1, 1}, {1, 1, 1}, {1, 0, 0}};
-    constexpr int k{2};
-    constexpr int expected_result{4};
-    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles_removed(grid, k));
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 1}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {4, 2};
+    constexpr int expected_result{-1};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
 }
+
 
 TEST(TestShortestPathINGridWithObstacles, simple4)
 {
-    std::vector<std::vector<int>> grid = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
-    };
-    constexpr int k{1};
-    constexpr int expected_result{20};
-    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles_removed(grid, k));
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    std::pair<int, int> source = {1, 2};
+    std::pair<int, int> target = {4, 0};
+    constexpr int expected_result{5};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
 }
+
 
 TEST(TestShortestPathINGridWithObstacles, simple5)
 {
     std::vector<std::vector<int>> grid = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
-        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
-        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
     };
-    constexpr int k{5};
-    constexpr int expected_result{20};
-    EXPECT_EQ(expected_result, shortest_path_with_k_obstactles_removed(grid, k));
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {11, 9};
+
+    constexpr int expected_result{26};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
 }
-*/
+
+TEST(TestShortestPathINGridWithObstacles, simple6)
+{
+    std::vector<std::vector<int>> grid = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {11, 9};
+
+    constexpr int expected_result{20};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
+}
+
