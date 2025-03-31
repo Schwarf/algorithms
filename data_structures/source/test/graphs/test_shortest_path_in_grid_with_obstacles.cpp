7462586af3ab97bbcce3a8482b9381651a45dadf
Schwarf <andreas.b.scharf@gmail.com>
2025-03-31 18:46:02 +0200

    Add tests for BFS solution.

--- data_structures/source/test/graphs/test_shortest_path_in_grid_with_obstacles.cpp
+++ data_structures/source/test/graphs/test_shortest_path_in_grid_with_obstacles.cpp
@@ -4,6 +4,16 @@
 #include "gtest/gtest.h"
 #include "graphs/shortest_path_in_grid_with_obstacles.h"
 
+
+TEST(TestShortestPathINGridWithObstacles, simple0)
+{
+    std::vector<std::vector<int>> grid = {{}};
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {4, 2};
+    constexpr int expected_result{-1};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
+}
+
 TEST(TestShortestPathINGridWithObstacles, simple1)
 {
     std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
@@ -87,4 +97,101 @@ TEST(TestShortestPathINGridWithObstacles, simple6)
 
     constexpr int expected_result{20};
     EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles(grid, source, target));
+}
+//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(TestShortestPathINGridWithObstaclesBFS, simple0)
+{
+    std::vector<std::vector<int>> grid = {{}};
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {4, 2};
+    constexpr int expected_result{-1};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles_bfs(grid, source, target));
+}
+
+TEST(TestShortestPathINGridWithObstaclesBFS, simple1)
+{
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {4, 2};
+    constexpr int expected_result{10};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles_bfs(grid, source, target));
+}
+
+TEST(TestShortestPathINGridWithObstaclesBFS, simple2)
+{
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 0, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {4, 2};
+    constexpr int expected_result{8};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles_bfs(grid, source, target));
+}
+
+
+TEST(TestShortestPathINGridWithObstaclesBFS, simple3)
+{
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 1}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {4, 2};
+    constexpr int expected_result{-1};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles_bfs(grid, source, target));
+}
+
+
+TEST(TestShortestPathINGridWithObstaclesBFS, simple4)
+{
+    std::vector<std::vector<int>> grid = {{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}};
+    std::pair<int, int> source = {1, 2};
+    std::pair<int, int> target = {4, 0};
+    constexpr int expected_result{5};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles_bfs(grid, source, target));
+}
+
+
+TEST(TestShortestPathINGridWithObstaclesBFS, simple5)
+{
+    std::vector<std::vector<int>> grid = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+    };
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {11, 9};
+
+    constexpr int expected_result{26};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles_bfs(grid, source, target));
+}
+
+TEST(TestShortestPathINGridWithObstaclesBFS, simple6)
+{
+    std::vector<std::vector<int>> grid = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+        {0, 1, 0, 1, 1, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 1, 1, 1, 1, 1, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    std::pair<int, int> source = {0, 0};
+    std::pair<int, int> target = {11, 9};
+
+    constexpr int expected_result{20};
+    EXPECT_EQ(expected_result, shortest_path_in_grid_with_obstacles_bfs(grid, source, target));
 }
