92c3c770531dc704d4626d7b34b3aed0f7bacf8a
Schwarf <andreas.b.scharf@gmail.com>
2025-04-06 14:41:04 +0200

    Add first tests.

--- data_structures/source/test/graphs/test_pacific_atlantic_flow.cpp
+++ data_structures/source/test/graphs/test_pacific_atlantic_flow.cpp
@@ -3,3 +3,19 @@
 //
 #include "graphs/pacific_atlantic_flow.h"
 #include "gtest/gtest.h"
+
+TEST(TestPacificAtlanticFlow, simple1)
+{
+    std::vector<std::vector<int>> input{
+        {1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}
+    };
+    std::vector<std::vector<int>> expected_output{{0, 4}, {1, 3}, {1, 4}, {2, 2}, {3, 0}, {3, 1}, {4, 0}};
+    EXPECT_EQ(expected_output, pacific_atlanctic_flow(input));
+}
+
+TEST(TestPacificAtlanticFlow, simple2)
+{
+    std::vector<std::vector<int>> input{{1}};
+    std::vector<std::vector<int>> expected_output{{0, 0}};
+    EXPECT_EQ(expected_output, pacific_atlanctic_flow(input));
+}
