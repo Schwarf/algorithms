e4b1178e6442aeb37946ec137b51eb435fedceeb
Schwarf <andreas.b.scharf@gmail.com>
2025-04-14 20:36:17 +0200

    Add optimal solution for number of valid triplets.

--- prefix_sums/number_of_valid_triplets.h
+++ prefix_sums/number_of_valid_triplets.h
@@ -13,36 +13,71 @@
 // Return the number of valid ways to select 3 buildings.
 #include <vector>
 #include <string>
-long long number_of_valid_triplets(const std::string & input) {
+
+long long number_of_valid_triplets(const std::string& input)
+{
     int n = input.size();
     std::vector<int> zero_prefix(n);
     std::vector<int> one_prefix(n);
 
     zero_prefix[0] = (input[0] == '0');
     one_prefix[0] = (input[0] == '1');
-    for(int i{1}; i< n; ++i)
+    for (int i{1}; i < n; ++i)
     {
-        zero_prefix[i] = zero_prefix[i-1] + (input[i] == '0');
-        one_prefix[i] = one_prefix[i-1] + (input[i] == '1');
+        zero_prefix[i] = zero_prefix[i - 1] + (input[i] == '0');
+        one_prefix[i] = one_prefix[i - 1] + (input[i] == '1');
     }
     long long result{};
-    for(int i{1}; i < n-1; ++i)
+    for (int i{1}; i < n - 1; ++i)
     {
-        if(input[i] == '0')
+        if (input[i] == '0')
         {
-            int left_ones = one_prefix[i-1];
-            int right_ones = one_prefix[n-1] - one_prefix[i];
-            result += static_cast<long long> (left_ones)*right_ones;
+            int left_ones = one_prefix[i - 1];
+            int right_ones = one_prefix[n - 1] - one_prefix[i];
+            result += static_cast<long long>(left_ones) * right_ones;
         }
         else
         {
-            int left_zeros = zero_prefix[i-1];
-            int right_zeros = zero_prefix[n-1] - zero_prefix[i];
-            result += static_cast<long long> (left_zeros) *right_zeros;
+            int left_zeros = zero_prefix[i - 1];
+            int right_zeros = zero_prefix[n - 1] - zero_prefix[i];
+            result += static_cast<long long>(left_zeros) * right_zeros;
         }
     }
     return result;
 }
 
+long long number_of_valid_triplets_optimal(const std::string& s)
+{
+    long long ones{};
+    long long zeros{};
+    long long result{};
+    for (const auto& c : s)
+    {
+        if (c == '1')
+            ones++;
+        else
+            zeros++;
+    }
+    long long occurences_one{};
+    long long occurences_zero{};
+    for (const auto& c : s)
+    {
+        if (c == '1')
+        {
+            result += occurences_zero * zeros;
+            ones--;
+            occurences_one++;
+        }
+        else
+        {
+            result += occurences_one * ones;
+            zeros--;
+            occurences_zero++;
+        }
+    }
+    return result;
+}
+
+};
 
 #endif //NUMBER_OF_VALID_TRIPLETS_H
