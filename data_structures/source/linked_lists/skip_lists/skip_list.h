24f96243e327a783542d65a7e058321555ff7c80
Schwarf <andreas.b.scharf@gmail.com>
2025-04-27 09:34:19 +0200

    Add constructor and destructor.

--- data_structures/source/linked_lists/skip_lists/skip_list.h
+++ data_structures/source/linked_lists/skip_lists/skip_list.h
@@ -6,6 +6,7 @@
 #define SKIP_LIST_H
 #include <vector>
 #include <random>
+// https://en.wikipedia.org/wiki/Skip_list
 
 template <typename KeyType, typename ValueType, int MaxLevel>
 class SkipList{
@@ -18,8 +19,27 @@ class SkipList{
          key(key), value(value), forward(level + 1, nullptr)
       {}
     };
-
+    Node * head;
+    float probability{};
+    int current_level{};
     std::mt19937 generator;
     std::bernoulli_distribution distribution;
+
+public:
+    explicit SkipList(float probability = 0.5) : probability(probability), generator(std::random_device{}()), distribution(probability){
+      head = new Node(0, KeyType(), ValueType());
+    }
+
+    ~SkipList(){
+      Node* current_node = head;
+      while(current_node){
+        Node* next_node = current_node->forward[0];
+        delete current_node;
+        current_node = next_node;
+      }
+    }
+
+
+
 };
 #endif //SKIP_LIST_H
