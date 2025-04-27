e36a233a3408b793402cb58e288ba638b4cecdf7
Schwarf <andreas.b.scharf@gmail.com>
2025-04-27 10:19:40 +0200

    Add insert functionality and docu.

--- data_structures/source/linked_lists/skip_lists/skip_list.h
+++ data_structures/source/linked_lists/skip_lists/skip_list.h
@@ -25,9 +25,20 @@ class SkipList{
     std::mt19937 generator;
     std::bernoulli_distribution distribution;
 
+    // Randomly generate a level for a new node
+    int randomLevel() {
+      int level{};
+      while (level < MaxLevel && distribution(generator)) {
+          level++;
+      }
+      return level;
+    }
+
+
 public:
     explicit SkipList(float probability = 0.5) : probability(probability), generator(std::random_device{}()), distribution(probability){
-      head = new Node(0, KeyType(), ValueType());
+      // create head on all levels
+      head = new Node(MaxLevel, KeyType(), ValueType());
     }
 
     ~SkipList(){
@@ -39,6 +50,47 @@ public:
       }
     }
 
+    void insert(const KeyType & key, const ValueType & value){
+      std::vector<Node*> update(MaxLevel+1);
+      auto current_node = head;
+      // find insertion point by scanning from current top-level to 0.
+      // At EACH level we walk forward along the forwrad pointers as long as the next(forward)-key is < key
+      // When we can’t move forward without overshooting, we record update[i] = current_node.
+      for(int level = current_level; level > -1; level--)
+      {
+        while(current_node->forward[level] && current_node->forward[level]->key < key){
+            current_node = current_node->forward[level];
+        }
+        update[level] = current_node;
+      }
+      // After that loop, current_node->forward[0] is either
+      // - the node with our key (if it exists)  OR
+      // - the first node greater than the provided key.
+      current_node = current_node->forward[0];
+
+      // If the key exist we just update the value
+      if(current_node && current_node->key == key){
+        current_node->value = value;
+        return;
+      }
+
+
+      int new_level = randomLevel();
+      if(new_level > current_level)
+      {
+        for(int level = current_level+1; level <= new_level; ++level)
+        {
+            update[level] = head;
+        }
+        current_level = new_level;
+      }
+      auto new_node = new Node(new_level, key, value);
+      for(int level = 0; level <= new_level; ++level)
+      {
+        new_node->forward[level] = update[level]->forward[level];
+        update[level]->forward[level] = new_node;
+      }
+    }
 
 
 };
