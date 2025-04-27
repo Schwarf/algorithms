dd697fd4d846c3741f8dbca5e8b49eb4c62f4c48
Schwarf <andreas.b.scharf@gmail.com>
2025-04-27 10:50:23 +0200

    Add detailed docu.

--- data_structures/source/linked_lists/skip_lists/skip_list.h
+++ data_structures/source/linked_lists/skip_lists/skip_list.h
@@ -9,89 +9,113 @@
 // https://en.wikipedia.org/wiki/Skip_list
 
 template <typename KeyType, typename ValueType, int MaxLevel>
-class SkipList{
-  private:
-    struct Node{
-      KeyType key;
-      ValueType value;
-      std::vector<Node*> forward;
-      Node(int level, const KeyType & key, const ValueType & value) :
-         key(key), value(value), forward(level + 1, nullptr)
-      {}
+class SkipList
+{
+private:
+    struct Node
+    {
+        KeyType key;
+        ValueType value;
+        std::vector<Node*> forward;
+
+        Node(int level, const KeyType& key, const ValueType& value) :
+            key(key), value(value), forward(level + 1, nullptr)
+        {
+        }
     };
-    Node * head;
+
+    Node* header;
     float probability{};
     int current_level{};
     std::mt19937 generator;
     std::bernoulli_distribution distribution;
 
     // Randomly generate a level for a new node
-    int randomLevel() {
-      int level{};
-      while (level < MaxLevel && distribution(generator)) {
-          level++;
-      }
-      return level;
+    int randomLevel()
+    {
+        int level{};
+        while (level < MaxLevel && distribution(generator))
+        {
+            level++;
+        }
+        return level;
     }
-
 
 public:
-    explicit SkipList(float probability = 0.5) : probability(probability), generator(std::random_device{}()), distribution(probability){
-      // create head on all levels
-      head = new Node(MaxLevel, KeyType(), ValueType());
+    explicit SkipList(float probability = 0.5) : probability(probability), generator(std::random_device{}()),
+                                                 distribution(probability)
+    {
+        // create head on all levels
+        header = new Node(MaxLevel, KeyType(), ValueType());
     }
 
-    ~SkipList(){
-      Node* current_node = head;
-      while(current_node){
-        Node* next_node = current_node->forward[0];
-        delete current_node;
-        current_node = next_node;
-      }
-    }
-
-    void insert(const KeyType & key, const ValueType & value){
-      std::vector<Node*> update(MaxLevel+1);
-      auto current_node = head;
-      // find insertion point by scanning from current top-level to 0.
-      // At EACH level we walk forward along the forwrad pointers as long as the next(forward)-key is < key
-      // When we can’t move forward without overshooting, we record update[i] = current_node.
-      for(int level = current_level; level > -1; level--)
-      {
-        while(current_node->forward[level] && current_node->forward[level]->key < key){
-            current_node = current_node->forward[level];
-        }
-        update[level] = current_node;
-      }
-      // After that loop, current_node->forward[0] is either
-      // - the node with our key (if it exists)  OR
-      // - the first node greater than the provided key.
-      current_node = current_node->forward[0];
-
-      // If the key exist we just update the value
-      if(current_node && current_node->key == key){
-        current_node->value = value;
-        return;
-      }
-
-
-      int new_level = randomLevel();
-      if(new_level > current_level)
-      {
-        for(int level = current_level+1; level <= new_level; ++level)
+    ~SkipList()
+    {
+        Node* current_node = header;
+        while (current_node)
         {
-            update[level] = head;
+            Node* next_node = current_node->forward[0];
+            delete current_node;
+            current_node = next_node;
         }
-        current_level = new_level;
-      }
-      auto new_node = new Node(new_level, key, value);
-      for(int level = 0; level <= new_level; ++level)
-      {
-        new_node->forward[level] = update[level]->forward[level];
-        update[level]->forward[level] = new_node;
-      }
     }
 
+    void insert(const KeyType& key, const ValueType& value)
+    {
+        std::vector<Node*> update(MaxLevel + 1);
+        auto current_node = header;
+        // find insertion point by scanning from current top-level to 0.
+        // At EACH level we walk forward along the forwrad pointers as long as the next(forward)-key is < key
+        // When we can’t move forward without overshooting, we record update[i] = current_node.
+        for (int level = current_level; level > -1; level--)
+        {
+            while (current_node->forward[level] && current_node->forward[level]->key < key)
+            {
+                current_node = current_node->forward[level];
+            }
+            update[level] = current_node;
+        }
+        // After that loop, current_node->forward[0] is either
+        // - the node with our key (if it exists)  OR
+        // - the first node greater than the provided key. (**)
+        current_node = current_node->forward[0];
 
+        // If the key exist we just update the value
+        if (current_node && current_node->key == key)
+        {
+            current_node->value = value;
+            return;
+        }
+
+        // We decide which levels (all, some, 1) get the new key-value pair
+        // for that we make coin-flip(s) and the level is defined by std::max(MaxLevel, consecutive-successful-coin-flips)
+        int new_level = randomLevel();
+        if (new_level > current_level)
+        {
+            // for all levels we have to set the header
+            for (int level = current_level + 1; level <= new_level; ++level)
+            {
+                update[level] = header;
+            }
+            current_level = new_level;
+        }
+
+        auto new_node = new Node(new_level, key, value);
+
+        // Splice the new node into each level 0..newLevel
+        // At each level i:
+        //   Before: update[i]->forward[i] points to oldNext
+        //   After:  update[i]->forward[i] points to newNode, and newNode->forward[i] points to oldNext
+        // Visual for level i:
+        //   [update[i]] --forward[i]--> [oldNext]
+        //                  |
+        //                  +--> now points to [newNode] --forward[i]--> [oldNext]
+        // This loop does exactly that for all levels of the new node.
+        for (int level = 0; level <= new_level; ++level)
+        {
+            new_node->forward[level] = update[level]->forward[level];
+            update[level]->forward[level] = new_node;
+        }
+    }
 };
 #endif //SKIP_LIST_H
---
+++ data_structures/source/test/linked_lists/skip_list/test_skip_list.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andreas on 27.04.25.
+//
+#include "linked_lists/skip_lists/skip_list.h"
+#include "gtest/gtest.h"
