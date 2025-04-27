482f6dbe7bf58fc577f8722c57fb582c5e877ebf
Schwarf <andreas.b.scharf@gmail.com>
2025-04-27 11:48:09 +0200

    Add first tests.

--- data_structures/source/test/linked_lists/skip_list/test_skip_list.cpp
+++ data_structures/source/test/linked_lists/skip_list/test_skip_list.cpp
@@ -1,5 +1,39 @@
 //
 // Created by andreas on 27.04.25.
 //
+#include <json.hpp>
+
 #include "linked_lists/skip_lists/skip_list.h"
 #include "gtest/gtest.h"
+
+TEST(SkipListTest, SearchEmpty) {
+    SkipList<int, std::string, 32> skip_list;
+    std::string value;
+    EXPECT_FALSE(skip_list.search(10, value));
+    EXPECT_EQ(skip_list.get(10), std::nullopt);
+}
+
+TEST(SkipListTest, InsertAndSerach)
+{
+    SkipList<int, uint32_t, 32> skip_list;
+    for (int i = 0; i < 30; i+=2)
+    {
+        skip_list.insert(i, i*10);
+    }
+
+    for (int i = 0 ; i < 30; ++i)
+    {
+        uint32_t value{};
+
+        if (i & 1)
+        {
+            EXPECT_FALSE(skip_list.search(i, value));
+        }
+        else
+        {
+            EXPECT_FALSE(skip_list.search(i, value));
+            EXPECT_EQ(value, i*10);
+        }
+
+    }
+}
