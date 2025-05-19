1f059a64eac4e6acc7dc9494a2fc208cc85a51b6
Schwarf <andreas.b.scharf@gmail.com>
2025-05-19 17:52:04 +0200

    Add alternative implementation of monotonic stack and tests.

--- data_structures/source/stacks_and_queues/monotonic_stack_and_queue/monotonic_stack.h
+++ data_structures/source/stacks_and_queues/monotonic_stack_and_queue/monotonic_stack.h
@@ -48,4 +48,44 @@ private:
 	std::stack<T> stack_;
 };
 
+template<typename T, bool is_increasing = false> requires std::totally_ordered<T>
+class MonotonicStackPair
+{
+public:
+	void push(T value)
+	{
+		if (is_increasing)
+			push_increasing(value);
+		else
+			push_decreasing(value);
+	}
+	void pop()
+	{
+		stack_.pop();
+	}
+	T top() const
+	{
+		return stack_.top().first;
+	}
+
+	[[nodiscard]] bool empty() const
+	{
+		return stack_.empty();
+	}
+
+private:
+	void push_increasing(T value)
+	{
+		auto new_value = std::make_pair(value, std::min(stack_.top().second, value));
+		stack_.push(new_value);
+	}
+	void push_decreasing(T value)
+	{
+		auto new_value = std::make_pair(value, std::max(stack_.top().second, value));
+		stack_.push(new_value);
+	}
+
+	std::stack<std::pair<T, T>> stack_;
+};
+
 #endif //MONOTONIC_STACK_H
--- data_structures/source/test/stacks_and_queues/monotonic_stack_and_queue/test_monotonic_stack.cpp
+++ data_structures/source/test/stacks_and_queues/monotonic_stack_and_queue/test_monotonic_stack.cpp
@@ -5,7 +5,7 @@
 #include "gtest/gtest.h"
 #include "stacks_and_queues/monotonic_stack_and_queue/monotonic_stack.h"
 
-TEST(monotonic_stack, simple_increasing1)
+TEST(TestMonotonicStack, simple_increasing1)
 {
     std::vector<int> input{1, 2, 3, 4, 5, 6};
     auto increasing_stack = MonotonicStack<int, true>();
@@ -20,7 +20,7 @@ TEST(monotonic_stack, simple_increasing1)
     }
 }
 
-TEST(monotonic_stack, simple_increasing2)
+TEST(TestMonotonicStack, simple_increasing2)
 {
     std::vector<int> input{1, 4, 3, 4, 5, 6};
     auto increasing_stack = MonotonicStack<int, true>();
@@ -35,7 +35,7 @@ TEST(monotonic_stack, simple_increasing2)
     }
 }
 
-TEST(monotonic_stack, simple_increasing3)
+TEST(TestMonotonicStack, simple_increasing3)
 {
     std::vector<int> input{5, 4, 3, 4, 7, 6};
     auto increasing_stack = MonotonicStack<int, true>();
@@ -51,7 +51,7 @@ TEST(monotonic_stack, simple_increasing3)
 }
 
 
-TEST(monotonic_stack, simple_decreasing1)
+TEST(TestMonotonicStack, simple_decreasing1)
 {
     std::vector<int> input{6, 5, 4, 3, 2, 1};
     auto decreasing_stack = MonotonicStack<int, false>();
@@ -66,7 +66,7 @@ TEST(monotonic_stack, simple_decreasing1)
     }
 }
 
-TEST(monotonic_stack, simple_decreasing2)
+TEST(TestMonotonicStack, simple_decreasing2)
 {
     std::vector<int> input{1, 4, 3, 4, 5, 6};
     auto decreasing_stack = MonotonicStack<int, false>();
@@ -81,7 +81,99 @@ TEST(monotonic_stack, simple_decreasing2)
     }
 }
 
-TEST(monotonic_stack, simple_decreasing3)
+TEST(TestMonotonicStack, simple_decreasing3)
+{
+    std::vector<int> input{5, 4, 3, 4, 3, 2};
+    auto decreasing_stack = MonotonicStack<int, false>();
+    for (const auto& element : input)
+        decreasing_stack.push(element);
+    std::vector<int> expected_result{5, 4, 4, 3, 2};
+    while (!decreasing_stack.empty())
+    {
+        EXPECT_EQ(decreasing_stack.top(), expected_result.back());
+        expected_result.pop_back();
+        decreasing_stack.pop();
+    }
+}
+
+
+TEST(TestMonotonicStackPair, simple_increasing1)
+{
+    std::vector<int> input{1, 2, 3, 4, 5, 6};
+    auto increasing_stack = MonotonicStack<int, true>();
+    for (const auto& element : input)
+        increasing_stack.push(element);
+
+    while (!increasing_stack.empty())
+    {
+        EXPECT_EQ(increasing_stack.top(), input.back());
+        input.pop_back();
+        increasing_stack.pop();
+    }
+}
+
+TEST(TestMonotonicStackPair, simple_increasing2)
+{
+    std::vector<int> input{1, 4, 3, 4, 5, 6};
+    auto increasing_stack = MonotonicStack<int, true>();
+    for (const auto& element : input)
+        increasing_stack.push(element);
+    std::vector<int> expected_result{1, 3, 4, 5, 6};
+    while (!increasing_stack.empty())
+    {
+        EXPECT_EQ(increasing_stack.top(), expected_result.back());
+        expected_result.pop_back();
+        increasing_stack.pop();
+    }
+}
+
+TEST(TestMonotonicStackPair, simple_increasing3)
+{
+    std::vector<int> input{5, 4, 3, 4, 7, 6};
+    auto increasing_stack = MonotonicStack<int, true>();
+    for (const auto& element : input)
+        increasing_stack.push(element);
+    std::vector<int> expected_result{3, 4, 6};
+    while (!increasing_stack.empty())
+    {
+        EXPECT_EQ(increasing_stack.top(), expected_result.back());
+        expected_result.pop_back();
+        increasing_stack.pop();
+    }
+}
+
+
+TEST(TestMonotonicStackPair, simple_decreasing1)
+{
+    std::vector<int> input{6, 5, 4, 3, 2, 1};
+    auto decreasing_stack = MonotonicStack<int, false>();
+    for (const auto& element : input)
+        decreasing_stack.push(element);
+
+    while (!decreasing_stack.empty())
+    {
+        EXPECT_EQ(decreasing_stack.top(), input.back());
+        input.pop_back();
+        decreasing_stack.pop();
+    }
+}
+
+TEST(TestMonotonicStackPair, simple_decreasing2)
+{
+    std::vector<int> input{1, 4, 3, 4, 5, 6};
+    auto decreasing_stack = MonotonicStack<int, false>();
+    for (const auto& element : input)
+        decreasing_stack.push(element);
+    std::vector<int> expected_result{6};
+    while (!decreasing_stack.empty())
+    {
+        EXPECT_EQ(decreasing_stack.top(), expected_result.back());
+        expected_result.pop_back();
+        decreasing_stack.pop();
+    }
+}
+
+TEST(TestMonotonicStackPair, simple_decreasing3)
 {
     std::vector<int> input{5, 4, 3, 4, 3, 2};
     auto decreasing_stack = MonotonicStack<int, false>();
