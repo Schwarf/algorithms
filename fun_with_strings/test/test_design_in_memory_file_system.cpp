7b2a15c78b3561643f7e405b8d93eef2d92de62b
Schwarf <andreas.b.scharf@gmail.com>
2025-05-24 18:45:37 +0200

    Start with mkdir tests.

--- fun_with_strings/test/test_design_in_memory_file_system.cpp
+++ fun_with_strings/test/test_design_in_memory_file_system.cpp
@@ -3,3 +3,36 @@
 //
 #include "./../design_in_memory_file_system.h"
 #include "gtest/gtest.h"
+TEST(FileSystemMkdirTest, CreateSingleDirectory) {
+    FileSystem fs;
+    fs.mkdir("/a");
+    std::vector<std::string> result = fs.ls("/");
+    EXPECT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], "a");
+}
+
+TEST(FileSystemMkdirTest, CreateNestedDirectories) {
+    FileSystem fs;
+    fs.mkdir("/a/b/c");
+    std::vector<std::string> result = fs.ls("/a/b");
+    EXPECT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], "c");
+}
+
+TEST(FileSystemMkdirTest, MultipleIndependentDirectories) {
+    FileSystem fs;
+    fs.mkdir("/a");
+    fs.mkdir("/b");
+    std::vector<std::string> result = fs.ls("/");
+    std::vector<std::string> expected = {"a", "b"};
+    EXPECT_EQ(result, expected);
+}
+
+TEST(FileSystemMkdirTest, IdempotentDirectoryCreation) {
+    FileSystem fs;
+    fs.mkdir("/a/b");
+    fs.mkdir("/a/b");  // Should not throw or create duplicates
+    std::vector<std::string> result = fs.ls("/a");
+    EXPECT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], "b");
+}
