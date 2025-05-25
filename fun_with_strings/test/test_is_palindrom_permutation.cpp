69baa15369f013b90ea3303e9b511a0c1e2f72c7
Schwarf <andreas.b.scharf@gmail.com>
2025-05-25 16:15:32 +0200

    Add tests case insensitive version only considering letter characters.

--- fun_with_strings/test/test_is_palindrom_permutation.cpp
+++ fun_with_strings/test/test_is_palindrom_permutation.cpp
@@ -39,3 +39,41 @@ TEST(TestIsPalindromePermutationCaseSensitive, simple7) {
     std::string s = "AA1!!aa";
     EXPECT_TRUE(is_palindrome_permutation_case_sensitive(s));
 }
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+TEST(TestIsPalindromePermutationCaseInsensitive, simple1) {
+    std::string s = "";
+    EXPECT_TRUE(is_palindrome_permutation_ignore_case(s));
+}
+
+TEST(TestIsPalindromePermutationCaseInsensitive, simple2) {
+    std::string s = "Tact Coa";
+    EXPECT_TRUE(is_palindrome_permutation_ignore_case(s));
+}
+
+TEST(TestIsPalindromePermutationCaseInsensitive, simple3) {
+    std::string s = "A man, a plan, a canal, Panama!";
+    EXPECT_TRUE(is_palindrome_permutation_ignore_case(s));
+}
+
+TEST(TestIsPalindromePermutationCaseInsensitive, simple4) {
+    std::string s = "Not a Palindrome";
+    EXPECT_FALSE(is_palindrome_permutation_ignore_case(s));
+}
+
+TEST(TestIsPalindromePermutationCaseInsensitive, simple5) {
+    std::string s = "Z";
+    EXPECT_TRUE(is_palindrome_permutation_ignore_case(s));
+}
+
+TEST(TestIsPalindromePermutationCaseInsensitive, simple6) {
+    std::string s = "AA1!!aa?";
+    EXPECT_TRUE(is_palindrome_permutation_ignore_case(s));
+}
+
+TEST(TestIsPalindromePermutationCaseInsensitive, simple7) {
+    std::string s = "AA1!!aa";
+    EXPECT_TRUE(is_palindrome_permutation_ignore_case(s));
+}
