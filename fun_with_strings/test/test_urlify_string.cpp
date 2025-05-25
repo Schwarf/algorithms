12307b232478ef4cba53f8a9f9b01b77433f8fb8
Schwarf <andreas.b.scharf@gmail.com>
2025-05-25 15:37:13 +0200

    Fix not-in-place solution.

--- fun_with_strings/test/test_urlify_string.cpp
+++ fun_with_strings/test/test_urlify_string.cpp
@@ -46,3 +46,56 @@ TEST(TestUrlifyString, EmptyInput) {
     EXPECT_EQ(urlify_string(input, true_size), expected_result);
 }
 
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+/*
+TEST(TestUrlifyStringInPlace, NoSpaces) {
+    std::string input = "HelloWorld      ";
+    constexpr size_t true_size = 10;
+    const std::string expected_result = "HelloWorld";
+    urlify_string_in_place(input, true_size);
+    EXPECT_EQ(input, expected_result);
+}
+
+TEST(TestUrlifyStringInPlace, SingleSpaceInMiddle) {
+    std::string input = "Hello World      ";
+    constexpr size_t true_size = 11;
+    const std::string expected_result = "Hello%20World";
+    urlify_string_in_place(input, true_size);
+    EXPECT_EQ(input, expected_result);
+}
+
+TEST(TestUrlifyStringInPlace, LeadingAndTrailingSpaces) {
+    std::string input = " Hello World  ";
+    constexpr size_t true_size = 12;
+    const std::string expected_result = "%20Hello%20World";
+    urlify_string_in_place(input, true_size);
+    EXPECT_EQ(input, expected_result);
+}
+
+TEST(TestUrlifyStringInPlace, AllSpaces) {
+    std::string input = "     ";
+    constexpr size_t true_size = 5;
+    const std::string expected_result = "%20%20%20%20%20";
+    urlify_string_in_place(input, true_size);
+EXPECT_EQ(input, expected_result);
+}
+
+TEST(TestUrlifyStringInPlace, MixedWithMultipleSpaces) {
+    std::string input = "Mr  John Smith    ";
+    constexpr size_t true_size = 14;
+    const std::string expected_result = "Mr%20%20John%20Smith";
+    urlify_string_in_place(input, true_size);
+EXPECT_EQ(input, expected_result);
+}
+
+TEST(TestUrlifyStringInPlace, EmptyInput) {
+    std::string input = "";
+    constexpr size_t true_size = 0;
+    const std::string expected_result = "";
+    urlify_string_in_place(input, true_size);
+EXPECT_EQ(input, expected_result);
+}
+*/
--- fun_with_strings/urlify_string.h
+++ fun_with_strings/urlify_string.h
@@ -7,27 +7,53 @@
 // Write a method to replace all spaces n a string with `%20`. You may assume that the string
 // has sufficient space at the end to hold additional characters and you are provided with the
 // true size of the string.
+#include <algorithm>
 #include <string>
-std::string urlify_string(std::string & input, size_t true_size)
-{
+
+// true_size is the size of the string without the additional space
+std::string urlify_string(const std::string& input, size_t true_size) {
+    // 1) Count how many spaces are in the "real" part
+    size_t space_count = std::count(
+        input.begin(),
+        input.begin() + std::min(true_size, input.size()),
+        ' '
+    );
+
+    // 2) Reserve exact final size: each space adds 2 extra chars ("%20" vs " ")
     std::string result;
-    const char space = ' ';
-    for(size_t index{}; index < true_size; ++index ) {
-      if(input[index] == space)
-        result += "%20";
-      else
-        result += input[index];
+    result.reserve(true_size + space_count * 2);
+
+    // 3) Build
+    for (size_t i = 0; i < true_size && i < input.size(); ++i) {
+        if (input[i] == ' ')
+            result += "%20";
+        else
+            result.push_back(input[i]);
     }
+
     return result;
 }
 
-void urlify_string_in_place(std::string & input, size_t true_size)
+void urlify_string_in_place(std::string& input, size_t true_size)
 {
     std::string result;
+    int space_count {};
     const char space = ' ';
-    size_t back_index = true_size - 1;
-    for(size_t index{true_size}; index--; ) {
-        if(input[index] == space)
+    for (size_t i = 0; i < true_size; ++i)
+    {
+        if (input[i] == space)
+            ++space_count;
+    }
+
+    // Compute the final length
+    size_t new_len = true_size + space_count * 2;
+    // (buffer must already be >= new_len)
+
+    // Traverse backward, writing into [0..new_len-1]
+    size_t back_index = new_len - 1;
+    for (size_t index{true_size}; index--;)
+    {
+        if (input[index] == space)
         {
             input[back_index--] = '0';
             input[back_index--] = '2';
@@ -35,7 +61,10 @@ void urlify_string_in_place(std::string & input, size_t true_size)
         }
         else
             input[back_index--] = input[index];
+        if (back_index == 0)
+            break;
     }
+    input.resize(new_len);
 }
 
 #endif //URLIFY_STRING_H
