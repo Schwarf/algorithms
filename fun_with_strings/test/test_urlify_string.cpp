c035c2d4d1692178815e4cd8e6e8331468f2aa8e
Schwarf <andreas.b.scharf@gmail.com>
2025-05-25 11:11:28 +0200

    Add tests for first implmentation.

---
+++ fun_with_strings/test/test_urlify_string.cpp
@@ -0,0 +1,48 @@
+//
+// Created by andreas on 25.05.25.
+//
+#include "./../urlify_string.h"
+#include "gtest/gtest.h"
+
+TEST(TestUrlifyString, NoSpaces) {
+    std::string input = "HelloWorld      ";
+    constexpr size_t true_size = 10;
+    const std::string expected_result = "HelloWorld";
+    EXPECT_EQ(urlify_string(input, true_size), expected_result);
+}
+
+TEST(TestUrlifyString, SingleSpaceInMiddle) {
+    std::string input = "Hello World      ";
+    constexpr size_t true_size = 11;
+    const std::string expected_result = "Hello%20World";
+    EXPECT_EQ(urlify_string(input, true_size), expected_result);
+}
+
+TEST(TestUrlifyString, LeadingAndTrailingSpaces) {
+    std::string input = " Hello World  ";
+    constexpr size_t true_size = 12;
+    const std::string expected_result = "%20Hello%20World";
+    EXPECT_EQ(urlify_string(input, true_size), expected_result);
+}
+
+TEST(TestUrlifyString, AllSpaces) {
+    std::string input = "     ";
+    constexpr size_t true_size = 5;
+    const std::string expected_result = "%20%20%20%20%20";
+    EXPECT_EQ(urlify_string(input, true_size), expected_result);
+}
+
+TEST(TestUrlifyString, MixedWithMultipleSpaces) {
+    std::string input = "Mr  John Smith    ";
+    constexpr size_t true_size = 14;
+    const std::string expected_result = "Mr%20%20John%20Smith";
+    EXPECT_EQ(urlify_string(input, true_size), expected_result);
+}
+
+TEST(TestUrlifyString, EmptyInput) {
+    std::string input = "";
+    constexpr size_t true_size = 0;
+    const std::string expected_result = "";
+    EXPECT_EQ(urlify_string(input, true_size), expected_result);
+}
+
