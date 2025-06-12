b69bd2387611e2267116c5dacc375e9892230442
Schwarf <andreas.b.scharf@gmail.com>
2025-06-12 17:44:55 +0200

    Add simple algo to check if number is palindrome.

---
+++ fun_with_algos/palindrome_number.h
@@ -0,0 +1,24 @@
+//
+// Created by andreas on 12.06.25.
+//
+
+#ifndef PALINDROME_NUMBER_H
+#define PALINDROME_NUMBER_H
+// Given an integer x, return true if x is a , and false otherwise.
+
+bool is_palindrome_number(int input)
+{
+  if(input < 0)
+    return false;
+  int reversed{};
+  int help = input;
+  while(true)
+  {
+    reversed = (input % 10) + reversed*10;
+    input /= 10;
+    if(!input)
+      break;
+  }
+  return reversed == help;
+}
+#endif //PALINDROME_NUMBER_H
