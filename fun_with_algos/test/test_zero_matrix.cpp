804fcac45ee890369371cdf84224775d9bba4797
Schwarf <andreas.b.scharf@gmail.com>
2025-06-14 14:43:25 +0200

    Add space optimized version.

--- fun_with_algos/test/test_zero_matrix.cpp
+++ fun_with_algos/test/test_zero_matrix.cpp
@@ -6,45 +6,45 @@
 
 TEST(TestZeroMatrix, simple1)
 {
-     std::vector<std::vector<int>> matrix{{1, 0},{1, 1}};
-     std::vector<std::vector<int>> expected_result{{0, 0},{1, 0}};
-     EXPECT_NE(expected_result, matrix);
-     zero_matrix(matrix);
-     EXPECT_EQ(expected_result, matrix);
+    std::vector<std::vector<int>> matrix{{1, 0}, {1, 1}};
+    std::vector<std::vector<int>> expected_result{{0, 0}, {1, 0}};
+    EXPECT_NE(expected_result, matrix);
+    zero_matrix(matrix);
+    EXPECT_EQ(expected_result, matrix);
 }
 
 TEST(TestZeroMatrix, simple2)
 {
-     std::vector<std::vector<int>> matrix{{1}};
-     std::vector<std::vector<int>> expected_result{{1}};
-     zero_matrix(matrix);
-     EXPECT_EQ(expected_result, matrix);
+    std::vector<std::vector<int>> matrix{{1}};
+    std::vector<std::vector<int>> expected_result{{1}};
+    zero_matrix(matrix);
+    EXPECT_EQ(expected_result, matrix);
 }
 
 TEST(TestZeroMatrix, simple3)
 {
-     std::vector<std::vector<int>> matrix = {
-          {1, 2, 3},
-          {4, 5, 6},
-          {7, 8, 9}
-     };
-     std::vector<std::vector<int>> expected_result = matrix;
-     zero_matrix(matrix);
-     EXPECT_EQ(matrix, expected_result);
+    std::vector<std::vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    std::vector<std::vector<int>> expected_result = matrix;
+    zero_matrix(matrix);
+    EXPECT_EQ(matrix, expected_result);
 }
 
 TEST(ZeroMatrixTest, simple4)
 {
-     std::vector<std::vector<int>> matrix = {
-          {1, 2, 3},
-          {4, 0, 6},
-          {7, 8, 9}
-     };
-     std::vector<std::vector<int>> expected_result = {
-          {1, 0, 3},
-          {0, 0, 0},
-          {7, 0, 9}
-     };
-     zero_matrix(matrix);
-     EXPECT_EQ(matrix, expected_result);
+    std::vector<std::vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 0, 6},
+        {7, 8, 9}
+    };
+    std::vector<std::vector<int>> expected_result = {
+        {1, 0, 3},
+        {0, 0, 0},
+        {7, 0, 9}
+    };
+    zero_matrix(matrix);
+    EXPECT_EQ(matrix, expected_result);
 }
--- fun_with_algos/zero_matrix.h
+++ fun_with_algos/zero_matrix.h
@@ -8,32 +8,98 @@
 // column are set to 0.
 #include <vector>
 #include <unordered_set>
+
 void zero_matrix(std::vector<std::vector<int>>& matrix)
 {
-      int rows = matrix.size();
-      int columns = matrix[0].size();
-      std::unordered_set<int> zero_cols;
-      std::unordered_set<int> zero_rows;
-      for(int row = 0; row < rows; ++row){
-            for(int col = 0; col < columns; ++col)
-            {
-                if(matrix[row][col] == 0)
-                {
-                    zero_cols.insert(col);
-                    zero_rows.insert(row);
-                }
-            }
-      }
-
-    for(int row = 0; row < rows; ++row){
-        for(int col = 0; col < columns; ++col)
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+    std::unordered_set<int> zero_cols;
+    std::unordered_set<int> zero_rows;
+    for (int row = 0; row < rows; ++row)
+    {
+        for (int col = 0; col < columns; ++col)
         {
-            if(zero_cols.contains(col) || zero_rows.contains(row))
+            if (matrix[row][col] == 0)
             {
-              matrix[row][col] = 0;
+                zero_cols.insert(col);
+                zero_rows.insert(row);
             }
         }
     }
 
+    for (int row = 0; row < rows; ++row)
+    {
+        for (int col = 0; col < columns; ++col)
+        {
+            if (zero_cols.contains(col) || zero_rows.contains(row))
+            {
+                matrix[row][col] = 0;
+            }
+        }
+    }
 }
+
+void zero_matrix_optimized(std::vector<std::vector<int>>& matrix)
+{
+    int rows = matrix.size();
+    int columns = matrix[0].size();
+    bool first_column_contains_zero{};
+    bool first_row_contains_zero{};
+    for (int row = 0; row < rows; ++row)
+    {
+        if (matrix[row][0] == 0)
+        {
+            first_column_contains_zero = true;
+            break;
+        }
+    }
+
+    for (int col = 0; col < rows; ++col)
+    {
+        if (matrix[0][col] == 0)
+        {
+            first_row_contains_zero = true;
+            break;
+        }
+    }
+
+    for (int row = 1; row < rows; ++row)
+    {
+        for (int col = 1; col < columns; ++col)
+        {
+            if (matrix[row][col] == 0)
+            {
+                matrix[row][0] = 0;
+                matrix[0][col] = 0;
+            }
+        }
+    }
+
+    for (int row = 1; row < rows; ++row)
+    {
+        for (int col = 1; col < columns; ++col)
+        {
+            if (matrix[row][0] == 0 || matrix[0][col] == 0)
+            {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+    if (first_column_contains_zero)
+    {
+        for (int row = 0; row < rows; ++row)
+        {
+            matrix[row][0] = 0;
+        }
+    }
+
+    if (first_row_contains_zero)
+    {
+        for (int col = 0; col < rows; ++col)
+        {
+            matrix[0][col] = 0;
+        }
+    }
+}
+
 #endif //ZERO_MATRIX_H
