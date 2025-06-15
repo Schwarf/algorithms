37fc51938689d6c026c5a273dd626fb399a1c142
Schwarf <andreas.b.scharf@gmail.com>
2025-06-15 18:31:40 +0200

    Add algo to transform double number to binary representation.

---
+++ bit_manipulation/double_number_to_binary.h
@@ -0,0 +1,40 @@
+//
+// Created by andreas on 15.06.25.
+//
+
+#ifndef DOUBLE_NUMBER_TO_BINARY_H
+#define DOUBLE_NUMBER_TO_BINARY_H
+// Binary to String: Given a reai number between 0 and 1 (e.g., 0.72) that is passed in as a double, print
+// the binary representation. If the number cannot be represented accurately in binary with at most 32
+// characters, print "ERROR."
+#include <string>
+
+std::string double_number_to_binary(double number)
+{
+    if (number >= 1.0 || number <= 0.0)
+    {
+        return "ERROR";
+    }
+
+    std::string result = "0.";
+    while (number > 0.0)
+    {
+        if (result.size() >= 32)
+        {
+            // too many characters => non-terminating in 32 bits
+            return "ERROR";
+        }
+        if (double scaled_value = number * 2; scaled_value >= 1.0)
+        {
+            result.push_back('1');
+            number = scaled_value - 1.0;
+        }
+        else
+        {
+            result.push_back('0');
+            number = scaled_value;
+        }
+    }
+}
+
+#endif //DOUBLE_NUMBER_TO_BINARY_H
---
+++ bit_manipulation/test/test_double_number_to_binary.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andreas on 15.06.25.
+//
+#include "gtest/gtest.h"
+#include "./../double_number_to_binary.h"
