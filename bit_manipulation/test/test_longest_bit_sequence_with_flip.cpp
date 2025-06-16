a985b3654d31d8df08a858179aab36558057bd97
Schwarf <andreas.b.scharf@gmail.com>
2025-06-16 18:05:21 +0200

    Add more tests.

--- bit_manipulation/test/test_longest_bit_sequence_with_flip.cpp
+++ bit_manipulation/test/test_longest_bit_sequence_with_flip.cpp
@@ -17,3 +17,31 @@ TEST(TestLongestBitSequenceWithFlip, simple2)
     constexpr int expected_result{1};
     EXPECT_EQ(expected_result, longest_bit_sequence_with_flip(input));
 }
+
+TEST(TestLongestBitSequenceWithFlip, simple3)
+{
+    constexpr int input{0b101};
+    constexpr int expected_result{3};
+    EXPECT_EQ(expected_result, longest_bit_sequence_with_flip(input));
+}
+
+TEST(TestLongestBitSequenceWithFlip, simple4)
+{
+    constexpr int input{0b1001};
+    constexpr int expected_result{2};
+    EXPECT_EQ(expected_result, longest_bit_sequence_with_flip(input));
+}
+
+TEST(TestLongestBitSequenceWithFlip, simple5)
+{
+    constexpr int input{0b1111};
+    constexpr int expected_result{4};
+    EXPECT_EQ(expected_result, longest_bit_sequence_with_flip(input));
+}
+
+TEST(TestLongestBitSequenceWithFlip, simple6)
+{
+    constexpr int input{0};
+    constexpr int expected_result{0};
+    EXPECT_EQ(expected_result, longest_bit_sequence_with_flip(input));
+}
