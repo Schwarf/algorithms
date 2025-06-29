77ddbd94a8fe35b3046b4e8f86c9b808c94501e8
Schwarf <andreas.b.scharf@gmail.com>
2025-06-29 14:39:59 +0200

    Add bottom-up version.

--- dynamic_programming/max_profit_in_job_scheduling.h
+++ dynamic_programming/max_profit_in_job_scheduling.h
@@ -10,51 +10,109 @@
 // If you choose a job that ends at time X you will be able to start another job that starts at time X.
 #include <vector>
 #include <algorithm>
-struct Job {
-  int start;
-  int end;
-  int profit;
+
+struct Job
+{
+    int start;
+    int end;
+    int profit;
 };
 
 int find_next_job(const std::vector<Job>& jobs, int current_end)
 {
-  int low = 0;
-  int high = jobs.size();
-  while(low < high)
-  {
-    int mid = (low + high) / 2;
-    if(jobs[mid].start >= current_end)
-      high = mid;
-    else
-      low = mid + 1;
-  }
-  return low;
+    int low = 0;
+    int high = jobs.size();
+    while (low < high)
+    {
+        const int mid = (low + high) / 2;
+        if (jobs[mid].start >= current_end)
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    return low;
 }
 
-int dp(int job_index, const std::vector<Job>& jobs, std::vector<int> & memo)
+// Binary search to find the last job that doesn't overlap
+int find_last_non_conflicting(const std::vector<Job>& jobs, int job_index)
 {
-  if (job_index >= jobs.size())
-    return 0;
-  if(memo[job_index] != -1)
-    return memo[job_index];
-  int next = find_next_job(jobs, jobs[job_index].end);
-  int take_next_job = jobs[job_index].profit + dp(next, jobs, memo);
-  int skip_next_job = dp(job_index + 1, jobs, memo);
-
-  return memo[job_index] = std::max(take_next_job, skip_next_job);
+    int low = 0;
+    int high = job_index - 1;
+    while (low <= high)
+    {
+        const int mid = (low + high) / 2;
+        if (jobs[mid].end <= jobs[job_index].start)
+        {
+            if (jobs[mid + 1].end <= jobs[job_index].start)
+                low = mid + 1;
+            else
+                return mid;
+        }
+        else
+            high = mid - 1;
+    }
+    return -1;
 }
 
+int dp(int job_index, const std::vector<Job>& jobs, std::vector<int>& memo)
+{
+    if (job_index >= jobs.size())
+        return 0;
+    if (memo[job_index] != -1)
+        return memo[job_index];
+    int next = find_next_job(jobs, jobs[job_index].end);
+    int take_next_job = jobs[job_index].profit + dp(next, jobs, memo);
+    int skip_next_job = dp(job_index + 1, jobs, memo);
+
+    return memo[job_index] = std::max(take_next_job, skip_next_job);
+}
+
+// top-down approach
 int job_scheduling(std::vector<int>& start_time, std::vector<int>& end_time, std::vector<int>& profit)
 {
-     int n = start_time.size();
-     std::vector<Job> jobs(n);
-     for (int i = 0; i < n; i++) {
-       jobs[i] = {start_time[i], end_time[i], profit[i]};
-     }
-     std::sort(jobs.begin(), jobs.end(), [](const Job & j1, const Job & j2) {
-       return j1.start < j2.start;
-     });
-     std::vector<int> memo(n, -1);
-     return dp(0, jobs, memo);
+    int n = start_time.size();
+    std::vector<Job> jobs(n);
+    for (int i = 0; i < n; i++)
+    {
+        jobs[i] = {start_time[i], end_time[i], profit[i]};
+    }
+    std::sort(jobs.begin(), jobs.end(), [](const Job& j1, const Job& j2)
+    {
+        return j1.start < j2.start;
+    });
+    std::vector<int> memo(n, -1);
+    return dp(0, jobs, memo);
 }
+
+// bottom-up approach
+int job_scheduling_bottom_up(std::vector<int>& start_time, std::vector<int>& end_time, std::vector<int>& profit)
+{
+    int n = start_time.size();
+    std::vector<Job> jobs(n);
+    for (int i = 0; i < n; i++)
+    {
+        jobs[i] = {start_time[i], end_time[i], profit[i]};
+    }
+    std::sort(jobs.begin(), jobs.end(), [](const Job& j1, const Job& j2)
+    {
+        return j1.end < j2.end;
+    });
+    std::vector<int> dp(n);
+    dp[0] = jobs[0].profit;
+    for (int job_index = 1; job_index < n; ++job_index)
+    {
+        int profit_if_taken = jobs[job_index].profit;
+
+        int last_compatible_index = find_last_non_conflicting(jobs, job_index);
+        if (last_compatible_index != -1)
+        {
+            profit_if_taken += dp[last_compatible_index];
+        }
+
+        dp[job_index] = std::max(dp[job_index - 1], profit_if_taken);
+    }
+
+    return dp[n - 1];
+}
+
 #endif //MAX_PROFIT_IN_JOB_SCHEDULING_H
