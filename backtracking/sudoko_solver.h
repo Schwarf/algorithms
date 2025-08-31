6a31e82de5d581dfa96982134c3e80e3f7ec5b64
Schwarf <andreas.b.scharf@gmail.com>
2025-08-31 17:17:44 +0200

    Add one test.

--- backtracking/sudoko_solver.h
+++ backtracking/sudoko_solver.h
@@ -4,5 +4,101 @@
 
 #ifndef ALGORITHMS_SUDOKO_SOLVER_H
 #define ALGORITHMS_SUDOKO_SOLVER_H
+#include <array>
+#include <functional>
+#include <vector>
+
+
+// Write a program to solve a Sudoku puzzle by filling the empty cells.
+//
+// A sudoku solution must satisfy all of the following rules:
+//
+//     Each of the digits 1-9 must occur exactly once in each row.
+//     Each of the digits 1-9 must occur exactly once in each column.
+//     Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+//
+// The '.' character indicates empty cells.
+
+
+void solve_sudoku(std::vector<std::vector<char>>& board)
+{
+    constexpr int N = 9;
+    constexpr int n = 3;
+
+    std::array<std::array<int, N + 1>, N> rows;
+    std::array<std::array<int, N + 1>, N> columns;
+    std::array<std::array<int, N + 1>, N> boxes;
+    bool sudoku_solved = false;
+    auto box_index = [&](int r, int c) { return (r / n) * n + c / n; };
+
+    auto place_number = [&](int digit, int row, int col)
+    {
+        int index = box_index(row, col);
+        rows[row][digit] = 1;
+        columns[col][digit] = 1;
+        boxes[index][digit] = 1;
+    };
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            char num = board[i][j];
+            if (num != '.')
+            {
+                int digit = num - '0';
+                place_number(digit, i, j);
+            }
+        }
+    }
+    auto can_place = [&](int digit, int row, int col) -> bool
+    {
+        int index = box_index(row, col);
+        return rows[row][digit] + columns[col][digit] + boxes[index][digit] == 0;
+    };
+
+    std::function<void(int,int)> backtrack; // declare first
+
+    auto place_next_number = [&](int row, int col)
+    {
+        if (row == N - 1 && col == N - 1) { sudoku_solved = true; return; }
+        int next_row = (col == N - 1) ? row + 1 : row;
+        int next_col = (col == N - 1) ? 0  : col + 1;
+        backtrack(next_row, next_col);
+
+    };
+
+    auto remove_number = [&](int digit, int row, int col)
+    {
+        int index = box_index(row, col);
+        rows[row][digit] = 0;
+        columns[col][digit] = 0;
+        boxes[index][digit] = 0;
+        board[row][col] = '.';
+    };
+
+
+    backtrack = [&](int row, int col)
+    {
+        if (board[row][col] != '.')
+        {
+            place_next_number(row, col);
+            return;
+        }
+
+        for (int digit =1 ; digit< 10; ++digit)
+        {
+            if(can_place(digit, row, col))
+            {
+                place_number(digit, row, col);
+                place_next_number(row, col);
+                if (!sudoku_solved)
+                    remove_number(digit, row, col);
+            }
+        }
+    };
+
+    backtrack(0, 0);
+}
 
 #endif //ALGORITHMS_SUDOKO_SOLVER_H
--- backtracking/test/test_sudoku_solver.cpp
+++ backtracking/test/test_sudoku_solver.cpp
@@ -2,97 +2,26 @@
 // Created by andreas on 31.08.25.
 //
 
-// Write a program to solve a Sudoku puzzle by filling the empty cells.
-//
-// A sudoku solution must satisfy all of the following rules:
-//
-//     Each of the digits 1-9 must occur exactly once in each row.
-//     Each of the digits 1-9 must occur exactly once in each column.
-//     Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
-//
-// The '.' character indicates empty cells.
+#include "../sudoko_solver.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
-#include <array>
-#include <functional>
-#include <vector>
-
-void solve_sudoku(std::vector<std::vector<char>>& board)
+TEST(TestSudokuSolver, firstBoard)
 {
-    constexpr int N = 9;
-    constexpr int n = 3;
-
-    std::array<std::array<int, N + 1>, N> rows;
-    std::array<std::array<int, N + 1>, N> columns;
-    std::array<std::array<int, N + 1>, N> boxes;
-    bool sudoku_solved = false;
-    auto box_index = [&](int r, int c) { return (r / n) * n + c / n; };
-
-    auto place_number = [&](int digit, int row, int col)
-    {
-        int index = box_index(row, col);
-        rows[row][digit] = 1;
-        columns[col][digit] = 1;
-        boxes[index][digit] = 1;
+    std::vector<std::vector<char>> board{
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'}, {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'}, {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
     };
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            char num = board[i][j];
-            if (num != '.')
-            {
-                int digit = num - '0';
-                place_number(digit, i, j);
-            }
-        }
-    }
-    auto can_place = [&](int digit, int row, int col) -> bool
-    {
-        int index = box_index(row, col);
-        return rows[row][digit] + columns[col][digit] + boxes[index][digit] == 0;
+    std::vector<std::vector<char>> expected{
+        {'5', '3', '4', '6', '7', '8', '9', '1', '2'}, {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+        {'1', '9', '8', '3', '4', '2', '5', '6', '7'}, {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+        {'4', '2', '6', '8', '5', '3', '7', '9', '1'}, {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+        {'9', '6', '1', '5', '3', '7', '2', '8', '4'}, {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+        {'3', '4', '5', '2', '8', '6', '1', '7', '9'}
     };
-
-    std::function<void(int,int)> backtrack; // declare first
-
-    auto place_next_number = [&](int row, int col)
-    {
-        if (row == N - 1 && col == N - 1) { sudoku_solved = true; return; }
-        int next_row = (col == N - 1) ? row + 1 : row;
-        int next_col = (col == N - 1) ? 0  : col + 1;
-        backtrack(next_row, next_col);
-
-    };
-
-    auto remove_number = [&](int digit, int row, int col)
-    {
-        int index = box_index(row, col);
-        rows[row][digit] = 0;
-        columns[col][digit] = 0;
-        boxes[index][digit] = 0;
-        board[row][col] = '.';
-    };
-
-
-    backtrack = [&](int row, int col)
-    {
-        if (board[row][col] != '.')
-        {
-            place_next_number(row, col);
-            return;
-        }
-
-        for (int digit =1 ; digit< 10; ++digit)
-        {
-            if(can_place(digit, row, col))
-            {
-                place_number(digit, row, col);
-                place_next_number(row, col);
-                if (!sudoku_solved)
-                    remove_number(digit, row, col);
-            }
-        }
-    };
-
-    backtrack(0, 0);
+    solve_sudoku(board);
+    EXPECT_THAT(board, ::testing::ContainerEq(expected));
 }
