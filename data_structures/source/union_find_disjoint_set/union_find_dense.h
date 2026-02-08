c61dbb979857e8c4804924a65846199076e46362
Schwarf <andreas.b.scharf@gmail.com>
2026-02-08 19:15:30 +0100

    Update union find dense case.

--- data_structures/source/union_find_disjoint_set/union_find_dense.h
+++ data_structures/source/union_find_disjoint_set/union_find_dense.h
@@ -5,54 +5,73 @@
 #ifndef OPTIMIZED_DISJOINT_SET_H
 #define OPTIMIZED_DISJOINT_SET_H
 #include <vector>
+#include <optional>
 
+template <typename IdType>
+requires std::is_signed_v<IdType>
 class UnionFindDense
 {
+    bool in_set(IdType value)
+    {
+        return 0 <= value && value < roots.size() && roots[value] != -1;
+    }
+
 public:
-    explicit UnionFindDense(size_t size)
+    explicit UnionFindDense(int size)
         :
         roots(size),
         ranks(size)
     {
-        for (size_t i{}; i < size; ++i)
+        for (int i{}; i < size; ++i)
         {
-            roots[i] = i;
-            ranks[i] = 1;
+            roots[i] = IdType{i};
+            ranks[i] = 0;
         }
     }
 
-    size_t find_root(size_t element)
+    std::optional<IdType> find_root(IdType value)
     {
-        if (element == roots[element])
-            return element;
-        return roots[element] = find_root(roots[element]);
+        if(!in_set(value))
+            return std::nullopt;
+        if (value == roots[value])
+            return value;
+        return roots[value] = *find_root(roots[value]);
     }
 
-    void union_set(size_t element1, size_t element2)
+    void union_set(IdType value1, IdType value2)
     {
-        auto root_element1 = find_root(element1);
-        auto root_element2 = find_root(element2);
-        if (root_element1 == root_element2)
+        auto root1 = find_root(value1);
+        auto root2 = find_root(value2);
+        if (!root1 || !root2)
             return;
-        if (ranks[root_element1] > ranks[root_element2])
-            roots[root_element2] = root_element1;
-        else if (ranks[root_element1] < ranks[root_element2])
-            roots[root_element1] = root_element2;
+        IdType root_value1 = *root1;
+        IdType root_value2 = *root2;
+        if (root_value1 == root_value2)
+            return;
+        if (ranks[root_value1] > ranks[root_value2])
+            roots[root_value2] = root_value1;
+        else if (ranks[root_value1] < ranks[root_value2])
+            roots[root_value1] = root_value2;
         else
         {
-            roots[root_element2] = root_element1;
-            ranks[root_element1]++;
+            roots[root_value2] = root_value1;
+            ranks[root_value1]++;
         }
     }
 
-    bool are_connected(size_t element1, size_t element2)
+    bool are_connected(IdType value1, IdType value2)
     {
-        return find_root(element1) == find_root(element2);
+        auto r1 = find_root(value1);
+        auto r2 = find_root(value2);
+        if (!r1 || !r2)
+            return false;
+
+        return *r1 == *r2;
     }
 
 private:
-    std::vector<size_t> roots;
-    std::vector<size_t> ranks;
+    std::vector<IdType> roots;
+    std::vector<IdType> ranks;
 };
 
 #endif //OPTIMIZED_DISJOINT_SET_H
