6a3bb4ab66d33a52133178d72a02db57066d8cfd
Schwarf <andreas.b.scharf@gmail.com>
2026-03-31 10:22:01 +0200

    Add design requirements.

--- data_structures/source/deques/double_ended_queue.h
+++ data_structures/source/deques/double_ended_queue.h
@@ -4,7 +4,18 @@
 
 #ifndef ALGORITHMS_DOUBLE_ENDED_QUEUE_H
 #define ALGORITHMS_DOUBLE_ENDED_QUEUE_H
-
+// Design a Double-ended Queue class.
+//
+// Your Deque class should support the following operations:
+//
+//     Deque() will initialize an empty queue.
+//     bool isEmpty() will return whether the queue is empty or not.
+//     void append(int value) will insert value at the end of the queue.
+//     void appendleft(int val) will insert value at the beginning of the queue.
+//     int pop() will remove and return the value at the end of the queue. If the queue is empty, return -1.
+//     int popleft() will remove and return the value at the beginning of the queue. If the queue is empty, return -1.
+//
+// Note: You should implement each operation in O(1)O(1) time complexity.
 class Deque {
   struct Node{
     Node(int val): value(val){}
