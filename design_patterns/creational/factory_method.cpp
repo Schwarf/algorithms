683bcbd677cd6a423b4a611941ef3a9be73594a3
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-04-05 03:34:21 +0100

    Simplify using std::unique_ptr, no delete.

--- design_patterns/creational/factory_method.cpp
+++ design_patterns/creational/factory_method.cpp
@@ -2,54 +2,72 @@
 // Created by andreas on 05.04.26.
 //
 #include <string>
+#include <iostream>
+#include <memory>
 
 class Vehicle {
 public:
-    virtual std::string getType() = 0;
+    virtual std::string getType() const = 0;
+
     virtual ~Vehicle() = default;
 };
 
 class Car : public Vehicle {
 public:
-    std::string getType() override {
+    std::string getType() const override {
         return "Car";
     }
 };
 
 class Bike : public Vehicle {
 public:
-    std::string getType() override {
+    std::string getType() const override {
         return "Bike";
     }
 };
 
 class Truck : public Vehicle {
 public:
-    std::string getType() override {
+    std::string getType() const override {
         return "Truck";
     }
 };
 
 class VehicleFactory {
 public:
-    virtual Vehicle* createVehicle() = 0;
+    virtual std::unique_ptr<Vehicle> createVehicle() const = 0;
+
     virtual ~VehicleFactory() = default;
 };
 
 class CarFactory : public VehicleFactory {
-    Vehicle* createVehicle() override {
-        return new Car();
+    std::unique_ptr<Vehicle> createVehicle() const override {
+        return std::make_unique<Car>();
     }
 };
 
 class BikeFactory : public VehicleFactory {
-    Vehicle* createVehicle() override {
-        return new Bike();
+     std::unique_ptr<Vehicle> createVehicle() const override {
+        return std::make_unique<Bike>();;
     }
 };
 
 class TruckFactory : public VehicleFactory {
-    Vehicle* createVehicle() override {
-        return new Truck();
+    std::unique_ptr<Vehicle> createVehicle() const override {
+        return std::make_unique<Truck>();
     }
 };
+
+int main() {
+    std::unique_ptr<VehicleFactory> carFactory = std::make_unique<CarFactory>();
+    std::unique_ptr<VehicleFactory> truckFactory = std::make_unique<TruckFactory>();
+    std::unique_ptr<VehicleFactory> bikeFactory = std::make_unique<BikeFactory>();
+
+    auto myCar = carFactory->createVehicle();
+    auto myTruck = truckFactory->createVehicle();
+    auto myBike = bikeFactory->createVehicle();
+
+    std::cout << myCar->getType() << '\n';
+    std::cout << myTruck->getType() << '\n';
+    std::cout << myBike->getType() << '\n';
+}
