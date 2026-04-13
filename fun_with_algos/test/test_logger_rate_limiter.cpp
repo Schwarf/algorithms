0d275773ef2c64c3813af0d9da6c775bb2f86e30
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-04-13 07:28:31 +0100

    Add test case and update ToDo.

--- fun_with_algos/test/test_logger_rate_limiter.cpp
+++ fun_with_algos/test/test_logger_rate_limiter.cpp
@@ -5,7 +5,7 @@
 #include "./../logger_rate_limiter.h"
 
 TEST(LoggerRateLimiter, blocks_repeated_message_within_ten_seconds) {
-    auto logger = Logger();
+    Logger logger;
     EXPECT_TRUE(logger.shall_print_message(1, "foo"));
     EXPECT_TRUE(logger.shall_print_message(2, "bar"));
     EXPECT_FALSE(logger.shall_print_message(3, "foo"));
@@ -16,7 +16,7 @@ TEST(LoggerRateLimiter, blocks_repeated_message_within_ten_seconds) {
 
 
 TEST(LoggerRateLimiter, handles_multiple_messages_at_same_timestamp_independently) {
-    auto logger = Logger();
+    Logger logger;
     EXPECT_TRUE(logger.shall_print_message(1, "foo"));
     EXPECT_TRUE(logger.shall_print_message(1, "bar"));
     EXPECT_FALSE(logger.shall_print_message(1, "foo"));
@@ -26,16 +26,24 @@ TEST(LoggerRateLimiter, handles_multiple_messages_at_same_timestamp_independentl
 }
 
 TEST(LoggerRateLimiter, allows_same_message_again_after_ten_seconds) {
-    auto logger = Logger();
+    Logger logger;
     EXPECT_TRUE(logger.shall_print_message(1, "foo"));
     EXPECT_TRUE(logger.shall_print_message(11, "foo"));
     EXPECT_TRUE(logger.shall_print_message(22, "foo"));
 }
 
 TEST(LoggerRateLimiter, allows_different_messages_at_same_timestamp) {
-    auto logger = Logger();
+    Logger logger;
     EXPECT_TRUE(logger.shall_print_message(1, "foo1"));
     EXPECT_TRUE(logger.shall_print_message(1, "foo2"));
     EXPECT_TRUE(logger.shall_print_message(1, "foo3"));
     EXPECT_TRUE(logger.shall_print_message(1, "foo4"));
+}
+
+TEST(LoggerRateLimiter, supports_timestamp_zero) {
+    Logger logger;
+    EXPECT_TRUE(logger.shall_print_message(0, "foo"));
+    EXPECT_FALSE(logger.shall_print_message(0, "foo"));
+    EXPECT_FALSE(logger.shall_print_message(9, "foo"));
+    EXPECT_TRUE(logger.shall_print_message(10, "foo"));
 }
