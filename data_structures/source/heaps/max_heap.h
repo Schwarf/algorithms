b15ec5a17ee8faa7d8bcabeb329333d40cf727c7
Schwarf <andreas.b.scharf@gmail.com>
2026-05-30 16:11:11 +0200

    Add max heap.

--- data_structures/source/heaps/max_heap.h
+++ data_structures/source/heaps/max_heap.h
@@ -2,15 +2,15 @@
 // Created by andreas on 30.05.26.
 //
 
-#ifndef ALGORITHMS_MIN_HEAP_H
-#define ALGORITHMS_MIN_HEAP_H
+#ifndef ALGORITHMS_MAX_HEAP_H
+#define ALGORITHMS_MAX_HEAP_H
 
 #include <vector>
 #include <stdexcept>
 #include <concepts>
 
 template <std::totally_ordered T>
-class MinHeap
+class MaxHeap
 {
     std::vector<T> heap;
     void heapify_down(int parent, int end, std::vector<T> & heap)
@@ -20,11 +20,11 @@ class MinHeap
             auto left_left = 2 * parent + 1;
             auto right_child = 2 * parent + 2;
             auto current = parent;
-            if (left_left <= end && heap[left_left] < heap[current])
+            if (left_left <= end && heap[left_left] > heap[current])
             {
                 current = left_left;
             }
-            if (right_child <= end && heap[right_child] < heap[current])
+            if (right_child <= end && heap[right_child] > heap[current])
             {
                 current = right_child;
             }
@@ -44,7 +44,7 @@ class MinHeap
         while (child > 0)
         {
             auto parent = (child-1) >> 1;
-            if (heap[parent] <= heap[child])
+            if (heap[parent] >= heap[child])
                 return;
             std::swap(heap[child], heap[parent]);
             child = parent;
@@ -52,9 +52,9 @@ class MinHeap
     }
 
 public:
-    explicit MinHeap(const std::vector<T> & values)
+    explicit MaxHeap(const std::vector<T> & values)
     {
-        build_min_heap(values);
+        build_max_heap(values);
     }
 
     bool empty() const
@@ -62,7 +62,7 @@ public:
         return heap.empty();
     }
 
-    std::vector<T> build_min_heap(const std::vector<T>& values)
+    std::vector<T> build_max_heap(const std::vector<T>& values)
     {
         heap = values;
         if (heap.size() < 2)
@@ -107,4 +107,4 @@ public:
     }
 };
 
-#endif //ALGORITHMS_MIN_HEAP_H
+#endif //ALGORITHMS_MAX_HEAP_H
