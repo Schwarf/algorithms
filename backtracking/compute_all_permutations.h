7fe7f727273dfc677d3cfb5d5ffdafb22b08348a
Schwarf <andreas.b.scharf@gmail.com>
2026-06-02 16:38:22 +0200

    Add second implementation for all permutations that uses in-place swapping.

--- backtracking/compute_all_permutations.h
+++ backtracking/compute_all_permutations.h
@@ -8,12 +8,14 @@
 #include <vector>
 #include <algorithm>
 
+
 template<typename T>
 void backtrack(std::vector<std::vector<T>> &result, std::vector<T> &current, const std::vector<T> &input) {
     if (current.size() == input.size()) {
         result.push_back(current);
         return;
     }
+
     for (const auto &element: input) {
         if (std::find(current.begin(), current.end(), element) == current.end()) {
             current.push_back(element);
@@ -24,10 +26,33 @@ void backtrack(std::vector<std::vector<T>> &result, std::vector<T> &current, con
 }
 
 template<typename T>
+void backtrack(std::vector<std::vector<T>> &result, std::vector<T> &input, int index) {
+    if (index == input.size()) {
+        result.push_back(input);
+        return;
+    }
+
+    for (int j{index} ; j < input.size(); ++j) {
+        std::swap(input[j], input[index]);
+        backtrack(result, input, index+1);
+        std::swap(input[j], input[index]);
+    }
+}
+
+
+
+template<typename T>
 std::vector<std::vector<T>> compute_all_permutations(const std::vector<T> &input) {
     std::vector<std::vector<T>> result;
     std::vector<T> current;
     backtrack(result, current, input);
+    return result;
+}
+
+template<typename T>
+std::vector<std::vector<T>> compute_all_permutations2(std::vector<T> &input) {
+    std::vector<std::vector<T>> result;
+    backtrack(result, input, 0);
     return result;
 }
 
--- backtracking/test/test_compute_all_permutations.cpp
+++ backtracking/test/test_compute_all_permutations.cpp
@@ -63,3 +63,66 @@ TEST(ComputeAllPermutations, medium1) {
                             {2, 6, 4, 5}};
     EXPECT_EQ(expected_result, compute_all_permutations(input));
 }
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+
+TEST(ComputeAllPermutations2, simple1) {
+    std::vector<int> input{1, 2, 3};
+    const std::vector<std::vector<int>>
+            expected_result{{1, 2, 3},
+                            {1, 3, 2},
+                            {2, 1, 3},
+                            {2, 3, 1},
+                            {3, 1, 2},
+                            {3, 2, 1}};
+    EXPECT_EQ(expected_result, compute_all_permutations2(input));
+}
+
+
+TEST(ComputeAllPermutations2, simple2) {
+    std::vector<char> input{0, 1};
+    const std::vector<std::vector<char>>
+            expected_result{{0, 1},
+                            {1, 0}};
+    EXPECT_EQ(expected_result, compute_all_permutations2(input));
+}
+
+TEST(ComputeAllPermutations2, simple3) {
+    std::vector<std::string> input{"s"};
+    const std::vector<std::vector<std::string>>
+            expected_result{{"s"}};
+    EXPECT_EQ(expected_result, compute_all_permutations2(input));
+}
+
+
+TEST(ComputeAllPermutations2, medium1) {
+    std::vector<int> input{5, 4, 6, 2};
+    const std::vector<std::vector<int>>
+            expected_result{{5, 4, 6, 2},
+                            {5, 4, 2, 6},
+                            {5, 6, 4, 2},
+                            {5, 6, 2, 4},
+                            {5, 2, 4, 6},
+                            {5, 2, 6, 4},
+                            {4, 5, 6, 2},
+                            {4, 5, 2, 6},
+                            {4, 6, 5, 2},
+                            {4, 6, 2, 5},
+                            {4, 2, 5, 6},
+                            {4, 2, 6, 5},
+                            {6, 5, 4, 2},
+                            {6, 5, 2, 4},
+                            {6, 4, 5, 2},
+                            {6, 4, 2, 5},
+                            {6, 2, 5, 4},
+                            {6, 2, 4, 5},
+                            {2, 5, 4, 6},
+                            {2, 5, 6, 4},
+                            {2, 4, 5, 6},
+                            {2, 4, 6, 5},
+                            {2, 6, 5, 4},
+                            {2, 6, 4, 5}};
+    EXPECT_EQ(expected_result, compute_all_permutations2(input));
+}
