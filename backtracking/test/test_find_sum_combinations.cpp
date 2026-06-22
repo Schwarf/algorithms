a4af5af103ab018d94de08d67b745c9d416da74a
Schwarf <andreas.b.scharf@gmail.com>
2026-06-22 17:29:07 +0200

    Add tests.

--- backtracking/test/test_find_sum_combinations.cpp
+++ backtracking/test/test_find_sum_combinations.cpp
@@ -2,3 +2,40 @@
 // Created by andreas on 22.06.26.
 //
 
+#include "gtest/gtest.h"
+#include "./../find_sum_combinations.h"
+
+TEST(TestFindAllombinations, simple1) {
+    std::vector<int> input{1,2,4};
+    constexpr int target{4};
+    std::vector<std::vector<int>> expected_output{
+                    {1, 1, 1, 1},
+                    {1,1, 2},
+                    {2, 2},
+                    {4}
+    };
+    EXPECT_EQ(find_sum_combinations(input, target), expected_output);
+}
+
+TEST(TestFindAllombinations, simple2) {
+    std::vector<int> input{};
+    constexpr int target{};
+    std::vector<std::vector<int>> expected_output{{}};
+    EXPECT_EQ(find_sum_combinations(input, target), expected_output);
+}
+
+TEST(TestFindAllombinations, simple3) {
+    std::vector<int> input{2};
+    constexpr int target{1};
+    std::vector<std::vector<int>> expected_output{};
+    EXPECT_EQ(find_sum_combinations(input, target), expected_output);
+}
+
+TEST(TestFindAllombinations, simple4) {
+    std::vector<int> input{3,4,5};
+    constexpr int target{10};
+    std::vector<std::vector<int>> expected_output{{3,3,4},{5,5}};
+    EXPECT_EQ(find_sum_combinations(input, target), expected_output);
+}
+
+
