a317eb696630473c7b96870753a2398d370edd9f
Schwarf <andreas.b.scharf@gmail.com>
2026-07-11 15:51:44 +0200

    Add DP coin permutations.

---
+++ dynamic_programming/knapsack-like/coin_permutations.h
@@ -0,0 +1,30 @@
+//
+// Created by andreas on 11.07.26.
+//
+
+#ifndef ALGORITHMS_COIN_PERMUTATIONS_H
+#define ALGORITHMS_COIN_PERMUTATIONS_H
+
+// Given a vector of distinct positive integers `coins` and a non-negative integer `target`,
+// return the number of ordered sequences of coin values whose sum equals `target`.
+// Each coin may be used any number of times.
+// Different orders count as different sequences.
+#include <vector>
+
+int coin_permutations(vector<int>& coins, int amount) {
+    std::vector<long> dp(amount+1);
+    std::sort(coins.begin(), coins.end());
+    dp[0] =1;
+    for(int t{1}; t <= amount; ++t)
+    {
+        for(const auto n: coins)
+        {
+            if(n <= t)
+                dp[t] += dp[t-n];
+        }
+    }
+    return dp[amount];
+}
+
+
+#endif //ALGORITHMS_COIN_PERMUTATIONS_H
