febcc7ea0ff9a0755f8444c0ec71c746152bbf75
Schwarf <andreas.b.scharf@gmail.com>
2026-07-13 17:31:36 +0200

    Update tests.

--- dynamic_programming/test/align_sequences/test_longest_common_substring.cpp
+++ dynamic_programming/test/align_sequences/test_longest_common_substring.cpp
@@ -33,3 +33,59 @@ TEST(LongestCommonSubstringTest, simple4) {
       constexpr int expected_result{6};
       EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
 }
+
+TEST(LongestCommonSubstringTest, both_empty) {
+      std::string s1{""};
+      std::string s2{""};
+      constexpr int expected_result{0};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
+
+TEST(LongestCommonSubstringTest, first_empty) {
+      std::string s1{""};
+      std::string s2{"abcde"};
+      constexpr int expected_result{0};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
+
+TEST(LongestCommonSubstringTest, no_common_characters) {
+      std::string s1{"abc"};
+      std::string s2{"DEF"};
+      constexpr int expected_result{0};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
+
+TEST(LongestCommonSubstringTest, identical_strings) {
+      std::string s1{"dynamic"};
+      std::string s2{"dynamic"};
+      constexpr int expected_result{7};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
+
+TEST(LongestCommonSubstringTest, common_subsequence_is_not_enough) {
+      std::string s1{"abcde"};
+      std::string s2{"ace"};
+      constexpr int expected_result{1};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
+
+TEST(LongestCommonSubstringTest, repeated_characters) {
+      std::string s1{"aaaaab"};
+      std::string s2{"baaaaac"};
+      constexpr int expected_result{5};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
+
+TEST(LongestCommonSubstringTest, case_sensitive) {
+      std::string s1{"ABCdef"};
+      std::string s2{"abcDEF"};
+      constexpr int expected_result{0};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
+
+TEST(LongestCommonSubstringTest, longest_match_in_middle) {
+      std::string s1{"xxalgorithmzz"};
+      std::string s2{"yyalgorithmqq"};
+      constexpr int expected_result{9};
+      EXPECT_EQ(longest_common_substring(s1, s2), expected_result);
+}
