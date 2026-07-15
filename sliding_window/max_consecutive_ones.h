728c05155bdfab5a1efc60fb5874a8e556c377c0
Schwarf <andreas.b.scharf@gmail.com>
2026-07-15 19:24:56 +0200

    Format.

--- sliding_window/max_consecutive_ones.h
+++ sliding_window/max_consecutive_ones.h
@@ -7,24 +7,25 @@
 
 #include <vector>
 
-// Given a binary array nums and an integer k, find the length of the longest contiguous subarray that contains at most k zeros.
-int max_consecutive_ones(const std::vector<int> & input, int k)
+// Given a binary array nums and an integer k, find the length of the longest contiguous subarray that contains at most
+// k zeros.
+int max_consecutive_ones(const std::vector<int>& input, int k)
 {
     int left = 0;
     int max_length = 0;
     int zero_count = 0;
     for (int right = 0; right < input.size(); ++right)
     {
-        if(input[right] ==0)
+        if (input[right] == 0)
             zero_count++;
         while (zero_count > k)
         {
-            if(input[left] ==0)
+            if (input[left] == 0)
                 zero_count--;
             left++;
         }
 
-        max_length = std::max(max_length, right-left+1);
+        max_length = std::max(max_length, right - left + 1);
     }
     return max_length;
 }
--- sliding_window/max_sum_subarray_size_k.h
+++ sliding_window/max_sum_subarray_size_k.h
@@ -9,22 +9,23 @@
 #include <numeric>
 #include <vector>
 
-// Given an array of integers input and an integer k, find the maximum sum of any contiguous subarray of exactly k elements.
-int max_sum_subarray_size_k(const std::vector<int> & input, int k)
+// Given an array of integers input and an integer k, find the maximum sum of any contiguous subarray of exactly k
+// elements.
+int max_sum_subarray_size_k(const std::vector<int>& input, int k)
 {
     int n = input.size();
-    if( n < k || k <= 0)
+    if (n < k || k <= 0)
         return 0;
-    if(k==1)
+    if (k == 1)
         return *std::max_element(input.begin(), input.end());
 
     int left{};
 
-    int current_sum = std::accumulate(input.begin(), input.begin()+k, 0);
+    int current_sum = std::accumulate(input.begin(), input.begin() + k, 0);
     int max_sum = current_sum;
-    for(int right{k}; right < n; ++right)
+    for (int right{k}; right < n; ++right)
     {
-        current_sum += -input[left]+input[right];
+        current_sum += -input[left] + input[right];
         max_sum = std::max(max_sum, current_sum);
         left++;
     }
