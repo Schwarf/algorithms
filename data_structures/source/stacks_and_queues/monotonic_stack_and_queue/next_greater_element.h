aefd3f3f388fbeb75479ba2dae2c8f6aae7d5192
Schwarf <andreas.b.scharf@gmail.com>
2026-08-23 11:09:31 +0200

    Add monotonic-stack version.

--- data_structures/source/stacks_and_queues/monotonic_stack_and_queue/next_greater_element.h
+++ data_structures/source/stacks_and_queues/monotonic_stack_and_queue/next_greater_element.h
@@ -30,5 +30,23 @@ std::vector<int> next_greater_element_brute_force(const std::vector<int>& input)
     return result;
 }
 
+std::vector<int> next_greater_element(const std::vector<int>& input)
+{
+    int n = input.size();
+    std::vector<int> result(n, -1);
+    std::stack<int> stack;
+    for (int i = 0; i < 2*n; ++i)
+    {
+        const int index = i % n;
+        while (!stack.empty() && input[stack.top()] < input[index])
+        {
+            result[stack.top()] = input[index];
+            stack.pop();
+        }
+        if (i < n)
+            stack.push(i);
+    }
+    return result;
+}
 
 #endif // ALGORITHMS_NEXT_GREATER_ELEMENT_H
