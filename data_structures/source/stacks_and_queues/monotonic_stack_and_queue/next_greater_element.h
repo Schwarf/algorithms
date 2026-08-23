e3b74f16b7c25a78991ce108c6311a02d22f82cb
Schwarf <andreas.b.scharf@gmail.com>
2026-08-23 11:07:49 +0200

    Add algo for brute-force greater-next-element.

---
+++ data_structures/source/stacks_and_queues/monotonic_stack_and_queue/next_greater_element.h
@@ -0,0 +1,34 @@
+//
+// Created by andreas on 23.08.26.
+//
+
+#ifndef ALGORITHMS_NEXT_GREATER_ELEMENT_H
+#define ALGORITHMS_NEXT_GREATER_ELEMENT_H
+// You are given a circular array of integers.
+// For each element in the array, find the next greater element when moving to the right.
+// The next greater element for a value is the first element encountered that is strictly greater than it.
+// If no greater element exists, return -1 for that position.
+// Return an array containing the next greater element for every index.
+#include <vector>
+#include <stack>
+
+std::vector<int> next_greater_element_brute_force(const std::vector<int>& input)
+{
+    int n = input.size();
+    std::vector<int> result(n, -1);
+    for (int i = 0; i < n; ++i)
+    {
+        for (int step = 1; step < n; ++step)
+        {
+            if (input[(i + step) % n] > input[i])
+            {
+                result[i] = input[(i + step) % n];
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+
+#endif // ALGORITHMS_NEXT_GREATER_ELEMENT_H
