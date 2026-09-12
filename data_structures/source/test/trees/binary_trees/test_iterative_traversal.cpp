b4f480bd56a8bf87e31d1425e32318f9cecaa9af
Schwarf <andreas.b.scharf@gmail.com>
2026-09-12 18:58:12 +0200

    Add tests for pre-order iterative traversal.

--- data_structures/source/test/trees/binary_trees/test_iterative_traversal.cpp
+++ data_structures/source/test/trees/binary_trees/test_iterative_traversal.cpp
@@ -49,6 +49,15 @@ protected:
         return result;
     }
 
+    std::vector<int> collect_preorder_values(Node* root)
+    {
+        std::vector<int> result;
+        visited_values = &result;
+        iterative_preorder_traversal(root, record_value);
+        visited_values = nullptr;
+        return result;
+    }
+
     // A complete binary search tree with 15 nodes.
     Node* create_complete_tree()
     {
@@ -149,4 +158,73 @@ TEST_F(SetupIterativeTraversal, visits_sparse_tree_in_inorder)
     connect(node14, node13, nullptr);
 
     EXPECT_EQ(collect_values(root), std::vector<int>({1, 3, 4, 6, 7, 8, 10, 13, 14}));
+}
+
+TEST_F(SetupIterativeTraversal, preorder_returns_empty_result_for_null_root)
+{
+    EXPECT_EQ(collect_preorder_values(nullptr), std::vector<int>{});
+}
+
+TEST_F(SetupIterativeTraversal, preorder_visits_single_node_tree)
+{
+    auto root = make_node(42);
+
+    EXPECT_EQ(collect_preorder_values(root), std::vector<int>{42});
+}
+
+TEST_F(SetupIterativeTraversal, visits_complete_tree_in_preorder)
+{
+    auto root = create_complete_tree();
+
+    const std::vector<int> expected{25, 15, 10, 4, 12, 22, 18, 24, 50, 35, 31, 44, 70, 66, 90};
+    EXPECT_EQ(collect_preorder_values(root), expected);
+}
+
+TEST_F(SetupIterativeTraversal, visits_left_skewed_tree_in_preorder)
+{
+    auto root = make_node(4);
+    auto node3 = make_node(3);
+    auto node2 = make_node(2);
+    auto node1 = make_node(1);
+
+    connect(root, node3, nullptr);
+    connect(node3, node2, nullptr);
+    connect(node2, node1, nullptr);
+
+    EXPECT_EQ(collect_preorder_values(root), std::vector<int>({4, 3, 2, 1}));
+}
+
+TEST_F(SetupIterativeTraversal, visits_right_skewed_tree_in_preorder)
+{
+    auto root = make_node(1);
+    auto node2 = make_node(2);
+    auto node3 = make_node(3);
+    auto node4 = make_node(4);
+
+    connect(root, nullptr, node2);
+    connect(node2, nullptr, node3);
+    connect(node3, nullptr, node4);
+
+    EXPECT_EQ(collect_preorder_values(root), std::vector<int>({1, 2, 3, 4}));
+}
+
+TEST_F(SetupIterativeTraversal, visits_sparse_tree_in_preorder)
+{
+    auto root = make_node(8);
+    auto node3 = make_node(3);
+    auto node10 = make_node(10);
+    auto node1 = make_node(1);
+    auto node6 = make_node(6);
+    auto node14 = make_node(14);
+    auto node4 = make_node(4);
+    auto node7 = make_node(7);
+    auto node13 = make_node(13);
+
+    connect(root, node3, node10);
+    connect(node3, node1, node6);
+    connect(node10, nullptr, node14);
+    connect(node6, node4, node7);
+    connect(node14, node13, nullptr);
+
+    EXPECT_EQ(collect_preorder_values(root), std::vector<int>({8, 3, 1, 6, 4, 7, 10, 14, 13}));
 }
