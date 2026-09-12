96c7c8d17eae71a79ecc06c919717e080fb30034
Schwarf <andreas.b.scharf@gmail.com>
2026-09-12 18:46:19 +0200

    Add algo to traverse binary tree inorder iteratively.

---
+++ data_structures/source/trees/binary_trees/iterative_traversal.h
@@ -0,0 +1,57 @@
+//
+// Created by andreas on 12.09.26.
+//
+
+#ifndef ALGORITHMS_ITERATIVE_TRAVERSAL_H
+#define ALGORITHMS_ITERATIVE_TRAVERSAL_H
+// Iterative In-order Traversal
+//
+// Given a binary tree where each node has pointers to its left child,
+// right child, and parent, traverse the tree in in-order without using
+// recursion or an explicit stack.
+
+#include "tree_node.h"
+
+template <typename T>
+void iterative_inorder_traversal(ParentedTreeNode<T>* root, void (*callback)(ParentedTreeNode<T>*))
+{
+    if (!root)
+        return;
+
+    ParentedTreeNode<T>* prev = nullptr;
+    auto current = root;
+
+    while (current)
+    {
+        ParentedTreeNode<T>* next = nullptr;
+        if (current->parent == prev)
+        {
+            if (current->left)
+                next = current->left;
+            else
+            {
+                callback(current);
+                if (current->right)
+                    next = current->right;
+                else
+                    next = current->parent;
+            }
+        }
+        else if (current->left == prev)
+        {
+            callback(current);
+            if (current->right)
+                next = current->right;
+            else
+                next = current->parent;
+        }
+        else if (current->right == prev)
+            next = current->parent;
+
+        prev = current;
+        current = next;
+    }
+
+}
+
+#endif // ALGORITHMS_ITERATIVE_TRAVERSAL_H
