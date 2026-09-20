f94d8c7116779ae8cdf29624614f5df3fc7dddcb
Schwarf <andreas.b.scharf@gmail.com>
2026-09-20 13:36:00 +0200

    Return multiple LIS.

--- dynamic_programming/sequence_chaining/longest_increasing_subsequence.h
+++ dynamic_programming/sequence_chaining/longest_increasing_subsequence.h
@@ -44,5 +44,67 @@ std::vector<int> longest_increasing_subsequence(const std::vector<int>& input)
     return result;
 }
 
+void build_subsequences(int index, const std::vector<int>& input,
+                        const std::vector<std::vector<int>>& predecessor_indices,
+                        std::vector<int> & current,
+                        std::vector<std::vector<int>>& result)
+{
+    current.push_back(input[index]);
+    if (predecessor_indices[index].empty())
+    {
+        auto sequence = current;
+        std::reverse(sequence.begin(), sequence.end());
+        result.push_back(sequence);
+    }
+    else
+    {
+        for (int predecessor_index : predecessor_indices[index])
+        {
+            build_subsequences(predecessor_index, input, predecessor_indices, current, result);
+        }
+    }
+    current.pop_back();
+}
+
+// multiple results
+std::vector<std::vector<int>> multiple_longest_increasing_subsequence(const std::vector<int>& input)
+{
+    if (input.empty())
+        return {};
+    if (input.size() == 1)
+        return {input};
+
+    int n = input.size();
+    std::vector<int> sizes(n, 1);
+    std::vector<std::vector<int>> predecessor_indices(n);
+    for (int i = 1; i < n; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            if (input[i] > input[j])
+            {
+                if (sizes[j] + 1 > sizes[i])
+                {
+                    sizes[i] = sizes[j] + 1;
+                    predecessor_indices[i].clear();
+                    predecessor_indices[i].push_back(j);
+                }
+                else if ((sizes[j] + 1) == sizes[i])
+                    predecessor_indices[i].push_back(j);
+            }
+        }
+    }
+    int max_size = *std::max_element(sizes.begin(), sizes.end());
+    std::vector<std::vector<int>> result;
+    std::vector<int> current;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (sizes[i] == max_size)
+            build_subsequences(i, input, predecessor_indices, current, result);
+    }
+    return result;
+}
+
 
 #endif // BEHAVIORAL_PATTERNS_LONGEST_INCREASING_SUBSEQUENCE_H
