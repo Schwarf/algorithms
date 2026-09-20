896e28432f1aefff6b2c932f711ef8d9bbd39f2c
Schwarf <andreas.b.scharf@gmail.com>
2026-09-20 13:43:49 +0200

    Add tests for multiple LIS.

--- dynamic_programming/test/sequence_chaining/test_longest_increasing_subsequence.cpp
+++ dynamic_programming/test/sequence_chaining/test_longest_increasing_subsequence.cpp
@@ -26,6 +26,14 @@ namespace
             ++input_it;
         }
     }
+
+    void ExpectSameSubsequences(std::vector<std::vector<int>> expected, std::vector<std::vector<int>> actual)
+    {
+        std::sort(expected.begin(), expected.end());
+        std::sort(actual.begin(), actual.end());
+
+        EXPECT_EQ(expected, actual);
+    }
 } // namespace
 
 TEST(LongestIncreasingSubsequence, empty_input)
@@ -99,4 +107,77 @@ TEST(LongestIncreasingSubsequence, allows_multiple_longest_subsequences_with_rep
     const std::vector<int> input{2, 1, 3, 2, 4, 3};
 
     ExpectValidLongestIncreasingSubsequence(input, longest_increasing_subsequence(input), 3);
+}
+
+TEST(MultipleLongestIncreasingSubsequence, empty_input)
+{
+    const std::vector<int> input{};
+
+    EXPECT_TRUE(multiple_longest_increasing_subsequence(input).empty());
+}
+
+TEST(MultipleLongestIncreasingSubsequence, single_element)
+{
+    const std::vector<int> input{7};
+    const std::vector<std::vector<int>> expected_result{{7}};
+
+    EXPECT_EQ(expected_result, multiple_longest_increasing_subsequence(input));
+}
+
+TEST(MultipleLongestIncreasingSubsequence, strictly_increasing_input_has_one_result)
+{
+    const std::vector<int> input{1, 2, 3, 4, 5};
+    const std::vector<std::vector<int>> expected_result{{1, 2, 3, 4, 5}};
+
+    EXPECT_EQ(expected_result, multiple_longest_increasing_subsequence(input));
+}
+
+TEST(MultipleLongestIncreasingSubsequence, strictly_decreasing_input_returns_each_element)
+{
+    const std::vector<int> input{5, 4, 3, 2, 1};
+    const std::vector<std::vector<int>> expected_result{{5}, {4}, {3}, {2}, {1}};
+
+    EXPECT_EQ(expected_result, multiple_longest_increasing_subsequence(input));
+}
+
+TEST(MultipleLongestIncreasingSubsequence, returns_multiple_subsequences_with_different_endpoints)
+{
+    const std::vector<int> input{2, 1, 3, 2, 4, 3};
+    const std::vector<std::vector<int>> expected_result{{2, 3, 4}, {1, 3, 4}, {1, 2, 4}, {1, 2, 3}};
+
+    ExpectSameSubsequences(expected_result, multiple_longest_increasing_subsequence(input));
+}
+
+TEST(MultipleLongestIncreasingSubsequence, returns_multiple_subsequences_with_same_endpoint)
+{
+    const std::vector<int> input{1, 2, 4, 3, 5};
+    const std::vector<std::vector<int>> expected_result{{1, 2, 4, 5}, {1, 2, 3, 5}};
+
+    ExpectSameSubsequences(expected_result, multiple_longest_increasing_subsequence(input));
+}
+
+TEST(MultipleLongestIncreasingSubsequence, handles_negative_zero_and_positive_values)
+{
+    const std::vector<int> input{-3, -1, -2, 0, 2, -1, 3};
+    const std::vector<std::vector<int>> expected_result{{-3, -1, 0, 2, 3}, {-3, -2, 0, 2, 3}};
+
+    ExpectSameSubsequences(expected_result, multiple_longest_increasing_subsequence(input));
+}
+
+TEST(MultipleLongestIncreasingSubsequence, returns_index_distinct_results_for_duplicate_values)
+{
+    const std::vector<int> input{1, 2, 2, 3};
+    const std::vector<std::vector<int>> expected_result{{1, 2, 3}, {1, 2, 3}};
+
+    ExpectSameSubsequences(expected_result, multiple_longest_increasing_subsequence(input));
+}
+
+
+TEST(MultipleLongestIncreasingSubsequence, returns_index_distinct_results_for_duplicate_values_and_duplicate_sequences)
+{
+    const std::vector<int> input{1, 2, 10, 10, 10, 1, 2, 10};
+    const std::vector<std::vector<int>> expected_result{{1, 2, 10}, {1, 2, 10}, {1, 2, 10},
+                                                        {1, 2, 10}, {1, 2, 10}, {1, 2, 10}};
+
+    ExpectSameSubsequences(expected_result, multiple_longest_increasing_subsequence(input));
 }
